24e1f1974b352a73f91dd177ec2ca6073cff43be
adrian09h <adrian09h@gmail.com>
2019-07-16 00:05:34 +0200

    update travis #1

---
+++ app/src/main/java/io/getstream/chat/sdk/AppSecretKey.java
@@ -0,0 +1,7 @@
+package io.getstream.chat.sdk;
+
+public final class AppSecretKey {
+//    public static String API_KEY        = "qk4nn7rpcn75";
+
+    public static String API_KEY       = "cranwty2xrfm";
+}
