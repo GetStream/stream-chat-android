b84ca42c9aede26b560e5f923f2d2db573d29751
Thierry Schellenbach <thierryschellenbach@gmail.com>
2019-08-09 10:46:48 -0600

    last change

--- library/src/main/java/com/getstream/sdk/chat/view/ChannelHeaderToolbar.java
+++ library/src/main/java/com/getstream/sdk/chat/view/ChannelHeaderToolbar.java
@@ -2,14 +2,22 @@ package com.getstream.sdk.chat.view;
 
 
 import android.content.Context;
+import android.text.TextUtils;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.widget.RelativeLayout;
 
 import androidx.annotation.Nullable;
 
 import com.getstream.sdk.chat.databinding.ToolbarChannelHeaderBinding;
+import com.getstream.sdk.chat.model.Channel;
 import com.getstream.sdk.chat.rest.Message;
+import com.getstream.sdk.chat.rest.User;
+import com.getstream.sdk.chat.rest.response.ChannelState;
+import com.getstream.sdk.chat.utils.Global;
+import com.getstream.sdk.chat.utils.StringUtility;
+import com.getstream.sdk.chat.utils.Utils;
 import com.getstream.sdk.chat.viewmodel.ChannelViewModel;
 
 public class ChannelHeaderToolbar extends RelativeLayout {
@@ -54,62 +62,66 @@ public class ChannelHeaderToolbar extends RelativeLayout {
     }
 
     private void configHeaderView() {
-        // Avatar
-//        if (!TextUtils.isEmpty(channel.getName())) {
-//            binding.tvChannelInitial.setText(channel.getInitials());
-//            Utils.circleImageLoad(binding.ivHeaderAvatar, channel.getImage());
-//            if (StringUtility.isValidImageUrl(channel.getImage())) {
-//                binding.ivHeaderAvatar.setVisibility(View.VISIBLE);
-//                binding.tvChannelInitial.setVisibility(View.INVISIBLE);
-//            } else {
-//                binding.ivHeaderAvatar.setVisibility(View.INVISIBLE);
-//                binding.tvChannelInitial.setVisibility(View.VISIBLE);
-//            }
-//        } else {
-//            User opponent = Global.getOpponentUser(channelState);
-//            if (opponent != null) {
-//                binding.tvChannelInitial.setText(opponent.getUserInitials());
-//                Utils.circleImageLoad(binding.ivHeaderAvatar, opponent.getImage());
-//                binding.tvChannelInitial.setVisibility(View.VISIBLE);
-//                binding.ivHeaderAvatar.setVisibility(View.VISIBLE);
-//            } else {
-//                binding.tvChannelInitial.setVisibility(View.VISIBLE);
-//                binding.ivHeaderAvatar.setVisibility(View.INVISIBLE);
-//            }
-//        }
-//        // Channel name
-//        String channelName = "";
-//
-//        if (!TextUtils.isEmpty(channelState.getChannel().getName())) {
-//            channelName = channelState.getChannel().getName();
-//        } else {
-//            User opponent = Global.getOpponentUser(channelState);
-//            if (opponent != null) {
-//                channelName = opponent.getName();
-//            }
-//        }
-//
-//        binding.tvChannelName.setText(channelName);
-//
-//        // Last Active
-//        Message lastMessage = channelState.getLastMessageFromOtherUser();
-//        configHeaderLastActive(lastMessage);
-//        // Online Mark
-//        try {
-//            if (Global.getOpponentUser(channelState) == null)
-//                binding.ivActiveMark.setVisibility(View.GONE);
-//            else {
-//                if (Global.getOpponentUser(channelState).getOnline()) {
-//                    binding.ivActiveMark.setVisibility(View.VISIBLE);
-//                } else {
-//                    binding.ivActiveMark.setVisibility(View.GONE);
-//                }
-//            }
-//        } catch (Exception e) {
-//            binding.ivActiveMark.setVisibility(View.GONE);
-//        }
-//
-//        binding.tvBack.setVisibility(singleConversation ? View.INVISIBLE : View.VISIBLE);
-//        binding.tvBack.setOnClickListener((View v) -> finish());
+
+        // TODO: Move this stuff
+
+        Channel channel = this.modelView.getChannel();
+        ChannelState channelState = this.modelView.channelState;
+
+        if (!TextUtils.isEmpty(channel.getName())) {
+            binding.tvChannelInitial.setText(channel.getInitials());
+            Utils.circleImageLoad(binding.ivHeaderAvatar, channel.getImage());
+            if (StringUtility.isValidImageUrl(channel.getImage())) {
+                binding.ivHeaderAvatar.setVisibility(View.VISIBLE);
+                binding.tvChannelInitial.setVisibility(View.INVISIBLE);
+            } else {
+                binding.ivHeaderAvatar.setVisibility(View.INVISIBLE);
+                binding.tvChannelInitial.setVisibility(View.VISIBLE);
+            }
+        } else {
+            User opponent = Global.getOpponentUser(channelState);
+            if (opponent != null) {
+                binding.tvChannelInitial.setText(opponent.getUserInitials());
+                Utils.circleImageLoad(binding.ivHeaderAvatar, opponent.getImage());
+                binding.tvChannelInitial.setVisibility(View.VISIBLE);
+                binding.ivHeaderAvatar.setVisibility(View.VISIBLE);
+            } else {
+                binding.tvChannelInitial.setVisibility(View.VISIBLE);
+                binding.ivHeaderAvatar.setVisibility(View.INVISIBLE);
+            }
+        }
+        // Channel name
+        String channelName = "";
+
+        if (!TextUtils.isEmpty(channelState.getChannel().getName())) {
+            channelName = channelState.getChannel().getName();
+        } else {
+            User opponent = Global.getOpponentUser(channelState);
+            if (opponent != null) {
+                channelName = opponent.getName();
+            }
+        }
+
+        binding.tvChannelName.setText(channelName);
+
+        // Last Active
+        Message lastMessage = channelState.getLastMessageFromOtherUser();
+        configHeaderLastActive(lastMessage);
+        // Online Mark
+        try {
+            if (Global.getOpponentUser(channelState) == null)
+                binding.ivActiveMark.setVisibility(View.GONE);
+            else {
+                if (Global.getOpponentUser(channelState).getOnline()) {
+                    binding.ivActiveMark.setVisibility(View.VISIBLE);
+                } else {
+                    binding.ivActiveMark.setVisibility(View.GONE);
+                }
+            }
+        } catch (Exception e) {
+            binding.ivActiveMark.setVisibility(View.GONE);
+        }
+
+        binding.tvBack.setOnClickListener((View v) -> finish());
     }
 }
