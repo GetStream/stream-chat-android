fd9b34c2b8f913283cbd2d832ef61da7b967c259
adrian09h <adrian09h@gmail.com>
2019-09-19 17:16:45 +0200

    fix mention issue.

--- library/src/main/java/com/getstream/sdk/chat/utils/Utils.java
+++ library/src/main/java/com/getstream/sdk/chat/utils/Utils.java
@@ -72,7 +72,7 @@ public class Utils {
     }
 
     public static boolean isSVGImage(String url){
-        return url.contains("random_svg");
+        return (TextUtils.isEmpty(url) || url.contains("random_svg"));
     }
 
     public static void showMessage(Context mContext, String message) {
