70eac9c99d8bf4f7f6b8c1de6173d95c1202ea91
adrian09h <adrian09h@gmail.com>
2019-10-01 01:05:31 +0200

    minor update

--- sample/src/main/java/io/getstream/chat/example/ChannelActivity.java
+++ sample/src/main/java/io/getstream/chat/example/ChannelActivity.java
@@ -71,7 +71,7 @@ public class ChannelActivity extends AppCompatActivity
         binding.messageList.setMessageLongClickListener(this);
         binding.messageList.setUserClickListener(this);
         binding.messageList.setAttachmentClickListener(this);
-        // If you are using own MessageInputView please commit this line.
+        // If you are using own MessageInputView please comment this line.
         binding.messageInput.setOpenCameraViewListener(this);
 
         binding.messageList.setViewHolderFactory(new MyMessageViewHolderFactory());
@@ -94,7 +94,7 @@ public class ChannelActivity extends AppCompatActivity
     @Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        // If you are using own MessageInputView please commit this line.
+        // If you are using own MessageInputView please comment this line.
         binding.messageInput.progressCapturedMedia(requestCode, resultCode, data);
     }
 
