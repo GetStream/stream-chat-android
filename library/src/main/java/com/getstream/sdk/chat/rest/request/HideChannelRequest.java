2158a006b51c10d3b9eaef0704c639bf61438550
Anton <anton.bevza@stfalcon.com>
2019-10-01 17:34:31 +0300

    remove unnecessary header

--- library/src/main/java/com/getstream/sdk/chat/rest/request/HideChannelRequest.java
+++ library/src/main/java/com/getstream/sdk/chat/rest/request/HideChannelRequest.java
@@ -2,9 +2,6 @@ package com.getstream.sdk.chat.rest.request;
 
 import com.google.gson.annotations.SerializedName;
 
-/*
- * Created by Anton Bevza on 2019-10-01.
- */
 public class HideChannelRequest {
     @SerializedName("user_id")
     private String userId;
