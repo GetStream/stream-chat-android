fe6f848a28072779557fe1c05779885af600efab
adrian09h <adrian09h@gmail.com>
2019-10-05 19:05:15 +0200

    write/read showAttachmentButton after request permission

--- sample/src/main/java/io/getstream/chat/example/ChannelActivity.java
+++ sample/src/main/java/io/getstream/chat/example/ChannelActivity.java
@@ -1,7 +1,6 @@
 package io.getstream.chat.example;
 
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -16,8 +15,6 @@ import com.getstream.sdk.chat.model.Channel;
 import com.getstream.sdk.chat.rest.Message;
 import com.getstream.sdk.chat.rest.User;
 import com.getstream.sdk.chat.rest.core.Client;
-import com.getstream.sdk.chat.utils.Constant;
-import com.getstream.sdk.chat.utils.PermissionChecker;
 import com.getstream.sdk.chat.view.Dialog.MoreActionDialog;
 import com.getstream.sdk.chat.view.MessageInputView;
 import com.getstream.sdk.chat.view.MessageListView;
