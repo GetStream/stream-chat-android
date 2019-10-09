5f5f4ef27a3e564cde3d8c8347558ee2f9c6510d
Thierry Schellenbach <thierryschellenbach@gmail.com>
2019-10-09 17:45:24 -0600

    cleanup

--- library/src/main/java/com/getstream/sdk/chat/rest/core/ClientState.java
+++ library/src/main/java/com/getstream/sdk/chat/rest/core/ClientState.java
@@ -13,22 +13,22 @@ import java.util.concurrent.ConcurrentHashMap;
 
 /**
  * Client state is responsible for storing:
- *
+ * <p>
  * - current user
  * - all users that the APIs returned
  * - the unread count for the current user
  * - the mutes for the current user
- *
+ * <p>
  * This means that the following API calls should update client state
- *
+ * <p>
  * - queryUsers
  * - queryChannels
  * - queryChannel
- *
+ * <p>
  * And we need to store mapping between channel id -> user for efficient updating of users
- *
+ * <p>
  * To keep this data in sync it needs to monitor the following events
- *
+ * <p>
  * - mute event (in case you mute a new user) (done)
  * - user.updated in case some of the current users have changed (done)
  * - mark read (for the unread count) (done)
@@ -37,11 +37,10 @@ import java.util.concurrent.ConcurrentHashMap;
  * - new message (done)
  * - channel.updated (members could change) (done)
  * - new channel notification (done)
- *
+ * <p>
  * When a user did update (either via update, or a presence change)
  * - we need to see which channels contain that user (members or channels)
  * - update the user object on the members and channels
- *
  */
 public class ClientState {
 
@@ -50,9 +49,14 @@ public class ClientState {
     private ConcurrentHashMap<String, User> users = new ConcurrentHashMap<>();
     private User currentUser;
     private Integer currentUserUnreadCount;
-    private ConcurrentHashMap<String,List<String>> userIDToChannelsMap;
+    private ConcurrentHashMap<String, List<String>> userIDToChannelsMap;
     private Client client;
 
+    /**
+     * Creates the client state, only required paramter is a client object
+     *
+     * @param client
+     */
     public ClientState(Client client) {
         this.client = client;
         currentUser = null;
@@ -62,28 +66,52 @@ public class ClientState {
     }
 
 
+    /**
+     * Returns the current user
+     *
+     * @return
+     */
     public User getCurrentUser() {
         return currentUser;
     }
 
+    void setCurrentUser(User currentUser) {
+        this.currentUser = currentUser;
+    }
+
+    /**
+     * Get a user by ID
+     *
+     * @param userID a string based ID for the user
+     * @return
+     */
     public User getUser(String userID) {
         return users.get(userID);
     }
 
-    public void setCurrentUser(User currentUser) {
-        this.currentUser = currentUser;
+    /**
+     * Returns the unread count for the current user
+     *
+     * @return
+     */
+    public Integer getCurrentUserUnreadCount() {
+        return currentUserUnreadCount;
     }
 
-    public void updateUser(User newUser) {
+    void setCurrentUserUnreadCount(Integer currentUserUnreadCount) {
+        this.currentUserUnreadCount = currentUserUnreadCount;
+    }
+
+    void updateUser(User newUser) {
         List<User> newUsers = new ArrayList();
         newUsers.add(newUser);
         updateUsers(newUsers);
     }
 
-    public void updateUsers(List<User> newUsers) {
+    void updateUsers(List<User> newUsers) {
         Map<String, Channel> channelMap = client.getActiveChannelMap();
 
-        for (User newUser: newUsers) {
+        for (User newUser : newUsers) {
             User oldUser = users.get(newUser.getId());
             if (oldUser != null) {
                 oldUser.shallowUpdate(newUser);
@@ -105,7 +133,7 @@ public class ClientState {
                         }
                         // update the watchers
                         // TODO: inefficient if you have many at the same time
-                        for (Watcher w : channel.getChannelState().getWatchers() ){
+                        for (Watcher w : channel.getChannelState().getWatchers()) {
                             if (w.getUserId().equals(newUser.getId())) {
                                 w.getUser().shallowUpdate(newUser);
                             }
@@ -119,43 +147,34 @@ public class ClientState {
 
     }
 
-    public void updateUserWithReference(User newUser, String cid) {
+    void updateUserWithReference(User newUser, String cid) {
         List<User> newUsers = new ArrayList();
         newUsers.add(newUser);
         updateUsersWithReference(newUsers, cid);
     }
 
-    public void updateUsersForChannel(ChannelState channelState) {
+    void updateUsersForChannel(ChannelState channelState) {
         List<User> users = new ArrayList<>();
         for (Member m : channelState.getMembers()) {
             users.add(m.getUser());
         }
-        for (Watcher w : channelState.getWatchers() ){
+        for (Watcher w : channelState.getWatchers()) {
             users.add(w.getUser());
         }
         updateUsersWithReference(users, channelState.getChannel().getCid());
     }
 
-    public void updateUsersWithReference(List<User> newUsers, String cid) {
+    void updateUsersWithReference(List<User> newUsers, String cid) {
         // update the users first
         updateUsers(newUsers);
 
         // set the references
-        for (User newUser: newUsers) {
+        for (User newUser : newUsers) {
             List<String> channelRefs = userIDToChannelsMap.get(newUser.getId());
             if (channelRefs == null) {
                 channelRefs = new ArrayList<>();
             }
             channelRefs.add(cid);
         }
-    }
-
-
-    public Integer getCurrentUserUnreadCount() {
-        return currentUserUnreadCount;
-    }
-
-    public void setCurrentUserUnreadCount(Integer currentUserUnreadCount) {
-        this.currentUserUnreadCount = currentUserUnreadCount;
     }
 }
