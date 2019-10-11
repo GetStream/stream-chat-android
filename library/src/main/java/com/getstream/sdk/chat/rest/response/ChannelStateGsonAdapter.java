489802481fdf6b1fd3f0eafe61bb7aee71120344
adrian09h <adrian09h@gmail.com>
2019-10-11 14:23:45 +0200

    filter wrong thread messages from ChannelState

--- library/src/main/java/com/getstream/sdk/chat/rest/response/ChannelStateGsonAdapter.java
+++ library/src/main/java/com/getstream/sdk/chat/rest/response/ChannelStateGsonAdapter.java
@@ -1,12 +1,17 @@
 package com.getstream.sdk.chat.rest.response;
 
+import android.text.TextUtils;
+
+import com.getstream.sdk.chat.rest.Message;
 import com.google.gson.Gson;
 import com.google.gson.TypeAdapter;
 import com.google.gson.stream.JsonReader;
 import com.google.gson.stream.JsonWriter;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Collections;
+import java.util.List;
 
 public class ChannelStateGsonAdapter extends TypeAdapter<ChannelState> {
     @Override
@@ -17,11 +22,26 @@ public class ChannelStateGsonAdapter extends TypeAdapter<ChannelState> {
     @Override
     public ChannelState read(JsonReader in) throws IOException {
         TypeAdapter adapter = new Gson().getAdapter(ChannelState.class);
-        ChannelState value = (ChannelState) adapter.read(in);
-        value.getChannel().setChannelState(value);
-        if (value.getReads() != null && !value.getReads().isEmpty()){
-            Collections.sort(value.getReads(), (ChannelUserRead o1, ChannelUserRead o2) -> o1.getLastRead().compareTo(o2.getLastRead()));
+        ChannelState channelState = (ChannelState) adapter.read(in);
+
+        /*Sort Reads by read date*/
+        if (channelState.getReads() != null && !channelState.getReads().isEmpty())
+            Collections.sort(channelState.getReads(), (ChannelUserRead o1, ChannelUserRead o2) -> o1.getLastRead().compareTo(o2.getLastRead()));
+
+        /*Filter wrong Thread messages from main channel message list*/
+        if(channelState.getMessages() != null && !channelState.getMessages().isEmpty()){
+            List<Message>messages = new ArrayList<>();
+            for (Message message : channelState.getMessages()){
+                if (!TextUtils.isEmpty(message.getParentId()))
+                    continue;
+                messages.add(message);
+            }
+            channelState.setMessages(messages);
         }
-        return value;
+
+        /*Set ChannelState to Channel*/
+        channelState.getChannel().setChannelState(channelState);
+
+        return channelState;
     }
 }
