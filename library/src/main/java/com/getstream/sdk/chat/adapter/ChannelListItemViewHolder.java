73a41e47d3bc591fdf71a8c38aa80a1bb8ba7e1f
adrian09h <adrian09h@gmail.com>
2019-10-27 17:02:55 +0100

    clean up

--- library/src/main/java/com/getstream/sdk/chat/adapter/ChannelListItemViewHolder.java
+++ library/src/main/java/com/getstream/sdk/chat/adapter/ChannelListItemViewHolder.java
@@ -13,9 +13,9 @@ import androidx.annotation.NonNull;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
 import com.getstream.sdk.chat.R;
-import com.getstream.sdk.chat.StreamChat;
 import com.getstream.sdk.chat.model.Attachment;
 import com.getstream.sdk.chat.model.Channel;
+import com.getstream.sdk.chat.model.ModelType;
 import com.getstream.sdk.chat.rest.Message;
 import com.getstream.sdk.chat.rest.User;
 import com.getstream.sdk.chat.rest.response.ChannelState;
@@ -28,7 +28,6 @@ import com.getstream.sdk.chat.view.ChannelListViewStyle;
 import com.getstream.sdk.chat.view.ReadStateView;
 
 import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.List;
 
 import io.noties.markwon.Markwon;
@@ -41,7 +40,7 @@ public class ChannelListItemViewHolder extends BaseChannelListItemViewHolder {
 
     public ConstraintLayout cl_root;
     public TextView tv_name, tv_last_message, tv_date, tv_click;
-    public ReadStateView read_state;
+    public ReadStateView<ChannelListViewStyle> read_state;
     public AvatarGroupView<ChannelListViewStyle> avatarGroupView;
 
     private Context context;
@@ -110,7 +109,7 @@ public class ChannelListItemViewHolder extends BaseChannelListItemViewHolder {
     }
 
     @Override
-    public void bind(Context context, ChannelState channelState, int position) {
+    public void bind(Context context, @NonNull ChannelState channelState, int position) {
 
         // setup the click listeners and the markdown builder
         this.context = context;
@@ -119,49 +118,32 @@ public class ChannelListItemViewHolder extends BaseChannelListItemViewHolder {
         // - lastMessage
         // - unread count
         // - read state for this channel
-        Message lastMessage = channelState.getLastMessage();
-        List<ChannelUserRead> lastMessageReads = channelState.getLastMessageReads();
-        List<User> otherUsers = channelState.getOtherUsers();
-        String channelName = channelState.getChannelNameOrMembers();
-        Channel channel = channelState.getChannel();
-
-        // set the data for the avatar
-        avatarGroupView.setChannelAndLastActiveUsers(channelState.getChannel(), otherUsers, style);
 
         // set the channel name
-        tv_name.setText(channelName);
-        // set the lastMessage and last messageDate
-        if (lastMessage != null) {
-            if (TextUtils.isEmpty(lastMessage.getText())) {
-                if (!lastMessage.getAttachments().isEmpty()) {
-                    Attachment attachment = lastMessage.getAttachments().get(0);
-                    tv_last_message.setText(!TextUtils.isEmpty(attachment.getTitle()) ? attachment.getTitle() : attachment.getFallback());
-                } else {
-                    tv_last_message.setText("");
-                }
-            } else {
-                if (markwon == null)
-                    markwon = Markwon.builder(context)
-                            .usePlugin(CorePlugin.create())
-                            .usePlugin(LinkifyPlugin.create())
-                            .build();
-                markwon.setMarkdown(tv_last_message, StringUtility.getDeletedOrMentionedText(lastMessage));
-            }
-
-            if (lastMessage.isToday())
-                tv_date.setText(lastMessage.getTime());
-            else
-                tv_date.setText(dateFormat.format(lastMessage.getCreatedAt()));
-        }
-
+        configChannelName(channelState);
+        // set the data for the avatar
+        configAvatarView(channelState);
+        // set the lastMessage
+        configLastMessage(channelState);
+        // set last message date
+        configLastMessageDate(channelState);
         // read indicators
-        read_state.setReads(lastMessageReads, true, style);
+        configReadState(channelState);
+        // set Click listeners
+        configClickListeners(channelState);
+        // apply style
+        applyStyle(channelState);
+    }
 
-        if (channelState.readLastMessage())
-            applyReadStyle();
-        else
-            applyUnreadStyle();
+    private void configChannelName(ChannelState channelState){
+        String channelName = channelState.getChannelNameOrMembers();
+        tv_name.setText(channelName);
+    }
 
+    private void configAvatarView(ChannelState channelState){
+        Channel channel = channelState.getChannel();
+        List<User> otherUsers = channelState.getOtherUsers();
+        avatarGroupView.setChannelAndLastActiveUsers(channelState.getChannel(), otherUsers, style);
         // click listeners
         avatarGroupView.setOnClickListener(view -> {
             // if there is 1 user
@@ -169,10 +151,67 @@ public class ChannelListItemViewHolder extends BaseChannelListItemViewHolder {
                 this.userClickListener.onUserClick(otherUsers.get(0));
             } else if (this.channelClickListener != null) {
                 this.channelClickListener.onClick(channel);
-
             }
         });
+    }
 
+    private void configLastMessage(ChannelState channelState){
+        Message lastMessage = channelState.getLastMessage();
+        // null check
+        if (lastMessage == null){
+            tv_last_message.setText("");
+            return;
+        }
+        // set Markwon
+        if (!TextUtils.isEmpty(lastMessage.getText())) {
+            if (markwon == null)
+                markwon = Markwon.builder(context)
+                        .usePlugin(CorePlugin.create())
+                        .usePlugin(LinkifyPlugin.create())
+                        .build();
+            markwon.setMarkdown(tv_last_message, StringUtility.getDeletedOrMentionedText(lastMessage));
+            return;
+        }
+
+        if (lastMessage.getAttachments().isEmpty()){
+            tv_last_message.setText("");
+            return;
+        }
+
+        Attachment attachment = lastMessage.getAttachments().get(0);
+        if (attachment.getType() == null)
+            return;
+
+        switch (attachment.getType()){
+            case ModelType.attach_image:
+                break;
+            case ModelType.attach_giphy:
+                break;
+        }
+        tv_last_message.setText(!TextUtils.isEmpty(attachment.getTitle()) ? attachment.getTitle() : attachment.getFallback());
+    }
+
+    private void configLastMessageDate(ChannelState channelState){
+        Message lastMessage = channelState.getLastMessage();
+        // null check
+        if (lastMessage == null) {
+            tv_date.setText("");
+            return;
+        }
+
+        if (lastMessage.isToday())
+            tv_date.setText(lastMessage.getTime());
+        else
+            tv_date.setText(dateFormat.format(lastMessage.getCreatedAt()));
+    }
+
+    private void configReadState(ChannelState channelState){
+        List<ChannelUserRead> lastMessageReads = channelState.getLastMessageReads();
+        read_state.setReads(lastMessageReads, true, style);
+    }
+
+    private void configClickListeners(ChannelState channelState){
+        Channel channel = channelState.getChannel();
         tv_click.setOnClickListener(view -> {
             Utils.setButtonDelayEnable(view);
             tv_click.setBackgroundColor(Color.parseColor("#14000000"));
@@ -187,8 +226,14 @@ public class ChannelListItemViewHolder extends BaseChannelListItemViewHolder {
             if (this.channelLongClickListener != null) {
                 this.channelLongClickListener.onClick(channel);
             }
-
             return true;
         });
+    }
+
+    private void applyStyle(ChannelState channelState){
+        if (channelState.readLastMessage())
+            applyReadStyle();
+        else
+            applyUnreadStyle();
     }
 }
