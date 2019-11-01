e9e0082dce1869df9128d2f5739d9123d1e9bfa4
adrian09h <adrian09h@gmail.com>
2019-11-01 18:44:00 +0100

    minor update

--- library/src/main/java/com/getstream/sdk/chat/utils/EditTextUtils.java
+++ library/src/main/java/com/getstream/sdk/chat/utils/EditTextUtils.java
@@ -19,6 +19,25 @@ public class EditTextUtils {
         }
     }
 
+    public static void beforeTextChanged(TextView textView, Actions.BeforeChangedAction action){
+        textView.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+                action.act(s, start, count, after);
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                //ignore
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                //ignore
+            }
+        });
+    }
+
     public static void onTextChanged(TextView textView, Actions.TextChangedAction action){
         textView.addTextChangedListener(new TextWatcher() {
             @Override
@@ -29,25 +48,6 @@ public class EditTextUtils {
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
                 action.act(s, start, before, count);
-            }
-
-            @Override
-            public void afterTextChanged(Editable editable) {
-                //ignore
-            }
-        });
-    }
-
-    public static void beforeTextChanged(TextView textView, Actions.BeforeChangedAction action){
-        textView.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s1, int start1, int count1, int after1) {
-                action.act(s1, start1, count1, after1);
-            }
-
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-                //ignore
             }
 
             @Override
