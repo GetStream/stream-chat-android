913d806ce7287379daa3183c150d691702f1091d
adrian09h <adrian09h@gmail.com>
2019-11-13 21:53:54 +0100

    minor update

--- library/src/test/java/com/getstream/sdk/chat/rest/core/ClientTest.java
+++ library/src/test/java/com/getstream/sdk/chat/rest/core/ClientTest.java
@@ -70,7 +70,6 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
-import java.util.Map;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
