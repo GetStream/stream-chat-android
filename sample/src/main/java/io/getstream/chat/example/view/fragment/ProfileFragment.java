8ce6818411cfc5458b88e99aceaf1dfd2fb161d4
adrian09h <adrian09h@gmail.com>
2019-11-14 10:08:53 +0100

    work profile fragment

--- sample/src/main/java/io/getstream/chat/example/view/fragment/ProfileFragment.java
+++ sample/src/main/java/io/getstream/chat/example/view/fragment/ProfileFragment.java
@@ -10,9 +10,11 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.getstream.sdk.chat.StreamChat;
+import com.getstream.sdk.chat.rest.core.Client;
+
 import io.getstream.chat.example.LoginActivity;
 import io.getstream.chat.example.databinding.FragmentProfileBinding;
-
 
 public class ProfileFragment extends Fragment {
 
@@ -28,9 +30,18 @@ public class ProfileFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         FragmentProfileBinding binding = FragmentProfileBinding.inflate(inflater, container, false);
+
+        Intent i = getActivity().getIntent();
+        String USER_NAME = i.getStringExtra("name");
+        String USER_IMAGE = i.getStringExtra("image");
+        binding.setName(USER_NAME);
+        binding.setImage(USER_IMAGE);
         binding.btnLogOut.setOnClickListener(view -> {
+            Client client = StreamChat.getInstance(getContext());
+            client.disconnect();
             Intent intent = new Intent(getContext(), LoginActivity.class);
             startActivity(intent);
+            getActivity().finish();
         });
         return binding.getRoot();
     }
