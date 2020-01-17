785422dde29512f7ebc992f6f75f587cd1c586c4
Eugene Levenetc <shumvlesu@gmail.com>
2020-01-17 11:50:31 +0100

    Naming cleanup

--- app/src/main/java/io/getstream/chat/android/core/poc/library/Call.kt
+++
@@ -1,8 +0,0 @@
-package io.getstream.chat.android.core.poc.library
-
-interface Call<T> {
-    fun execute(): Result<T>
-    fun enqueue(callback: (Result<T>) -> Unit)
-    fun cancel()
-    fun <K> map(mapper: (T) -> K): Call<K>
-}
