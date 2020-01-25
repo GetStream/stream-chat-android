2c1d5f4ce5a86ee267bb63640805bb55255b055c
Thierry Schellenbach <thierryschellenbach@gmail.com>
2020-01-24 20:48:04 -0700

    improve the viewmodel factory

--- library/src/main/java/com/getstream/sdk/chat/adapter/ChannelViewHolderFactory.java
+++ library/src/main/java/com/getstream/sdk/chat/adapter/ChannelViewHolderFactory.java
@@ -13,27 +13,31 @@ import com.getstream.sdk.chat.view.ChannelListViewStyle;
  * Allows you to easily customize channel rendering
  */
 public class ChannelViewHolderFactory {
-    private static int CHANNEL_GENERAL = 0;
+    public static int CHANNEL_GENERAL = 0;
 
     public int getChannelViewType(Channel channel) {
         return CHANNEL_GENERAL;
     }
 
-
-    public BaseChannelListItemViewHolder createChannelViewHolder(ChannelListItemAdapter adapter, ViewGroup parent, int viewType) {
-        if (viewType == CHANNEL_GENERAL) {
-            ChannelListViewStyle style = adapter.getStyle();
-            View v = LayoutInflater.from(parent.getContext()).inflate(style.channelPreviewLayout, parent, false);
-            ChannelListItemViewHolder holder = new ChannelListItemViewHolder(v);
-            holder.setStyle(style);
-            holder.setMarkdownListener(MarkdownImpl.getMarkdownListener());
-            holder.setChannelClickListener(adapter.getChannelClickListener());
-            holder.setChannelLongClickListener(adapter.getChannelLongClickListener());
-            holder.setUserClickListener(adapter.getUserClickListener());
-            return holder;
-        } else {
-            return null;
-        }
+    protected void configureHolder(BaseChannelListItemViewHolder holder, ChannelListItemAdapter adapter) {
+        holder.setStyle(adapter.getStyle());
+        holder.setMarkdownListener(MarkdownImpl.getMarkdownListener());
+        holder.setChannelClickListener(adapter.getChannelClickListener());
+        holder.setChannelLongClickListener(adapter.getChannelLongClickListener());
+        holder.setUserClickListener(adapter.getUserClickListener());
     }
 
+    public BaseChannelListItemViewHolder createChannelViewHolder(ChannelListItemAdapter adapter, ViewGroup parent, int viewType) {
+        // get the style object
+        ChannelListViewStyle style = adapter.getStyle();
+        // inflate the layout specified in the style
+        View v = LayoutInflater.from(parent.getContext()).inflate(style.channelPreviewLayout, parent, false);
+
+        // configure the viewholder
+        ChannelListItemViewHolder holder = new ChannelListItemViewHolder(v);
+        configureHolder(holder, adapter);
+        // return..
+
+        return holder;
+    }
 }
