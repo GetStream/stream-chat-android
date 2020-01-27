c6edefae688ae7e899bc2219858bd1f482d86416
Eugene Levenetc <shumvlesu@gmail.com>
2020-01-27 16:21:06 +0100

    Add devices tests

--- app/src/main/java/io/getstream/chat/android/core/poc/library/rest/GetDeviceResponse.kt
+++ app/src/main/java/io/getstream/chat/android/core/poc/library/rest/GetDeviceResponse.kt
@@ -3,6 +3,4 @@ package io.getstream.chat.android.core.poc.library.rest
 import io.getstream.chat.android.core.poc.library.Device
 
 
-class GetDevicesResponse {
-    val devices = listOf<Device>()
-}
+data class GetDevicesResponse(val devices:List<Device> = emptyList())
