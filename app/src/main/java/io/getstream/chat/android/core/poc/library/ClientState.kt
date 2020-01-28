a60ef1f951d5e9db6b5d24911d6bba4b3fb14bd8
Eugene Levenetc <shumvlesu@gmail.com>
2020-01-28 11:27:04 +0100

    Add check if user set

--- app/src/main/java/io/getstream/chat/android/core/poc/library/ClientState.kt
+++ app/src/main/java/io/getstream/chat/android/core/poc/library/ClientState.kt
@@ -3,10 +3,11 @@ package io.getstream.chat.android.core.poc.library
 class ClientState {
 
     var user: User? = null
-    var connectionId:String? = null
+    var connectionId: String? = null
 
     fun reset() {
         user = null
+        connectionId = null
     }
 
     fun getUser(id: String): User {
