0209cadf8cbf6ebf23e5e011826374434903356f
andrii.zhumela <andrii.zhumela@stfalcon.com>
2020-01-30 17:02:21 +0200

    Add Stream chat logger

--- app/src/main/java/io/getstream/chat/android/core/poc/app/App.kt
+++ app/src/main/java/io/getstream/chat/android/core/poc/app/App.kt
@@ -1,6 +1,7 @@
 package io.getstream.chat.android.core.poc.app
 
 import android.app.Application
+import io.getstream.chat.android.core.poc.BuildConfig
 import io.getstream.chat.android.core.poc.app.cache.AppDatabase
 import io.getstream.chat.android.core.poc.app.common.KeyValue
 import io.getstream.chat.android.core.poc.app.repositories.ChannelsRepositoryLive
@@ -9,27 +10,12 @@ import io.getstream.chat.android.core.poc.app.repositories.ChannelsRepositorySyn
 import io.getstream.chat.android.core.poc.library.ChatClient
 import io.getstream.chat.android.core.poc.library.ChatClientBuilder
 import io.getstream.chat.android.core.poc.library.api.ApiClientOptions
+import io.getstream.chat.android.core.poc.library.logger.StreamChatLogger
+import io.getstream.chat.android.core.poc.library.logger.StreamLogger
+import io.getstream.chat.android.core.poc.library.logger.StreamLoggerHandler
+import io.getstream.chat.android.core.poc.library.logger.StreamLoggerLevel
 
 class App : Application() {
-
-
-    override fun onCreate() {
-        super.onCreate()
-        db = AppDatabase.getInstance(this)
-        val apiKey = "d2q3juekvgsf"
-        val apiOptions = ApiClientOptions.Builder()
-            .baseURL("chat-us-east-staging.stream-io-api.com")
-            .cdnUrl("chat-us-east-staging.stream-io-api.com")
-            .timeout(10000)
-            .cdnTimeout(10000)
-            .build()
-        client = ChatClientBuilder(apiKey, apiOptions).build()
-        keyValue = KeyValue(this)
-        cache = ChannelsCache(db.channels())
-        channelsRepositorySync = ChannelsRepositorySync(client, cache)
-        channelsRepositoryRx = ChannelsRepositoryRx(client, cache)
-        channelsRepositoryLive = ChannelsRepositoryLive(client, cache)
-    }
 
     companion object {
         lateinit var client: ChatClient
@@ -39,5 +25,63 @@ class App : Application() {
         lateinit var db: AppDatabase
         lateinit var cache: ChannelsCache
         lateinit var keyValue: KeyValue
+    }
+
+    private lateinit var logger: StreamLogger
+
+    override fun onCreate() {
+        super.onCreate()
+        db = AppDatabase.getInstance(this)
+
+        val apiKey = "d2q3juekvgsf"
+
+        val apiOptions = ApiClientOptions.Builder()
+            .baseURL("chat-us-east-staging.stream-io-api.com")
+            .cdnUrl("chat-us-east-staging.stream-io-api.com")
+            .timeout(10000)
+            .cdnTimeout(10000)
+            .build()
+
+        setupLogger()
+
+        client = ChatClientBuilder(apiKey, apiOptions, logger).build()
+        keyValue = KeyValue(this)
+        cache = ChannelsCache(db.channels())
+        channelsRepositorySync = ChannelsRepositorySync(client, cache)
+        channelsRepositoryRx = ChannelsRepositoryRx(client, cache)
+        channelsRepositoryLive = ChannelsRepositoryLive(client, cache)
+    }
+
+    private fun setupLogger() {
+        val loggerHandler: StreamLoggerHandler = object : StreamLoggerHandler {
+            override fun logT(throwable: Throwable) {
+                // display throwable logs here
+            }
+
+            override fun logT(className: String, throwable: Throwable) {
+                // display throwable logs here
+            }
+
+            override fun logI(className: String, message: String) {
+                // display info logs here
+            }
+
+            override fun logD(className: String, message: String) {
+                // display debug logs here
+            }
+
+            override fun logW(className: String, message: String) {
+                // display warning logs here
+            }
+
+            override fun logE(className: String, message: String) {
+                // display error logs here
+            }
+        }
+
+        logger = StreamChatLogger.Builder()
+            .loggingLevel(if (BuildConfig.DEBUG) StreamLoggerLevel.ALL else StreamLoggerLevel.NOTHING)
+            .setLoggingHandler(loggerHandler)
+            .build()
     }
 }
