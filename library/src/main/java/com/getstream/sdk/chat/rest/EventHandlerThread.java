28ee1eb63cc91a84e3e6b75f630e65f1e56478ae
Jeff Lockhart <jeff@salesrabbit.com>
2020-02-01 22:27:21 -0700

    Fix event thread handler running on UI thread

--- library/src/main/java/com/getstream/sdk/chat/rest/EventHandlerThread.java
+++ library/src/main/java/com/getstream/sdk/chat/rest/EventHandlerThread.java
@@ -1,11 +1,20 @@
 package com.getstream.sdk.chat.rest;
 
+import android.os.Looper;
+
 class EventHandlerThread extends Thread {
     EventHandler mHandler;
+    private WebSocketService webSocketService;
 
     public EventHandlerThread(WebSocketService webSocketService) {
-        mHandler = new EventHandler(webSocketService);
+        this.webSocketService = webSocketService;
     }
 
-
+    @Override
+    public void run() {
+        super.run();
+        Looper.prepare();
+        mHandler = new EventHandler(webSocketService);
+        Looper.loop();
+    }
 }
