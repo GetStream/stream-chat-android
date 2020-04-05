31034b69fd78132557975f0a40db54b212ef4400
Thierry Schellenbach <thierryschellenbach@gmail.com>
2020-04-04 18:40:57 -0600

    remove hashCode overwrite which doesn't seem needed on the data class

--- library/src/main/java/io/getstream/chat/android/client/utils/FilterObject.kt
+++ library/src/main/java/io/getstream/chat/android/client/utils/FilterObject.kt
@@ -37,20 +37,4 @@ data class FilterObject(val data: HashMap<String, Any> = HashMap()) {
         return FilterObject(clone)
     }
 
-    override fun equals(other: Any?): Boolean {
-        if (this === other) return true
-        if (javaClass != other?.javaClass) return false
-
-        other as FilterObject
-
-        if (data != other.data) return false
-
-        return true
-    }
-
-    override fun hashCode(): Int {
-        return data.hashCode()
-    }
-
-
 }
