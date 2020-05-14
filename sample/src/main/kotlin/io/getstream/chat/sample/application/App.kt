1b5c55b279f32470df64b9c6a73f2b7ffd9a5bc1
Samuel Urbanowicz <samiuelson@gmail.com>
2020-05-14 15:02:40 +0200

    Add logs

--- sample/src/main/kotlin/io/getstream/chat/sample/application/App.kt
+++ sample/src/main/kotlin/io/getstream/chat/sample/application/App.kt
@@ -11,6 +11,7 @@ import org.koin.android.ext.koin.androidContext
 import org.koin.android.ext.koin.androidLogger
 import org.koin.core.context.startKoin
 import org.koin.dsl.module
+import timber.log.Timber
 
 class App : Application() {
 
@@ -20,6 +21,9 @@ class App : Application() {
         super.onCreate()
         initKoin()
         Chat.Builder(appConfig.apiKey, this).build()
+        if (BuildConfig.DEBUG) {
+            Timber.plant(Timber.DebugTree())
+        }
     }
 
     private fun initKoin() {
---
+++ sample/src/main/kotlin/io/getstream/chat/sample/common/CommonsModule.kt
@@ -0,0 +1,8 @@
+package io.getstream.chat.sample.common
+
+import com.bumptech.glide.Glide
+import org.koin.dsl.module
+
+val commonsModule = module {
+    single {  }
+}
