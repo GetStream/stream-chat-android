4a814734a4b114bd1c4d1e0dda61b769931ecda3
Samuel Urbanowicz <samiuelson@gmail.com>
2020-05-14 21:05:07 +0200

    Remove unused import

--- library/src/main/java/com/getstream/sdk/chat/view/ChannelListView.java
+++ library/src/main/java/com/getstream/sdk/chat/view/ChannelListView.java
@@ -5,7 +5,6 @@ import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.Drawable;
 import android.util.AttributeSet;
-import android.util.Log;
 import android.view.View;
 
 import androidx.annotation.NonNull;
