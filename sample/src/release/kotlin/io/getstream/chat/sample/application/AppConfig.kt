e1fba41bd7578da897d0ef6cd5ec99e1bed42f89
Samuel Urbanowicz <samiuelson@gmail.com>
2020-05-15 14:02:25 +0200

    Add release AppConfig variant

---
+++ sample/src/release/kotlin/io/getstream/chat/sample/application/AppConfig.kt
@@ -0,0 +1,11 @@
+package io.getstream.chat.sample.application
+
+import io.getstream.chat.sample.data.user.User
+
+class AppConfig {
+    val apiKey: String = ""
+    val apiUrl: String = ""
+    val apiTimeout: Int = 0
+    val cndTimeout: Int = 0
+    val availableUsers: List<User> = listOf()
+}
