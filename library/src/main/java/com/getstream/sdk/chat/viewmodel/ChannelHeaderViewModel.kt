27480f4704f7cf7fbec228c9833f79f322b86e12
Jc Miñarro <josecarlos.minarro@gmail.com>
2020-05-20 11:24:13 +0200

    Create ChannelHeaderViewModel

---
+++ library/src/main/java/com/getstream/sdk/chat/viewmodel/ChannelHeaderViewModel.kt
@@ -0,0 +1,33 @@
+package com.getstream.sdk.chat.viewmodel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations.map
+import androidx.lifecycle.ViewModel
+import io.getstream.chat.android.client.models.Channel
+import io.getstream.chat.android.client.models.Member
+import io.getstream.chat.android.livedata.ChatDomain
+import io.getstream.chat.android.livedata.controller.ChannelController
+
+private const val MESSAGE_LIMIT = 30
+class ChannelHeaderViewModel(
+		cid: String,
+		messageLimit: Int = MESSAGE_LIMIT,
+		private val chatDomain: ChatDomain = ChatDomain.instance()
+) : ViewModel() {
+
+	val members: LiveData<List<Member>>
+	val channelState: LiveData<Channel>
+	val anyOtherUsersOnline: LiveData<Boolean>
+
+	init {
+		val channelController: ChannelController = chatDomain.useCases.watchChannel.invoke(cid, messageLimit).execute().data()
+		members = channelController.members
+		channelState = MutableLiveData(channelController.toChannel())
+		anyOtherUsersOnline = map(members) { members ->
+			members.asSequence()
+					.filter { it.user != chatDomain.currentUser }
+					.any { it.user.online }
+		}
+	}
+}
