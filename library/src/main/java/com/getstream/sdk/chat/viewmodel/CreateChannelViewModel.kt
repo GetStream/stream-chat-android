51d1c771dcd68055a38afc5aba05b059fc416f9f
Samuel Urbanowicz <samiuelson@gmail.com>
2020-05-22 18:34:35 +0200

    Rename properties

--- library/src/main/java/com/getstream/sdk/chat/viewmodel/CreateChannelViewModel.kt
+++ library/src/main/java/com/getstream/sdk/chat/viewmodel/CreateChannelViewModel.kt
@@ -10,8 +10,8 @@ import io.getstream.chat.android.client.models.name
 import io.getstream.chat.android.livedata.ChatDomain
 
 class CreateChannelViewModel(
-        private val chatDomain: ChatDomain = ChatDomain.instance(),
-        private val chatClient: ChatClient = ChatClient.instance()
+        private val domain: ChatDomain = ChatDomain.instance(),
+        private val client: ChatClient = ChatClient.instance()
 ) : ViewModel() {
     private val stateMerger = MediatorLiveData<State>()
     val state: LiveData<State> = stateMerger
@@ -30,7 +30,7 @@ class CreateChannelViewModel(
 
     private fun queryChannel(channelName: String) {
         val channelId: String = channelName.replace(" ".toRegex(), "-").toLowerCase()
-        val members = chatClient.getCurrentUser()?.run {
+        val members = client.getCurrentUser()?.run {
             listOf(Member(this))
         } ?: listOf()
         val channel = Channel().apply {
@@ -40,7 +40,7 @@ class CreateChannelViewModel(
             this.name = channelName
             this.members = members
         }
-        chatDomain.useCases.createChannel.invoke(channel).execute().run {
+        domain.useCases.createChannel.invoke(channel).execute().run {
             when  {
                 isSuccess -> {
                     stateMerger.postValue(State.ChannelCreated)
