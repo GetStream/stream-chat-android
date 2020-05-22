26d8ee62134f63eb8d71fa6091a61f0ced5ac2ce
“Eugene <shumvlesu@gmail.com>
2020-05-22 17:46:30 +0200

    Update Member model: set fields role and isInvited as optional

--- library/src/main/java/io/getstream/chat/android/client/models/Member.kt
+++ library/src/main/java/io/getstream/chat/android/client/models/Member.kt
@@ -6,8 +6,15 @@ import java.util.*
 
 data class Member(
     override var user: User,
+    var role: String? = null,
     @SerializedName("created_at")
     var createdAt: Date? = null,
     @SerializedName("updated_at")
-    var updatedAt: Date? = null
+    var updatedAt: Date? = null,
+    @SerializedName("invited")
+    var isInvited: Boolean? = null,
+    @SerializedName("invite_accepted_at")
+    var inviteAcceptedAt: Date? = null,
+    @SerializedName("invite_rejected_at")
+    var inviteRejectedAt: Date? = null
 ) : UserEntity
