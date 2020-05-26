dc86ad7396a3805ecf9c7cb6bdfad7c248b17c8b
Jc Miñarro <josecarlos.minarro@gmail.com>
2020-05-26 14:24:23 +0200

    Bind send message behavior on the view

--- library/src/main/java/com/getstream/sdk/chat/viewmodel/MessageInputViewModelBinding.kt
+++ library/src/main/java/com/getstream/sdk/chat/viewmodel/MessageInputViewModelBinding.kt
@@ -6,6 +6,9 @@ import com.getstream.sdk.chat.view.MessageInputView
 
 fun MessageInputViewModel.bindView(view: MessageInputView, lifecycleOwner: LifecycleOwner) {
 	view.setViewModel(this, lifecycleOwner)
+	view.onSendMessageListener = object : MessageInputView.OnSendMessageListener {
+		override fun onSendTextMessage(message: String) = sendMessage(message)
+	}
 	members.observe(lifecycleOwner, Observer { view.configureMembers(it) })
 	commands.observe(lifecycleOwner, Observer { view.configureCommands(it) })
 }
