4cd03602c13169330354e8666a9d479480522ea6
Samuel Urbanowicz <samiuelson@gmail.com>
2020-05-28 14:48:06 +0200

    Implement basic MessageListViewModel features

--- library/src/main/java/com/getstream/sdk/chat/viewmodel/MessageListViewModel.kt
+++ library/src/main/java/com/getstream/sdk/chat/viewmodel/MessageListViewModel.kt
@@ -2,39 +2,63 @@ package com.getstream.sdk.chat.viewmodel
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MediatorLiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.getstream.sdk.chat.utils.MessageListItemLiveData
+import com.getstream.sdk.chat.utils.MessageListItemWrapper
 import io.getstream.chat.android.client.models.Message
 import io.getstream.chat.android.livedata.ChatDomain
 
+private const val MESSAGES_LIMIT = 30
 class MessageListViewModel(private val channelId: String,
+                           private val cid: String,
                            private val domain: ChatDomain = ChatDomain.instance()) : ViewModel() {
+    private val threadMessages: LiveData<List<Message>> = MutableLiveData()
     private val stateMerger = MediatorLiveData<State>()
     val state: LiveData<State> = stateMerger
 
     init {
-
+        val channelController = domain.useCases.watchChannel(cid, MESSAGES_LIMIT).execute().data()
+        val currentUser = domain.currentUser
+        val listItems = MessageListItemLiveData(
+                currentUser,
+                channelController.messages,
+                threadMessages,
+                channelController.typing,
+                channelController.reads
+        )
+        stateMerger.addSource(listItems) { State.Result(it) }
     }
 
     fun onEvent(event: Event) {
         when (event) {
-            is Event.LoadMore -> {
-                // TODO
+            is Event.EndRegionReached -> {
+                domain.useCases.loadOlderMessages(cid, MESSAGES_LIMIT)// limit was 50 previously
             }
-            is Event.EditMessage -> {}
-            is Event.ReplyToMessage -> {}
-            is Event.DeleteMessage -> {}
+            is Event.ThreadEndRegionReached -> {
+                domain.useCases.threadLoadMore(cid, event.parentMessage.id, MESSAGES_LIMIT).execute()
+            }
+            is Event.EditMessage -> {
+            }
+            is Event.ThreadModeEntered -> {
+            }
+            is Event.DeleteMessage -> {
+            }
         }
     }
 }
 
 sealed class State {
     object Loading : State()
-    data class Result(val messages: List<Message>) : State()
+    object LoadingMore : State()
+    data class Result(val messageListItem: MessageListItemWrapper) : State()
 }
 
 sealed class Event {
-    object LoadMore : Event()
+    object EndRegionReached : Event()
+    data class ThreadEndRegionReached(val parentMessage: Message): Event()
+    object LastMessageRead : Event()
     data class EditMessage(val message: Message) : Event()
-    data class ReplyToMessage(val message: Message) : Event()
+    data class ThreadModeEntered(val parentMessage: Message) : Event()
     data class DeleteMessage(val message: Message) : Event()
 }
