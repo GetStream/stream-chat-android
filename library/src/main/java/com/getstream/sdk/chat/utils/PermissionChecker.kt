f0a2c956d17dc27f5e484746233a27fec21dc031
Jc Miñarro <josecarlos.minarro@gmail.com>
2020-06-01 10:09:33 +0200

    Improve PermissionChecker Kotlin code

--- library/src/main/java/com/getstream/sdk/chat/utils/PermissionChecker.kt
+++ library/src/main/java/com/getstream/sdk/chat/utils/PermissionChecker.kt
@@ -2,62 +2,29 @@ package com.getstream.sdk.chat.utils
 
 import android.Manifest
 import android.R
-import android.app.Activity
 import android.content.Context
 import android.content.DialogInterface
 import android.content.pm.PackageManager
 import android.os.Build
 import android.view.View
 import androidx.appcompat.app.AlertDialog
-import androidx.fragment.app.Fragment
+import androidx.core.content.ContextCompat
 import com.getstream.sdk.chat.Chat.Companion.getInstance
 import com.getstream.sdk.chat.navigation.destinations.AppSettingsDestination
-import java.util.ArrayList
 
 object PermissionChecker {
-	fun permissionCheck(activity: Activity, fragment: Fragment?) {
-		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M &&
-				(activity.checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED || activity.checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED || activity.checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED)) {
-			val hasStoragePermission = activity.checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)
-			val hasReadPermission = activity.checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
-			val hasCameraPermission = activity.checkSelfPermission(Manifest.permission.CAMERA)
-			val permissions: MutableList<String> = ArrayList()
-			if (hasStoragePermission != PackageManager.PERMISSION_GRANTED) {
-				permissions.add(Manifest.permission.WRITE_EXTERNAL_STORAGE)
-			}
-			if (hasReadPermission != PackageManager.PERMISSION_GRANTED) {
-				permissions.add(Manifest.permission.READ_EXTERNAL_STORAGE)
-			}
-			if (hasCameraPermission != PackageManager.PERMISSION_GRANTED) {
-				permissions.add(Manifest.permission.CAMERA)
-			}
-			if (! permissions.isEmpty()) {
-				if (fragment == null) activity.requestPermissions(permissions.toTypedArray(),
-						Constant.PERMISSIONS_REQUEST) else fragment.requestPermissions(permissions.toTypedArray(),
-						Constant.PERMISSIONS_REQUEST)
-			}
-		}
-	}
 
-	fun isGrantedStoragePermissions(context: Context): Boolean {
-		return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-			val hasStoragePermission = context.checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)
-			val hasReadPermission = context.checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
-			(hasStoragePermission == PackageManager.PERMISSION_GRANTED
-					&& hasReadPermission == PackageManager.PERMISSION_GRANTED)
-		} else true
-	}
+	fun isGrantedStoragePermissions(context: Context): Boolean = Build.VERSION.SDK_INT < Build.VERSION_CODES.M ||
+			listOf(Manifest.permission.WRITE_EXTERNAL_STORAGE,
+					Manifest.permission.WRITE_EXTERNAL_STORAGE,
+					Manifest.permission.READ_EXTERNAL_STORAGE)
+					.all { ContextCompat.checkSelfPermission(context, it) == PackageManager.PERMISSION_GRANTED }
 
-	fun isGrantedCameraPermissions(context: Context): Boolean {
-		return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-			val hasStoragePermission = context.checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE)
-			val hasReadPermission = context.checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
-			val hasCameraPermission = context.checkSelfPermission(Manifest.permission.CAMERA)
-			(hasStoragePermission == PackageManager.PERMISSION_GRANTED
-					&& hasReadPermission == PackageManager.PERMISSION_GRANTED
-					&& hasCameraPermission == PackageManager.PERMISSION_GRANTED)
-		} else true
-	}
+	fun isGrantedCameraPermissions(context: Context): Boolean = Build.VERSION.SDK_INT < Build.VERSION_CODES.M ||
+			listOf(Manifest.permission.WRITE_EXTERNAL_STORAGE,
+					Manifest.permission.WRITE_EXTERNAL_STORAGE,
+					Manifest.permission.CAMERA)
+					.all { ContextCompat.checkSelfPermission(context, it) == PackageManager.PERMISSION_GRANTED }
 
 	fun showPermissionSettingDialog(context: Context, message: String) {
 		val appName = Utils.getApplicationName(context)
