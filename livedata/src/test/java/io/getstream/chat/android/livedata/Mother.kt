c7cbad6c36b885b1feb3d7c47a0405cb0917e19f
Jc Miñarro <josecarlos.minarro@gmail.com>
2020-06-05 11:55:01 +0200

    Create Mother for tests

---
+++ livedata/src/test/java/io/getstream/chat/android/livedata/Mother.kt
@@ -0,0 +1,21 @@
+package io.getstream.chat.android.livedata
+
+import java.util.concurrent.ThreadLocalRandom
+
+private val charPool: List<Char> = ('a'..'z') + ('A'..'Z') + ('0'..'9')
+val random
+	get() = ThreadLocalRandom.current()
+
+fun positveRandomInt(maxInt: Int = Int.MAX_VALUE - 1): Int =
+		random.nextInt(maxInt + 1).takeIf { it > 0 } ?: positveRandomInt(maxInt)
+
+fun positveRandomLong(maxLong: Long = Long.MAX_VALUE - 1): Long =
+		random.nextLong(maxLong + 1).takeIf { it > 0 } ?: positveRandomLong(maxLong)
+
+fun randomInt() = random.nextInt()
+fun randomIntBetween(min: Int, max: Int) = random.nextInt(max - min) + min
+fun randomLong() = random.nextLong()
+fun randomBoolean() = random.nextBoolean()
+fun randomString(size: Int = 20): String = (0..size)
+		.map { charPool[random.nextInt(0, charPool.size)] }
+		.joinToString("")
