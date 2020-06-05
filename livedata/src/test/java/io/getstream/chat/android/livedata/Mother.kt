d3d11390094b6e40d6599ea6bdfd8889896c199d
Jc Miñarro <josecarlos.minarro@gmail.com>
2020-06-05 17:37:47 +0200

    Add some Mother methods

--- livedata/src/test/java/io/getstream/chat/android/livedata/Mother.kt
+++ livedata/src/test/java/io/getstream/chat/android/livedata/Mother.kt
@@ -1,5 +1,6 @@
 package io.getstream.chat.android.livedata
 
+import java.io.File
 import java.util.concurrent.ThreadLocalRandom
 
 private val charPool: List<Char> = ('a'..'z') + ('A'..'Z') + ('0'..'9')
@@ -19,3 +20,13 @@ fun randomBoolean() = random.nextBoolean()
 fun randomString(size: Int = 20): String = (0..size)
 		.map { charPool[random.nextInt(0, charPool.size)] }
 		.joinToString("")
+
+fun randomCID() = "${randomString()}:${randomString()}"
+
+fun randomFile(extension: String = randomString(3)) = File("${randomString()}.$extension")
+fun randomFiles(
+	size: Int = positveRandomInt(10),
+	creationFunction: (Int) -> File = { randomFile() }
+): List<File> =(1..size).map(creationFunction)
+
+fun randomImageFile() = randomFile(extension = "jpg")
