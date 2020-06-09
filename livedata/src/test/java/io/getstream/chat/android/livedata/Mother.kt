a22dc5343986e6111b270800377da4ae06008a64
Jc Miñarro <josecarlos.minarro@gmail.com>
2020-06-09 13:34:41 +0200

    Add Mother's Methods to generate random Messages and Users

--- livedata/src/test/java/io/getstream/chat/android/livedata/Mother.kt
+++ livedata/src/test/java/io/getstream/chat/android/livedata/Mother.kt
@@ -1,6 +1,9 @@
 package io.getstream.chat.android.livedata
 
+import io.getstream.chat.android.client.models.*
+import io.getstream.chat.android.client.utils.SyncStatus
 import java.io.File
+import java.util.*
 import java.util.concurrent.ThreadLocalRandom
 
 private val charPool: List<Char> = ('a'..'z') + ('A'..'Z') + ('0'..'9')
@@ -30,3 +33,53 @@ fun randomFiles(
 ): List<File> =(1..size).map(creationFunction)
 
 fun randomImageFile() = randomFile(extension = "jpg")
+
+fun randomUser(
+	id: String = randomString(),
+	role: String = randomString(),
+	invisible: Boolean = randomBoolean(),
+	banned: Boolean = randomBoolean(),
+	devices: List<Device> = mutableListOf(),
+	online: Boolean = randomBoolean(),
+	createdAt: Date? = null,
+	updatedAt: Date? = null,
+	lastActive: Date? = null,
+	totalUnreadCount: Int = positveRandomInt(),
+	unreadChannels: Int = positveRandomInt(),
+	unreadCount: Int = positveRandomInt(),
+	mutes: List<Mute> = mutableListOf(),
+	teams:List<String> = listOf(),
+	extraData: MutableMap<String, Any> = mutableMapOf()
+): User = User(id, role, invisible, banned, devices, online, createdAt, updatedAt, lastActive, totalUnreadCount, unreadChannels, unreadCount, mutes, teams, extraData)
+
+fun randomMessage(
+	id: String = randomString(),
+	cid: String = randomCID(),
+	text: String = randomString(),
+	html: String = randomString(),
+	parentId: String? = randomString(),
+	command: String? = randomString(),
+	isStartDay: Boolean = randomBoolean(),
+	isYesterday: Boolean = randomBoolean(),
+	isToday: Boolean = randomBoolean(),
+	date: String = randomString(),
+	time: String = randomString(),
+	commandInfo: Map<String, String> = mutableMapOf(),
+	attachments: MutableList<Attachment> = mutableListOf(),
+	mentionedUsers: MutableList<User> = mutableListOf(),
+	replyCount: Int = randomInt(),
+	reactionCounts: MutableMap<String, Int> = mutableMapOf(),
+	reactionScores: MutableMap<String, Int> = mutableMapOf(),
+	syncStatus: SyncStatus = randomSyncStatus(),
+	type: String = randomString(),
+	latestReactions: MutableList<Reaction> = mutableListOf(),
+	ownReactions: MutableList<Reaction> = mutableListOf(),
+	createdAt: Date? = null,
+	updatedAt: Date? = null,
+	deletedAt: Date? = null,
+	user: User = randomUser(),
+	extraData: MutableMap<String, Any> = mutableMapOf(),
+	silent: Boolean = randomBoolean()
+): Message =  Message(id, cid, text, html, parentId, command, isStartDay, isYesterday, isToday, date, time, commandInfo, attachments, mentionedUsers, replyCount, reactionCounts, reactionScores, syncStatus, type, latestReactions, ownReactions, createdAt, updatedAt, deletedAt, user, extraData, silent)
+
+fun randomSyncStatus(): SyncStatus = SyncStatus.values().asList().shuffled(random).first()
