72d843854406a19fb47a0b758c371ee407cb7752
Jc Miñarro <josecarlos.minarro@gmail.com>
2020-07-22 18:28:39 +0200

    Handle nullability properly on EventHandlerImpl

--- livedata/src/main/java/io/getstream/chat/android/livedata/EventHandlerImpl.kt
+++ livedata/src/main/java/io/getstream/chat/android/livedata/EventHandlerImpl.kt
@@ -1,7 +1,35 @@
 package io.getstream.chat.android.livedata
 
-import io.getstream.chat.android.client.events.*
+import io.getstream.chat.android.client.events.ChannelDeletedEvent
+import io.getstream.chat.android.client.events.ChannelHiddenEvent
+import io.getstream.chat.android.client.events.ChannelTruncated
+import io.getstream.chat.android.client.events.ChannelUpdatedEvent
+import io.getstream.chat.android.client.events.ChannelVisible
+import io.getstream.chat.android.client.events.ChatEvent
+import io.getstream.chat.android.client.events.ConnectedEvent
+import io.getstream.chat.android.client.events.DisconnectedEvent
+import io.getstream.chat.android.client.events.MemberAddedEvent
+import io.getstream.chat.android.client.events.MemberRemovedEvent
+import io.getstream.chat.android.client.events.MemberUpdatedEvent
+import io.getstream.chat.android.client.events.MessageDeletedEvent
+import io.getstream.chat.android.client.events.MessageReadEvent
+import io.getstream.chat.android.client.events.MessageUpdatedEvent
+import io.getstream.chat.android.client.events.NewMessageEvent
+import io.getstream.chat.android.client.events.NotificationAddedToChannelEvent
+import io.getstream.chat.android.client.events.NotificationChannelTruncated
+import io.getstream.chat.android.client.events.NotificationInviteAccepted
+import io.getstream.chat.android.client.events.NotificationInviteRejected
+import io.getstream.chat.android.client.events.NotificationInvited
+import io.getstream.chat.android.client.events.NotificationMessageNew
+import io.getstream.chat.android.client.events.NotificationMutesUpdated
+import io.getstream.chat.android.client.events.NotificationRemovedFromChannel
+import io.getstream.chat.android.client.events.ReactionDeletedEvent
+import io.getstream.chat.android.client.events.ReactionNewEvent
+import io.getstream.chat.android.client.events.UserBanned
+import io.getstream.chat.android.client.events.UserUnbanned
+import io.getstream.chat.android.client.events.UserUpdated
 import io.getstream.chat.android.client.logger.ChatLogger
+import io.getstream.chat.android.client.models.Channel
 import io.getstream.chat.android.client.models.ChannelUserRead
 import io.getstream.chat.android.client.models.User
 import io.getstream.chat.android.livedata.entity.ChannelEntity
@@ -10,9 +38,10 @@ import io.getstream.chat.android.livedata.entity.UserEntity
 import io.getstream.chat.android.livedata.extensions.getCid
 import io.getstream.chat.android.livedata.extensions.isChannelEvent
 import io.getstream.chat.android.livedata.extensions.users
-import kotlinx.coroutines.*
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 
-class EventHandlerImpl(var domainImpl: io.getstream.chat.android.livedata.ChatDomainImpl, var runAsync: Boolean = true) {
+class EventHandlerImpl(var domainImpl: ChatDomainImpl, var runAsync: Boolean = true) {
     private val logger = ChatLogger.get("ChatDomain EventHandler")
 
     fun handleEvents(events: List<ChatEvent>) {
@@ -48,26 +77,22 @@ class EventHandlerImpl(var domainImpl: io.getstream.chat.android.livedata.ChatDo
             val cid = event.cid ?: event.channel?.cid ?: ""
             logger.logI("Handling event of type ${event.type} for cid $cid, event batch size is ${events.size}")
             when (event) {
-                is MessageReadEvent, is MemberAddedEvent, is MemberRemovedEvent, is NotificationRemovedFromChannel,
-                is MemberUpdatedEvent, is ChannelUpdatedEvent, is ChannelDeletedEvent, is ChannelHiddenEvent, is ChannelVisible,
-                is NotificationAddedToChannelEvent, is NotificationInvited, is NotificationInviteAccepted, is NotificationInviteRejected
-                -> {
-                    // get the channel, update reads, write the channel
-                    channelsToFetch.add(cid)
-                }
-                is ReactionNewEvent -> {
-                    // get the message, update the reaction data, update the message
-                    // note that we need to use event.reaction and not event.message
-                    // event.message only has a subset of reactions
-                    messagesToFetch.add(event.reaction!!.messageId)
-                }
-                is ReactionDeletedEvent -> {
-                    // get the message, update the reaction data, update the message
-                    messagesToFetch.add(event.reaction!!.messageId)
-                }
-                is UserBanned, is UserUnbanned -> {
-                    me = domainImpl.repos.users.selectMe()
-                }
+                is MessageReadEvent,
+                is MemberAddedEvent,
+                is MemberRemovedEvent,
+                is NotificationRemovedFromChannel,
+                is MemberUpdatedEvent,
+                is ChannelUpdatedEvent,
+                is ChannelDeletedEvent,
+                is ChannelHiddenEvent,
+                is ChannelVisible,
+                is NotificationAddedToChannelEvent,
+                is NotificationInvited,
+                is NotificationInviteAccepted,
+                is NotificationInviteRejected -> channelsToFetch.add(cid)
+                is ReactionNewEvent,
+                is ReactionDeletedEvent -> event.reaction?.messageId?.let(messagesToFetch::add)
+                is UserBanned, is UserUnbanned -> { me = domainImpl.repos.users.selectMe() }
             }
         }
         // actually fetch the data
@@ -87,106 +112,125 @@ class EventHandlerImpl(var domainImpl: io.getstream.chat.android.livedata.ChatDo
             when (event) {
                 // keep the data in Room updated based on the various events..
                 // note that many of these events should also update user information
-                is NewMessageEvent, is MessageDeletedEvent, is MessageUpdatedEvent -> {
+                is NewMessageEvent,
+                is MessageDeletedEvent,
+                is MessageUpdatedEvent -> {
                     val message = event.message
-                    message.cid = event.cid!!
-                    messages[message.id] = MessageEntity(message)
-                    users.putAll(message.users().map { UserEntity(it) }.associateBy { it.id })
+                    event.cid?.let { cid ->
+                        message.cid = cid
+                        messages[message.id] = MessageEntity(message)
+                        users.putAll(message.users().map { UserEntity(it) }.associateBy { it.id })
+                    }
                 }
                 is NotificationMessageNew -> {
                     // add both the event and the channel
                     val message = event.message
-                    message.cid = event.cid!!
-                    messages[message.id] = MessageEntity(message)
-                    channels[event.channel!!.cid] = ChannelEntity(event.channel!!)
-                    users.putAll(message.users().map { UserEntity(it) }.associateBy { it.id })
-                    users.putAll(event.channel!!.users().map { UserEntity(it) }.associateBy { it.id })
+                    event.cid?.let { cid ->
+                        message.cid = cid
+                        messages[message.id] = MessageEntity(message)
+                        channels[cid] = ChannelEntity(event.channel ?: Channel(cid = cid))
+                        users.putAll(message.users().map { UserEntity(it) }.associateBy { it.id })
+                        event.channel?.let { channel ->
+                            users.putAll(channel.users().map { UserEntity(it) }.associateBy { it.id })
+                        }
+                    }
                 }
-                is NotificationAddedToChannelEvent, is NotificationInvited, is NotificationInviteAccepted, is NotificationInviteRejected -> {
-                    channels[event.channel!!.cid] = ChannelEntity(event.channel!!)
-                    users.putAll(event.channel!!.users().map { UserEntity(it) }.associateBy { it.id })
+                is NotificationAddedToChannelEvent,
+                is NotificationInvited,
+                is NotificationInviteAccepted,
+                is NotificationInviteRejected -> {
+                    event.channel?.let { channel ->
+                        channels[channel.cid] = ChannelEntity(channel)
+                        users.putAll(channel.users().map { UserEntity(it) }.associateBy { it.id })
+                    }
                 }
                 is ChannelHiddenEvent -> {
-                    val channelEntity = ChannelEntity(event.channel!!)
-                    channelEntity.hidden = true
-                    if (event.clearHistory != null && event.clearHistory!!) {
-                        channelEntity.hideMessagesBefore = event.createdAt
+                    event.cid?.let { cid ->
+                        val channelEntity = ChannelEntity(Channel(cid = cid))
+                        channelEntity.hidden = true
+                        if (event.clearHistory == true) {
+                            channelEntity.hideMessagesBefore = event.createdAt
+                        }
+                        channels[cid] = channelEntity
                     }
-                    channels[event.channel!!.cid] = channelEntity
                 }
                 is ChannelVisible -> {
-                    val channelEntity = ChannelEntity(event.channel!!)
-                    // TODO: this approach is not so nice since it's overwritten by other data
-                    channelEntity.hidden = false
-                    channels[event.channel!!.cid] = channelEntity
+                    event.cid?.let { cid ->
+                        val channelEntity = ChannelEntity(Channel(cid = cid))
+                        channelEntity.hidden = false
+                        channels[cid] = channelEntity
+                    }
                 }
-                is UserBanned, is UserUnbanned -> {
+                is UserBanned,
+                is UserUnbanned -> {
                     me?.let { it.banned = event is UserBanned }
                 }
                 is NotificationMutesUpdated -> {
-                    val muteEvent: NotificationMutesUpdated = event
-                    me = muteEvent.me
+                    me = event.me
                 }
                 is ConnectedEvent -> {
-                    val connectedEvent: ConnectedEvent = event
-                    me = connectedEvent.me
+                    if (event.isValid()) {
+                        me = event.me
+                    }
                 }
                 is MessageReadEvent -> {
                     // get the channel, update reads, write the channel
-                    val channel = channelMap[event.cid]
-                    val read = ChannelUserRead(event.user!!)
-                    read.lastRead = event.createdAt
-                    channel?.let {
-                        it.updateReads(read)
-                        channels[it.cid] = it
+                    channelMap[event.cid]?.let { channelEntity ->
+                        event.user?.let { user ->
+                            val read = ChannelUserRead(user)
+                            read.lastRead = event.createdAt
+                            channelEntity.updateReads(read)
+                        }
+                        channels[channelEntity.cid] = channelEntity
                     }
                 }
-                is UserUpdated -> {
-                    event.user?.let { users[it.id] = UserEntity(it) }
-                }
+                is UserUpdated -> event.user?.let { users[it.id] = UserEntity(it) }
                 is ReactionNewEvent -> {
                     // get the message, update the reaction data, update the message
                     // note that we need to use event.reaction and not event.message
                     // event.message only has a subset of reactions
-                    val message = messageMap[event.reaction!!.messageId]
-                    message?.let {
-                        val userId = event.reaction!!.user!!.id
-                        it.addReaction(event.reaction!!, domainImpl.currentUser.id == userId)
-                        messages[it.id] = it
+                    event.reaction?.let { reaction ->
+                        messageMap[reaction.messageId]?.let { messageEntity ->
+                            messageEntity.addReaction(reaction, domainImpl.currentUser.id == reaction.user?.id)
+                            messages[messageEntity.id] = messageEntity
+                        }
                     }
                 }
                 is ReactionDeletedEvent -> {
                     // get the message, update the reaction data, update the message
-                    val message = messageMap[event.reaction!!.messageId]
-                    message?.let {
-                        it.removeReaction(event.reaction!!, false)
-                        it.reactionCounts = event.message.reactionCounts
-                        messages[it.id] = it
+                    event.reaction?.let { reaction ->
+                        messageMap[reaction.messageId]?.let { messageEntity ->
+                            messageEntity.removeReaction(reaction, false)
+                            messageEntity.reactionCounts = event.message.reactionCounts
+                            messages[messageEntity.id] = messageEntity
+                        }
                     }
                 }
                 is MemberAddedEvent, is MemberUpdatedEvent -> {
-                    val channelEntity = channelMap[event.cid!!]
-                    channelEntity?.let { channelEntity ->
-                        event.channel?.members?.forEach { member ->
-                            channelEntity.setMember(member.user.id, member)
+                    event.cid?.let { cid ->
+                        channelMap[cid]?.let { channelEntity ->
+                            event.channel?.members?.forEach { member ->
+                                channelEntity.setMember(member.user.id, member)
+                            }
+                            channels[channelEntity.cid] = channelEntity
                         }
-                        channels[channelEntity.cid] = channelEntity
-                    }
-                    event.channel?.let { c ->
-                        users.putAll(c.users().map { UserEntity(it) }.associateBy { it.id })
+                        event.channel?.let { c ->
+                            users.putAll(c.users().map { UserEntity(it) }.associateBy { it.id })
+                        }
                     }
                 }
                 is MemberRemovedEvent, is NotificationRemovedFromChannel -> {
                     // get the channel, update members, write the channel
-                    val channelEntity = channelMap[event.getCid()!!]
                     var user = event.user
                     // quite confusing but NotificationRemovedFromChannel is only fired for the current user
                     if (event is NotificationRemovedFromChannel) {
                         user = domainImpl.currentUser
                     }
-                    channelEntity?.let {
-                        it.setMember(user!!.id, null)
-                        channels[it.cid] = it
+                    event.getCid()?.let { cid ->
+                        channelMap[cid]?.let { channelEntity ->
+                            user?.let { channelEntity.setMember(it.id, null) }
+                            channels[cid] = channelEntity
+                        }
                     }
                     event.channel?.let { c ->
                         users.putAll(c.users().map { UserEntity(it) }.associateBy { it.id })
@@ -212,15 +256,23 @@ class EventHandlerImpl(var domainImpl: io.getstream.chat.android.livedata.ChatDo
         // handle delete and truncate events
         for (event in events) {
             when (event) {
-                is NotificationChannelTruncated, is ChannelTruncated -> {
-                    domainImpl.repos.messages.deleteChannelMessagesBefore(event.getCid()!!, event.createdAt!!)
+                is NotificationChannelTruncated,
+                is ChannelTruncated -> {
+                    event.getCid()?.let { cid ->
+                        event.createdAt?.let { createdAt ->
+                            domainImpl.repos.messages.deleteChannelMessagesBefore(cid, createdAt)
+                        }
+                    }
                 }
                 is ChannelDeletedEvent -> {
-                    domainImpl.repos.messages.deleteChannelMessagesBefore(event.getCid()!!, event.createdAt!!)
-                    val channel = domainImpl.repos.channels.select(event.getCid()!!)
-                    channel?.let {
-                        it.deletedAt = event.createdAt!!
-                        domainImpl.repos.channels.insert(it)
+                    event.getCid()?.let { cid ->
+                        event.createdAt?.let { createdAt ->
+                            domainImpl.repos.messages.deleteChannelMessagesBefore(cid, createdAt)
+                            domainImpl.repos.channels.select(cid)?.let {
+                                it.deletedAt = createdAt
+                                domainImpl.repos.channels.insert(it)
+                            }
+                        }
                     }
                 }
             }
@@ -234,26 +286,22 @@ class EventHandlerImpl(var domainImpl: io.getstream.chat.android.livedata.ChatDo
         totalUnreadCount?.let { domainImpl.setTotalUnreadCount(it) }
     }
 
-    internal suspend fun handleEventsInternal(events: List<ChatEvent>) {
+    private suspend fun handleEventsInternal(events: List<ChatEvent>) {
         events.sortedBy { it.createdAt }
         updateOfflineStorageFromEvents(events)
 
         // step 3 - forward the events to the active chanenls
 
-        val channelEvents: MutableMap<String, MutableList<ChatEvent>> = mutableMapOf()
-        for (event in events) {
-            if (event.isChannelEvent()) {
-                if (!channelEvents.containsKey(event.cid!!)) {
-                    channelEvents[event.cid!!] = mutableListOf()
+        events.filter { it.isChannelEvent() }
+            .groupBy { it.cid ?: "" }
+            .filterNot { it.key.isBlank() }
+            .forEach {
+                val (cid, eventList) = it
+                if (domainImpl.isActiveChannel(cid)) {
+                    domainImpl.channel(cid).handleEvents(eventList)
                 }
-                channelEvents[event.cid!!]!!.add(event)
             }
-        }
-        for ((cid, cEvents) in channelEvents) {
-            if (domainImpl.isActiveChannel(cid)) {
-                domainImpl.channel(cid).handleEvents(cEvents)
-            }
-        }
+
         // only afterwards forward to the queryRepo since it borrows some data from the channel
         // queryRepo mainly monitors for the notification added to channel event
         for (queryRepo in domainImpl.getActiveQueries()) {
