5019f9138278de4675402d7c83035214b731b958
Thierry Schellenbach <thierryschellenbach@gmail.com>
2020-08-26 17:16:55 -0600

    add explicit order by createdAt to the selectSyncNeeded. this prevents the retry flow from syncing messages in the wrong order. fixes #31

--- livedata/src/test/java/io/getstream/chat/android/livedata/repository/MessageRepositoryTest.kt
+++ livedata/src/test/java/io/getstream/chat/android/livedata/repository/MessageRepositoryTest.kt
@@ -30,7 +30,6 @@ class MessageRepositoryTest : BaseDomainTest() {
     }
 
     @Test
-    @Ignore("LLC issues")
     fun testMessageObject() = runBlocking(Dispatchers.IO) {
         val messagea = Message(text = "hi").apply { reactionCounts = mutableMapOf("like" to 10) }
         val messageb = Message(text = "hi")
@@ -55,18 +54,25 @@ class MessageRepositoryTest : BaseDomainTest() {
 
     @Test
     fun testSyncNeeded() = runBlocking(Dispatchers.IO) {
+        val oldDate = calendar(2019, 11, 1)
         val message1 =
-            data.createMessage().apply { text = "yoyo"; syncStatus = SyncStatus.SYNC_NEEDED }
+            data.createMessage().apply { id="testSyncNeeded-1"; text = "yoyo"; syncStatus = SyncStatus.SYNC_NEEDED; createdAt =
+                oldDate }
         val message2 = data.createMessage()
-            .apply { id = "helloworld"; text = "hi123"; syncStatus = SyncStatus.FAILED_PERMANENTLY }
-        repo.insertMessages(listOf(message1, message2), true)
+            .apply { id = "testSyncNeeded-2"; text = "hi123"; syncStatus = SyncStatus.FAILED_PERMANENTLY }
+        val message3 = data.createMessage()
+            .apply { id = "testSyncNeeded-3"; text = "hi123"; syncStatus = SyncStatus.SYNC_NEEDED }
+        repo.insertMessages(listOf(message1, message2, message3), true)
 
         var messages = repo.selectSyncNeeded()
-        Truth.assertThat(messages.size).isEqualTo(1)
+
+        Truth.assertThat(messages.size).isEqualTo(2)
+        // verify that the old item is returned first
+        Truth.assertThat(messages.first().createdAt).isEqualTo(oldDate)
         Truth.assertThat(messages.first().syncStatus).isEqualTo(SyncStatus.SYNC_NEEDED)
 
         messages = repo.retryMessages()
-        Truth.assertThat(messages.size).isEqualTo(1)
+        Truth.assertThat(messages.size).isEqualTo(2)
         Truth.assertThat(messages.first().syncStatus).isEqualTo(SyncStatus.COMPLETED)
 
         messages = repo.selectSyncNeeded()
