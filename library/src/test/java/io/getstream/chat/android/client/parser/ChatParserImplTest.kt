9bbf7e6510a2bd035f9af0bdcc0a35c2e1ea125f
Jc Miñarro <josecarlos.minarro@gmail.com>
2020-08-27 17:58:49 +0200

    Add test for date parsing

--- library/src/test/java/io/getstream/chat/android/client/parser/ChatParserImplTest.kt
+++ library/src/test/java/io/getstream/chat/android/client/parser/ChatParserImplTest.kt
@@ -4,6 +4,7 @@ import org.amshove.kluent.`should be equal to`
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.Arguments
 import org.junit.jupiter.params.provider.MethodSource
+import java.util.Date
 
 internal class ChatParserImplTest {
     private val chatParser = ChatParserImpl()
@@ -12,6 +13,12 @@ internal class ChatParserImplTest {
     @MethodSource("io.getstream.chat.android.client.parser.ChatParserImplTest#mapArguments")
     fun `Should render proper maps`(map: Map<Any, Any>, expectedResult: String) {
         chatParser.toJson(map) `should be equal to` expectedResult
+    }
+
+    @ParameterizedTest
+    @MethodSource("io.getstream.chat.android.client.parser.ChatParserImplTest#dateFromJsonArguments")
+    fun `Should convert to proper date`(jsonDate: String, expectedDateTime: Long) {
+        chatParser.fromJson(jsonDate, Date::class.java).time `should be equal to` expectedDateTime
     }
 
     companion object {
@@ -23,6 +30,15 @@ internal class ChatParserImplTest {
             Arguments.of(mapOf("a" to null), "null"),
             Arguments.of(mapOf("a" to "b"), "{\"a\":\"b\"}"),
             Arguments.of(mapOf("a" to null, "b" to "c"), "{\"b\":\"c\"}")
+        )
+
+        @JvmStatic
+        fun dateFromJsonArguments() = listOf(
+            Arguments.of("\"2020-06-29T06:14:28.000Z\"", 1593411268000),
+            Arguments.of("\"2020-06-29T06:14:28.0Z\"", 1593411268000),
+            Arguments.of("\"2020-06-29T06:14:28.00Z\"", 1593411268000),
+            Arguments.of("\"2020-06-29T06:14:28.000Z\"", 1593411268000),
+            Arguments.of("\"2020-06-29T06:14:28.100Z\"", 1593411268100)
         )
     }
 }
