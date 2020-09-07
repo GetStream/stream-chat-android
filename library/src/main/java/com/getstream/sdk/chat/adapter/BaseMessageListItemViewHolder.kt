4fb533115f94973819c0d27f6f9427382011034d
Márton Braun <marton@getstream.io>
2020-09-07 15:32:51 +0200

    Convert MessageListItemAdapter to Kotlin

--- library/src/main/java/com/getstream/sdk/chat/adapter/BaseMessageListItemViewHolder.kt
+++ library/src/main/java/com/getstream/sdk/chat/adapter/BaseMessageListItemViewHolder.kt
@@ -1,28 +1,43 @@
-package com.getstream.sdk.chat.adapter;
+package com.getstream.sdk.chat.adapter
 
-import android.content.Context;
-import android.view.LayoutInflater;
-import android.view.ViewGroup;
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.getstream.sdk.chat.view.MessageListView.BubbleHelper
+import com.getstream.sdk.chat.view.MessageListViewStyle
+import io.getstream.chat.android.client.models.Channel
 
-import androidx.annotation.NonNull;
-import androidx.recyclerview.widget.RecyclerView;
+abstract class BaseMessageListItemViewHolder<T : MessageListItem>(
+    resId: Int,
+    parent: ViewGroup
+) : RecyclerView.ViewHolder(
+    LayoutInflater.from(parent.context).inflate(resId, parent, false)
+) {
 
-import com.getstream.sdk.chat.view.MessageListView;
-import com.getstream.sdk.chat.view.MessageListViewStyle;
-
-import io.getstream.chat.android.client.models.Channel;
-
-public abstract class BaseMessageListItemViewHolder<T extends MessageListItem> extends RecyclerView.ViewHolder {
-
-    public BaseMessageListItemViewHolder(int resId, ViewGroup parent) {
-        super(LayoutInflater.from(parent.getContext()).inflate(resId, parent, false));
+    /**
+     * Workaround to allow a downcast of the MessageListItem to T
+     */
+    fun bindListItem(
+        context: Context,
+        channel: Channel,
+        messageListItem: MessageListItem,
+        style: MessageListViewStyle,
+        bubbleHelper: BubbleHelper,
+        factory: MessageViewHolderFactory,
+        position: Int
+    ) {
+        @Suppress("UNCHECKED_CAST")
+        bind(context, channel, messageListItem as T, style, bubbleHelper, factory, position)
     }
 
-    public abstract void bind(@NonNull Context context,
-                              @NonNull Channel channel,
-                              @NonNull T messageListItem,
-                              @NonNull MessageListViewStyle style,
-                              @NonNull MessageListView.BubbleHelper bubbleHelper,
-                              @NonNull MessageViewHolderFactory factory,
-                              int position);
+    protected abstract fun bind(
+        context: Context,
+        channel: Channel,
+        messageListItem: T,
+        style: MessageListViewStyle,
+        bubbleHelper: BubbleHelper,
+        factory: MessageViewHolderFactory,
+        position: Int
+    )
 }
