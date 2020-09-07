fd0575788c2c2b53efcf8eb677b72258034c6b01
Samuel Urbanowicz <samiuelson@gmail.com>
2020-09-07 18:00:57 +0200

    Improve immutability of UseCaseHelper class props

--- livedata/src/main/java/io/getstream/chat/android/livedata/usecase/UseCaseHelper.kt
+++ livedata/src/main/java/io/getstream/chat/android/livedata/usecase/UseCaseHelper.kt
@@ -2,25 +2,25 @@ package io.getstream.chat.android.livedata.usecase
 
 import io.getstream.chat.android.livedata.ChatDomainImpl
 
-data class UseCaseHelper(var chatDomainImpl: ChatDomainImpl) {
+class UseCaseHelper(chatDomainImpl: ChatDomainImpl) {
 
     // replaying events
     /**
      * Adds the provided channel to the active channels and replays events for all active channels
      */
-    var replayEventsForActiveChannels: ReplayEventsForActiveChannels = ReplayEventsForActiveChannelsImpl(chatDomainImpl)
+    val replayEventsForActiveChannels: ReplayEventsForActiveChannels = ReplayEventsForActiveChannelsImpl(chatDomainImpl)
 
     // getting controllers
     /**
      * Watch a channel/ Start listening for events on a channel
      * Returns a channel controller object
      */
-    var watchChannel: WatchChannel = WatchChannelImpl(chatDomainImpl)
+    val watchChannel: WatchChannel = WatchChannelImpl(chatDomainImpl)
     /**
      * Query channels and start listening for changes using events
      * Returns a QueryChannelsController object
      */
-    var queryChannels: QueryChannels = QueryChannelsImpl(chatDomainImpl)
+    val queryChannels: QueryChannels = QueryChannelsImpl(chatDomainImpl)
     /**
      * Returns a ThreadController for the specified thread
      */
@@ -30,22 +30,22 @@ data class UseCaseHelper(var chatDomainImpl: ChatDomainImpl) {
     /**
      * Returns a livedata object for the total number of unread messages
      */
-    var getTotalUnreadCount: GetTotalUnreadCount = GetTotalUnreadCountImpl(chatDomainImpl)
+    val getTotalUnreadCount: GetTotalUnreadCount = GetTotalUnreadCountImpl(chatDomainImpl)
     /**
      * Returns a livedata object for the number of channels with unread messages
      */
-    var getUnreadChannelCount: GetUnreadChannelCount = GetUnreadChannelCountImpl(chatDomainImpl)
+    val getUnreadChannelCount: GetUnreadChannelCount = GetUnreadChannelCountImpl(chatDomainImpl)
 
     // loading more
     /**
      * Loads older messages for the given channel
      */
-    var loadOlderMessages: LoadOlderMessages = LoadOlderMessagesImpl(chatDomainImpl)
+    val loadOlderMessages: LoadOlderMessages = LoadOlderMessagesImpl(chatDomainImpl)
 
     /**
      * Loads newer messages for the given channel
      */
-    var loadNewerMessages: LoadNewerMessages = LoadNewerMessagesImpl(chatDomainImpl)
+    val loadNewerMessages: LoadNewerMessages = LoadNewerMessagesImpl(chatDomainImpl)
 
     /**
      * Load more channels for the given query.
@@ -54,80 +54,80 @@ data class UseCaseHelper(var chatDomainImpl: ChatDomainImpl) {
     /**
      * Loads more messages for a thread
      */
-    var threadLoadMore: ThreadLoadMore = ThreadLoadMoreImpl(chatDomainImpl)
+    val threadLoadMore: ThreadLoadMore = ThreadLoadMoreImpl(chatDomainImpl)
 
     // updating channel data
     /**
      * Create a channel and retry using the retry policy if the request fails
      */
-    var createChannel: CreateChannel = CreateChannelImpl(chatDomainImpl)
+    val createChannel: CreateChannel = CreateChannelImpl(chatDomainImpl)
     /**
      * Send a message. This message is immediately added to local storage.
      * The API call to create the message is retried using the retry policy
      */
-    var sendMessage: SendMessage = SendMessageImpl(chatDomainImpl)
+    val sendMessage: SendMessage = SendMessageImpl(chatDomainImpl)
     /**
      * Cancel an emphemeral message. This message is immediately removed from local storage.
      * The API call to delete the message is retried using the retry policy
      */
-    var cancelMessage: CancelMessage = CancelMessage(chatDomainImpl)
+    val cancelMessage: CancelMessage = CancelMessage(chatDomainImpl)
     /**
     * Performs giphy shuffle operation. Removes the original "ephemeral" message from local storage.
     * Returns new "ephemeral" message with new giphy url.
     * API call to remove the message is retried according to the retry policy specified on the chatDomain
     */
-    var shuffleGiphy: ShuffleGiphy = ShuffleGiphy(chatDomainImpl)
+    val shuffleGiphy: ShuffleGiphy = ShuffleGiphy(chatDomainImpl)
     /**
      * Sends selected giphy message to the channel.
      * Replaces the original "ephemeral" message in local storage with the one received from backend.
      * Returns new "ephemeral" message with new giphy url.
      * API call to remove the message is retried according to the retry policy specified on the chatDomain
      */
-    var sendGiphy: SendGiphy = SendGiphy(chatDomainImpl)
+    val sendGiphy: SendGiphy = SendGiphy(chatDomainImpl)
     /**
      * Send a message with attachments.
      */
-    var sendMessageWithAttachments: SendMessageWithAttachments = SendMessageWithAttachmentsImpl(chatDomainImpl)
+    val sendMessageWithAttachments: SendMessageWithAttachments = SendMessageWithAttachmentsImpl(chatDomainImpl)
     /**
      * Edit a message. This message is immediately updated in local storage.
      * The API call to edit the message is retried using the retry policy
      */
-    var editMessage: EditMessage = EditMessageImpl(chatDomainImpl)
+    val editMessage: EditMessage = EditMessageImpl(chatDomainImpl)
     /**
      * Delete a message. This message is immediately marked as deleted (message.deletedAt) in local storage.
      * The API call to delete the message is retried using the retry policy
      */
-    var deleteMessage: DeleteMessage = DeleteMessageImpl(chatDomainImpl)
+    val deleteMessage: DeleteMessage = DeleteMessageImpl(chatDomainImpl)
     /**
      * Send a reaction. This reaction is immediately added to local storage.
      * The API call to send a reaction is retried using the retry policy
      */
-    var sendReaction: SendReaction = SendReactionImpl(chatDomainImpl)
+    val sendReaction: SendReaction = SendReactionImpl(chatDomainImpl)
     /**
      * Delete a reaction. This reaction is immediately marked as deleted in local storage.
      * The API call to delete a reaction is retried using the retry policy
      */
-    var deleteReaction: DeleteReaction = DeleteReactionImpl(chatDomainImpl)
+    val deleteReaction: DeleteReaction = DeleteReactionImpl(chatDomainImpl)
     /**
      * Keystroke should be called whenever the user enters something in the message input
      * It handles the deduplication and removal of typing events automatically
      */
     // TODO: Confirm this
-    var keystroke: Keystroke = KeystrokeImpl(chatDomainImpl)
+    val keystroke: Keystroke = KeystrokeImpl(chatDomainImpl)
     /**
      * stopTyping is typically called manually when the message is submitted
      */
-    var stopTyping: StopTyping = StopTypingImpl(chatDomainImpl)
+    val stopTyping: StopTyping = StopTypingImpl(chatDomainImpl)
     /**
      * markRead marks all messages on a channel read
      */
-    var markRead: MarkRead = MarkReadImpl(chatDomainImpl)
+    val markRead: MarkRead = MarkReadImpl(chatDomainImpl)
     /**
      * hideChannel hides the channel till a new message event is received
      */
-    var hideChannel: HideChannel = HideChannelImpl(chatDomainImpl)
+    val hideChannel: HideChannel = HideChannelImpl(chatDomainImpl)
     /**
      * showChannels shows a channel which was previously hidden
      */
-    var showChannel: ShowChannel = ShowChannelImpl(chatDomainImpl)
+    val showChannel: ShowChannel = ShowChannelImpl(chatDomainImpl)
 }
