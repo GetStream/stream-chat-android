eba1f2b50dc7c9062d3a3843e570d784de0dfcd4
Thierry Schellenbach <thierryschellenbach@gmail.com>
2020-09-12 09:34:00 -0600

    message list item diff to kotlin and fix check on same item

---
+++ library/src/main/java/com/getstream/sdk/chat/adapter/MessageListItemDiffCallback.kt
@@ -0,0 +1,32 @@
+package com.getstream.sdk.chat.adapter
+
+import androidx.recyclerview.widget.DiffUtil
+
+class MessageListItemDiffCallback(
+        private val old: List<MessageListItem>,
+        private val new: List<MessageListItem>
+) :
+        DiffUtil.Callback() {
+
+    override fun getOldListSize(): Int {
+        return old.size
+    }
+
+    override fun getNewListSize(): Int {
+        return new.size
+    }
+
+    override fun areItemsTheSame(
+            oldItemPosition: Int,
+            newItemPosition: Int
+    ): Boolean {
+        return old[oldItemPosition].getStableId() == new[newItemPosition].getStableId()
+    }
+
+    override fun areContentsTheSame(
+            oldItemPosition: Int,
+            newItemPosition: Int
+    ): Boolean {
+        return old[oldItemPosition] == new[newItemPosition]
+    }
+}
