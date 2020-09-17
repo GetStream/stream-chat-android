599ec9519c5cc17fcfefc26ce40d8751f95515f3
Márton Braun <marton@getstream.io>
2020-09-17 08:51:15 +0200

    Update Mother.kt implementation

--- library/src/test/java/com/getstream/sdk/chat/Mother.kt
+++ library/src/test/java/com/getstream/sdk/chat/Mother.kt
@@ -17,25 +17,25 @@ import io.getstream.chat.android.client.utils.SyncStatus
 import java.io.File
 import java.time.Instant
 import java.util.Date
-import java.util.concurrent.ThreadLocalRandom
+import kotlin.random.Random
 
-private val charPool: List<Char> = ('a'..'z') + ('A'..'Z') + ('0'..'9')
-val random
-    get() = ThreadLocalRandom.current()
+private val charPool: CharArray = (('a'..'z') + ('A'..'Z') + ('0'..'9')).toCharArray()
 
-fun positveRandomInt(maxInt: Int = Int.MAX_VALUE - 1): Int =
-    random.nextInt(maxInt + 1).takeIf { it > 0 } ?: positveRandomInt(maxInt)
+fun positiveRandomInt(maxInt: Int = Int.MAX_VALUE - 1): Int =
+    Random.nextInt(1, maxInt + 1)
 
-fun positveRandomLong(maxLong: Long = Long.MAX_VALUE - 1): Long =
-    random.nextLong(maxLong + 1).takeIf { it > 0 } ?: positveRandomLong(maxLong)
+fun positiveRandomLong(maxLong: Long = Long.MAX_VALUE - 1): Long =
+    Random.nextLong(1, maxLong + 1)
 
-fun randomInt() = random.nextInt()
-fun randomIntBetween(min: Int, max: Int) = random.nextInt(max - min) + min
-fun randomLong() = random.nextLong()
-fun randomBoolean() = random.nextBoolean()
-fun randomString(size: Int = 20): String = (0..size)
-    .map { charPool[random.nextInt(0, charPool.size)] }
-    .joinToString("")
+fun randomInt() = Random.nextInt()
+fun randomIntBetween(min: Int, max: Int) = Random.nextInt(min, max + 1)
+fun randomLong() = Random.nextLong()
+fun randomBoolean() = Random.nextBoolean()
+fun randomString(size: Int = 20): String = buildString(capacity = size) {
+    repeat(size) {
+        append(charPool.random())
+    }
+}
 
 fun randomCID() = "${randomString()}:${randomString()}"
 fun createUser(
@@ -48,9 +48,9 @@ fun createUser(
     createdAt: Date? = randomDate(),
     updatedAt: Date? = randomDate(),
     lastActive: Date? = randomDate(),
-    totalUnreadCount: Int = positveRandomInt(),
-    unreadChannels: Int = positveRandomInt(),
-    unreadCount: Int = positveRandomInt(),
+    totalUnreadCount: Int = positiveRandomInt(),
+    unreadChannels: Int = positiveRandomInt(),
+    unreadCount: Int = positiveRandomInt(),
     mutes: List<Mute> = mutableListOf(),
     teams: List<String> = listOf(),
     channelMutes: List<ChannelMute> = emptyList(),
@@ -85,9 +85,9 @@ fun createMember(
 ): Member = Member(user, role, createdAt, updatedAt, isInvited, inviteAcceptedAt, inviteRejectedAt)
 
 fun createMembers(
-    size: Int = positveRandomInt(10),
+    size: Int = positiveRandomInt(10),
     creationFunction: (Int) -> Member = { createMember() }
-): List<Member> = (1..size).map { creationFunction(it) }
+): List<Member> = List(size, creationFunction)
 
 fun createChannel(cid: String = randomCID(), config: Config = Config()): Channel =
     Channel(cid = cid, config = config)
@@ -167,12 +167,12 @@ fun createMessage(
 
 fun randomDate() = Date(randomLong())
 
-fun randomSyncStatus(): SyncStatus = SyncStatus.values().asList().shuffled(random).first()
+fun randomSyncStatus(): SyncStatus = SyncStatus.values().random()
 
 fun createMessageList(
     size: Int = 10,
     creationFunction: (Int) -> Message = { createMessage() }
-): List<Message> = (1..size).map(creationFunction)
+): List<Message> = List(size, creationFunction)
 
 fun createChannelUserRead(
     user: User = createUser(),
@@ -187,4 +187,4 @@ fun createCommand(
     set: String = randomString()
 ): Command = Command(name, description, args, set)
 
-fun createCommands(size: Int = 10): List<Command> = (1 until size).map { createCommand() }
+fun createCommands(size: Int = 10): List<Command> = List(size) { createCommand() }
