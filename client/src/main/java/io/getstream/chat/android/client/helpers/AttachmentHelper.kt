8d1489c97140e3162ec5bd2635966ab27b61b40a
Oleg Kuzmin <oleg@getstream.io>
2020-10-07 12:48:11 +0300

    Added pattern url validation

--- client/src/main/java/io/getstream/chat/android/client/helpers/AttachmentHelper.kt
+++ client/src/main/java/io/getstream/chat/android/client/helpers/AttachmentHelper.kt
@@ -5,9 +5,12 @@ import io.getstream.chat.android.client.models.Attachment
 class AttachmentHelper {
 
     fun hasValidUrl(attachment: Attachment): Boolean {
-        if (attachment.url == null) {
-            return false
-        }
-        return false
+        val url = attachment.url ?: return false
+        if (URL_PATTERN.matches(url).not()) return false
+        return true
+    }
+
+    companion object {
+        private val URL_PATTERN = Regex("^((https?|ftp|smtp):\\/\\/)?(www.)?[a-z0-9]+\\.[a-z]+(\\/[a-zA-Z0-9#]+\\/?)*\$")
     }
 }
--- client/src/test/java/io/getstream/chat/android/client/helpers/AttachmentHelperTests.kt
+++ client/src/test/java/io/getstream/chat/android/client/helpers/AttachmentHelperTests.kt
@@ -1,25 +1,37 @@
 package io.getstream.chat.android.client.helpers
 
-import com.flextrade.kfixture.KFixture
-import io.getstream.chat.android.client.models.Attachment
+import io.getstream.chat.android.client.Mother
 import org.amshove.kluent.shouldBeEqualTo
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.MethodSource
 
 internal class AttachmentHelperTests {
 
     private val sut = AttachmentHelper()
 
     @Test
-    fun `When has valid Url If attachment has null url Should return false`() {
-        val attachment = KFixture {
-            sameInstance(
-                Attachment.UploadState::class.java,
-                Attachment.UploadState.Success
-            )
-        }.invoke<Attachment>().copy(url = null)
+    fun `When has valid url If attachment has null url Should return false`() {
+        val attachment = Mother.randomAttachment { url = null }
 
         val result = sut.hasValidUrl(attachment)
 
         result shouldBeEqualTo false
+    }
+
+    @ParameterizedTest
+    @MethodSource("nonValidUrls")
+    fun `When has valida url if attachment url is not any valid Url Should return false`(notValidUrl: String) {
+        val attachment = Mother.randomAttachment { url = notValidUrl }
+
+        val result = sut.hasValidUrl(attachment)
+
+        result shouldBeEqualTo false
+    }
+
+    companion object {
+        @JvmStatic
+        fun nonValidUrls() =
+            listOf<String>("someNotValidUrl", "https://????.com", "https://domain.com/xxIII ioi", "www.++++.---.com")
     }
 }
