6b51aa12905c2e39c91e36b602f6ca859bc5a4a2
Márton Braun <marton@getstream.io>
2020-10-07 10:50:23 +0200

    [CAS-283] Implement message retry handling

--- library/src/main/java/com/getstream/sdk/chat/adapter/ListenerContainer.kt
+++ library/src/main/java/com/getstream/sdk/chat/adapter/ListenerContainer.kt
@@ -4,6 +4,7 @@ import com.getstream.sdk.chat.view.MessageListView.AttachmentClickListener
 import com.getstream.sdk.chat.view.MessageListView.GiphySendListener
 import com.getstream.sdk.chat.view.MessageListView.MessageClickListener
 import com.getstream.sdk.chat.view.MessageListView.MessageLongClickListener
+import com.getstream.sdk.chat.view.MessageListView.MessageRetryListener
 import com.getstream.sdk.chat.view.MessageListView.ReactionViewClickListener
 import com.getstream.sdk.chat.view.MessageListView.ReadStateClickListener
 import com.getstream.sdk.chat.view.MessageListView.UserClickListener
@@ -11,6 +12,7 @@ import com.getstream.sdk.chat.view.MessageListView.UserClickListener
 interface ListenerContainer {
     var messageClickListener: MessageClickListener
     var messageLongClickListener: MessageLongClickListener
+    var messageRetryListener: MessageRetryListener
     var attachmentClickListener: AttachmentClickListener
     var reactionViewClickListener: ReactionViewClickListener
     var userClickListener: UserClickListener
