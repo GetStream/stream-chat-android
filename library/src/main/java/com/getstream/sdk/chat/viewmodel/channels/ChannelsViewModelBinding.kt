1a008b4518a9434d18427953c61715052e5a615d
Márton Braun <marton@getstream.io>
2020-10-09 15:33:49 +0200

    [CAS-280] Update to Kotlin 1.4.10, clean up inspection warnings

--- library/src/main/java/com/getstream/sdk/chat/viewmodel/channels/ChannelsViewModelBinding.kt
+++ library/src/main/java/com/getstream/sdk/chat/viewmodel/channels/ChannelsViewModelBinding.kt
@@ -3,31 +3,27 @@
 package com.getstream.sdk.chat.viewmodel.channels
 
 import androidx.lifecycle.LifecycleOwner
-import androidx.lifecycle.Observer
 import com.getstream.sdk.chat.view.channels.ChannelsView
 
 @JvmName("bind")
 fun ChannelsViewModel.bindView(view: ChannelsView, lifecycle: LifecycleOwner) {
-    state.observe(
-        lifecycle,
-        Observer {
-            when (it) {
-                is ChannelsViewModel.State.Result -> {
-                    view.setChannels(it.channels)
-                    view.hideEmptyStateView()
-                    view.hideLoadingView()
-                }
-                is ChannelsViewModel.State.Loading -> {
-                    view.hideEmptyStateView()
-                    view.showLoadingView()
-                }
-                ChannelsViewModel.State.NoChannelsAvailable -> {
-                    view.showEmptyStateView()
-                    view.hideLoadingView()
-                }
+    state.observe(lifecycle) {
+        when (it) {
+            is ChannelsViewModel.State.Result -> {
+                view.setChannels(it.channels)
+                view.hideEmptyStateView()
+                view.hideLoadingView()
+            }
+            is ChannelsViewModel.State.Loading -> {
+                view.hideEmptyStateView()
+                view.showLoadingView()
+            }
+            ChannelsViewModel.State.NoChannelsAvailable -> {
+                view.showEmptyStateView()
+                view.hideLoadingView()
             }
         }
-    )
+    }
 
     view.setOnEndReachedListener {
         onEvent(ChannelsViewModel.Event.ReachedEndOfList)
