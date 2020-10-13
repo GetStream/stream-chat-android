1b2c55ded7356a673b9638eb1fa3cd75bbdc1dca
ogkuzmin <oleg@getstream.io>
2020-10-13 14:58:51 +0300

    Update client/src/main/java/io/getstream/chat/android/client/utils/SystemTimeProvider.kt
    Co-authored-by: Jc Miñarro <josecarlos.minarro@gmail.com>

--- client/src/main/java/io/getstream/chat/android/client/utils/SystemTimeProvider.kt
+++ client/src/main/java/io/getstream/chat/android/client/utils/SystemTimeProvider.kt
@@ -1,7 +1,7 @@
 package io.getstream.chat.android.client.utils
 
 class SystemTimeProvider {
-    fun provideTime() = System.currentTimeMillis() / MILLIS_TO_SECONDS_FACTOR
+    fun provideCurrentTimeInSeconds() = System.currentTimeMillis() / MILLIS_TO_SECONDS_FACTOR
 
     companion object {
         private const val MILLIS_TO_SECONDS_FACTOR = 1000L
