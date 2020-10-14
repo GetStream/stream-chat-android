a15fbaad3df54e8addb78063edf2927e15c311f2
Márton Braun <marton@getstream.io>
2020-10-14 14:15:00 +0200

    [CAS-281] Add missing return type to comply with explicit mode

--- client/src/main/java/io/getstream/chat/android/client/utils/SystemTimeProvider.kt
+++ client/src/main/java/io/getstream/chat/android/client/utils/SystemTimeProvider.kt
@@ -1,7 +1,9 @@
 package io.getstream.chat.android.client.utils
 
 public class SystemTimeProvider {
-    public fun provideCurrentTimeInSeconds() = System.currentTimeMillis() / MILLIS_TO_SECONDS_FACTOR
+    public fun provideCurrentTimeInSeconds(): Long {
+        return System.currentTimeMillis() / MILLIS_TO_SECONDS_FACTOR
+    }
 
     private companion object {
         const val MILLIS_TO_SECONDS_FACTOR = 1000L
