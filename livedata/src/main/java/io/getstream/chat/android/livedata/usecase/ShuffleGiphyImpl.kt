12a0d70d966769813f5dd9da6f4f22ec15426f11
Márton Braun <marton@getstream.io>
2020-10-16 14:03:35 +0200

    [CAS-281] Move comment to method

--- livedata/src/main/java/io/getstream/chat/android/livedata/usecase/ShuffleGiphyImpl.kt
+++ livedata/src/main/java/io/getstream/chat/android/livedata/usecase/ShuffleGiphyImpl.kt
@@ -7,17 +7,17 @@ import io.getstream.chat.android.livedata.utils.CallImpl2
 import io.getstream.chat.android.livedata.utils.validateCid
 
 public interface ShuffleGiphy {
+    /**
+     * Performs giphy shuffle operation. Removes the original "ephemeral" message from local storage.
+     * Returns new "ephemeral" message with new giphy url.
+     * API call to remove the message is retried according to the retry policy specified on the chatDomain
+     * @param message the message to send
+     * @return A call object with Message as the return type
+     * @see io.getstream.chat.android.livedata.utils.RetryPolicy
+     */
     public operator fun invoke(message: Message): Call2<Message>
 }
 
-/**
- * Performs giphy shuffle operation. Removes the original "ephemeral" message from local storage.
- * Returns new "ephemeral" message with new giphy url.
- * API call to remove the message is retried according to the retry policy specified on the chatDomain
- * @param message the message to send
- * @return A call object with Message as the return type
- * @see io.getstream.chat.android.livedata.utils.RetryPolicy
- */
 internal class ShuffleGiphyImpl(private val domainImpl: ChatDomainImpl) : ShuffleGiphy {
     override operator fun invoke(message: Message): Call2<Message> {
         val cid = message.cid
