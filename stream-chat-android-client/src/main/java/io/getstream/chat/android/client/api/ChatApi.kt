ac36df86c16b4c04e989799ce1881fbfb749a143
Dmitrii Bychkov <dmitrii@getstream.io>
2020-10-27 23:06:19 +0300

    PR comments

--- stream-chat-android-client/src/main/java/io/getstream/chat/android/client/api/ChatApi.kt
+++ stream-chat-android-client/src/main/java/io/getstream/chat/android/client/api/ChatApi.kt
@@ -688,7 +688,7 @@ internal class ChatApi(
     }
 
     fun warmUp() {
-        retrofitAnonymousApi.warmUp().enqueue()
+        retrofitApi.warmUp().enqueue()
     }
 
     private fun <T : Any> noConnectionIdError(): ErrorCall<T> {
