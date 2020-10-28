f1cbb52c6a26e33fed8eab4c8c876d1cfd23bfe9
Márton Braun <marton@getstream.io>
2020-10-28 17:01:17 +0100

    [CAS-335] Add documentation for the Call class

--- stream-chat-android-client/src/main/java/io/getstream/chat/android/client/call/Call.kt
+++ stream-chat-android-client/src/main/java/io/getstream/chat/android/client/call/Call.kt
@@ -6,16 +6,44 @@ import io.getstream.chat.android.client.utils.Result
 import kotlinx.coroutines.suspendCancellableCoroutine
 import kotlin.coroutines.resume
 
+/**
+ * A pending operation waiting to be ran.
+ *
+ * There are several ways to run a [Call]:
+ * - [execute]: synchronous, blocking
+ * - [enqueue]: async, callback based
+ * - [await]: async, suspending call
+ *
+ * Running a [Call] more than once results in undefined behaviour.
+ */
 public interface Call<T : Any> {
+    /**
+     * Executes the call synchronously, in a blocking way. Only call this from a background thread.
+     */
     @WorkerThread
     public fun execute(): Result<T>
 
+    /**
+     * Executes the call asynchronously, on a background thread. Safe to call from the main
+     * thread. The [callback] will always be invoked on the main thread.
+     */
     @UiThread
     public fun enqueue(callback: (Result<T>) -> Unit = {})
 
+    /**
+     * Cancels the execution of the call, if cancellation is supported for the operation.
+     *
+     * Note that calls can not be cancelled when running them with [execute].
+     */
     public fun cancel()
 }
 
+/**
+ * Awaits the result of [this] Call in a suspending way, asynchronously.
+ * Safe to call from any CoroutineContext.
+ *
+ * Does not throw exceptions. Any errors will be wrapped in the [Result] that's returned.
+ */
 public suspend fun <T : Any> Call<T>.await(): Result<T> = suspendCancellableCoroutine { continuation ->
     this.enqueue { result ->
         continuation.resume(result)
