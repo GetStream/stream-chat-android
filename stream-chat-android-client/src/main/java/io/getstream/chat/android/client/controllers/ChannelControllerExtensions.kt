46448ea89e5f11fec7f988f86629ed062bedfc59
Carter Hudson <carter@getstream.io>
2020-10-28 12:45:43 -0600

    implements a subscribefor method that is lifecycle sensitive

--- stream-chat-android-client/src/main/java/io/getstream/chat/android/client/controllers/ChannelControllerExtensions.kt
+++ stream-chat-android-client/src/main/java/io/getstream/chat/android/client/controllers/ChannelControllerExtensions.kt
@@ -1,5 +1,9 @@
 package io.getstream.chat.android.client.controllers
 
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleObserver
+import androidx.lifecycle.LifecycleOwner
+import androidx.lifecycle.OnLifecycleEvent
 import io.getstream.chat.android.client.events.ChatEvent
 import io.getstream.chat.android.client.utils.observable.Disposable
 import kotlin.reflect.KClass
@@ -27,4 +31,37 @@ public inline fun <reified T : ChatEvent> ChannelController.subscribeForSingle(
     noinline listener: (event: T) -> Unit
 ): Disposable {
     return this.subscribeForSingle(T::class.java, listener)
+}
+
+public fun ChannelController.subscribeFor(
+    lifecycleOwner: LifecycleOwner,
+    vararg eventTypes: KClass<out ChatEvent>,
+    listener: (ChatEvent) -> Unit
+) {
+    subscribeFor(
+        lifecycleOwner = lifecycleOwner,
+        eventTypes = eventTypes.map { it.java }.toTypedArray(),
+        listener = listener
+    )
+}
+
+public fun ChannelController.subscribeFor(
+    lifecycleOwner: LifecycleOwner,
+    vararg eventTypes: Class<out ChatEvent>,
+    listener: (ChatEvent) -> Unit,
+) {
+    subscribeFor(eventTypes = eventTypes, listener = listener).also { subscription ->
+        lifecycleOwner.lifecycle.addObserver(
+            object : LifecycleObserver {
+                @OnLifecycleEvent(Lifecycle.Event.ON_DESTROY)
+                fun onDestroy() {
+                    if (subscription.isDisposed) {
+                        return
+                    }
+
+                    subscription.dispose()
+                }
+            }
+        )
+    }
 }
