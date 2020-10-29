814085be9194f26fe5320e5eeae18b56f4e97511
Samuel Urbanowicz <samiuelson@gmail.com>
2020-10-29 15:44:45 +0100

    Fix typo
    Co-authored-by: Márton Braun <marton@getstream.io>

--- stream-chat-android/src/main/java/com/getstream/sdk/chat/view/channels/ChannelsView.kt
+++ stream-chat-android/src/main/java/com/getstream/sdk/chat/view/channels/ChannelsView.kt
@@ -53,7 +53,7 @@ public class ChannelsView @JvmOverloads constructor(
     /**
      * @param view will be added to the view hierarchy of [ChannelsView] and managed by it.
      * The view should not be added to another [ViewGroup] instance elsewhere.
-     * @param layoutParams defines how the view will be situated inside it's container ViewGroup.
+     * @param layoutParams defines how the view will be situated inside its container ViewGroup.
      */
     public fun setEmptyStateView(view: View, layoutParams: LayoutParams = defaultChildLayoutParams()) {
         removeView(this.emptyStateView)
