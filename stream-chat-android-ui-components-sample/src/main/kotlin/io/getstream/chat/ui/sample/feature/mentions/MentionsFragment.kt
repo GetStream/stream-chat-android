9560a005c3cdb746fa6b2ead755a5c502ba9ac55
Dmitrii Bychkov <mail@dbychkov.com>
2020-11-02 02:39:24 +0300

    CAS-370 CAS-367 Drawer layout and navigation bar

---
+++ stream-chat-android-ui-components-sample/src/main/kotlin/io/getstream/chat/ui/sample/feature/mentions/MentionsFragment.kt
@@ -0,0 +1,28 @@
+package io.getstream.chat.ui.sample.feature.mentions
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import io.getstream.chat.ui.sample.databinding.FragmentMentionsBinding
+
+class MentionsFragment : Fragment() {
+
+    private var _binding: FragmentMentionsBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentMentionsBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
