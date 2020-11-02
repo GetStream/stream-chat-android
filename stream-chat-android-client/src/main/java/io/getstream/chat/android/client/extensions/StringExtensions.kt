af2fd2616096d416a7f42849c38eac45a6eee8aa
Oleg Kuzmin <oleg@getstream.io>
2020-11-02 15:00:15 +0300

    Hide StringExtensions make it internal. Add comments to extensions.

--- stream-chat-android-client/src/main/java/io/getstream/chat/android/client/extensions/StringExtensions.kt
+++ stream-chat-android-client/src/main/java/io/getstream/chat/android/client/extensions/StringExtensions.kt
@@ -4,9 +4,10 @@ private val snakeRegex = "_[a-zA-Z]".toRegex()
 private val camelRegex = "(?<=[a-zA-Z])[A-Z]".toRegex()
 
 /**
- * turns created_at into createdAt
+ * Converts string written in snake case to String in camel case with the first symbol in lower case.
+ * For example string "created_at_some_time" is converted to "createdAtSomeTime".
  */
-public fun String.snakeToLowerCamelCase(): String {
+internal fun String.snakeToLowerCamelCase(): String {
     return snakeRegex.replace(this) {
         it.value.replace("_", "")
             .toUpperCase()
@@ -14,8 +15,9 @@ public fun String.snakeToLowerCamelCase(): String {
 }
 
 /**
- * turns createdAt into created_at
+ * Converts String written in camel case to String in snake case.
+ * For example string "createdAtSomeTime" is converted to "created_at_some_time".
  */
-public fun String.camelCaseToSnakeCase(): String {
+internal fun String.camelCaseToSnakeCase(): String {
     return camelRegex.replace(this) { "_${it.value}" }.toLowerCase()
 }
