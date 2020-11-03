068c1384856be7ca94bab4de8ac9444a922ba035
Oleg Kuzmin <oleg@getstream.io>
2020-11-03 18:33:07 +0300

    Revert update DB version

--- stream-chat-android-offline/src/main/java/io/getstream/chat/android/livedata/ChatDatabase.kt
+++ stream-chat-android-offline/src/main/java/io/getstream/chat/android/livedata/ChatDatabase.kt
@@ -41,7 +41,7 @@ import io.getstream.chat.android.livedata.entity.UserEntity
         ChannelConfigEntity::class,
         SyncStateEntity::class
     ],
-    version = 28,
+    version = 27,
     exportSchema = false
 )
 
