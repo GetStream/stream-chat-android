c6973225fa5b08caf0524d7660360847c7f29cf6
Dmitrii Bychkov <mail@dbychkov.com>
2020-11-04 19:36:00 +0300

    CAS-180 Implement TypedArray.use() extension function with contract

--- stream-chat-android-ui-components/src/main/kotlin/io/getstream/chat/android/ui/avatar/AvatarStyle.kt
+++ stream-chat-android-ui-components/src/main/kotlin/io/getstream/chat/android/ui/avatar/AvatarStyle.kt
@@ -1,13 +1,13 @@
 package io.getstream.chat.android.ui.avatar
 
 import android.content.Context
-import android.content.res.TypedArray
 import android.graphics.Color
 import android.graphics.Typeface
 import android.util.AttributeSet
 import com.getstream.sdk.chat.style.TextStyle
 import io.getstream.chat.android.ui.R
 import io.getstream.chat.android.ui.utils.extensions.getDimension
+import io.getstream.chat.android.ui.utils.extensions.use
 
 public class AvatarStyle(context: Context, attrs: AttributeSet?) {
     public val avatarWidth: Int
@@ -18,52 +18,50 @@ public class AvatarStyle(context: Context, attrs: AttributeSet?) {
     public val onlineIndicatorEnabled: Boolean
 
     init {
-        val a: TypedArray = context.obtainStyledAttributes(
+        context.obtainStyledAttributes(
             attrs,
             R.styleable.AvatarView,
             0,
             0
-        )
-
-        avatarWidth = a.getDimensionPixelSize(
-            R.styleable.AvatarView_streamAvatarWidth,
-            context.getDimension(R.dimen.stream_message_avatar_width)
-        )
-        avatarHeight = a.getDimensionPixelSize(
-            R.styleable.AvatarView_streamAvatarHeight,
-            context.getDimension(R.dimen.stream_message_avatar_height)
-        )
-        avatarBorderWidth = a.getDimensionPixelSize(
-            R.styleable.AvatarView_streamAvatarBorderWidth,
-            context.getDimension(R.dimen.stream_channel_avatar_border_width)
-        )
-        avatarBorderColor = a.getColor(
-            R.styleable.AvatarView_streamAvatarBorderColor,
-            Color.WHITE
-        )
-        avatarInitialText = TextStyle.Builder(a)
-            .size(
-                R.styleable.AvatarView_streamAvatarTextSize,
-                context.getDimension(R.dimen.stream_channel_initials)
+        ).use {
+            avatarWidth = it.getDimensionPixelSize(
+                R.styleable.AvatarView_streamAvatarWidth,
+                context.getDimension(R.dimen.stream_message_avatar_width)
             )
-            .color(
-                R.styleable.AvatarView_streamAvatarTextColor,
+            avatarHeight = it.getDimensionPixelSize(
+                R.styleable.AvatarView_streamAvatarHeight,
+                context.getDimension(R.dimen.stream_message_avatar_height)
+            )
+            avatarBorderWidth = it.getDimensionPixelSize(
+                R.styleable.AvatarView_streamAvatarBorderWidth,
+                context.getDimension(R.dimen.stream_channel_avatar_border_width)
+            )
+            avatarBorderColor = it.getColor(
+                R.styleable.AvatarView_streamAvatarBorderColor,
                 Color.WHITE
             )
-            .font(
-                R.styleable.AvatarView_streamAvatarTextFontAssets,
-                R.styleable.AvatarView_streamAvatarTextFont
+            avatarInitialText = TextStyle.Builder(it)
+                .size(
+                    R.styleable.AvatarView_streamAvatarTextSize,
+                    context.getDimension(R.dimen.stream_channel_initials)
+                )
+                .color(
+                    R.styleable.AvatarView_streamAvatarTextColor,
+                    Color.WHITE
+                )
+                .font(
+                    R.styleable.AvatarView_streamAvatarTextFontAssets,
+                    R.styleable.AvatarView_streamAvatarTextFont
+                )
+                .style(
+                    R.styleable.AvatarView_streamAvatarTextStyle,
+                    Typeface.BOLD
+                )
+                .build()
+            onlineIndicatorEnabled = it.getBoolean(
+                R.styleable.AvatarView_streamAvatarOnlineIndicatorEnabled,
+                false
             )
-            .style(
-                R.styleable.AvatarView_streamAvatarTextStyle,
-                Typeface.BOLD
-            )
-            .build()
-        onlineIndicatorEnabled = a.getBoolean(
-            R.styleable.AvatarView_streamAvatarOnlineIndicatorEnabled,
-            false
-        )
-
-        a.recycle()
+        }
     }
 }
