d11375ab7d2edfd7d25b7381459bb81168613843
Rafał Adasiewicz <adasiewicz.r@gmail.com>
2020-11-09 10:54:45 +0100

    Move checking user logic to ChatInitializer

--- stream-chat-android-sample/src/main/kotlin/io/getstream/chat/sample/feature/startup/StartupFragment.kt
+++ stream-chat-android-sample/src/main/kotlin/io/getstream/chat/sample/feature/startup/StartupFragment.kt
@@ -3,16 +3,19 @@ package io.getstream.chat.sample.feature.startup
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
-import io.getstream.chat.android.client.ChatClient
 import io.getstream.chat.sample.R
+import io.getstream.chat.sample.application.ChatInitializer
 import io.getstream.chat.sample.common.navigateSafely
+import org.koin.android.ext.android.inject
 
 class StartupFragment : Fragment() {
+
+    private val chatInitializer: ChatInitializer by inject()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         findNavController().navigateSafely(
-            if (ChatClient.isInitialized && ChatClient.instance().getCurrentUser() != null) {
+            if (chatInitializer.isUserSet()) {
                 R.id.action_startupFragmentFragment_to_channelsFragment
             } else {
                 R.id.action_startupFragmentFragment_to_userLoginFragment
