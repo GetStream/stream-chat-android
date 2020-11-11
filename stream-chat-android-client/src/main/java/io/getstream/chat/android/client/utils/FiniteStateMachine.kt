d0202a8d82586d43b200e3cf25e86f57cd18de2b
Oleg Kuzmin <oleg@getstream.io>
2020-11-11 14:03:01 +0300

    Add defaultHadnler to FSM in order to do different default tasks (logging e.g.)

--- stream-chat-android-client/src/main/java/io/getstream/chat/android/client/utils/FiniteStateMachine.kt
+++ stream-chat-android-client/src/main/java/io/getstream/chat/android/client/utils/FiniteStateMachine.kt
@@ -7,7 +7,8 @@ import kotlin.reflect.KClass
 
 internal class FiniteStateMachine<S : State, E : Event>(
     initialState: S,
-    private val stateFunctions: Map<KClass<in S>, Map<KClass<in E>, Function2<S, E, S>>>
+    private val stateFunctions: Map<KClass<out S>, Map<KClass<out E>, Function2<S, E, S>>>,
+    private val defaultEventHandler: Function2<S, E, Unit>
 ) {
     private val mutex = Mutex()
     private var _state: S = initialState
@@ -21,7 +22,8 @@ internal class FiniteStateMachine<S : State, E : Event>(
         mutex.withLock {
             val currentState = state
             transitionTo(
-                stateFunctions.get(currentState::class)?.get(event::class)?.invoke(currentState, event) ?: currentState
+                stateFunctions[currentState::class]?.get(event::class)?.invoke(currentState, event)
+                    ?: currentState.also { defaultEventHandler(it, event) }
             )
         }
     }
@@ -41,29 +43,34 @@ internal class FiniteStateMachine<S : State, E : Event>(
 
 internal class FSMBuilder<S : State, E : Event> {
     private lateinit var _initialState: S
-    private var stateFunctions: Map<KClass<in S>, Map<KClass<in E>, Function2<S, E, S>>> = emptyMap()
+    private var stateFunctions: Map<KClass<out S>, Map<KClass<out E>, Function2<S, E, S>>> = emptyMap()
+    private var _defaultHandler: (S, E) -> Unit = { _, _ -> Unit }
     internal fun initialState(state: S) {
         _initialState = state
     }
 
+    internal fun defaultHandler(defaultHandler: (S, E) -> Unit) {
+        _defaultHandler = defaultHandler
+    }
+
     internal inline fun <reified S1 : S> state(stateHandlerBuilder: StateHandler<S, E>.() -> Unit) {
         stateFunctions =
-            stateFunctions + ((S1::class as KClass<S>) to StateHandler<S, E>().apply(stateHandlerBuilder).get())
+            stateFunctions + (S1::class to StateHandler<S, E>().apply(stateHandlerBuilder).get())
     }
 
     internal fun build(): FiniteStateMachine<S, E> {
         if (this::_initialState.isInitialized.not()) {
             error("Initial state must be set!")
         }
-        return FiniteStateMachine(_initialState, stateFunctions)
+        return FiniteStateMachine(_initialState, stateFunctions, _defaultHandler)
     }
 }
 
 internal class StateHandler<S : State, E : Event> {
-    private var eventHandlers: Map<KClass<in E>, Function2<S, E, S>> = mapOf()
+    private var eventHandlers: Map<KClass<out E>, Function2<S, E, S>> = mapOf()
 
     inline fun <reified E1 : E> onEvent(noinline func: (S, E) -> S) {
-        eventHandlers = eventHandlers + ((E1::class as KClass<E>) to func)
+        eventHandlers = eventHandlers + (E1::class to func)
     }
 
     internal fun get() = eventHandlers
