4aa5ad01640f296037a482b0e88e73bc1d6ea43a
Samuel Urbanowicz <samiuelson@gmail.com>
2020-11-18 20:31:17 +0100

    CAS-327 Create MessagesHeader page in storybook

---
+++ stream-chat-android-ui-components-sample/src/main/kotlin/io/getstream/chat/ui/sample/feature/component_browser/messages/header/ComponentBrowserMessagesHeaderViewFragment.kt
@@ -0,0 +1,74 @@
+package io.getstream.chat.ui.sample.feature.component_browser.messages.header
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import io.getstream.chat.ui.sample.databinding.FragmentComponentBrowserMessagesHeaderViewBinding
+import io.getstream.chat.ui.sample.feature.component_browser.utils.randomChannel
+import io.getstream.chat.ui.sample.feature.component_browser.utils.randomUser
+import io.getstream.chat.ui.sample.feature.component_browser.utils.randomUsers
+
+class ComponentBrowserMessagesHeaderViewFragment : Fragment() {
+    private var _binding: FragmentComponentBrowserMessagesHeaderViewBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentComponentBrowserMessagesHeaderViewBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.headerOnlineStatus.apply {
+            showBackButtonBadge("23")
+            setTitle("Chat title")
+            setOnlineStateSubtitle("Chat status")
+            setData(randomChannel())
+        }
+        binding.headerOnlineNoBadgeStatus.apply {
+            setTitle("Chat title")
+            setOnlineStateSubtitle("Chat status")
+            setData(randomChannel(), randomUsers())
+            showBackButtonBadge("")
+        }
+        binding.headerOnlineAvatar.apply {
+            setTitle("Chat title")
+            setOnlineStateSubtitle("Chat status")
+            setData(randomUser(isOnline = true))
+        }
+        binding.headerOfflineAvatar.apply {
+            setTitle("Chat title")
+            setOnlineStateSubtitle("Chat status")
+            setData(randomUser(isOnline = false))
+        }
+        binding.headerSearchingForNetwork.apply {
+            setTitle("Chat title")
+            setOnlineStateSubtitle("Chat status")
+            setData(randomUser(isOnline = false))
+            showSearchingForNetworkLabel()
+        }
+        binding.headerDeviceOffline.apply {
+            setTitle("Chat title")
+            setOnlineStateSubtitle("Chat status")
+            setData(randomUser(isOnline = false))
+            showOfflineStateLabel()
+        }
+        binding.headerTypingSubtitle.apply {
+            setTitle("Chat title")
+            setOnlineStateSubtitle("Chat status")
+            setData(randomUser(isOnline = false))
+            showTypingStateLabel()
+        }
+    }
+}
