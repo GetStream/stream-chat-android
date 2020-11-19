8fa974e13fcccc0499c6739dcf4eace8d0ccf859
Márton Braun <marton@getstream.io>
2020-11-19 11:39:38 +0100

    [CAS-369] Add search result list

---
+++ stream-chat-android-ui-components/src/main/kotlin/io/getstream/chat/android/ui/search/SearchResultListAdapter.kt
@@ -0,0 +1,64 @@
+package io.getstream.chat.android.ui.search
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import io.getstream.chat.android.client.models.Message
+import io.getstream.chat.android.ui.databinding.StreamItemMentionListBinding
+import io.getstream.chat.android.ui.messagepreview.MessagePreviewView
+import io.getstream.chat.android.ui.search.SearchResultListAdapter.MessagePreviewViewHolder
+import io.getstream.chat.android.ui.search.SearchResultListView.SearchResultSelectedListener
+
+public class SearchResultListAdapter : ListAdapter<Message, MessagePreviewViewHolder>(MessageDiffCallback) {
+
+    private var searchResultSelectedListener: SearchResultSelectedListener? = null
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MessagePreviewViewHolder {
+        return StreamItemMentionListBinding
+            .inflate(LayoutInflater.from(parent.context), parent, false)
+            .let { MessagePreviewViewHolder(it.root) }
+    }
+
+    override fun onBindViewHolder(holder: MessagePreviewViewHolder, position: Int) {
+        holder.bind(getItem(position))
+    }
+
+    public fun setSearchResultSelectedListener(searchResultSelectedListener: SearchResultSelectedListener?) {
+        this.searchResultSelectedListener = searchResultSelectedListener
+    }
+
+    public inner class MessagePreviewViewHolder(
+        private val view: MessagePreviewView
+    ) : RecyclerView.ViewHolder(view) {
+
+        private lateinit var message: Message
+
+        init {
+            view.setOnClickListener {
+                searchResultSelectedListener?.onSearchResultSelected(message)
+            }
+        }
+
+        internal fun bind(message: Message) {
+            this.message = message
+            view.setMessage(message)
+        }
+    }
+
+    private object MessageDiffCallback : DiffUtil.ItemCallback<Message>() {
+        override fun areItemsTheSame(oldItem: Message, newItem: Message): Boolean {
+            return oldItem.id == newItem.id
+        }
+
+        override fun areContentsTheSame(oldItem: Message, newItem: Message): Boolean {
+            // Comparing only properties used by the ViewHolder
+            return oldItem.id == newItem.id &&
+                oldItem.createdAt == newItem.createdAt &&
+                oldItem.createdLocallyAt == newItem.createdLocallyAt &&
+                oldItem.text == newItem.text &&
+                oldItem.user == newItem.user
+        }
+    }
+}
