66439d689a24e2cb2b92e2281115f07e5367ba1c
Samuel Urbanowicz <samiuelson@gmail.com>
2020-11-20 19:21:54 +0100

    CAS-442 Create ChatFragment

---
+++ stream-chat-android-ui-components-sample/src/main/kotlin/io/getstream/chat/ui/sample/feature/chat/ChatFragment.kt
@@ -0,0 +1,41 @@
+package io.getstream.chat.ui.sample.feature.chat
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.navArgs
+import com.getstream.sdk.chat.viewmodel.ChannelHeaderViewModel
+import com.getstream.sdk.chat.viewmodel.factory.ChannelViewModelFactory
+import io.getstream.chat.android.ui.messages.header.bindView
+import io.getstream.chat.ui.sample.databinding.FragmentChatBinding
+
+class ChatFragment : Fragment() {
+
+    private val cid: String by lazy { navArgs<ChatFragmentArgs>().value.cid }
+    private val factory: ChannelViewModelFactory by lazy { ChannelViewModelFactory(cid) }
+    private val headerViewModel: ChannelHeaderViewModel by viewModels { factory }
+
+    private var _binding: FragmentChatBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentChatBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        headerViewModel.bindView(binding.header, viewLifecycleOwner)
+    }
+}
