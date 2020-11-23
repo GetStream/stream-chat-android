c9e2848d447e983f88ca0c8fe7812b83d6597ee8
Jc Miñarro <josecarlos.minarro@gmail.com>
2020-11-23 13:57:51 +0100

    Fix render date of the last message in a channel

--- stream-chat-android/src/main/java/com/getstream/sdk/chat/adapter/ChannelListItemViewHolder.java
+++ stream-chat-android/src/main/java/com/getstream/sdk/chat/adapter/ChannelListItemViewHolder.java
@@ -24,7 +24,6 @@ import com.getstream.sdk.chat.view.ReadStateView;
 import com.getstream.sdk.chat.view.channels.ChannelListView;
 import com.getstream.sdk.chat.view.channels.ChannelListViewStyle;
 
-import java.util.Date;
 import java.util.List;
 
 import io.getstream.chat.android.client.models.Attachment;
@@ -182,16 +181,7 @@ public class ChannelListItemViewHolder extends BaseChannelListItemViewHolder {
     }
 
     protected void configLastMessageDate(Channel channel) {
-
-        Message lastMessage = LlcMigrationUtils.computeLastMessage(channel);
-        if (lastMessage == null) {
-            tv_date.setText("");
-            return;
-        }
-
-        Date messageDate = lastMessage.getCreatedAt() != null ? lastMessage.getCreatedAt() : lastMessage.getCreatedLocallyAt();
-
-        tv_date.setText(DateFormatter.formatAsTimeOrDate(messageDate));
+        tv_date.setText(DateFormatter.formatAsTimeOrDate(channel.getLastMessageAt()));
     }
 
     protected void configReadState(Channel channel) {
