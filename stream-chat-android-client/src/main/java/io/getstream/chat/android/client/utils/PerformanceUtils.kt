359f8c25620d2e7fbfb40b03fd6a1437d1bec74e
Oleg Kuzmin <oleg@getstream.io>
2020-11-24 10:58:21 +0300

    Make PerformanceUtils capable to count average duration of task

--- stream-chat-android-client/src/main/java/io/getstream/chat/android/client/utils/PerformanceUtils.kt
+++ stream-chat-android-client/src/main/java/io/getstream/chat/android/client/utils/PerformanceUtils.kt
@@ -3,46 +3,73 @@ package io.getstream.chat.android.client.utils
 import android.util.Log
 
 public object PerformanceUtils {
-    private var tasks: Map<String, Long> = emptyMap()
+    private var tasks: Map<String, TaskEntry> = emptyMap()
 
     public fun startTask(taskName: String) {
-        val startTime = System.currentTimeMillis()
-        tasks = tasks + (taskName to startTime)
+        synchronized(taskName) {
+            val startTime = System.currentTimeMillis()
+            val lastEntry = tasks[taskName]
+            tasks = if (lastEntry == null) {
+                tasks + (taskName to TaskEntry(name = taskName, count = 0, sumDuration = 0.0, lastStart = startTime))
+            } else {
+                tasks + (taskName to lastEntry.copy(lastStart = startTime))
+            }
 
-        log("Task \"$taskName\" started")
-    }
-
-    public fun stopTask(taskName: String) {
-        val startTime = tasks[taskName]
-        if (startTime == null) {
-            log("There is no such task \"$taskName\"")
-            return
-        } else {
-            tasks = tasks - taskName
-            measure(taskName, startTime)
+            log("Task \"$taskName\" started")
         }
     }
 
-    private fun measure(taskName: String, startTime: Long) {
-        val duration = (System.currentTimeMillis() - startTime) / 1000.0
-        log("Task \"$taskName\" completed for $duration seconds")
+    public fun stopTask(taskName: String) {
+        synchronized(taskName) {
+            val lastEntry = tasks[taskName]
+            if (lastEntry == null) {
+                log("There is no such task \"$taskName\"")
+                return
+            } else {
+                doMeasureAndUpdateResult(taskName, lastEntry)
+            }
+        }
+    }
+
+    private fun doMeasureAndUpdateResult(taskName: String, entry: TaskEntry) {
+        entry.lastStart?.let { startTime ->
+            val lastDuration = (System.currentTimeMillis() - startTime) / 1000.0
+            val newSumDuration = entry.sumDuration + lastDuration
+            val newCount = entry.count + 1
+            tasks = tasks + (taskName to entry.copy(count = newCount, sumDuration = newSumDuration, lastStart = null))
+            val avgDuration = newSumDuration / newCount
+
+            log(
+                "Task \"$taskName\" completed for $lastDuration seconds\n" +
+                    "The average duration for task \"$taskName\" is $avgDuration seconds"
+            )
+        }
     }
 
     public fun <T> task(taskName: String, task: () -> T): T {
-        val startTime = System.currentTimeMillis()
-        return task().also {
-            measure(taskName, startTime)
+        synchronized(taskName) {
+            val startTime = System.currentTimeMillis()
+            return task().also {
+                doMeasureAndUpdateResult(taskName, TaskEntry(name = taskName, lastStart = startTime))
+            }
         }
     }
 
     public suspend fun <T> suspendTask(taskName: String, task: suspend () -> T): T {
         val startTime = System.currentTimeMillis()
         return task().also {
-            measure(taskName, startTime)
+            doMeasureAndUpdateResult(taskName, TaskEntry(name = taskName, lastStart = startTime))
         }
     }
 
     public fun log(message: String) {
         Log.d("Performance", message)
     }
+
+    private data class TaskEntry(
+        val name: String,
+        val count: Int = 0,
+        val sumDuration: Double = 0.0,
+        val lastStart: Long?
+    )
 }
