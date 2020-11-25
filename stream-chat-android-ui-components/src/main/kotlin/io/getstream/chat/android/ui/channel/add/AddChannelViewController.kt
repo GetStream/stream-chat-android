9838441f59097688eb1de5866674fa576a8b472b
Rafal Adasiewicz <rafal@getstream.io>
2020-11-25 15:23:40 +0100

    [CAS-425] Add creating new DM channel logic  (#904)
    * Add creating new DM channel logic
    
    PR adds AddChannelViewModel which is responsible for providing data for AddUserView
    and handling channel creation process.
    Creation channel flow can be described in following steps:
    1. Create draft channel when member is added
    2. Update draft channel (remove draft flag) after sending the message
    Draft channels are required to show past messages and they are not visible for user - they are filtered during queryChannels request.
    Creating new DM channels flow works only while user is online.
    In next PR I'm going to rename all AddChannel* views (to NewChannel*) and move them to sample app to match other SDKs.
    
    * Remove unused layout resource
    
    * Fix typo in empty users title

--- stream-chat-android-ui-components/src/main/kotlin/io/getstream/chat/android/ui/channel/add/AddChannelViewController.kt
+++ stream-chat-android-ui-components/src/main/kotlin/io/getstream/chat/android/ui/channel/add/AddChannelViewController.kt
@@ -1,45 +1,61 @@
 package io.getstream.chat.android.ui.channel.add
 
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.core.view.isVisible
 import androidx.recyclerview.widget.RecyclerView
 import io.getstream.chat.android.client.models.User
 import io.getstream.chat.android.client.models.name
 
 internal class AddChannelViewController(
     private val headerView: AddChannelHeaderView,
+    private val usersTitle: TextView,
     private val usersRecyclerView: RecyclerView,
     private val createGroupContainer: ViewGroup
 ) {
 
     private val usersAdapter = AddChannelUsersAdapter()
     private val members: MutableList<User> = mutableListOf()
-    private var userInfoList: MutableList<UserInfo> = mutableListOf()
+    private val userInfoList: MutableList<UserInfo> = mutableListOf()
+
+    var membersChangedListener = AddChannelView.MembersChangedListener {}
+    var addMemberButtonClickListener = AddChannelView.AddMemberButtonClickListener {}
 
     init {
         usersRecyclerView.adapter = usersAdapter
         usersAdapter.userClickListener = AddChannelUsersAdapter.UserClickListener {
             onUserClicked(it)
         }
-        headerView.membersInputListener = object : AddChannelHeaderView.Listener {
-            override fun onInputChanged(query: String) {
-                // Filter users list
+        headerView.apply {
+            membersInputListener = object : AddChannelHeaderView.Listener {
+                override fun onInputChanged(query: String) {
+                    // Filter users list
+                }
             }
-        }
-
-        headerView.setAddMemberButtonClickListener {
-            addMemberButtonClicked()
+            setAddMemberButtonClickListener {
+                showInput()
+                addMemberButtonClickListener.onAddMemberButtonClicked()
+                usersRecyclerView.isVisible = true
+            }
+            setMemberClickListener {
+                onUserClicked(UserInfo(it, true))
+            }
         }
     }
 
     fun setUsers(users: List<User>) {
         userInfoList.clear()
+        addMoreUsers(users)
+    }
+
+    fun addMoreUsers(users: List<User>) {
         userInfoList.addAll(users.map { UserInfo(it, false) })
         showSectionedUsers(userInfoList)
     }
 
     private fun showSectionedUsers(userInfoList: List<UserInfo>) {
         val sectionedUsers = userInfoList
-            .groupBy { it.user.name.first() }
+            .groupBy { it.user.name.first().toUpperCase() }
             .toSortedMap()
             .flatMap { (letter, users) ->
                 mutableListOf(UserListItem.Separator(letter)) + users.map { UserListItem.UserItem(it) }
@@ -50,16 +66,13 @@ internal class AddChannelViewController(
     private fun hideInput() {
         headerView.hideInput()
         headerView.showAddMemberButton()
+        usersTitle.isVisible = false
     }
 
     private fun showInput() {
         headerView.showInput()
         headerView.hideAddMemberButton()
-    }
-
-    private fun addMemberButtonClicked() {
-        headerView.showInput()
-        headerView.hideAddMemberButton()
+        usersTitle.isVisible = true
     }
 
     private fun onUserClicked(userInfo: UserInfo) {
@@ -70,11 +83,16 @@ internal class AddChannelViewController(
             members.add(userInfo.user)
         }
         if (members.isEmpty()) {
+            createGroupContainer.isVisible = true
+            usersRecyclerView.isVisible = true
             showInput()
         } else {
+            createGroupContainer.isVisible = false
+            usersRecyclerView.isVisible = false
             hideInput()
         }
         headerView.setMembers(members.toList())
+        membersChangedListener.onMembersChanged(members)
 
         // Update user list
         val index = userInfoList.indexOf(userInfo)
