830441063660759157225ec9c752486020cffb4f
Oleg Kuzmin <oleg@getstream.io>
2020-11-26 18:15:37 +0300

    Proper hide mechanism

--- stream-chat-android-ui-components/src/main/kotlin/io/getstream/chat/android/ui/messages/adapter/viewholder/MessageDeletedViewHolder.kt
+++ stream-chat-android-ui-components/src/main/kotlin/io/getstream/chat/android/ui/messages/adapter/viewholder/MessageDeletedViewHolder.kt
@@ -1,6 +1,7 @@
 package io.getstream.chat.android.ui.messages.adapter.viewholder
 
 import androidx.core.view.isVisible
+import androidx.recyclerview.widget.RecyclerView
 import com.getstream.sdk.chat.adapter.MessageListItem
 import io.getstream.chat.android.client.models.User
 import io.getstream.chat.android.ui.databinding.StreamItemMessageDeletedBinding
@@ -11,9 +12,18 @@ public class MessageDeletedViewHolder(
     private val binding: StreamItemMessageDeletedBinding
 ) : BaseMessageItemViewHolder<MessageListItem.MessageItem>(binding.root) {
     override fun bind(data: MessageListItem.MessageItem) {
-        binding.root.isVisible = when {
-            data.message.deletedAt != null && data.message.user == currentUser -> true
-            else -> false
+        val shouldBeVisible = data.message.deletedAt != null && data.message.user == currentUser
+        binding.root.run {
+            isVisible = shouldBeVisible
+            layoutParams = if (shouldBeVisible) DEFAULT_LAYOUT_PARAMS else HIDDEN_LAYOUT_PARAMS
         }
+    }
+
+    private companion object {
+        private val DEFAULT_LAYOUT_PARAMS = RecyclerView.LayoutParams(
+            RecyclerView.LayoutParams.MATCH_PARENT,
+            RecyclerView.LayoutParams.WRAP_CONTENT
+        )
+        private val HIDDEN_LAYOUT_PARAMS = RecyclerView.LayoutParams(0, 0)
     }
 }
