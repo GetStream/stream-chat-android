6942545479db7121f0f6c0ab70db1f051be499ec
Oleg Kuzmin <oleg@getstream.io>
2020-11-27 14:53:21 +0300

    Add messages detelted component browser

--- stream-chat-android-ui-components-sample/src/main/kotlin/io/getstream/chat/ui/sample/feature/component_browser/messages/MessageListComponentBrowserFragment.kt
+++ stream-chat-android-ui-components-sample/src/main/kotlin/io/getstream/chat/ui/sample/feature/component_browser/messages/MessageListComponentBrowserFragment.kt
@@ -5,6 +5,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import io.getstream.chat.ui.sample.R
+import io.getstream.chat.ui.sample.common.navigateSafely
 import io.getstream.chat.ui.sample.databinding.FragmentComponentBrowserMessageListBinding
 
 class MessageListComponentBrowserFragment : Fragment() {
@@ -13,5 +16,15 @@ class MessageListComponentBrowserFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         binding = FragmentComponentBrowserMessageListBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        setupDeletedMessages()
+    }
+
+    private fun setupDeletedMessages() {
+        binding.deletedMessages.setOnClickListener {
+            findNavController().navigateSafely(R.id.action_componentBrowserMessageList_to_componentBrowserDeletedMessages)
+        }
     }
 }
