837e154b6d8cfaf14acef8f8da01a6f2713a8986
Oleg Kuzmin <oleg@getstream.io>
2020-11-27 18:52:58 +0300

    Fix proper hiding in recyclerView

--- stream-chat-android-ui-components-sample/src/main/kotlin/io/getstream/chat/ui/sample/feature/component_browser/messages/viewholder/DeletedMessagesComponentBrowserFragment.kt
+++ stream-chat-android-ui-components-sample/src/main/kotlin/io/getstream/chat/ui/sample/feature/component_browser/messages/viewholder/DeletedMessagesComponentBrowserFragment.kt
@@ -5,7 +5,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.getstream.sdk.chat.adapter.MessageListItem
+import io.getstream.chat.android.client.models.Message
+import io.getstream.chat.android.ui.messages.adapter.viewholder.MessageDeletedViewHolder
 import io.getstream.chat.ui.sample.databinding.FragmentDeletedMessageComponentBrowserBinding
+import java.util.Date
 
 class DeletedMessagesComponentBrowserFragment : Fragment() {
 
@@ -14,5 +18,43 @@ class DeletedMessagesComponentBrowserFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         binding = FragmentDeletedMessageComponentBrowserBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        binding.recyclerView.adapter = DefaultAdapter(
+            getDummyDeletedMessagesList(),
+            ::MessageDeletedViewHolder,
+            MessageDeletedViewHolder::bind
+        )
+    }
+
+    private fun getDummyDeletedMessagesList(): List<MessageListItem.MessageItem> {
+        return listOf(
+            MessageListItem.MessageItem(
+                message = Message(deletedAt = Date()),
+                positions = listOf(MessageListItem.Position.TOP),
+                isMine = true
+            ),
+            MessageListItem.MessageItem(
+                message = Message(deletedAt = Date()),
+                positions = listOf(MessageListItem.Position.MIDDLE),
+                isMine = true
+            ),
+            MessageListItem.MessageItem(
+                message = Message(deletedAt = Date()),
+                positions = listOf(MessageListItem.Position.BOTTOM),
+                isMine = true
+            ),
+            MessageListItem.MessageItem(
+                message = Message(deletedAt = Date()),
+                positions = listOf(MessageListItem.Position.TOP, MessageListItem.Position.BOTTOM),
+                isMine = false
+            ),
+            MessageListItem.MessageItem(
+                message = Message(deletedAt = Date()),
+                positions = listOf(MessageListItem.Position.TOP, MessageListItem.Position.BOTTOM),
+                isMine = true
+            )
+        )
     }
 }
--- stream-chat-android-ui-components/src/main/kotlin/io/getstream/chat/android/ui/messages/adapter/viewholder/MessageDeletedViewHolder.kt
+++ stream-chat-android-ui-components/src/main/kotlin/io/getstream/chat/android/ui/messages/adapter/viewholder/MessageDeletedViewHolder.kt
@@ -1,29 +1,27 @@
 package io.getstream.chat.android.ui.messages.adapter.viewholder
 
+import android.view.LayoutInflater
+import android.view.ViewGroup
 import androidx.core.view.isVisible
-import androidx.recyclerview.widget.RecyclerView
 import com.getstream.sdk.chat.adapter.MessageListItem
-import io.getstream.chat.android.client.models.User
 import io.getstream.chat.android.ui.databinding.StreamItemMessageDeletedBinding
 import io.getstream.chat.android.ui.messages.adapter.BaseMessageItemViewHolder
 
 public class MessageDeletedViewHolder(
-    private val currentUser: User,
-    private val binding: StreamItemMessageDeletedBinding
+    parent: ViewGroup,
+    private val binding: StreamItemMessageDeletedBinding = StreamItemMessageDeletedBinding.inflate(
+        LayoutInflater.from(
+            parent.context
+        ), parent, false
+    )
 ) : BaseMessageItemViewHolder<MessageListItem.MessageItem>(binding.root) {
     override fun bind(data: MessageListItem.MessageItem) {
-        val shouldBeVisible = data.message.deletedAt != null && data.message.user == currentUser
+        val shouldBeVisible = data.message.deletedAt != null && data.isMine
         binding.root.run {
             isVisible = shouldBeVisible
-            layoutParams = if (shouldBeVisible) DEFAULT_LAYOUT_PARAMS else HIDDEN_LAYOUT_PARAMS
+            layoutParams = layoutParams.apply {
+                height = if (shouldBeVisible) ViewGroup.LayoutParams.WRAP_CONTENT else 0
+            }
         }
-    }
-
-    private companion object {
-        private val DEFAULT_LAYOUT_PARAMS = RecyclerView.LayoutParams(
-            RecyclerView.LayoutParams.MATCH_PARENT,
-            RecyclerView.LayoutParams.WRAP_CONTENT
-        )
-        private val HIDDEN_LAYOUT_PARAMS = RecyclerView.LayoutParams(0, 0)
     }
 }
