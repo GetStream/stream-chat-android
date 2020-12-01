c03cecbb976ab0454b2238243b4ba30569a85f1f
Oleg Kuzmin <oleg@getstream.io>
2020-12-01 14:24:42 +0300

    Add background decorator for deleted message

--- stream-chat-android-ui-components/src/main/kotlin/io/getstream/chat/android/ui/messages/adapter/viewholder/MessageDeletedViewHolder.kt
+++ stream-chat-android-ui-components/src/main/kotlin/io/getstream/chat/android/ui/messages/adapter/viewholder/MessageDeletedViewHolder.kt
@@ -6,16 +6,22 @@ import androidx.core.view.isVisible
 import com.getstream.sdk.chat.adapter.MessageListItem
 import io.getstream.chat.android.ui.databinding.StreamItemMessageDeletedBinding
 import io.getstream.chat.android.ui.messages.adapter.BaseMessageItemViewHolder
+import io.getstream.chat.android.ui.messages.adapter.viewholder.decorator.BackgroundDecorator
+import io.getstream.chat.android.ui.messages.adapter.viewholder.decorator.Decorator
 
 public class MessageDeletedViewHolder(
     parent: ViewGroup,
-    private val binding: StreamItemMessageDeletedBinding = StreamItemMessageDeletedBinding.inflate(
+    internal val binding: StreamItemMessageDeletedBinding = StreamItemMessageDeletedBinding.inflate(
         LayoutInflater.from(
             parent.context
         ), parent, false
     )
 ) : BaseMessageItemViewHolder<MessageListItem.MessageItem>(binding.root) {
+
+    private val decorators = listOf<Decorator>(BackgroundDecorator())
+
     override fun bind(data: MessageListItem.MessageItem) {
+        decorators.forEach { it.decorate(this, data) }
         val shouldBeVisible = data.message.deletedAt != null && data.isMine
         binding.root.run {
             isVisible = shouldBeVisible
---
+++ stream-chat-android-ui-components/src/main/kotlin/io/getstream/chat/android/ui/messages/adapter/viewholder/decorator/Decorator.kt
@@ -0,0 +1,8 @@
+package io.getstream.chat.android.ui.messages.adapter.viewholder.decorator
+
+import com.getstream.sdk.chat.adapter.MessageListItem
+import io.getstream.chat.android.ui.messages.adapter.BaseMessageItemViewHolder
+
+internal interface Decorator {
+    fun <T : MessageListItem> decorate(viewHolder: BaseMessageItemViewHolder<T>, data: T)
+}
