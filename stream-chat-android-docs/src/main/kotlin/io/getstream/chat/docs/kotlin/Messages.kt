efa80795e35fe70795a8d5da5ce383c5b16a8343
Zetra <zetbaitsu@gmail.com>
2020-12-03 10:21:24 +0700

    Message crud kotlin

---
+++ stream-chat-android-docs/src/main/kotlin/io/getstream/chat/docs/kotlin/Messages.kt
@@ -0,0 +1,56 @@
+package io.getstream.chat.docs.kotlin
+
+import io.getstream.chat.android.client.channel.ChannelClient
+import io.getstream.chat.android.client.models.Attachment
+import io.getstream.chat.android.client.models.Message
+import io.getstream.chat.android.client.models.User
+
+object Messages {
+    val channelController: ChannelClient = TODO()
+    val message: Message = TODO()
+
+    fun sendAMessage() {
+        val message = Message()
+        message.text =
+            "Josh I told them I was pesca-pescatarian. Which is one who eats solely fish who eat other fish."
+        message.extraData["anotherCustomField"] = 234
+
+        // add an image attachment to the message
+        val attachment = Attachment()
+        attachment.type = "image"
+        attachment.imageUrl = "https://bit.ly/2K74TaG"
+        attachment.fallback = "test image"
+        // add some custom data to the attachment
+        attachment.extraData["myCustomField"] = 123
+
+        message.attachments.add(attachment)
+
+        // include the user id of the mentioned user
+        message.mentionedUsers.add(User("josh-id"))
+
+        channelController.sendMessage(message).enqueue {
+            val message = it.data()
+        }
+    }
+
+    fun getAMessage() {
+        channelController.getMessage("messageId").enqueue {
+            val message = it.data()
+        }
+    }
+
+    fun updateAMessage() {
+        // update some field of the message
+        message.text = "my updated text"
+        // send the message to the channel
+        channelController.updateMessage(message).enqueue {
+            val message = it.data()
+        }
+    }
+
+    fun deleteAMessage() {
+        channelController.deleteMessage("messageId").enqueue {
+            val deletedMessage = it.data()
+        }
+    }
+}
