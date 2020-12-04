423b38e47d7cace347d8b134ceacf3bab0d8ce16
Zetra <zetbaitsu@gmail.com>
2020-12-04 15:42:06 +0700

    Restructure Messages Code Snippet Docs (#956)

--- stream-chat-android-docs/src/main/kotlin/io/getstream/chat/docs/kotlin/Messages.kt
+++ stream-chat-android-docs/src/main/kotlin/io/getstream/chat/docs/kotlin/Messages.kt
@@ -22,241 +22,292 @@ class Messages(
     val parentMessage: Message
 ) {
 
-    fun sendAMessage() {
-        // create a message
-        val message = Message()
-        message.text =
-            "Josh I told them I was pesca-pescatarian. Which is one who eats solely fish who eat other fish."
-        message.extraData["anotherCustomField"] = 234
+    /**
+     * @see <a href="https://getstream.io/chat/docs/send_message/?language=kotlin">Messages Overview</a>
+     */
+    inner class MessagesOverview {
+        fun sendAMessage() {
+            // Create a message
+            val message = Message()
+            message.text =
+                "Josh I told them I was pesca-pescatarian. Which is one who eats solely fish who eat other fish."
+            message.extraData["anotherCustomField"] = 234
 
-        // add an image attachment to the message
-        val attachment = Attachment()
-        attachment.type = "image"
-        attachment.imageUrl = "https://bit.ly/2K74TaG"
-        attachment.fallback = "test image"
-        // add some custom data to the attachment
-        attachment.extraData["myCustomField"] = 123
+            // Add an image attachment to the message
+            val attachment = Attachment()
+            attachment.type = "image"
+            attachment.imageUrl = "https://bit.ly/2K74TaG"
+            attachment.fallback = "test image"
+            // Add some custom data to the attachment
+            attachment.extraData["myCustomField"] = 123
 
-        message.attachments.add(attachment)
+            message.attachments.add(attachment)
 
-        // include the user id of the mentioned user
-        message.mentionedUsers.add(User("josh-id"))
+            // Include the user id of the mentioned user
+            message.mentionedUsers.add(User("josh-id"))
 
-        // send the message to the channel
-        channelController.sendMessage(message).enqueue {
-            if (it.isSuccess) {
-                val sentMessage = it.data()
-            } else {
-                Log.e(TAG, String.format("There was an error %s", it.error(), it.error().cause))
-            }
-        }
-    }
-
-    fun getAMessage() {
-        channelController.getMessage("message-id").enqueue {
-            if (it.isSuccess) {
-                val message = it.data()
-            } else {
-                Log.e(TAG, String.format("There was an error %s", it.error(), it.error().cause))
-            }
-        }
-    }
-
-    fun updateAMessage() {
-        // update some field of the message
-        message.text = "my updated text"
-
-        // send the message to the channel
-        channelController.updateMessage(message).enqueue {
-            if (it.isSuccess) {
-                val updatedMessage = it.data()
-            } else {
-                Log.e(TAG, String.format("There was an error %s", it.error(), it.error().cause))
-            }
-        }
-    }
-
-    fun deleteAMessage() {
-        channelController.deleteMessage("message-id").enqueue {
-            if (it.isSuccess) {
-                val deletedMessage = it.data()
-            } else {
-                Log.e(TAG, String.format("There was an error %s", it.error(), it.error().cause))
-            }
-        }
-    }
-
-    fun fileUploads() {
-        val imageFile = File("path")
-        val anyOtherFile = File("path")
-
-        // upload an image
-        channelController.sendImage(
-            imageFile,
-            object : ProgressCallback {
-                override fun onSuccess(file: String) {
-                    val fileUrl = file
-                }
-
-                override fun onError(error: ChatError) {
-                    Log.e(TAG, String.format("There was an error %s", error, error.cause))
-                }
-
-                override fun onProgress(progress: Long) {
-                    // You can render uploading progress here
+            // Send the message to the channel
+            channelController.sendMessage(message).enqueue {
+                if (it.isSuccess) {
+                    val sentMessage = it.data()
+                } else {
+                    Log.e(TAG, String.format("There was an error %s", it.error(), it.error().cause))
                 }
             }
-        )
+        }
 
-        // upload a file
-        channelController.sendFile(
-            anyOtherFile,
-            object : ProgressCallback {
-                override fun onSuccess(file: String) {
-                    val fileUrl = file
-                }
-
-                override fun onError(error: ChatError) {
-                    Log.e(TAG, String.format("There was an error %s", error, error.cause))
-                }
-
-                override fun onProgress(progress: Long) {
-                    // You can render uploading progress here
+        /**
+         * @see <a href="https://getstream.io/chat/docs/send_message/?language=kotlin#get-a-message">Get A Message</a>
+         */
+        fun getAMessage() {
+            channelController.getMessage("message-id").enqueue {
+                if (it.isSuccess) {
+                    val message = it.data()
+                } else {
+                    Log.e(TAG, String.format("There was an error %s", it.error(), it.error().cause))
                 }
             }
-        )
-    }
+        }
 
-    fun sendAReaction() {
-        val reaction = Reaction("message-id", "like", 1)
-        channelController.sendReaction(reaction).enqueue {
-            if (it.isSuccess) {
-                val sentReaction = it.data()
-            } else {
-                Log.e(TAG, String.format("There was an error %s", it.error(), it.error().cause))
+        /**
+         * @see <a href="https://getstream.io/chat/docs/send_message/?language=kotlin#update-a-message">Update A Message</a>
+         */
+        fun updateAMessage() {
+            // Update some field of the message
+            message.text = "my updated text"
+
+            // Send the message to the channel
+            channelController.updateMessage(message).enqueue {
+                if (it.isSuccess) {
+                    val updatedMessage = it.data()
+                } else {
+                    Log.e(TAG, String.format("There was an error %s", it.error(), it.error().cause))
+                }
+            }
+        }
+
+        /**
+         * @see <a href="https://getstream.io/chat/docs/send_message/?language=kotlin#delete-a-message">Delete A Message</a>
+         */
+        fun deleteAMessage() {
+            channelController.deleteMessage("message-id").enqueue {
+                if (it.isSuccess) {
+                    val deletedMessage = it.data()
+                } else {
+                    Log.e(TAG, String.format("There was an error %s", it.error(), it.error().cause))
+                }
             }
         }
     }
 
-    fun removeAReaction() {
-        channelController.deleteReaction("message-id", "like").enqueue {
-            if (it.isSuccess) {
-                val message = it.data()
-            } else {
-                Log.e(TAG, String.format("There was an error %s", it.error(), it.error().cause))
-            }
-        }
-    }
+    /**
+     * @see <a href="https://getstream.io/chat/docs/file_uploads/?language=kotlin">File Uploads</a>
+     */
+    inner class FileUploads {
+        fun fileUploads() {
+            val imageFile = File("path")
+            val anyOtherFile = File("path")
 
-    fun paginatingReactions() {
-        // get the first 10 reactions
-        channelController.getReactions("message-id", 0, 10).enqueue {
-            if (it.isSuccess) {
-                val reactions = it.data()
-            } else {
-                Log.e(TAG, String.format("There was an error %s", it.error(), it.error().cause))
-            }
-        }
+            // Upload an image
+            channelController.sendImage(
+                imageFile,
+                object : ProgressCallback {
+                    override fun onSuccess(file: String) {
+                        val fileUrl = file
+                    }
 
-        // get the second 10 reactions
-        channelController.getReactions("message-id", 10, 10).enqueue {
-            if (it.isSuccess) {
-                val reactions = it.data()
-            } else {
-                Log.e(TAG, String.format("There was an error %s", it.error(), it.error().cause))
-            }
-        }
+                    override fun onError(error: ChatError) {
+                        Log.e(TAG, String.format("There was an error %s", error, error.cause))
+                    }
 
-        // get 10 reactions after particular reaction
-        val reactionId = "reaction-id"
-        channelController.getReactions("message-id", reactionId, 10).enqueue {
-            if (it.isSuccess) {
-                val reactions = it.data()
-            } else {
-                Log.e(TAG, String.format("There was an error %s", it.error(), it.error().cause))
-            }
-        }
-    }
-
-    fun cumulativeReactions() {
-        val score = 5
-        val reaction = Reaction("message-id", "like", score)
-        channelController.sendReaction(reaction).enqueue {
-            if (it.isSuccess) {
-                val sentReaction = it.data()
-            } else {
-                Log.e(TAG, String.format("There was an error %s", it.error(), it.error().cause))
-            }
-        }
-    }
-
-    fun startAThread() {
-        // set the parent id to make sure a message shows up in a thread
-        val message = Message()
-        message.text = "hello world"
-        message.parentId = parentMessage.id
-
-        // send the message to the channel
-        channelController.sendMessage(message).enqueue {
-            if (it.isSuccess) {
-                val sentMessage = it.data()
-            } else {
-                Log.e(TAG, String.format("There was an error %s", it.error(), it.error().cause))
-            }
-        }
-    }
-
-    fun threadPagination() {
-        val limit = 20
-        // retrieve the first 20 messages inside the thread
-        client.getReplies(parentMessage.id, limit).enqueue {
-            if (it.isSuccess) {
-                val replies = it.data()
-            } else {
-                Log.e(TAG, String.format("There was an error %s", it.error(), it.error().cause))
-            }
-        }
-
-        // retrieve the 20 more messages before the message with id "42"
-        client.getRepliesMore(parentMessage.id, "42", limit).enqueue {
-            if (it.isSuccess) {
-                val replies = it.data()
-            } else {
-                Log.e(TAG, String.format("There was an error %s", it.error(), it.error().cause))
-            }
-        }
-    }
-
-    fun silentMessage() {
-        val message = Message("text-of-a-message", silent = true)
-        channelController.sendMessage(message).enqueue {
-            if (it.isSuccess) {
-                val sentMessage = it.data()
-            } else {
-                Log.e(TAG, String.format("There was an error %s", it.error(), it.error().cause))
-            }
-        }
-    }
-
-    fun searchMessages() {
-        val offset = 0
-        val limit = 10
-        val query = "supercalifragilisticexpialidocious"
-        val channelFilter: FilterObject = Filters.`in`("members", listOf("john"))
-        val messageFilter: FilterObject = Filters.autocomplete("text", query)
-
-        client.searchMessages(
-            SearchMessagesRequest(
-                offset,
-                limit,
-                channelFilter,
-                messageFilter
+                    override fun onProgress(progress: Long) {
+                        // You can render uploading progress here
+                    }
+                }
             )
-        ).enqueue {
-            if (it.isSuccess) {
-                val messages: List<Message> = it.data()
-            } else {
-                Log.e(TAG, String.format("There was an error %s", it.error(), it.error().cause))
+
+            // Upload a file
+            channelController.sendFile(
+                anyOtherFile,
+                object : ProgressCallback {
+                    override fun onSuccess(file: String) {
+                        val fileUrl = file
+                    }
+
+                    override fun onError(error: ChatError) {
+                        Log.e(TAG, String.format("There was an error %s", error, error.cause))
+                    }
+
+                    override fun onProgress(progress: Long) {
+                        // You can render uploading progress here
+                    }
+                }
+            )
+        }
+    }
+
+    /**
+     * @see <a href="https://getstream.io/chat/docs/send_reaction/?language=kotlin">Reactions</a>
+     */
+    inner class Reactions {
+        fun sendAReaction() {
+            val reaction = Reaction("message-id", "like", 1)
+            channelController.sendReaction(reaction).enqueue {
+                if (it.isSuccess) {
+                    val sentReaction = it.data()
+                } else {
+                    Log.e(TAG, String.format("There was an error %s", it.error(), it.error().cause))
+                }
+            }
+        }
+
+        /**
+         * @see <a href="https://getstream.io/chat/docs/send_reaction/?language=kotlin#removing-a-reaction">Removing A Reaction</a>
+         */
+        fun removeAReaction() {
+            channelController.deleteReaction("message-id", "like").enqueue {
+                if (it.isSuccess) {
+                    val message = it.data()
+                } else {
+                    Log.e(TAG, String.format("There was an error %s", it.error(), it.error().cause))
+                }
+            }
+        }
+
+        /**
+         * @see <a href="https://getstream.io/chat/docs/send_reaction/?language=kotlin#paginating-reactions">Paginating Reactions</a>
+         */
+        fun paginatingReactions() {
+            // Get the first 10 reactions
+            channelController.getReactions("message-id", 0, 10).enqueue {
+                if (it.isSuccess) {
+                    val reactions = it.data()
+                } else {
+                    Log.e(TAG, String.format("There was an error %s", it.error(), it.error().cause))
+                }
+            }
+
+            // Get the second 10 reactions
+            channelController.getReactions("message-id", 10, 10).enqueue {
+                if (it.isSuccess) {
+                    val reactions = it.data()
+                } else {
+                    Log.e(TAG, String.format("There was an error %s", it.error(), it.error().cause))
+                }
+            }
+
+            // Get 10 reactions after particular reaction
+            val reactionId = "reaction-id"
+            channelController.getReactions("message-id", reactionId, 10).enqueue {
+                if (it.isSuccess) {
+                    val reactions = it.data()
+                } else {
+                    Log.e(TAG, String.format("There was an error %s", it.error(), it.error().cause))
+                }
+            }
+        }
+
+        /**
+         * @see <a href="https://getstream.io/chat/docs/send_reaction/?language=kotlin#cumulative-clap-reactions">Cumulative (Clap) Reactions</a>
+         */
+        fun cumulativeReactions() {
+            val score = 5
+            val reaction = Reaction("message-id", "like", score)
+            channelController.sendReaction(reaction).enqueue {
+                if (it.isSuccess) {
+                    val sentReaction = it.data()
+                } else {
+                    Log.e(TAG, String.format("There was an error %s", it.error(), it.error().cause))
+                }
+            }
+        }
+    }
+
+    /**
+     * @see <a href="https://getstream.io/chat/docs/threads/?language=kotlin">Threads & Replies</a>
+     */
+    inner class ThreadsAndReplies {
+        fun startAThread() {
+            // Set the parent id to make sure a message shows up in a thread
+            val message = Message()
+            message.text = "hello world"
+            message.parentId = parentMessage.id
+
+            // Send the message to the channel
+            channelController.sendMessage(message).enqueue {
+                if (it.isSuccess) {
+                    val sentMessage = it.data()
+                } else {
+                    Log.e(TAG, String.format("There was an error %s", it.error(), it.error().cause))
+                }
+            }
+        }
+
+        /**
+         * @see <a href="https://getstream.io/chat/docs/threads/?language=kotlin#thread-pagination">Thread Pagination</a>
+         */
+        fun threadPagination() {
+            val limit = 20
+            // Retrieve the first 20 messages inside the thread
+            client.getReplies(parentMessage.id, limit).enqueue {
+                if (it.isSuccess) {
+                    val replies = it.data()
+                } else {
+                    Log.e(TAG, String.format("There was an error %s", it.error(), it.error().cause))
+                }
+            }
+
+            // Retrieve the 20 more messages before the message with id "42"
+            client.getRepliesMore(parentMessage.id, "42", limit).enqueue {
+                if (it.isSuccess) {
+                    val replies = it.data()
+                } else {
+                    Log.e(TAG, String.format("There was an error %s", it.error(), it.error().cause))
+                }
+            }
+        }
+    }
+
+    /**
+     * @see <a href="https://getstream.io/chat/docs/silent_messages/?language=kotlin">Silent Messages</a>
+     */
+    inner class SilentMessages {
+        fun silentMessage() {
+            val message = Message("text-of-a-message", silent = true)
+            channelController.sendMessage(message).enqueue {
+                if (it.isSuccess) {
+                    val sentMessage = it.data()
+                } else {
+                    Log.e(TAG, String.format("There was an error %s", it.error(), it.error().cause))
+                }
+            }
+        }
+    }
+
+    /**
+     * @see <a href="https://getstream.io/chat/docs/search/?language=kotlin">Search</a>
+     */
+    inner class Search {
+        fun searchMessages() {
+            val offset = 0
+            val limit = 10
+            val query = "supercalifragilisticexpialidocious"
+            val channelFilter: FilterObject = Filters.`in`("members", listOf("john"))
+            val messageFilter: FilterObject = Filters.autocomplete("text", query)
+
+            client.searchMessages(
+                SearchMessagesRequest(
+                    offset,
+                    limit,
+                    channelFilter,
+                    messageFilter
+                )
+            ).enqueue {
+                if (it.isSuccess) {
+                    val messages: List<Message> = it.data()
+                } else {
+                    Log.e(TAG, String.format("There was an error %s", it.error(), it.error().cause))
+                }
             }
         }
     }
