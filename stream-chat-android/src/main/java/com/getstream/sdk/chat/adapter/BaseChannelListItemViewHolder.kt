3a74aad70d3e343f218ebcfb68f949575cdbc089
Jc Miñarro <josecarlos.minarro@gmail.com>
2020-12-07 16:47:40 +0100

    Update argument names

--- stream-chat-android/src/main/java/com/getstream/sdk/chat/adapter/BaseChannelListItemViewHolder.kt
+++ stream-chat-android/src/main/java/com/getstream/sdk/chat/adapter/BaseChannelListItemViewHolder.kt
@@ -9,7 +9,7 @@ import io.getstream.chat.android.client.models.Channel
 public abstract class BaseChannelListItemViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
     public abstract fun bind(channelState: Channel, position: Int, payloads: ChannelItemPayloadDiff)
     public abstract fun setStyle(style: ChannelListViewStyle)
-    public abstract fun setUserClickListener(l: ChannelListView.UserClickListener?)
-    public abstract fun setChannelClickListener(l: ChannelListView.ChannelClickListener?)
-    public abstract fun setChannelLongClickListener(l: ChannelListView.ChannelClickListener?)
+    public abstract fun setUserClickListener(userClickListener: ChannelListView.UserClickListener?)
+    public abstract fun setChannelClickListener(channelClickListener: ChannelListView.ChannelClickListener?)
+    public abstract fun setChannelLongClickListener(channelClickListener: ChannelListView.ChannelClickListener?)
 }
