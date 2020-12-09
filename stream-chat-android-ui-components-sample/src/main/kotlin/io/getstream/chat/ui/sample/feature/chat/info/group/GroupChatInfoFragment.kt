679c8bd48411cbd3c19dc6bddefd29a3bfc12a6c
Rafał Adasiewicz <adasiewicz.r@gmail.com>
2020-12-09 17:36:55 +0100

    Get rid of GroupChatInfoViewModelBinding

--- stream-chat-android-ui-components-sample/src/main/kotlin/io/getstream/chat/ui/sample/feature/chat/info/group/GroupChatInfoFragment.kt
+++ stream-chat-android-ui-components-sample/src/main/kotlin/io/getstream/chat/ui/sample/feature/chat/info/group/GroupChatInfoFragment.kt
@@ -10,11 +10,13 @@ import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import com.getstream.sdk.chat.viewmodel.ChannelHeaderViewModel
 import com.getstream.sdk.chat.viewmodel.factory.ChannelViewModelFactory
+import io.getstream.chat.android.client.ChatClient
+import io.getstream.chat.android.client.events.NotificationChannelMutesUpdatedEvent
+import io.getstream.chat.android.client.subscribeFor
 import io.getstream.chat.android.ui.messages.header.bindView
 import io.getstream.chat.ui.sample.R
 import io.getstream.chat.ui.sample.databinding.FragmentGroupChatInfoBinding
 import io.getstream.chat.ui.sample.feature.chat.ChatViewModelFactory
-import io.getstream.chat.ui.sample.feature.chat.info.ChatInfoAdapter
 import io.getstream.chat.ui.sample.feature.chat.info.ChatInfoItem
 
 class GroupChatInfoFragment : Fragment() {
@@ -38,9 +40,9 @@ class GroupChatInfoFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding.optionsRecyclerView.adapter = adapter
         headerViewModel.bindView(binding.headerView, viewLifecycleOwner)
-        viewModel.bindView(this, viewLifecycleOwner)
-        initAdapter()
+        bindGroupInfoViewModel()
     }
 
     override fun onDestroyView() {
@@ -48,28 +50,48 @@ class GroupChatInfoFragment : Fragment() {
         super.onDestroyView()
     }
 
-    fun showOptions(options: List<ChatInfoItem>) {
-        adapter.submitList(options)
+    private fun bindGroupInfoViewModel() {
+        subscribeForChannelMutesUpdatedEvents()
+        setOnClickListeners()
+
+        viewModel.channelLeftState.observe(viewLifecycleOwner) { wasLeft ->
+            if (wasLeft) {
+                findNavController().popBackStack(R.id.homeFragment, false)
+            }
+        }
+
+        viewModel.state.observe(viewLifecycleOwner) { state ->
+            val members = if (state.shouldExpandMembers != false) {
+                state.members.map { ChatInfoItem.MemberItem(it) }
+            } else {
+                state.members.take(GroupChatInfoViewModel.COLLAPSED_MEMBERS_COUNT)
+                    .map { ChatInfoItem.MemberItem(it) } + ChatInfoItem.MembersSeparator(state.membersToShowCount)
+            }
+            adapter.submitList(
+                members +
+                    listOf(
+                        ChatInfoItem.Separator,
+                        ChatInfoItem.ChannelName(state.channelName),
+                        ChatInfoItem.Option.Stateful.Mute(isChecked = state.channelMuted),
+                        ChatInfoItem.Option.SharedMedia,
+                        ChatInfoItem.Option.SharedFiles,
+                        ChatInfoItem.Option.LeaveGroup,
+                    )
+            )
+        }
     }
 
-    fun setMembersSeparatorClickListener(listener: GroupChatInfoAdapter.MembersSeparatorClickListener) {
-        adapter.setMembersSeparatorClickListener(listener)
-    }
-
-    fun setNameChangedListener(listener: GroupChatInfoAdapter.NameChangedListener) {
-        adapter.setNameChangedListener(listener)
-    }
-
-    fun setChatInfoStatefulOptionChangedListener(listener: ChatInfoAdapter.ChatInfoStatefulOptionChangedListener?) {
-        adapter.setChatInfoStatefulOptionChangedListener(listener)
-    }
-
-    fun navigateUpToHome() {
-        findNavController().popBackStack(R.id.homeFragment, false)
-    }
-
-    private fun initAdapter() {
-        binding.optionsRecyclerView.adapter = adapter
+    private fun setOnClickListeners() {
+        adapter.setChatInfoStatefulOptionChangedListener { option, isChecked ->
+            viewModel.onEvent(
+                when (option) {
+                    is ChatInfoItem.Option.Stateful.Mute -> GroupChatInfoViewModel.Event.MuteChannelClicked(
+                        isChecked
+                    )
+                    else -> throw IllegalStateException("Chat info option $option is not supported!")
+                }
+            )
+        }
         adapter.setChatInfoOptionClickListener { option ->
             when (option) {
                 ChatInfoItem.Option.SharedMedia -> Unit // TODO: Not supported yet
@@ -77,6 +99,14 @@ class GroupChatInfoFragment : Fragment() {
                 ChatInfoItem.Option.LeaveGroup -> viewModel.onEvent(GroupChatInfoViewModel.Event.LeaveChannel)
                 else -> throw IllegalStateException("Group chat info option $option is not supported!")
             }
+        }
+        adapter.setMembersSeparatorClickListener { viewModel.onEvent(GroupChatInfoViewModel.Event.MembersSeparatorClick) }
+        adapter.setNameChangedListener { viewModel.onEvent(GroupChatInfoViewModel.Event.NameChanged(it)) }
+    }
+
+    private fun subscribeForChannelMutesUpdatedEvents() {
+        ChatClient.instance().subscribeFor<NotificationChannelMutesUpdatedEvent>(viewLifecycleOwner) {
+            viewModel.onEvent(GroupChatInfoViewModel.Event.ChannelMutesUpdated(it.me.channelMutes))
         }
     }
 }
