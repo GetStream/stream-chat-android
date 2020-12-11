75797128d71ab013cebfcb9d723e39d5e1b3ff4d
Márton Braun <marton@getstream.io>
2020-12-11 15:24:16 +0100

    Add more samples for MessagePreviewView to cover different cases

--- stream-chat-android-ui-components-sample/src/main/kotlin/io/getstream/chat/ui/sample/feature/component_browser/messagepreview/ComponentBrowserMessagePreviewFragment.kt
+++ stream-chat-android-ui-components-sample/src/main/kotlin/io/getstream/chat/ui/sample/feature/component_browser/messagepreview/ComponentBrowserMessagePreviewFragment.kt
@@ -5,7 +5,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import io.getstream.chat.android.client.models.Attachment
+import io.getstream.chat.android.client.models.Channel
 import io.getstream.chat.android.client.models.Message
+import io.getstream.chat.android.client.models.name
 import io.getstream.chat.ui.sample.databinding.FragmentComponentBrowserMessagePreviewBinding
 import io.getstream.chat.ui.sample.feature.component_browser.utils.randomUser
 import java.util.Date
@@ -32,12 +35,88 @@ class ComponentBrowserMessagePreviewFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        binding.messagePreviewView.setMessage(
+        binding.messagePreviewViewDirectMessage.setMessage(
             Message(
                 id = "",
                 user = randomUser(),
-                createdAt = Date(2020, 7, 15, 14, 22),
+                createdAt = Date(120, 7, 15, 14, 22),
                 text = "Hello world, how are you doing?",
+                channel = Channel(
+                    memberCount = 2,
+                ).apply {
+                    name = "Direct Message"
+                }
+            )
+        )
+
+        binding.messagePreviewViewInChannel.setMessage(
+            Message(
+                id = "",
+                user = randomUser(),
+                createdAt = Date(120, 7, 15, 14, 22),
+                text = "Hello world, how are you doing?",
+                channel = Channel(
+                    memberCount = 3,
+                ).apply {
+                    name = "General"
+                }
+            )
+        )
+
+        binding.messagePreviewViewWithImageAttachment.setMessage(
+            Message(
+                id = "",
+                user = randomUser(),
+                createdAt = Date(120, 7, 15, 14, 22),
+                text = "Hello world, how are you doing?",
+                attachments = mutableListOf(
+                    Attachment(
+                        type = "image",
+                        name = "DSC_20201125.jpg",
+                    ),
+                ),
+            )
+        )
+
+        binding.messagePreviewViewWithFileAttachment.setMessage(
+            Message(
+                id = "",
+                user = randomUser(),
+                createdAt = Date(120, 7, 15, 14, 22),
+                text = "Hello world, how are you doing?",
+                attachments = mutableListOf(
+                    Attachment(
+                        type = "file",
+                        name = "my-important-document.pdf",
+                        mimeType = "image/jpeg"
+                    ),
+                ),
+            )
+        )
+
+        binding.messagePreviewViewWithMentions.setMessage(
+            Message(
+                id = "",
+                user = randomUser(),
+                createdAt = Date(120, 7, 15, 14, 22),
+                text = "Hey @James, what's @James up to?",
+            ),
+            currentUserMention = "@James"
+        )
+
+        binding.messagePreviewViewWithLongText.setMessage(
+            Message(
+                id = "",
+                user = randomUser().apply {
+                    name = "Mr Someone Longname"
+                },
+                createdAt = Date(120, 7, 15, 14, 22),
+                text = "This is a really long message which will not fit on a single line in the preview",
+                channel = Channel(
+                    memberCount = 3,
+                ).apply {
+                    name = "General Team Chat"
+                }
             )
         )
     }
