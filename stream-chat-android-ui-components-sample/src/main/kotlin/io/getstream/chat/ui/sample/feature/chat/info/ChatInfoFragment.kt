5a0a3f60c8ba94465728ad7f5ec9239ea00027fb
Rafał Adasiewicz <adasiewicz.r@gmail.com>
2020-12-15 11:20:50 +0100

    Remove ChatInfoViewModel::bindView function

--- stream-chat-android-ui-components-sample/src/main/kotlin/io/getstream/chat/ui/sample/feature/chat/info/ChatInfoFragment.kt
+++ stream-chat-android-ui-components-sample/src/main/kotlin/io/getstream/chat/ui/sample/feature/chat/info/ChatInfoFragment.kt
@@ -8,6 +8,9 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
+import io.getstream.chat.android.client.ChatClient
+import io.getstream.chat.android.client.events.NotificationChannelMutesUpdatedEvent
+import io.getstream.chat.android.client.subscribeFor
 import io.getstream.chat.ui.sample.R
 import io.getstream.chat.ui.sample.common.getFragmentManager
 import io.getstream.chat.ui.sample.common.initToolbar
@@ -38,8 +41,8 @@ class ChatInfoFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         initToolbar(binding.toolbar)
-        initAdapter()
-        viewModel.bindView(this, viewLifecycleOwner)
+        binding.optionsRecyclerView.adapter = adapter
+        bindChatInfoViewModel()
     }
 
     override fun onDestroyView() {
@@ -47,20 +50,47 @@ class ChatInfoFragment : Fragment() {
         super.onDestroyView()
     }
 
-    fun showOptions(options: List<ChatInfoItem>) {
-        adapter.submitList(options)
+    private fun bindChatInfoViewModel() {
+        subscribeForChannelMutesUpdatedEvents()
+        setOnClickListeners()
+
+        viewModel.channelDeletedState.observe(viewLifecycleOwner) { isDeleted ->
+            if (isDeleted) {
+                findNavController().popBackStack(R.id.homeFragment, false)
+            }
+        }
+
+        viewModel.state.observe(viewLifecycleOwner) { state ->
+            adapter.submitList(
+                listOf(
+                    ChatInfoItem.MemberItem(state.member),
+                    ChatInfoItem.Separator,
+                    ChatInfoItem.Option.Stateful.Notifications(isChecked = state.notificationsEnabled),
+                    ChatInfoItem.Option.Stateful.MuteUser(isChecked = state.isMemberMuted),
+                    ChatInfoItem.Option.Stateful.Block(isChecked = state.isMemberBlocked),
+                    ChatInfoItem.Option.SharedMedia,
+                    ChatInfoItem.Option.SharedFiles,
+                    ChatInfoItem.Option.SharedGroups,
+                    ChatInfoItem.Separator,
+                    ChatInfoItem.Option.DeleteConversation,
+                )
+            )
+        }
     }
 
-    fun setChatInfoStatefulOptionChangedListener(listener: ChatInfoAdapter.ChatInfoStatefulOptionChangedListener?) {
-        adapter.setChatInfoStatefulOptionChangedListener(listener)
-    }
-
-    fun navigateUpToHome() {
-        findNavController().popBackStack(R.id.homeFragment, false)
-    }
-
-    private fun initAdapter() {
-        binding.optionsRecyclerView.adapter = adapter
+    private fun setOnClickListeners() {
+        adapter.setChatInfoStatefulOptionChangedListener { option, isChecked ->
+            viewModel.onEvent(
+                when (option) {
+                    is ChatInfoItem.Option.Stateful.Notifications -> ChatInfoViewModel.Event.OptionNotificationClicked(
+                        isChecked
+                    )
+                    is ChatInfoItem.Option.Stateful.MuteUser -> ChatInfoViewModel.Event.OptionMuteUserClicked(isChecked)
+                    is ChatInfoItem.Option.Stateful.Block -> ChatInfoViewModel.Event.OptionBlockUserClicked(isChecked)
+                    else -> throw IllegalStateException("Chat info option $option is not supported!")
+                }
+            )
+        }
         adapter.setChatInfoOptionClickListener { option ->
             when (option) {
                 ChatInfoItem.Option.SharedMedia -> Unit // TODO: Not supported yet
@@ -86,6 +116,12 @@ class ChatInfoFragment : Fragment() {
                 }
                 else -> throw IllegalStateException("Chat info option $option is not supported!")
             }
+        }
+    }
+
+    private fun subscribeForChannelMutesUpdatedEvents() {
+        ChatClient.instance().subscribeFor<NotificationChannelMutesUpdatedEvent>(viewLifecycleOwner) {
+            viewModel.onEvent(ChatInfoViewModel.Event.ChannelMutesUpdated(it.me.channelMutes))
         }
     }
 }
