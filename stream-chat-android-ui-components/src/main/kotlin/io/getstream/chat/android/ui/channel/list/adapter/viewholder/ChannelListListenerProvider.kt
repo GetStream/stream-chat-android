b388434a5eea8c3f66edbb59d6c2054188885668
Márton Braun <marton@getstream.io>
2020-12-17 11:34:52 +0100

    Remove some duplicated functionality from the UI components module

--- stream-chat-android-ui-components/src/main/kotlin/io/getstream/chat/android/ui/channel/list/adapter/viewholder/ChannelListListenerProvider.kt
+++ stream-chat-android-ui-components/src/main/kotlin/io/getstream/chat/android/ui/channel/list/adapter/viewholder/ChannelListListenerProvider.kt
@@ -1,6 +1,7 @@
 package io.getstream.chat.android.ui.channel.list.adapter.viewholder
 
 import androidx.recyclerview.widget.RecyclerView
+import com.getstream.sdk.chat.utils.ListenerDelegate
 import io.getstream.chat.android.ui.channel.list.ChannelListView
 
 public class ChannelListListenerProvider(
@@ -11,37 +12,37 @@ public class ChannelListListenerProvider(
     userClickListener: ChannelListView.UserClickListener = ChannelListView.UserClickListener.DEFAULT,
     swipeListener: ChannelListView.SwipeListener = ChannelListView.SwipeListener.DEFAULT
 ) {
-    public var channelClickListener: ChannelListView.ChannelClickListener by Provider(channelClickListener) { getListener ->
+    public var channelClickListener: ChannelListView.ChannelClickListener by ListenerDelegate(channelClickListener) { getListener ->
         ChannelListView.ChannelClickListener { channel ->
             getListener().onClick(channel)
         }
     }
 
-    public var channelLongClickListener: ChannelListView.ChannelClickListener by Provider(channelLongClickListener) { getListener ->
+    public var channelLongClickListener: ChannelListView.ChannelClickListener by ListenerDelegate(channelLongClickListener) { getListener ->
         ChannelListView.ChannelClickListener { channel ->
             getListener().onClick(channel)
         }
     }
 
-    public var deleteClickListener: ChannelListView.ChannelClickListener by Provider(deleteClickListener) { getListener ->
+    public var deleteClickListener: ChannelListView.ChannelClickListener by ListenerDelegate(deleteClickListener) { getListener ->
         ChannelListView.ChannelClickListener { channel ->
             getListener().onClick(channel)
         }
     }
 
-    public var moreOptionsClickListener: ChannelListView.ChannelClickListener by Provider(moreOptionsClickListener) { getListener ->
+    public var moreOptionsClickListener: ChannelListView.ChannelClickListener by ListenerDelegate(moreOptionsClickListener) { getListener ->
         ChannelListView.ChannelClickListener { channel ->
             getListener().onClick(channel)
         }
     }
 
-    public var userClickListener: ChannelListView.UserClickListener by Provider(userClickListener) { getListener ->
+    public var userClickListener: ChannelListView.UserClickListener by ListenerDelegate(userClickListener) { getListener ->
         ChannelListView.UserClickListener { user ->
             getListener().onUserClick(user)
         }
     }
 
-    public var swipeListener: ChannelListView.SwipeListener by Provider(swipeListener) { getDelegate ->
+    public var swipeListener: ChannelListView.SwipeListener by ListenerDelegate(swipeListener) { getDelegate ->
         object : ChannelListView.SwipeListener {
             override fun onSwipeStarted(viewHolder: RecyclerView.ViewHolder, adapterPosition: Int, x: Float, y: Float) {
                 getDelegate().onSwipeStarted(viewHolder, adapterPosition, x, y)
