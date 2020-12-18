f57dca4c1e57f8b4bacfa27afdc366c7288ac15a
Márton Braun <marton@getstream.io>
2020-12-18 13:14:26 +0100

    Use LocalTime as the parameter for formatTime

--- stream-chat-android-ui-common/src/main/kotlin/com/getstream/sdk/chat/utils/DateFormatter.kt
+++ stream-chat-android-ui-common/src/main/kotlin/com/getstream/sdk/chat/utils/DateFormatter.kt
@@ -3,11 +3,12 @@ package com.getstream.sdk.chat.utils
 import android.content.Context
 import io.getstream.chat.android.core.internal.InternalStreamChatApi
 import org.threeten.bp.LocalDateTime
+import org.threeten.bp.LocalTime
 import java.util.Date
 
 public interface DateFormatter {
     public fun formatDate(localDateTime: LocalDateTime?): String
-    public fun formatTime(localDateTime: LocalDateTime?): String
+    public fun formatTime(localTime: LocalTime?): String
 
     public companion object {
         @JvmStatic
@@ -28,5 +29,5 @@ public fun DateFormatter.formatDate(date: Date?): String {
  */
 @InternalStreamChatApi
 public fun DateFormatter.formatTime(date: Date?): String {
-    return formatTime(date?.let(DateConverter::toLocalDateTime))
+    return formatTime(date?.let(DateConverter::toLocalTime))
 }
--- stream-chat-android-ui-common/src/main/kotlin/com/getstream/sdk/chat/utils/LegacyDateFormatter.kt
+++ stream-chat-android-ui-common/src/main/kotlin/com/getstream/sdk/chat/utils/LegacyDateFormatter.kt
@@ -1,14 +1,33 @@
 package com.getstream.sdk.chat.utils
 
 import org.threeten.bp.LocalDateTime
+import org.threeten.bp.LocalTime
 import java.util.Date
 
 public abstract class LegacyDateFormatter : DateFormatter {
+    /**
+     * Formats the given date as a String.
+     */
     public abstract fun formatDate(date: Date?): String
-    public abstract fun formatTime(date: Date?): String
+
+    /**
+     * Formats the given time as a String.
+     *
+     * Note that only the hour, minute, and hour values contained by the parameter
+     * [Date] object are valid.
+     */
+    public abstract fun formatTime(time: Date?): String
+
     final override fun formatDate(localDateTime: LocalDateTime?): String =
         formatDate(localDateTime?.let(DateConverter::toDate))
+    final override fun formatTime(localTime: LocalTime?): String =
+        formatTime(localTime?.let(::toTime))
 
-    final override fun formatTime(localDateTime: LocalDateTime?): String =
-        formatTime(localDateTime?.let(DateConverter::toDate))
+    private fun toTime(localTime: LocalTime): Date {
+        return Date().apply {
+            hours = localTime.hour
+            minutes = localTime.minute
+            seconds = localTime.second
+        }
+    }
 }
