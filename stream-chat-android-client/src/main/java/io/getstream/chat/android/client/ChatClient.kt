5d2ea9390ece48d2d9aa4854931d5e7691c2c49d
Rafał Adasiewicz <adasiewicz.r@gmail.com>
2020-12-21 13:34:31 +0100

    Extract different attachment types to constants

--- stream-chat-android-client/src/main/java/io/getstream/chat/android/client/ChatClient.kt
+++ stream-chat-android-client/src/main/java/io/getstream/chat/android/client/ChatClient.kt
@@ -25,6 +25,8 @@ import io.getstream.chat.android.client.errors.ChatError
 import io.getstream.chat.android.client.events.ChatEvent
 import io.getstream.chat.android.client.events.ConnectedEvent
 import io.getstream.chat.android.client.events.DisconnectedEvent
+import io.getstream.chat.android.client.extensions.ATTACHMENT_TYPE_FILE
+import io.getstream.chat.android.client.extensions.ATTACHMENT_TYPE_IMAGE
 import io.getstream.chat.android.client.extensions.isImage
 import io.getstream.chat.android.client.extensions.isValid
 import io.getstream.chat.android.client.helpers.QueryChannelsPostponeHelper
@@ -449,7 +451,7 @@ public class ChatClient internal constructor(
         offset: Int,
         limit: Int
     ): Call<List<AttachmentWithDate>> =
-        getAttachments(channelType, channelId, offset, limit, "file")
+        getAttachments(channelType, channelId, offset, limit, ATTACHMENT_TYPE_FILE)
 
     public fun getImageAttachments(
         channelType: String,
@@ -457,7 +459,7 @@ public class ChatClient internal constructor(
         offset: Int,
         limit: Int
     ): Call<List<AttachmentWithDate>> =
-        getAttachments(channelType, channelId, offset, limit, "image")
+        getAttachments(channelType, channelId, offset, limit, ATTACHMENT_TYPE_IMAGE)
 
     private fun getAttachments(
         channelType: String,
@@ -471,7 +473,7 @@ public class ChatClient internal constructor(
 
         return searchMessages(SearchMessagesRequest(offset, limit, channelFilter, messageFilter)).map { messages ->
             messages.flatMap { message ->
-                if (type == "image") {
+                if (type == ATTACHMENT_TYPE_IMAGE) {
                     message.attachments.filter { attachment -> attachment.isImage }
                 } else {
                     message.attachments
--- stream-chat-android-client/src/main/java/io/getstream/chat/android/client/extensions/AttachmentExtensions.kt
+++ stream-chat-android-client/src/main/java/io/getstream/chat/android/client/extensions/AttachmentExtensions.kt
@@ -2,5 +2,8 @@ package io.getstream.chat.android.client.extensions
 
 import io.getstream.chat.android.client.models.Attachment
 
+internal const val ATTACHMENT_TYPE_IMAGE = "image"
+internal const val ATTACHMENT_TYPE_FILE = "file"
+
 internal val Attachment.isImage: Boolean
-    get() = mimeType?.startsWith("image") ?: false
+    get() = mimeType?.startsWith(ATTACHMENT_TYPE_IMAGE) ?: false
