60200246d0ed12e721388dac980a6acdbd17b9f8
Oleg Kuzmin <oleg@getstream.io>
2020-12-22 17:41:59 +0300

    Add layout and binding logic to giphyVh

--- stream-chat-android-ui-components/src/main/kotlin/io/getstream/chat/android/ui/messages/adapter/viewholder/GiphyViewHolder.kt
+++ stream-chat-android-ui-components/src/main/kotlin/io/getstream/chat/android/ui/messages/adapter/viewholder/GiphyViewHolder.kt
@@ -3,8 +3,10 @@ package io.getstream.chat.android.ui.messages.adapter.viewholder
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import com.getstream.sdk.chat.adapter.MessageListItem
+import com.getstream.sdk.chat.enums.GiphyAction
 import io.getstream.chat.android.ui.databinding.StreamUiItemMessageGiphyBinding
 import io.getstream.chat.android.ui.messages.adapter.BaseMessageItemViewHolder
+import io.getstream.chat.android.ui.messages.adapter.ListenerContainer
 import io.getstream.chat.android.ui.messages.adapter.MessageListItemPayloadDiff
 
 public class GiphyViewHolder(
@@ -15,9 +17,37 @@ public class GiphyViewHolder(
         ),
         parent,
         false
-    )
+    ),
+    private val listenerContainer: ListenerContainer? = null
 ) : BaseMessageItemViewHolder<MessageListItem.MessageItem>(binding.root) {
     override fun bindData(data: MessageListItem.MessageItem, diff: MessageListItemPayloadDiff?) {
+        data.message.attachments.firstOrNull()?.let(binding.mediaAttachmentView::showAttachment)
 
+        binding.giphyTextLabel.text = data.message.text
+
+        listenerContainer?.also { listeners ->
+            binding.cardView.setOnLongClickListener {
+                listeners.messageLongClickListener.onMessageLongClick(data.message)
+                true
+            }
+            binding.cancelButton.setOnClickListener {
+                listeners.giphySendListener.onGiphySend(
+                    data.message,
+                    GiphyAction.CANCEL
+                )
+            }
+            binding.sendButton.setOnClickListener {
+                listeners.giphySendListener.onGiphySend(
+                    data.message,
+                    GiphyAction.SEND
+                )
+            }
+            binding.nextButton.setOnClickListener {
+                listeners.giphySendListener.onGiphySend(
+                    data.message,
+                    GiphyAction.SHUFFLE
+                )
+            }
+        }
     }
 }
