bba0d076059f91f23dba33a5d1de9f68b624e87c
Márton Braun <marton@getstream.io>
2020-12-23 10:13:53 +0100

    Fix spacing when avatar is hidden

--- stream-chat-android-ui-components/src/main/kotlin/io/getstream/chat/android/ui/messages/adapter/viewholder/decorator/AvatarDecorator.kt
+++ stream-chat-android-ui-components/src/main/kotlin/io/getstream/chat/android/ui/messages/adapter/viewholder/decorator/AvatarDecorator.kt
@@ -1,9 +1,6 @@
 package io.getstream.chat.android.ui.messages.adapter.viewholder.decorator
 
-import android.view.View
-import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.view.isVisible
-import androidx.core.view.updateLayoutParams
 import com.getstream.sdk.chat.adapter.MessageListItem
 import io.getstream.chat.android.ui.avatar.AvatarView
 import io.getstream.chat.android.ui.messages.adapter.viewholder.GiphyViewHolder
@@ -12,61 +9,50 @@ import io.getstream.chat.android.ui.messages.adapter.viewholder.OnlyFileAttachme
 import io.getstream.chat.android.ui.messages.adapter.viewholder.OnlyMediaAttachmentsViewHolder
 import io.getstream.chat.android.ui.messages.adapter.viewholder.PlainTextWithFileAttachmentsViewHolder
 import io.getstream.chat.android.ui.messages.adapter.viewholder.PlainTextWithMediaAttachmentsViewHolder
-import io.getstream.chat.android.ui.utils.extensions.dpToPx
 
 internal class AvatarDecorator : BaseDecorator() {
     override fun decorateOnlyMediaAttachmentsMessage(
         viewHolder: OnlyMediaAttachmentsViewHolder,
         data: MessageListItem.MessageItem
     ) {
-        setupAvatar(viewHolder.binding.avatarView, viewHolder.binding.mediaAttachmentsGroupView, data)
+        setupAvatar(viewHolder.binding.avatarView, data)
     }
 
     override fun decoratePlainTextMessage(viewHolder: MessagePlainTextViewHolder, data: MessageListItem.MessageItem) {
-        setupAvatar(viewHolder.binding.avatarView, viewHolder.binding.messageText, data)
+        setupAvatar(viewHolder.binding.avatarView, data)
     }
 
     override fun decoratePlainTextWithMediaAttachmentsMessage(
         viewHolder: PlainTextWithMediaAttachmentsViewHolder,
         data: MessageListItem.MessageItem
     ) {
-        setupAvatar(viewHolder.binding.avatarView, viewHolder.binding.mediaAttachmentsGroupView, data)
+        setupAvatar(viewHolder.binding.avatarView, data)
     }
 
     override fun decorateOnlyFileAttachmentsMessage(
         viewHolder: OnlyFileAttachmentsViewHolder,
         data: MessageListItem.MessageItem
     ) {
-        setupAvatar(viewHolder.binding.avatarView, viewHolder.binding.fileAttachmentsView, data)
+        setupAvatar(viewHolder.binding.avatarView, data)
     }
 
     override fun decoratePlainTextWithFileAttachmentsMessage(
         viewHolder: PlainTextWithFileAttachmentsViewHolder,
         data: MessageListItem.MessageItem
     ) {
-        setupAvatar(viewHolder.binding.avatarView, viewHolder.binding.fileAttachmentsView, data)
+        setupAvatar(viewHolder.binding.avatarView, data)
     }
 
     override fun decorateGiphyMessage(viewHolder: GiphyViewHolder, data: MessageListItem.MessageItem) = Unit
 
-    private fun setupAvatar(avatarView: AvatarView, viewNextFromAvatar: View, data: MessageListItem.MessageItem) {
+    private fun setupAvatar(avatarView: AvatarView, data: MessageListItem.MessageItem) {
         if (data.isTheirs) {
             avatarView.setUserData(data.message.user)
         }
-        viewNextFromAvatar.updateLayoutParams<ConstraintLayout.LayoutParams> {
-            marginStart = 0
-        }
-        when {
-            data.isTheirs && data.positions.contains(MessageListItem.Position.BOTTOM) -> avatarView.isVisible = true
-            data.isTheirs -> {
-                avatarView.isVisible = false
-                viewNextFromAvatar.updateLayoutParams<ConstraintLayout.LayoutParams> { marginStart = AVATAR_SIDE_SPACE }
-            }
-            else -> avatarView.isVisible = false
-        }
-    }
 
-    companion object {
-        private val AVATAR_SIDE_SPACE = 48.dpToPx()
+        avatarView.isVisible = when {
+            data.isTheirs && data.positions.contains(MessageListItem.Position.BOTTOM) -> true
+            else -> false
+        }
     }
 }
