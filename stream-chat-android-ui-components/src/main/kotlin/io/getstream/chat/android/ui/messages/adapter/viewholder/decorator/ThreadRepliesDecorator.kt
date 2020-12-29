647f4def9d2aadd0e8837177517d0025d66eab32
Samuel Urbanowicz <samiuelson@gmail.com>
2020-12-29 18:15:01 +0100

    Decorate media messages

--- stream-chat-android-ui-components/src/main/kotlin/io/getstream/chat/android/ui/messages/adapter/viewholder/decorator/ThreadRepliesDecorator.kt
+++ stream-chat-android-ui-components/src/main/kotlin/io/getstream/chat/android/ui/messages/adapter/viewholder/decorator/ThreadRepliesDecorator.kt
@@ -5,7 +5,9 @@ import androidx.core.view.isVisible
 import com.getstream.sdk.chat.adapter.MessageListItem
 import com.getstream.sdk.chat.utils.extensions.updateConstraints
 import io.getstream.chat.android.ui.R
+import io.getstream.chat.android.ui.databinding.StreamUiItemMessageGiphyBinding
 import io.getstream.chat.android.ui.databinding.StreamUiItemMessagePlainTextBinding
+import io.getstream.chat.android.ui.databinding.StreamUiItemMessagePlainTextWithMediaAttachmentsBinding
 import io.getstream.chat.android.ui.messages.adapter.viewholder.GiphyViewHolder
 import io.getstream.chat.android.ui.messages.adapter.viewholder.MessagePlainTextViewHolder
 import io.getstream.chat.android.ui.messages.adapter.viewholder.OnlyFileAttachmentsViewHolder
@@ -27,7 +29,9 @@ internal class ThreadRepliesDecorator : BaseDecorator() {
     override fun decoratePlainTextWithMediaAttachmentsMessage(
         viewHolder: PlainTextWithMediaAttachmentsViewHolder,
         data: MessageListItem.MessageItem
-    ) = Unit
+    ) {
+        setupThreadRepliesView(viewHolder.binding, data)
+    }
 
     override fun decorateOnlyMediaAttachmentsMessage(
         viewHolder: OnlyMediaAttachmentsViewHolder,
@@ -38,7 +42,55 @@ internal class ThreadRepliesDecorator : BaseDecorator() {
         setupThreadRepliesView(viewHolder.binding, data)
     }
 
-    override fun decorateGiphyMessage(viewHolder: GiphyViewHolder, data: MessageListItem.MessageItem) = Unit
+    override fun decorateGiphyMessage(viewHolder: GiphyViewHolder, data: MessageListItem.MessageItem) {
+        setupThreadRepliesView(viewHolder.binding, data)
+    }
+
+    private fun setupThreadRepliesView(binding: StreamUiItemMessageGiphyBinding, data: MessageListItem.MessageItem) {
+        val replyCount = data.message.replyCount
+        if (replyCount == 0) {
+            binding.threadRepliesFootnote.root.isVisible = false
+            return
+        }
+
+        if (data.isTheirs) {
+            binding.threadRepliesFootnote.threadsOrnamentLeft.isVisible = true
+            binding.threadRepliesFootnote.threadsOrnamentRight.isVisible = false
+        } else {
+            binding.threadRepliesFootnote.threadsOrnamentLeft.isVisible = false
+            binding.threadRepliesFootnote.threadsOrnamentRight.isVisible = true
+        }
+
+        binding.root.updateConstraints {
+            val threadRepliesFootnoteId = binding.threadRepliesFootnote.root.id
+            val footnoteId = binding.footnote.root.id
+
+            clear(threadRepliesFootnoteId, ConstraintSet.START)
+            clear(threadRepliesFootnoteId, ConstraintSet.LEFT)
+            clear(threadRepliesFootnoteId, ConstraintSet.END)
+            clear(threadRepliesFootnoteId, ConstraintSet.RIGHT)
+            clear(footnoteId, ConstraintSet.END)
+            clear(footnoteId, ConstraintSet.LEFT)
+            clear(footnoteId, ConstraintSet.START)
+            clear(footnoteId, ConstraintSet.RIGHT)
+
+            if (data.isTheirs) {
+                connect(threadRepliesFootnoteId, ConstraintSet.LEFT, binding.cardView.id, ConstraintSet.LEFT)
+                connect(footnoteId, ConstraintSet.LEFT, threadRepliesFootnoteId, ConstraintSet.RIGHT)
+            } else {
+                connect(threadRepliesFootnoteId, ConstraintSet.RIGHT, binding.cardView.id, ConstraintSet.RIGHT)
+                connect(footnoteId, ConstraintSet.RIGHT, threadRepliesFootnoteId, ConstraintSet.LEFT)
+            }
+        }
+
+        binding.root.isVisible = true
+        binding.threadRepliesFootnote.threadRepliesButton.text =
+            binding.threadRepliesFootnote.threadRepliesButton.resources.getQuantityString(
+                R.plurals.stream_ui_thread_messages_indicator,
+                replyCount,
+                replyCount
+            )
+    }
 
     private fun setupThreadRepliesView(
         binding: StreamUiItemMessagePlainTextBinding,
@@ -77,6 +129,56 @@ internal class ThreadRepliesDecorator : BaseDecorator() {
             }
         }
 
+        binding.threadRepliesFootnote.threadRepliesButton.text =
+            binding.threadRepliesFootnote.threadRepliesButton.resources.getQuantityString(
+                R.plurals.stream_ui_thread_messages_indicator,
+                replyCount,
+                replyCount
+            )
+    }
+
+
+    private fun setupThreadRepliesView(
+        binding: StreamUiItemMessagePlainTextWithMediaAttachmentsBinding,
+        data: MessageListItem.MessageItem
+    ) {
+        val replyCount = data.message.replyCount
+        if (replyCount == 0) {
+            binding.threadRepliesFootnote.root.isVisible = false
+            return
+        }
+
+        if (data.isTheirs) {
+            binding.threadRepliesFootnote.threadsOrnamentLeft.isVisible = true
+            binding.threadRepliesFootnote.threadsOrnamentRight.isVisible = false
+        } else {
+            binding.threadRepliesFootnote.threadsOrnamentLeft.isVisible = false
+            binding.threadRepliesFootnote.threadsOrnamentRight.isVisible = true
+        }
+
+        binding.root.updateConstraints {
+            val threadRepliesFootnoteId = binding.threadRepliesFootnote.root.id
+            val footnoteId = binding.footnote.root.id
+
+            clear(threadRepliesFootnoteId, ConstraintSet.START)
+            clear(threadRepliesFootnoteId, ConstraintSet.LEFT)
+            clear(threadRepliesFootnoteId, ConstraintSet.END)
+            clear(threadRepliesFootnoteId, ConstraintSet.RIGHT)
+            clear(footnoteId, ConstraintSet.END)
+            clear(footnoteId, ConstraintSet.LEFT)
+            clear(footnoteId, ConstraintSet.START)
+            clear(footnoteId, ConstraintSet.RIGHT)
+
+            if (data.isTheirs) {
+                connect(threadRepliesFootnoteId, ConstraintSet.LEFT, binding.messageContainer.id, ConstraintSet.LEFT)
+                connect(footnoteId, ConstraintSet.LEFT, threadRepliesFootnoteId, ConstraintSet.RIGHT)
+            } else {
+                connect(threadRepliesFootnoteId, ConstraintSet.RIGHT, binding.messageContainer.id, ConstraintSet.RIGHT)
+                connect(footnoteId, ConstraintSet.RIGHT, threadRepliesFootnoteId, ConstraintSet.LEFT)
+            }
+        }
+
+        binding.root.isVisible = true
         binding.threadRepliesFootnote.threadRepliesButton.text =
             binding.threadRepliesFootnote.threadRepliesButton.resources.getQuantityString(
                 R.plurals.stream_ui_thread_messages_indicator,
