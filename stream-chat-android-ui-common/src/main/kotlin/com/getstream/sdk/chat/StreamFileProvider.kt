0be582c03d5016cea6c59e0b95eded33f4ce84ba
Jc Miñarro <josecarlos.minarro@gmail.com>
2021-01-04 16:47:35 +0100

    Create method to obtain fileUri with our FileProvider

--- stream-chat-android-ui-common/src/main/kotlin/com/getstream/sdk/chat/StreamFileProvider.kt
+++ stream-chat-android-ui-common/src/main/kotlin/com/getstream/sdk/chat/StreamFileProvider.kt
@@ -1,5 +1,22 @@
 package com.getstream.sdk.chat
 
+import android.content.ComponentName
+import android.content.Context
+import android.net.Uri
 import androidx.core.content.FileProvider
+import java.io.File
 
-public class StreamFileProvider : FileProvider()
+public class StreamFileProvider : FileProvider() {
+
+    public companion object {
+
+        private fun getFileProviderAuthority(context: Context): String {
+            val compName = ComponentName(context, StreamFileProvider::class.java.name)
+            val providerInfo = context.packageManager.getProviderInfo(compName, 0)
+            return providerInfo.authority
+        }
+
+        public fun getUriForFile(context: Context, file: File): Uri =
+            getUriForFile(context, getFileProviderAuthority(context), file)
+    }
+}
