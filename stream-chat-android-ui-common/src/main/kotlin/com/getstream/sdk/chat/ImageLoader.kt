f4e18fa40c22356bfb5f07a5bbd7ecfd2c77aac7
Jc Miñarro <josecarlos.minarro@gmail.com>
2021-01-04 13:07:00 +0100

    Create a method to obtain an BitmapUri

--- stream-chat-android-ui-common/src/main/kotlin/com/getstream/sdk/chat/ImageLoader.kt
+++ stream-chat-android-ui-common/src/main/kotlin/com/getstream/sdk/chat/ImageLoader.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.graphics.Bitmap
 import android.graphics.drawable.BitmapDrawable
 import android.net.Uri
+import android.os.Environment
 import android.widget.ImageView
 import androidx.annotation.DrawableRes
 import androidx.annotation.RawRes
@@ -17,6 +18,9 @@ import com.getstream.sdk.chat.coil.StreamCoil.streamImageLoader
 import io.getstream.chat.android.core.internal.InternalStreamChatApi
 import io.getstream.chat.android.core.internal.coroutines.DispatcherProvider
 import kotlinx.coroutines.withContext
+import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
 import coil.loadAny as coilLoadAny
 
 @InternalStreamChatApi
@@ -36,6 +40,29 @@ public object ImageLoader {
                 )
                 (imageResult.drawable as? BitmapDrawable)?.bitmap
             }
+    }
+
+    public suspend fun getBitmapUri(
+        context: Context,
+        url: String,
+        transformation: ImageTransformation = ImageTransformation.None
+    ): Uri? = getBitmap(context, url, transformation)?.getLocalBitmapUri(context)
+
+    private fun Bitmap.getLocalBitmapUri(context: Context): Uri? {
+        var bmpUri: Uri? = null
+        try {
+            val file = File(
+                context.getExternalFilesDir(Environment.DIRECTORY_PICTURES),
+                "share_image_" + System.currentTimeMillis() + ".png"
+            )
+            val out = FileOutputStream(file)
+            compress(Bitmap.CompressFormat.PNG, 90, out)
+            out.close()
+            bmpUri = Uri.fromFile(file)
+        } catch (e: IOException) {
+            e.printStackTrace()
+        }
+        return bmpUri
     }
 
     public fun ImageView.load(
