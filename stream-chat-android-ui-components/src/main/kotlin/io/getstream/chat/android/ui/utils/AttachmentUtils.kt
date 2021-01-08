f45bc86e5e6c02176705d0a1593bd84a59097502
Dmitrii Bychkov <mail@dbychkov.com>
2021-01-08 18:21:53 +0300

    CAS-511 Address PR comments

--- stream-chat-android-ui-components/src/main/kotlin/io/getstream/chat/android/ui/utils/AttachmentUtils.kt
+++ stream-chat-android-ui-components/src/main/kotlin/io/getstream/chat/android/ui/utils/AttachmentUtils.kt
@@ -1,6 +1,5 @@
 package io.getstream.chat.android.ui.utils
 
-import android.net.Uri
 import android.widget.ImageView
 import com.getstream.sdk.chat.ImageLoader.ImageTransformation.RoundedCorners
 import com.getstream.sdk.chat.ImageLoader.load
@@ -10,41 +9,24 @@ import com.getstream.sdk.chat.model.ModelType
 import io.getstream.chat.android.client.models.Attachment
 import io.getstream.chat.android.ui.utils.extensions.dpToPxPrecise
 
-private val THUMB_ROUNDED_CORNERS_TRANSFORMATION = RoundedCorners(3.dpToPxPrecise())
+private val FILE_THUMB_TRANSFORMATION = RoundedCorners(3.dpToPxPrecise())
 
 internal fun ImageView.loadAttachmentThumb(attachment: Attachment) {
-    loadAttachmentThumb(
-        type = attachment.type,
-        mimeType = attachment.mimeType,
-        imageThumb = attachment.imageUrl,
-        videoThumb = attachment.thumbUrl
-    )
+    with(attachment) {
+        when (type) {
+            ModelType.attach_video -> load(thumbUrl, FILE_THUMB_TRANSFORMATION)
+            ModelType.attach_image -> load(imageUrl, FILE_THUMB_TRANSFORMATION)
+            else -> load(UiUtils.getIcon(mimeType))
+        }
+    }
 }
 
 internal fun ImageView.loadAttachmentThumb(attachment: AttachmentMetaData) {
-    loadAttachmentThumb(
-        type = attachment.type,
-        mimeType = attachment.mimeType,
-        imageThumb = attachment.uri,
-        videoThumb = attachment.uri
-    )
-}
-
-private fun ImageView.loadAttachmentThumb(
-    type: String?,
-    mimeType: String?,
-    imageThumb: Any?,
-    videoThumb: Any?
-) {
-    when (type) {
-        ModelType.attach_video -> {
-            if (videoThumb is String) {
-                load(videoThumb, THUMB_ROUNDED_CORNERS_TRANSFORMATION)
-            } else if (videoThumb is Uri) {
-                loadVideoThumbnail(videoThumb, THUMB_ROUNDED_CORNERS_TRANSFORMATION)
-            }
+    with(attachment) {
+        when (type) {
+            ModelType.attach_video -> loadVideoThumbnail(uri, null, FILE_THUMB_TRANSFORMATION)
+            ModelType.attach_image -> load(uri, FILE_THUMB_TRANSFORMATION)
+            else -> load(UiUtils.getIcon(mimeType))
         }
-        ModelType.attach_image -> load(imageThumb, THUMB_ROUNDED_CORNERS_TRANSFORMATION)
-        else -> load(UiUtils.getIcon(mimeType))
     }
 }
