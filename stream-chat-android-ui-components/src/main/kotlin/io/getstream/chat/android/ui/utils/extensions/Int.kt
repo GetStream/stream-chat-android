a0b0f7a24e17e4c1e5c0c0c9f767db749cf5ad77
Oleg Kuzmin <oleg@getstream.io>
2021-01-12 10:51:31 +0100

    Use roundToInt instead of manual ceiling and casting to Int

--- stream-chat-android-ui-components/src/main/kotlin/io/getstream/chat/android/ui/utils/extensions/Int.kt
+++ stream-chat-android-ui-components/src/main/kotlin/io/getstream/chat/android/ui/utils/extensions/Int.kt
@@ -1,7 +1,7 @@
 package io.getstream.chat.android.ui.utils.extensions
 
 import android.content.res.Resources
-import kotlin.math.ceil
+import kotlin.math.roundToInt
 
-internal fun Int.dpToPx(): Int = ceil(dpToPxPrecise()).toInt()
+internal fun Int.dpToPx(): Int = dpToPxPrecise().roundToInt()
 internal fun Int.dpToPxPrecise(): Float = (this * Resources.getSystem().displayMetrics.density)
