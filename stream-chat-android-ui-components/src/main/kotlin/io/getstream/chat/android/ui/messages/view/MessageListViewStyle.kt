2f7d4774d31312f81f9fd46a11dba5cf9516c3c9
Márton Braun <marton@getstream.io>
2021-01-13 09:48:52 +0100

    Remove stream_ui_grey_light

--- stream-chat-android-ui-components/src/main/kotlin/io/getstream/chat/android/ui/messages/view/MessageListViewStyle.kt
+++ stream-chat-android-ui-components/src/main/kotlin/io/getstream/chat/android/ui/messages/view/MessageListViewStyle.kt
@@ -33,7 +33,7 @@ internal class MessageListViewStyle(context: Context, attrs: AttributeSet?) {
                 )
                 .scrollButtonRippleColor(
                     R.styleable.MessageListView_streamUiScrollButtonRippleColor,
-                    context.getColorCompat(R.color.stream_ui_grey_light)
+                    context.getColorCompat(R.color.stream_ui_white_smoke)
                 )
                 .scrollButtonBadgeColor(
                     R.styleable.MessageListView_streamUiScrollButtonBadgeColor,
