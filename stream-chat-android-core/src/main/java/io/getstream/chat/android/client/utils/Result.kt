0a4c7b6b40ad2c4801137b9f4caa423fba0909ca
Leandro Borges Ferreira <lehen01@gmail.com>
2021-01-15 13:26:28 +0100

    Fixing unit tests and deleting wrong unit tests

--- stream-chat-android-core/src/main/java/io/getstream/chat/android/client/utils/Result.kt
+++ stream-chat-android-core/src/main/java/io/getstream/chat/android/client/utils/Result.kt
@@ -31,4 +31,26 @@ public class Result<T : Any> {
     public fun error(): ChatError {
         return checkNotNull(error) { "Result is successful, not an error. Check result.isSuccess before reading the error." }
     }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as Result<*>
+
+        if (data != other.data) return false
+        if (error != other.error) return false
+        if (isSuccess != other.isSuccess) return false
+        if (isError != other.isError) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = data?.hashCode() ?: 0
+        result = 31 * result + (error?.hashCode() ?: 0)
+        result = 31 * result + isSuccess.hashCode()
+        result = 31 * result + isError.hashCode()
+        return result
+    }
 }
