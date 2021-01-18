f56cf38101914453c9ddc4b9cb942a11eb4dd4a9
Samuel Urbanowicz <samiuelson@gmail.com>
2021-01-18 16:41:44 +0100

    Setup menu options handler

--- stream-chat-android-ui-components/src/main/kotlin/io/getstream/chat/android/ui/options/AttachmentOptionsDialogFragment.kt
+++ stream-chat-android-ui-components/src/main/kotlin/io/getstream/chat/android/ui/options/AttachmentOptionsDialogFragment.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.os.bundleOf
 import io.getstream.chat.android.ui.databinding.StreamUiFragmentAttachmentOptionsBinding
 import io.getstream.chat.android.ui.view.FullScreenDialogFragment
 
@@ -12,22 +11,65 @@ internal class AttachmentOptionsDialogFragment: FullScreenDialogFragment() {
     private var _binding: StreamUiFragmentAttachmentOptionsBinding? = null
     private val binding get() = _binding!!
 
+    private var showInChatHandler: ShowInChatHandler? = null
+    private var deleteHandler: DeleteHandler? = null
+    private var replyHandler: ReplyHandler? = null
+    private var saveImageHandler: SaveImageHandler? = null
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         return StreamUiFragmentAttachmentOptionsBinding.inflate(inflater, container, false)
             .apply { _binding = this }
             .root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        binding.attachmentOptionsMenu.setReplyClickListener(object : AttachmentOptionsView.ReplyClickListener {
+            override fun onClick() {
+                replyHandler?.onClick()
+                dismiss()
+            }
+        })
+        binding.attachmentOptionsMenu.setDeleteClickListener(object : AttachmentOptionsView.DeleteClickListener {
+            override fun onClick() {
+                deleteHandler?.onClick()
+                dismiss()
+            }
+        })
+        binding.attachmentOptionsMenu.setShowInChatClickListener(object : AttachmentOptionsView.ShowInChatClickListener {
+            override fun onClick() {
+                showInChatHandler?.onClick()
+                dismiss()
+            }
+        })
+        binding.attachmentOptionsMenu.setSaveImageClickListener(object : AttachmentOptionsView.SaveImageClickListener {
+            override fun onClick() {
+                saveImageHandler?.onClick()
+                dismiss()
+            }
+        })
+        binding.root.setOnClickListener { dismiss() }
+    }
+
+    internal interface ShowInChatHandler { fun onClick(): Unit }
+    internal interface DeleteHandler { fun onClick(): Unit }
+    internal interface ReplyHandler { fun onClick(): Unit }
+    internal interface SaveImageHandler { fun onClick(): Unit }
+
     companion object {
         const val TAG = "AttachmentOptionsDialogFragment"
 
-        private const val ARG_OPTIONS_ATTACHMENT_URL = "attachmentUrl"
-
-        fun newInstance(url: String): AttachmentOptionsDialogFragment {
+        fun newInstance(
+            showInChatHandler: ShowInChatHandler? = null,
+            deleteHandler: DeleteHandler? = null,
+            replyHandler: ReplyHandler? = null,
+            saveImageHandler: SaveImageHandler? = null,
+        ): AttachmentOptionsDialogFragment {
             return AttachmentOptionsDialogFragment().apply {
-                arguments = bundleOf(ARG_OPTIONS_ATTACHMENT_URL to url)
+                this.showInChatHandler = showInChatHandler
+                this.deleteHandler = deleteHandler
+                this.replyHandler = replyHandler
+                this.saveImageHandler = saveImageHandler
             }
         }
     }
-
 }
