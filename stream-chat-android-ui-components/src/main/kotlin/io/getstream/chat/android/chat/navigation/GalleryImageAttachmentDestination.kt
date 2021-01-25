fa772d9a538be6304276d9ad954ed71651324d3c
Samuel Urbanowicz <samiuelson@gmail.com>
2021-01-25 14:10:25 +0100

    Add trailing comma

--- stream-chat-android-ui-components/src/main/kotlin/io/getstream/chat/android/chat/navigation/GalleryImageAttachmentDestination.kt
+++ stream-chat-android-ui-components/src/main/kotlin/io/getstream/chat/android/chat/navigation/GalleryImageAttachmentDestination.kt
@@ -42,7 +42,7 @@ public class GalleryImageAttachmentDestination(
                 attachmentShowInChatOptionHandler,
                 attachmentReplyOptionHandler,
                 attachmentDownloadOptionHandler,
-                onAttachmentDeleteOptionClickHandler
+                onAttachmentDeleteOptionClickHandler,
             )
         )
     }
