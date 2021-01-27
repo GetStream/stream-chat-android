25c57d52ec9d5e96ee0966fae099966f415bc941
Márton Braun <marton@getstream.io>
2021-01-27 11:09:44 +0100

    Add coroutine scope to AttachmentsProgressViewHolder

--- stream-chat-android-ui-components/src/main/kotlin/io/getstream/chat/android/ui/messages/adapter/viewholder/AttachmentsProgressViewHolder.kt
+++ stream-chat-android-ui-components/src/main/kotlin/io/getstream/chat/android/ui/messages/adapter/viewholder/AttachmentsProgressViewHolder.kt
@@ -5,11 +5,12 @@ import com.getstream.sdk.chat.adapter.MessageListItem
 import com.getstream.sdk.chat.utils.extensions.inflater
 import io.getstream.chat.android.client.extensions.uploadId
 import io.getstream.chat.android.client.uploader.ProgressTrackerFactory
+import io.getstream.chat.android.core.internal.coroutines.DispatcherProvider
 import io.getstream.chat.android.ui.databinding.StreamUiItemMessageEphemeralProgressBinding
 import io.getstream.chat.android.ui.messages.adapter.BaseMessageItemViewHolder
 import io.getstream.chat.android.ui.messages.adapter.MessageListItemPayloadDiff
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.cancel
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.launch
 
@@ -23,15 +24,25 @@ internal class AttachmentsProgressViewHolder(
         ),
 ) : BaseMessageItemViewHolder<MessageListItem.MessageItem>(binding.root) {
 
+    private var scope: CoroutineScope? = null
+
+    private fun clearScope() {
+        scope?.cancel()
+        scope = null
+    }
+
     override fun bindData(data: MessageListItem.MessageItem, diff: MessageListItemPayloadDiff?) {
+        clearScope()
+        val scope = CoroutineScope(DispatcherProvider.Main)
+
         data.message.uploadId?.let(ProgressTrackerFactory::getOrCreate)?.let { tracker ->
-            GlobalScope.launch(Dispatchers.Main) {
+            scope.launch {
                 tracker.lapsCompleted().collect { laps ->
                     binding.sentFiles.text = "$laps / ${tracker.getNumberOfItems()}"
                 }
             }
 
-            GlobalScope.launch(Dispatchers.Main) {
+            scope.launch {
                 tracker.currentItemProgress().collect { progress ->
                     val message = if (progress == 100) {
                         "Upload complete, processing..."
@@ -43,5 +54,12 @@ internal class AttachmentsProgressViewHolder(
                 }
             }
         }
+
+        this.scope = scope
+    }
+
+    override fun unbind() {
+        super.unbind()
+        clearScope()
     }
 }
