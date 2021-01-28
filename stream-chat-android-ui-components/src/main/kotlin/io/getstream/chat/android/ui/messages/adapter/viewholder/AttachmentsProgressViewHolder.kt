19c4ed52931fc134d8fb8dd91478c4b0699f49a3
Leandro Borges Ferreira <lehen01@gmail.com>
2021-01-27 21:11:04 -0300

    [CAS-245] Extracting string resource

--- stream-chat-android-ui-components/src/main/kotlin/io/getstream/chat/android/ui/messages/adapter/viewholder/AttachmentsProgressViewHolder.kt
+++ stream-chat-android-ui-components/src/main/kotlin/io/getstream/chat/android/ui/messages/adapter/viewholder/AttachmentsProgressViewHolder.kt
@@ -6,6 +6,7 @@ import com.getstream.sdk.chat.utils.extensions.inflater
 import io.getstream.chat.android.client.extensions.uploadId
 import io.getstream.chat.android.client.uploader.ProgressTrackerFactory
 import io.getstream.chat.android.core.internal.coroutines.DispatcherProvider
+import io.getstream.chat.android.ui.R
 import io.getstream.chat.android.ui.databinding.StreamUiItemMessageEphemeralProgressBinding
 import io.getstream.chat.android.ui.messages.adapter.BaseMessageItemViewHolder
 import io.getstream.chat.android.ui.messages.adapter.MessageListItemPayloadDiff
@@ -15,6 +16,7 @@ import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.filter
 import kotlinx.coroutines.launch
 
+//Todo: Adapt this behaviour in a latter moment.
 internal class AttachmentsProgressViewHolder(
     parent: ViewGroup,
     internal val binding: StreamUiItemMessageEphemeralProgressBinding =
@@ -43,7 +45,7 @@ internal class AttachmentsProgressViewHolder(
                 }
 
                 tracker.isComplete().filter { isComplete -> isComplete }.collect {
-                    binding.sentFiles.text = "Upload complete, processing..."
+                    binding.sentFiles.text = context.getString(R.string.stream_ui_upload_complete)
                 }
             }
         }
