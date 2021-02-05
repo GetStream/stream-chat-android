6cc0c94da850b3642464cb4b6a02428f6fe49077
Leandro Borges Ferreira <lehen01@gmail.com>
2021-02-05 13:30:16 -0300

    [CAS-660] Message input view documentation

--- stream-chat-android-docs/src/main/kotlin/io/getstream/chat/docs/kotlin/Android.kt
+++ stream-chat-android-docs/src/main/kotlin/io/getstream/chat/docs/kotlin/Android.kt
@@ -2,9 +2,12 @@ package io.getstream.chat.docs.kotlin
 
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import com.getstream.sdk.chat.viewmodel.MessageInputViewModel
 import io.getstream.chat.android.ui.channel.list.header.ChannelListHeaderView
 import io.getstream.chat.android.ui.channel.list.header.viewmodel.ChannelListHeaderViewModel
 import io.getstream.chat.android.ui.channel.list.header.viewmodel.bindView
+import io.getstream.chat.android.ui.textinput.MessageInputView
+import io.getstream.chat.android.ui.textinput.bindView
 
 class Android {
 
@@ -18,6 +21,19 @@ class Android {
             val viewModel: ChannelListHeaderViewModel by viewModels()
             // Bind it with ChannelListHeaderView
             viewModel.bindView(channelListHeaderView, viewLifecycleOwner)
+        }
+    }
+
+    /**
+     * @see <a href="https://getstream.io/chat/docs/android/message_input_view_neo">Message Input View</a>
+     */
+    inner class MessageInput(private val messageInputView: MessageInputView) : Fragment() {
+
+        fun bindingWithViewModel() {
+            // Get ViewModel
+            val viewModel: MessageInputViewModel by viewModels()
+            // Bind it with ChannelListHeaderView
+            viewModel.bindView(messageInputView, viewLifecycleOwner)
         }
     }
 }
