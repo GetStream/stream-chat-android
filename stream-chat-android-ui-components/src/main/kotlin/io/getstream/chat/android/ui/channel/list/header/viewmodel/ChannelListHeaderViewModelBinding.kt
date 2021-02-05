861bd24cfe29579e5af421d0eea9473184884477
Rafał Adasiewicz <adasiewicz.r@gmail.com>
2021-02-05 10:27:32 +0100

    [CAS-657] Add channel list header view docs

---
+++ stream-chat-android-ui-components/src/main/kotlin/io/getstream/chat/android/ui/channel/list/header/viewmodel/ChannelListHeaderViewModelBinding.kt
@@ -0,0 +1,24 @@
+@file:JvmName("ChannelListHeaderViewModelBinding")
+
+package io.getstream.chat.android.ui.channel.list.header.viewmodel
+
+import androidx.lifecycle.LifecycleOwner
+import io.getstream.chat.android.ui.channel.list.header.ChannelListHeaderView
+
+/**
+ * Binds [ChannelListHeaderView] with [ChannelListHeaderViewModel], updating the view's state
+ * based on data provided by the ViewModel.
+ */
+@JvmName("bind")
+public fun ChannelListHeaderViewModel.bindView(view: ChannelListHeaderView, lifecycleOwner: LifecycleOwner) {
+    with(view) {
+        setUser(currentUser)
+        online.observe(lifecycleOwner) { isOnline ->
+            if (isOnline) {
+                showOnlineTitle()
+            } else {
+                showOfflineTitle()
+            }
+        }
+    }
+}
