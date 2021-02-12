cd2dfcd3912dd8e59987a8565d6f72c1d9e34e4c
Oleg Kuzmin <oleg@getstream.io>
2021-02-12 17:49:09 +0100

    Perform search only if online state

--- stream-chat-android-offline/src/main/java/io/getstream/chat/android/livedata/usecase/SearchUsersByName.kt
+++ stream-chat-android-offline/src/main/java/io/getstream/chat/android/livedata/usecase/SearchUsersByName.kt
@@ -8,6 +8,7 @@ import io.getstream.chat.android.client.call.CoroutineCall
 import io.getstream.chat.android.client.models.Filters
 import io.getstream.chat.android.client.models.User
 import io.getstream.chat.android.client.models.name
+import io.getstream.chat.android.client.utils.Result
 import io.getstream.chat.android.livedata.ChatDomainImpl
 
 public class SearchUsersByName internal constructor(private val chatDomainImpl: ChatDomainImpl) {
@@ -28,27 +29,31 @@ public class SearchUsersByName internal constructor(private val chatDomainImpl: 
      */
     public operator fun invoke(querySearch: String, offset: Int, userLimit: Int): Call<List<User>> {
         return CoroutineCall(chatDomainImpl.scope) {
-            val filter = if (querySearch.isEmpty()) {
-                defaultUsersQueryFilter
-            } else {
-                Filters.and(
-                    Filters.autocomplete(FIELD_NAME, querySearch),
-                    Filters.ne(FIELD_ID, chatDomainImpl.currentUser.id)
-                )
-            }
-
-            chatDomainImpl.client.queryUsers(
-                QueryUsersRequest(
-                    filter = filter,
-                    offset = offset,
-                    limit = userLimit,
-                    querySort = USERS_QUERY_SORT,
-                    presence = true
-                )
-            ).execute().also { result ->
-                if (result.isSuccess && result.data().isNotEmpty()) {
-                    chatDomainImpl.repos.insertUsers(result.data())
+            if (chatDomainImpl.isOnline()) {
+                val filter = if (querySearch.isEmpty()) {
+                    defaultUsersQueryFilter
+                } else {
+                    Filters.and(
+                        Filters.autocomplete(FIELD_NAME, querySearch),
+                        Filters.ne(FIELD_ID, chatDomainImpl.currentUser.id)
+                    )
                 }
+
+                chatDomainImpl.client.queryUsers(
+                    QueryUsersRequest(
+                        filter = filter,
+                        offset = offset,
+                        limit = userLimit,
+                        querySort = USERS_QUERY_SORT,
+                        presence = true
+                    )
+                ).execute().also { result ->
+                    if (result.isSuccess && result.data().isNotEmpty()) {
+                        chatDomainImpl.repos.insertUsers(result.data())
+                    }
+                }
+            } else {
+                Result(emptyList())
             }
         }
     }
