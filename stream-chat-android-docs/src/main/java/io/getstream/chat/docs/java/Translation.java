909b978e035dcd3ececff6aa142ff15e559a26cf
Márton Braun <marton@getstream.io>
2021-02-17 13:40:36 +0100

    Update a handful of documentation snippets

--- stream-chat-android-docs/src/main/java/io/getstream/chat/docs/java/Translation.java
+++ stream-chat-android-docs/src/main/java/io/getstream/chat/docs/java/Translation.java
@@ -1,12 +1,8 @@
 package io.getstream.chat.docs.java;
 
-import android.util.Log;
-
 import io.getstream.chat.android.client.ChatClient;
 import io.getstream.chat.android.client.channel.ChannelClient;
 import io.getstream.chat.android.client.models.Message;
-
-import static io.getstream.chat.docs.StaticInstances.TAG;
 
 public class Translation {
     private ChatClient client;
@@ -16,7 +12,7 @@ public class Translation {
      */
     public void messageTranslation() {
         // Translate message to French
-        ChannelClient channelClient = client.channel("messaging:general");
+        ChannelClient channelClient = client.channel("messaging", "general");
 
         Message message = new Message();
         message.setText("Hello, I would like to have more information about your product.");
@@ -30,11 +26,11 @@ public class Translation {
                         Message translatedMessage = translationResult.data();
                         String translation = translatedMessage.getI18n().get("fr_text");
                     } else {
-                        Log.e(TAG, String.format("There was an error %s", result.error()), result.error().getCause());
+                        // Handle translationResult.error()
                     }
                 });
             } else {
-                Log.e(TAG, String.format("There was an error %s", result.error()), result.error().getCause());
+                // Handle result.error()
             }
         });
     }
