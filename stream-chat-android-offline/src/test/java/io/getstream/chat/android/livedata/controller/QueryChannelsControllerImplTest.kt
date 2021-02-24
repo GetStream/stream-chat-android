30a1a8fbc134cf17152c02a955ed9abe5c88e63d
Leandro Borges Ferreira <lehen01@gmail.com>
2021-02-24 11:04:07 -0300

    [CAS-738] Adding unit tests

--- stream-chat-android-offline/src/test/java/io/getstream/chat/android/livedata/controller/QueryChannelsControllerImplTest.kt
+++ stream-chat-android-offline/src/test/java/io/getstream/chat/android/livedata/controller/QueryChannelsControllerImplTest.kt
@@ -5,16 +5,20 @@ import com.nhaarman.mockitokotlin2.doAnswer
 import com.nhaarman.mockitokotlin2.doReturn
 import com.nhaarman.mockitokotlin2.eq
 import com.nhaarman.mockitokotlin2.mock
+import com.nhaarman.mockitokotlin2.never
 import com.nhaarman.mockitokotlin2.verify
 import com.nhaarman.mockitokotlin2.whenever
 import io.getstream.chat.android.client.ChatClient
 import io.getstream.chat.android.client.api.models.QuerySort
+import io.getstream.chat.android.client.events.NotificationMessageNewEvent
 import io.getstream.chat.android.client.models.Channel
+import io.getstream.chat.android.client.models.Message
 import io.getstream.chat.android.livedata.ChatDomainImpl
 import io.getstream.chat.android.livedata.randomChannel
 import io.getstream.chat.android.test.InstantTaskExecutorExtension
 import io.getstream.chat.android.test.TestCoroutineExtension
 import io.getstream.chat.android.test.getOrAwaitValue
+import io.getstream.chat.android.test.randomString
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.Job
@@ -23,6 +27,7 @@ import org.amshove.kluent.shouldBeEqualTo
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.extension.ExtendWith
 import org.junit.jupiter.api.extension.RegisterExtension
+import java.util.Date
 
 @ExperimentalCoroutinesApi
 @ExtendWith(InstantTaskExecutorExtension::class)
@@ -79,6 +84,52 @@ internal class QueryChannelsControllerImplTest {
             result.size shouldBeEqualTo 1
             result.first().cid shouldBeEqualTo "ChannelType:ChannelID"
         }
+
+    @Test
+    fun `when a new message arrives in a new channel, it should update the channels`() =
+        runBlockingTest {
+            val channelController: ChannelControllerImpl = mock()
+
+            val queryController = Fixture(testCoroutines.scope)
+                .givenNewChannelController(channelController)
+                .get()
+
+            queryController.handleEvent(notificationNewMessage())
+
+            verify(channelController).updateLiveDataFromChannel(any())
+        }
+
+    @Test
+    fun `when a new message arrives in a new channel, it NOT should update the channels when it is already there`() =
+        runBlockingTest {
+            val cid = randomString()
+            val channelController: ChannelControllerImpl = mock()
+
+            val queryController = Fixture(testCoroutines.scope)
+                .givenNewChannelController(channelController)
+                .get()
+
+            queryController.queryChannelsSpec.cids = listOf(cid)
+
+            queryController.handleEvent(notificationNewMessage(cid))
+
+            verify(channelController, never()).updateLiveDataFromChannel(any())
+        }
+}
+
+private fun notificationNewMessage(channelCid: String = "cid"): NotificationMessageNewEvent {
+    return NotificationMessageNewEvent(
+        type = "type",
+        createdAt = Date(),
+        cid = "cid",
+        channelType = "channelType",
+        channelId = "channelId",
+        channel = Channel(cid = channelCid),
+        message = Message(),
+        watcherCount = 0,
+        totalUnreadCount = 0,
+        unreadChannels = 0
+    )
 }
 
 private class Fixture(scope: CoroutineScope) {
