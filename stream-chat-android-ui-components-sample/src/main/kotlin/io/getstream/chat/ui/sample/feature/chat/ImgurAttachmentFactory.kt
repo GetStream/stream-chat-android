7077ef00e430e4bff13b612d78c3fc16c9a4bcd9
Márton Braun <marton@getstream.io>
2021-03-01 13:23:53 +0100

    Update imgur factory

--- stream-chat-android-ui-components-sample/src/main/kotlin/io/getstream/chat/ui/sample/feature/chat/ImgurAttachmentFactory.kt
+++ stream-chat-android-ui-components-sample/src/main/kotlin/io/getstream/chat/ui/sample/feature/chat/ImgurAttachmentFactory.kt
@@ -4,26 +4,26 @@ import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import coil.load
+import com.getstream.sdk.chat.adapter.MessageListItem
 import io.getstream.chat.android.client.models.Attachment
+import io.getstream.chat.android.ui.message.list.adapter.MessageListListenerContainer
 import io.getstream.chat.android.ui.message.list.adapter.viewholder.attachment.AttachmentViewFactory
+import io.getstream.chat.android.ui.message.list.internal.MessageListItemStyle
 import io.getstream.chat.ui.sample.R
 import io.getstream.chat.ui.sample.databinding.ListItemAttachmentImgurBinding
 
 class ImgurAttachmentFactory : AttachmentViewFactory() {
 
-    override fun createLinkAttachmentView(linkAttachment: Attachment, context: Context): View {
-        val imgurAttachment = linkAttachment.takeIf { it.isImgurAttachment() }
+    override fun createAttachmentViews(
+        data: MessageListItem.MessageItem,
+        listeners: MessageListListenerContainer,
+        style: MessageListItemStyle,
+        parent: View,
+    ): View {
+        val imgurAttachment = data.message.attachments.firstOrNull { it.isImgurAttachment() }
         return when {
-            imgurAttachment != null -> createImgurAttachment(imgurAttachment, context)
-            else -> super.createLinkAttachmentView(linkAttachment, context)
-        }
-    }
-
-    override fun createAttachmentsView(attachments: List<Attachment>, context: Context): View {
-        val imgurAttachment = attachments.firstOrNull { it.isImgurAttachment() }
-        return when {
-            imgurAttachment != null -> createImgurAttachment(imgurAttachment, context)
-            else -> super.createAttachmentsView(attachments, context)
+            imgurAttachment != null -> createImgurAttachment(imgurAttachment, parent.context)
+            else -> super.createAttachmentViews(data, listeners, style, parent)
         }
     }
 
