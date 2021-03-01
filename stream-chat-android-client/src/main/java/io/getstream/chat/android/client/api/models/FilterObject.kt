ec8ea4500a376ea404b5cfbb66603a5adf6fee87
Jc Miñarro <josecarlos.minarro@gmail.com>
2021-03-01 14:24:14 +0100

    Create sealed class that cover the different types of FilterObjects

---
+++ stream-chat-android-client/src/main/java/io/getstream/chat/android/client/api/models/FilterObject.kt
@@ -0,0 +1,33 @@
+package io.getstream.chat.android.client.api.models
+
+public sealed class FilterObject
+
+public data class AndFilterObject(val filterObjects: List<FilterObject>) : FilterObject()
+
+public data class OrFilterObject(val filterObjects: List<FilterObject>) : FilterObject()
+
+public data class NorFilterObject(val filterObjects: List<FilterObject>) : FilterObject()
+
+public data class ContainsFilterObject(val fieldName: String, val value: Any) : FilterObject()
+
+public data class AutocompleteFilterObject(val fieldName: String, val value: String) : FilterObject()
+
+public data class ExistsFilterObject(val fieldName: String) : FilterObject()
+
+public data class NonExistsFilterObject(val fieldName: String) : FilterObject()
+
+public data class EqualsFilterObject(val fieldName: String, val value: Any) : FilterObject()
+
+public data class NotEqualsFilterObject(val fieldName: String, val value: Any) : FilterObject()
+
+public data class GreaterThanFilterObject(val fieldName: String, val value: Any) : FilterObject()
+
+public data class GreaterThanOrEqualsFilterObject(val fieldName: String, val value: Any) : FilterObject()
+
+public data class LessThanFilterObject(val fieldName: String, val value: Any) : FilterObject()
+
+public data class LessThanOrEqualsFilterObject(val fieldName: String, val value: Any) : FilterObject()
+
+public data class InFilterObject(val fieldName: String, val values: List<Any>) : FilterObject()
+
+public data class NotInFilterObject(val fieldName: String, val values: List<Any>) : FilterObject()
