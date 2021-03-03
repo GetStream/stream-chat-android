faf56fd8ba5e82d7cb8965110b761ffcfad7d804
Oleg Kuzmin <oleg@getstream.io>
2021-03-03 13:33:06 +0100

    Store for ChannelUserRead unreadCount in DB

--- stream-chat-android-offline/src/main/java/io/getstream/chat/android/livedata/extensions/DateExtensions.kt
+++ stream-chat-android-offline/src/main/java/io/getstream/chat/android/livedata/extensions/DateExtensions.kt
@@ -2,9 +2,7 @@ package io.getstream.chat.android.livedata.extensions
 
 import java.util.Date
 
-internal fun Date?.greaterOrEqual(other: Date): Boolean = this?.after(other) == true || this == other
-
 /**
  * Check if current date has difference with [other] no more that [offset].
  */
-internal fun Date.inOffsetWith(other: Date, offset: Long): Boolean = (time + offset) > other.time
+internal fun Date.inOffsetWith(other: Date, offset: Long): Boolean = (time + offset) >= other.time
