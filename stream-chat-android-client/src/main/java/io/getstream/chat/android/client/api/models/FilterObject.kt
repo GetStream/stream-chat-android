da4a615d34d0cccc3e50e6a585e4014e1c7a6764
Jc Miñarro <josecarlos.minarro@gmail.com>
2021-03-04 13:28:08 +0100

    Make FilterObjects constructors internal

--- stream-chat-android-client/src/main/java/io/getstream/chat/android/client/api/models/FilterObject.kt
+++ stream-chat-android-client/src/main/java/io/getstream/chat/android/client/api/models/FilterObject.kt
@@ -2,36 +2,20 @@ package io.getstream.chat.android.client.api.models
 
 public sealed class FilterObject
 
-public data class AndFilterObject(val filterObjects: Set<FilterObject>) : FilterObject()
-
-public data class OrFilterObject(val filterObjects: Set<FilterObject>) : FilterObject()
-
-public data class NorFilterObject(val filterObjects: Set<FilterObject>) : FilterObject()
-
-public data class ContainsFilterObject(val fieldName: String, val value: Any) : FilterObject()
-
-public data class AutocompleteFilterObject(val fieldName: String, val value: String) : FilterObject()
-
-public data class ExistsFilterObject(val fieldName: String) : FilterObject()
-
-public data class NonExistsFilterObject(val fieldName: String) : FilterObject()
-
-public data class EqualsFilterObject(val fieldName: String, val value: Any) : FilterObject()
-
-public data class NotEqualsFilterObject(val fieldName: String, val value: Any) : FilterObject()
-
-public data class GreaterThanFilterObject(val fieldName: String, val value: Any) : FilterObject()
-
-public data class GreaterThanOrEqualsFilterObject(val fieldName: String, val value: Any) : FilterObject()
-
-public data class LessThanFilterObject(val fieldName: String, val value: Any) : FilterObject()
-
-public data class LessThanOrEqualsFilterObject(val fieldName: String, val value: Any) : FilterObject()
-
-public data class InFilterObject(val fieldName: String, val values: Set<Any>) : FilterObject()
-
-public data class NotInFilterObject(val fieldName: String, val values: Set<Any>) : FilterObject()
-
-public data class DistinctFilterObject(val memberIds: Set<String>) : FilterObject()
-
+public data class AndFilterObject internal constructor(val filterObjects: Set<FilterObject>) : FilterObject()
+public data class OrFilterObject internal constructor(val filterObjects: Set<FilterObject>) : FilterObject()
+public data class NorFilterObject internal constructor(val filterObjects: Set<FilterObject>) : FilterObject()
+public data class ContainsFilterObject internal constructor(val fieldName: String, val value: Any) : FilterObject()
+public data class AutocompleteFilterObject internal constructor(val fieldName: String, val value: String) : FilterObject()
+public data class ExistsFilterObject internal constructor(val fieldName: String) : FilterObject()
+public data class NonExistsFilterObject internal constructor(val fieldName: String) : FilterObject()
+public data class EqualsFilterObject internal constructor(val fieldName: String, val value: Any) : FilterObject()
+public data class NotEqualsFilterObject internal constructor(val fieldName: String, val value: Any) : FilterObject()
+public data class GreaterThanFilterObject internal constructor(val fieldName: String, val value: Any) : FilterObject()
+public data class GreaterThanOrEqualsFilterObject internal constructor(val fieldName: String, val value: Any) : FilterObject()
+public data class LessThanFilterObject internal constructor(val fieldName: String, val value: Any) : FilterObject()
+public data class LessThanOrEqualsFilterObject internal constructor(val fieldName: String, val value: Any) : FilterObject()
+public data class InFilterObject internal constructor(val fieldName: String, val values: Set<Any>) : FilterObject()
+public data class NotInFilterObject internal constructor(val fieldName: String, val values: Set<Any>) : FilterObject()
+public data class DistinctFilterObject internal constructor(val memberIds: Set<String>) : FilterObject()
 public object NeutralFilterObject : FilterObject()
