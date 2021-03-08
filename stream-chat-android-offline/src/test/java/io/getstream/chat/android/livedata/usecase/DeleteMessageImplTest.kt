38a2365664fd20b13f5ca600e15d1d9736ab5f32
Oleg Kuzmin <oleg@getstream.io>
2021-03-08 17:46:12 +0100

    Add first versions of send message

--- stream-chat-android-offline/src/test/java/io/getstream/chat/android/livedata/usecase/DeleteMessageImplTest.kt
+++ stream-chat-android-offline/src/test/java/io/getstream/chat/android/livedata/usecase/DeleteMessageImplTest.kt
@@ -1,24 +1,34 @@
 package io.getstream.chat.android.livedata.usecase
 
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import com.google.common.truth.Truth
-import io.getstream.chat.android.livedata.BaseConnectedIntegrationTest
+import com.nhaarman.mockitokotlin2.any
+import com.nhaarman.mockitokotlin2.eq
+import com.nhaarman.mockitokotlin2.whenever
+import io.getstream.chat.android.client.ChatClient
+import io.getstream.chat.android.client.models.Message
+import io.getstream.chat.android.client.utils.Result
+import io.getstream.chat.android.livedata.BaseConnectedIntegrationTest.Companion.data
+import io.getstream.chat.android.livedata.BaseConnectedMockedTest
+import io.getstream.chat.android.livedata.ChatDomain
+import io.getstream.chat.android.livedata.controller.ChannelController
+import io.getstream.chat.android.test.TestCall
 import io.getstream.chat.android.test.getOrAwaitValue
 import kotlinx.coroutines.runBlocking
-import org.junit.Ignore
 import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
-internal class DeleteMessageImplTest : BaseConnectedIntegrationTest() {
+internal class DeleteMessageImplTest : BaseConnectedMockedTest() {
 
     @Test
-    @Ignore("flaky")
     fun deleteMessageUseCase() = runBlocking {
+        /*val message = data.createMessage()
+        val fixture = Fixture(chatDomain).givenMessage(message)
         val message1 = data.createMessage()
-        client.subscribe { println(it.type) }
+
         val channelState = chatDomain.useCases.watchChannel(data.channel1.cid, 10).execute().data()
-        val result = chatDomain.useCases.sendMessage(message1).execute()
+
+        val result =
         assertSuccess(result)
         var messages = channelState.messages.getOrAwaitValue()
         Truth.assertThat(messages.last()).isEqualTo(message1)
@@ -26,6 +36,31 @@ internal class DeleteMessageImplTest : BaseConnectedIntegrationTest() {
         assertSuccess(result2)
         messages = channelState.messages.getOrAwaitValue()
         Truth.assertThat(messages.last().id).isEqualTo(result.data().id)
-        Truth.assertThat(messages.last().deletedAt).isNotNull()
+        Truth.assertThat(messages.last().deletedAt).isNotNull()*/
+    }
+}
+
+private class Fixture {
+    private lateinit var chatDomain: ChatDomain
+    private lateinit var chatClient: ChatClient
+
+    private lateinit var channelState: ChannelController
+
+    fun givenChatClient(chatClient: ChatClient) = apply {
+        this.chatClient = chatClient
+    }
+
+    fun givenChatDomain(chatDomain: ChatDomain) = apply {
+        this.chatDomain = chatDomain
+        channelState = chatDomain.useCases.watchChannel(data.channel1.cid, 10).execute().data()
+    }
+
+    fun givenMessage(message: Message) = apply {
+        whenever(chatClient.sendMessage(any(), any(), eq(message))).thenReturn(TestCall(Result(message)))
+        chatDomain.useCases.sendMessage(message).execute()
+    }
+
+    fun build(): List<Message> {
+        return channelState.messages.getOrAwaitValue()
     }
 }
---
+++ stream-chat-android-offline/src/test/java/io/getstream/chat/android/livedata/usecase/SendMessageTest.kt
@@ -0,0 +1,49 @@
+package io.getstream.chat.android.livedata.usecase
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.google.common.truth.Truth
+import com.nhaarman.mockitokotlin2.any
+import com.nhaarman.mockitokotlin2.mock
+import com.nhaarman.mockitokotlin2.whenever
+import io.getstream.chat.android.client.ChatClient
+import io.getstream.chat.android.client.utils.Result
+import io.getstream.chat.android.livedata.BaseConnectedIntegrationTest.Companion.data
+import io.getstream.chat.android.livedata.BaseConnectedMockedTest
+import io.getstream.chat.android.livedata.ChatDomain
+import io.getstream.chat.android.livedata.controller.ChannelController
+import io.getstream.chat.android.test.TestCall
+import io.getstream.chat.android.test.getOrAwaitValue
+import kotlinx.coroutines.runBlocking
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+internal class SendMessageTest : BaseConnectedMockedTest() {
+    @Test
+    fun `Should send message`() = runBlocking {
+        val message1 = data.createMessage().apply { extraData = mutableMapOf("location" to "Amsterdam") }
+        val channelState = Fixture().givenChatDomain(chatDomain).givenChatClient(client).build()
+        // Check that current state is empty
+        Truth.assertThat(channelState.messages.getOrAwaitValue()).isEmpty()
+
+        chatDomain.useCases.sendMessage(message1).execute()
+
+        Truth.assertThat(channelState.messages.getOrAwaitValue()).isEqualTo(message1)
+    }
+
+    private class Fixture {
+        private lateinit var chatDomain: ChatDomain
+
+        fun givenChatDomain(chatDomain: ChatDomain) = apply {
+            this.chatDomain = chatDomain
+        }
+
+        fun givenChatClient(chatClient: ChatClient) = apply {
+            whenever(chatClient.sendMessage(any(), any(), any())).thenReturn(TestCall(Result(mock())))
+        }
+
+        fun build(): ChannelController {
+            return chatDomain.useCases.watchChannel(data.channel1.cid, 10).execute().data()
+        }
+    }
+}
