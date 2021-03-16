8b9df7b8f36d01007a34bd063957bee64554ff0a
Dmitrii Bychkov <mail@dbychkov.com>
2021-03-16 14:14:51 +0300

    Add reasonable default values to channel config

--- stream-chat-android-client/src/main/java/io/getstream/chat/android/client/models/Config.kt
+++ stream-chat-android-client/src/main/java/io/getstream/chat/android/client/models/Config.kt
@@ -26,78 +26,78 @@ public data class Config(
      * Controls if typing indicators are shown. Enabled by default.
      */
     @SerializedName("typing_events")
-    val isTypingEvents: Boolean = false,
+    val isTypingEvents: Boolean = true,
 
     /**
      * Controls whether the chat shows how far you’ve read. Enabled by default.
      */
     @SerializedName("read_events")
-    val isReadEvents: Boolean = false,
+    val isReadEvents: Boolean = true,
 
     /**
      * Determines if events are fired for connecting and disconnecting to a chat. Enabled by default.
      */
     @SerializedName("connect_events")
-    val isConnectEvents: Boolean = false,
+    val isConnectEvents: Boolean = true,
 
     /**
      * Controls if messages should be searchable (this is a premium feature). Disabled by default.
      */
     @SerializedName("search")
-    val isSearch: Boolean = false,
+    val isSearch: Boolean = true,
 
     /**
      * If users are allowed to add reactions to messages. Enabled by default.
      */
     @SerializedName("reactions")
-    val isReactionsEnabled: Boolean = false,
+    val isReactionsEnabled: Boolean = true,
 
     /**
      * Enables message threads and replies. Enabled by default.
      */
     @SerializedName("replies")
-    val isRepliesEnabled: Boolean = false,
+    val isRepliesEnabled: Boolean = true,
 
     /**
      * Determines if users are able to mute other users. Enabled by default.
      */
     @SerializedName("mutes")
-    val isMutes: Boolean = false,
+    val isMutes: Boolean = true,
 
     /**
      * Allows image and file uploads within messages. Enabled by default.
      */
     @SerializedName("uploads")
-    val uploadsEnabled: Boolean = false,
+    val uploadsEnabled: Boolean = true,
 
     /**
      * Determines if URL enrichment enabled to show they as attachments. Enabled by default.
      */
     @SerializedName("url_enrichment")
-    val urlEnrichmentEnabled: Boolean = false,
+    val urlEnrichmentEnabled: Boolean = true,
 
     @SerializedName("custom_events")
     val customEventsEnabled: Boolean = false,
 
     @SerializedName("push_notifications")
-    val pushNotificationsEnabled: Boolean = false,
+    val pushNotificationsEnabled: Boolean = true,
 
     /**
      * A number of days or infinite. "Infinite" by default.
      */
     @SerializedName("message_retention")
-    val messageRetention: String = "",
+    val messageRetention: String = "infinite",
 
     /**
      * The max message length. 5000 by default.
      */
     @SerializedName("max_message_length")
-    val maxMessageLength: Int = Int.MAX_VALUE,
+    val maxMessageLength: Int = 5000,
 
     /**
      * Disabled, simple or AI are valid options for the Automod. AI based moderation is a premium feature.
      */
-    val automod: String = "",
+    val automod: String = "disabled",
 
     @SerializedName("automod_behavior")
     val automodBehavior: String = "",
