7508202077e392c2a259d2181aab8751817288c3
Rafal Adasiewicz <rafal@getstream.io>
2021-03-17 13:45:38 +0100

    [CAS-797] Add possibility to enable/disable delete message option (#1569)

--- stream-chat-android-ui-components/src/main/kotlin/io/getstream/chat/android/ui/message/list/options/message/internal/MessageOptionsView.kt
+++ stream-chat-android-ui-components/src/main/kotlin/io/getstream/chat/android/ui/message/list/options/message/internal/MessageOptionsView.kt
@@ -91,10 +91,7 @@ internal class MessageOptionsView : FrameLayout {
         binding.flagTV.isVisible = false
         binding.muteTV.isVisible = false
         binding.blockTV.isVisible = false
-        binding.deleteTV.run {
-            configureListItem(configuration.deleteIcon, iconsTint)
-            setTextColor(ContextCompat.getColor(context, R.color.stream_ui_accent_red))
-        }
+        configureDeleteMessage(configuration)
     }
 
     private fun configureCopyMessage(iconsTint: Int, configuration: Configuration) {
@@ -103,6 +100,18 @@ internal class MessageOptionsView : FrameLayout {
             binding.copyTV.configureListItem(configuration.copyIcon, iconsTint)
         } else {
             binding.copyTV.isVisible = false
+        }
+    }
+
+    private fun configureDeleteMessage(configuration: Configuration) {
+        if (configuration.deleteMessageEnabled) {
+            binding.deleteTV.apply {
+                isVisible = true
+                configureListItem(configuration.deleteIcon, configuration.iconsTint)
+                setTextColor(ContextCompat.getColor(context, R.color.stream_ui_accent_red))
+            }
+        } else {
+            binding.deleteTV.isVisible = false
         }
     }
 
@@ -118,6 +127,7 @@ internal class MessageOptionsView : FrameLayout {
         val muteIcon: Int,
         val blockIcon: Int,
         val deleteIcon: Int,
+        val deleteMessageEnabled: Boolean,
         val copyTextEnabled: Boolean,
         val deleteConfirmationEnabled: Boolean,
     ) : Serializable
