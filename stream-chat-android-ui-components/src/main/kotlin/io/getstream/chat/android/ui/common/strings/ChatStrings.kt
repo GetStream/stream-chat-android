e132736d4287b9e5db7d86dba0981ab3eed19256
Samuel Urbanowicz <samiuelson@gmail.com>
2021-03-19 16:47:38 +0100

    Remove chat strings from ui-components

--- stream-chat-android-ui-components/src/main/kotlin/io/getstream/chat/android/ui/common/strings/ChatStrings.kt
+++
@@ -1,8 +0,0 @@
-package com.getstream.sdk.chat.utils.strings
-
-import androidx.annotation.StringRes
-
-public interface ChatStrings {
-    public fun get(@StringRes resId: Int): String
-    public fun get(@StringRes resId: Int, vararg formatArgs: Any?): String
-}
