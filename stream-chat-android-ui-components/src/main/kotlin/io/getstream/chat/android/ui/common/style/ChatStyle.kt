90c654001aad5a1744d15447a773e208384a435d
Samuel Urbanowicz <samiuelson@gmail.com>
2021-03-19 16:48:32 +0100

    Transform ChatStyle interface and Imp into 1 class

--- stream-chat-android-ui-components/src/main/kotlin/io/getstream/chat/android/ui/common/style/ChatStyle.kt
+++ stream-chat-android-ui-components/src/main/kotlin/io/getstream/chat/android/ui/common/style/ChatStyle.kt
@@ -1,28 +1,6 @@
 package com.getstream.sdk.chat.style
 
-import androidx.annotation.FontRes
-
-public interface ChatStyle {
-
-    public fun hasDefaultFont(): Boolean
-
-    public fun getDefaultTextStyle(): TextStyle
-
-    public class Builder {
-        private var result = ChatStyleImpl()
-
-        public fun setDefaultFont(assetPath: String): Builder = apply {
-            result.defaultTextStyle = TextStyle().apply {
-                fontAssetsPath = assetPath
-            }
-        }
-
-        public fun setDefaultFont(@FontRes fontRes: Int): Builder = apply {
-            result.defaultTextStyle = TextStyle().apply {
-                fontResource = fontRes
-            }
-        }
-
-        public fun build(): ChatStyle = result
-    }
+public class ChatStyle {
+    public var defaultTextStyle: TextStyle? = null
+    public fun hasDefaultFont(): Boolean = defaultTextStyle?.hasFont() == true
 }
