d4af2277f18b7a76a09ce623af31e19780f11c64
Samuel Urbanowicz <samiuelson@gmail.com>
2021-03-22 16:53:45 +0100

    Implement new ChatNavigator, add it to new ChatUI

--- stream-chat-android-ui-components/src/main/kotlin/io/getstream/chat/android/ui/ChatUI.kt
+++ stream-chat-android-ui-components/src/main/kotlin/io/getstream/chat/android/ui/ChatUI.kt
@@ -5,6 +5,7 @@ import com.getstream.sdk.chat.style.ChatStyle
 import io.getstream.chat.android.ui.common.UrlSigner
 import io.getstream.chat.android.ui.common.markdown.ChatMarkdown
 import io.getstream.chat.android.ui.common.markdown.ChatMarkdownImpl
+import io.getstream.chat.android.ui.common.navigation.ChatNavigator
 import io.getstream.chat.android.ui.common.style.ChatFonts
 import io.getstream.chat.android.ui.common.style.ChatFontsImpl
 
@@ -23,7 +24,7 @@ public object ChatUI {
     internal lateinit var appContext: Context
 
     public var style: ChatStyle = ChatStyle()
-
+    public var navigator: ChatNavigator = ChatNavigator()
     public var urlSigner: UrlSigner = UrlSigner.DefaultUrlSigner()
 
     private var fontsOverride: ChatFonts? = null
