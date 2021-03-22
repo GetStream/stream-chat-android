f93a3294a2c4e13444afba857914e3c5071247f6
Oleg Kuzmin <oleg@getstream.io>
2021-03-22 12:48:47 +0100

    Rename state flow controller accordingly to what we have

--- stream-chat-android-offline/src/main/java/io/getstream/chat/android/livedata/controller/ChannelControllerImpl.kt
+++ stream-chat-android-offline/src/main/java/io/getstream/chat/android/livedata/controller/ChannelControllerImpl.kt
@@ -19,7 +19,7 @@ import io.getstream.chat.android.livedata.ChatDomainImpl
 import io.getstream.chat.android.livedata.controller.helper.MessageHelper
 import io.getstream.chat.android.livedata.request.QueryChannelPaginationRequest
 import java.io.File
-import io.getstream.chat.android.offline.channel.ChannelController as FlowChannelController
+import io.getstream.chat.android.offline.channel.ChannelController as ChannelControllerStateFlow
 
 internal class ChannelControllerImpl(
     override val channelType: String,
@@ -28,112 +28,113 @@ internal class ChannelControllerImpl(
     val domainImpl: ChatDomainImpl,
     messageHelper: MessageHelper = MessageHelper(),
 ) : ChannelController {
-    private val flowChannelController = FlowChannelController(channelType, channelId, client, domainImpl, messageHelper)
+    private val channelControllerStateFlow =
+        ChannelControllerStateFlow(channelType, channelId, client, domainImpl, messageHelper)
 
-    override val repliedMessage: LiveData<Message?> = flowChannelController.repliedMessage.asLiveData()
-    override val messages: LiveData<List<Message>> = flowChannelController.messages.asLiveData()
-    override val messagesState = flowChannelController.messagesState.asLiveData()
-    override val oldMessages: LiveData<List<Message>> = flowChannelController.oldMessages.asLiveData()
-    override val watcherCount: LiveData<Int> = flowChannelController.watcherCount.asLiveData()
-    override val watchers: LiveData<List<User>> = flowChannelController.watchers.asLiveData()
-    override val typing: LiveData<TypingEvent> = flowChannelController.typing.asLiveData()
-    override val reads: LiveData<List<ChannelUserRead>> = flowChannelController.reads.asLiveData()
-    override val read: LiveData<ChannelUserRead?> = flowChannelController.read.asLiveData()
-    override val unreadCount: LiveData<Int?> = flowChannelController.unreadCount.asLiveData()
-    override val members: LiveData<List<Member>> = flowChannelController.members.asLiveData()
-    override val channelData: LiveData<ChannelData> = flowChannelController.channelData.asLiveData()
-    override val hidden: LiveData<Boolean> = flowChannelController.hidden.asLiveData()
-    override val muted: LiveData<Boolean> = flowChannelController.muted.asLiveData()
-    override val loading: LiveData<Boolean> = flowChannelController.loading.asLiveData()
-    override val loadingOlderMessages: LiveData<Boolean> = flowChannelController.loadingOlderMessages.asLiveData()
-    override val loadingNewerMessages: LiveData<Boolean> = flowChannelController.loadingNewerMessages.asLiveData()
-    override val endOfOlderMessages: LiveData<Boolean> = flowChannelController.endOfOlderMessages.asLiveData()
-    override val endOfNewerMessages: LiveData<Boolean> = flowChannelController.endOfNewerMessages.asLiveData()
+    override val repliedMessage: LiveData<Message?> = channelControllerStateFlow.repliedMessage.asLiveData()
+    override val messages: LiveData<List<Message>> = channelControllerStateFlow.messages.asLiveData()
+    override val messagesState = channelControllerStateFlow.messagesState.asLiveData()
+    override val oldMessages: LiveData<List<Message>> = channelControllerStateFlow.oldMessages.asLiveData()
+    override val watcherCount: LiveData<Int> = channelControllerStateFlow.watcherCount.asLiveData()
+    override val watchers: LiveData<List<User>> = channelControllerStateFlow.watchers.asLiveData()
+    override val typing: LiveData<TypingEvent> = channelControllerStateFlow.typing.asLiveData()
+    override val reads: LiveData<List<ChannelUserRead>> = channelControllerStateFlow.reads.asLiveData()
+    override val read: LiveData<ChannelUserRead?> = channelControllerStateFlow.read.asLiveData()
+    override val unreadCount: LiveData<Int?> = channelControllerStateFlow.unreadCount.asLiveData()
+    override val members: LiveData<List<Member>> = channelControllerStateFlow.members.asLiveData()
+    override val channelData: LiveData<ChannelData> = channelControllerStateFlow.channelData.asLiveData()
+    override val hidden: LiveData<Boolean> = channelControllerStateFlow.hidden.asLiveData()
+    override val muted: LiveData<Boolean> = channelControllerStateFlow.muted.asLiveData()
+    override val loading: LiveData<Boolean> = channelControllerStateFlow.loading.asLiveData()
+    override val loadingOlderMessages: LiveData<Boolean> = channelControllerStateFlow.loadingOlderMessages.asLiveData()
+    override val loadingNewerMessages: LiveData<Boolean> = channelControllerStateFlow.loadingNewerMessages.asLiveData()
+    override val endOfOlderMessages: LiveData<Boolean> = channelControllerStateFlow.endOfOlderMessages.asLiveData()
+    override val endOfNewerMessages: LiveData<Boolean> = channelControllerStateFlow.endOfNewerMessages.asLiveData()
 
-    override var recoveryNeeded = flowChannelController.recoveryNeeded
-    override val cid = flowChannelController.cid
+    override var recoveryNeeded = channelControllerStateFlow.recoveryNeeded
+    override val cid = channelControllerStateFlow.cid
 
-    fun getThread(threadId: String): ThreadControllerImpl = flowChannelController.getThread(threadId)
-    fun keystroke(parentId: String?): Result<Boolean> = flowChannelController.keystroke(parentId)
-    fun stopTyping(parentId: String?): Result<Boolean> = flowChannelController.stopTyping(parentId)
-    internal fun markRead(): Boolean = flowChannelController.markRead()
+    fun getThread(threadId: String): ThreadControllerImpl = channelControllerStateFlow.getThread(threadId)
+    fun keystroke(parentId: String?): Result<Boolean> = channelControllerStateFlow.keystroke(parentId)
+    fun stopTyping(parentId: String?): Result<Boolean> = channelControllerStateFlow.stopTyping(parentId)
+    internal fun markRead(): Boolean = channelControllerStateFlow.markRead()
 
-    suspend fun hide(clearHistory: Boolean): Result<Unit> = flowChannelController.hide(clearHistory)
-    suspend fun show(): Result<Unit> = flowChannelController.show()
-    suspend fun leave(): Result<Unit> = flowChannelController.leave()
-    suspend fun delete(): Result<Unit> = flowChannelController.delete()
-    suspend fun watch(limit: Int = 30) = flowChannelController.watch(limit)
-    suspend fun loadOlderMessages(limit: Int = 30): Result<Channel> = flowChannelController.loadOlderMessages(limit)
+    suspend fun hide(clearHistory: Boolean): Result<Unit> = channelControllerStateFlow.hide(clearHistory)
+    suspend fun show(): Result<Unit> = channelControllerStateFlow.show()
+    suspend fun leave(): Result<Unit> = channelControllerStateFlow.leave()
+    suspend fun delete(): Result<Unit> = channelControllerStateFlow.delete()
+    suspend fun watch(limit: Int = 30) = channelControllerStateFlow.watch(limit)
+    suspend fun loadOlderMessages(limit: Int = 30): Result<Channel> = channelControllerStateFlow.loadOlderMessages(limit)
     suspend fun loadOlderMessages(messageId: String, limit: Int): Result<Channel> =
-        flowChannelController.loadOlderMessages(messageId, limit)
+        channelControllerStateFlow.loadOlderMessages(messageId, limit)
 
     suspend fun loadNewerMessages(messageId: String, limit: Int): Result<Channel> =
-        flowChannelController.loadNewerMessages(messageId, limit)
+        channelControllerStateFlow.loadNewerMessages(messageId, limit)
 
-    suspend fun loadNewerMessages(limit: Int = 30): Result<Channel> = flowChannelController.loadNewerMessages(limit)
+    suspend fun loadNewerMessages(limit: Int = 30): Result<Channel> = channelControllerStateFlow.loadNewerMessages(limit)
 
     suspend fun runChannelQuery(pagination: QueryChannelPaginationRequest): Result<Channel> =
-        flowChannelController.runChannelQuery(pagination)
+        channelControllerStateFlow.runChannelQuery(pagination)
 
     suspend fun runChannelQueryOnline(pagination: QueryChannelPaginationRequest): Result<Channel> =
-        flowChannelController.runChannelQueryOnline(pagination)
+        channelControllerStateFlow.runChannelQueryOnline(pagination)
 
     suspend fun sendMessage(
         message: Message,
         attachmentTransformer: ((at: Attachment, file: File) -> Attachment)? = null,
-    ): Result<Message> = flowChannelController.sendMessage(message, attachmentTransformer)
+    ): Result<Message> = channelControllerStateFlow.sendMessage(message, attachmentTransformer)
 
-    suspend fun cancelMessage(message: Message): Result<Boolean> = flowChannelController.cancelMessage(message)
+    suspend fun cancelMessage(message: Message): Result<Boolean> = channelControllerStateFlow.cancelMessage(message)
 
     internal suspend fun uploadAttachment(
         attachment: Attachment,
         attachmentTransformer: ((at: Attachment, file: File) -> Attachment)? = null,
         progressCallback: ProgressCallback? = null,
-    ): Result<Attachment> = flowChannelController.uploadAttachment(attachment, attachmentTransformer, progressCallback)
+    ): Result<Attachment> = channelControllerStateFlow.uploadAttachment(attachment, attachmentTransformer, progressCallback)
 
-    suspend fun sendGiphy(message: Message): Result<Message> = flowChannelController.sendGiphy(message)
-    suspend fun shuffleGiphy(message: Message): Result<Message> = flowChannelController.shuffleGiphy(message)
+    suspend fun sendGiphy(message: Message): Result<Message> = channelControllerStateFlow.sendGiphy(message)
+    suspend fun shuffleGiphy(message: Message): Result<Message> = channelControllerStateFlow.shuffleGiphy(message)
 
-    suspend fun sendImage(file: File): Result<String> = flowChannelController.sendImage(file)
-    suspend fun sendFile(file: File): Result<String> = flowChannelController.sendFile(file)
+    suspend fun sendImage(file: File): Result<String> = channelControllerStateFlow.sendImage(file)
+    suspend fun sendFile(file: File): Result<String> = channelControllerStateFlow.sendFile(file)
 
     suspend fun sendReaction(reaction: Reaction, enforceUnique: Boolean): Result<Reaction> =
-        flowChannelController.sendReaction(reaction, enforceUnique)
+        channelControllerStateFlow.sendReaction(reaction, enforceUnique)
 
-    suspend fun deleteReaction(reaction: Reaction): Result<Message> = flowChannelController.deleteReaction(reaction)
+    suspend fun deleteReaction(reaction: Reaction): Result<Message> = channelControllerStateFlow.deleteReaction(reaction)
 
-    internal fun upsertMessage(message: Message) = flowChannelController.upsertMessage(message)
+    internal fun upsertMessage(message: Message) = channelControllerStateFlow.upsertMessage(message)
 
-    override fun getMessage(messageId: String): Message? = flowChannelController.getMessage(messageId)
-    override fun clean() = flowChannelController.clean()
+    override fun getMessage(messageId: String): Message? = channelControllerStateFlow.getMessage(messageId)
+    override fun clean() = channelControllerStateFlow.clean()
 
-    fun setTyping(userId: String, event: ChatEvent?) = flowChannelController.setTyping(userId, event)
-    fun isHidden(): Boolean = flowChannelController.isHidden()
+    fun setTyping(userId: String, event: ChatEvent?) = channelControllerStateFlow.setTyping(userId, event)
+    fun isHidden(): Boolean = channelControllerStateFlow.isHidden()
 
-    internal suspend fun handleEvents(events: List<ChatEvent>) = flowChannelController.handleEvents(events)
-    internal fun handleEvent(event: ChatEvent) = flowChannelController.handleEvent(event)
+    internal suspend fun handleEvents(events: List<ChatEvent>) = channelControllerStateFlow.handleEvents(events)
+    internal fun handleEvent(event: ChatEvent) = channelControllerStateFlow.handleEvent(event)
 
-    fun upsertMembers(members: List<Member>) = flowChannelController.upsertMembers(members)
-    fun upsertMember(member: Member) = flowChannelController.upsertMember(member)
+    fun upsertMembers(members: List<Member>) = channelControllerStateFlow.upsertMembers(members)
+    fun upsertMember(member: Member) = channelControllerStateFlow.upsertMember(member)
 
-    fun updateLiveDataFromChannel(c: Channel) = flowChannelController.updateDataFromChannel(c)
+    fun updateLiveDataFromChannel(c: Channel) = channelControllerStateFlow.updateDataFromChannel(c)
 
-    suspend fun editMessage(message: Message): Result<Message> = flowChannelController.editMessage(message)
-    suspend fun deleteMessage(message: Message): Result<Message> = flowChannelController.deleteMessage(message)
+    suspend fun editMessage(message: Message): Result<Message> = channelControllerStateFlow.editMessage(message)
+    suspend fun deleteMessage(message: Message): Result<Message> = channelControllerStateFlow.deleteMessage(message)
 
-    override fun toChannel(): Channel = flowChannelController.toChannel()
+    override fun toChannel(): Channel = channelControllerStateFlow.toChannel()
 
     internal fun loadOlderThreadMessages(
         threadId: String,
         limit: Int,
         firstMessage: Message? = null,
-    ): Result<List<Message>> = flowChannelController.loadOlderThreadMessages(threadId, limit, firstMessage)
+    ): Result<List<Message>> = channelControllerStateFlow.loadOlderThreadMessages(threadId, limit, firstMessage)
 
     internal suspend fun loadMessageById(
         messageId: String,
         newerMessagesOffset: Int,
         olderMessagesOffset: Int,
-    ): Result<Message> = flowChannelController.loadMessageById(messageId, newerMessagesOffset, olderMessagesOffset)
+    ): Result<Message> = channelControllerStateFlow.loadMessageById(messageId, newerMessagesOffset, olderMessagesOffset)
 
-    internal fun replyMessage(repliedMessage: Message?) = flowChannelController.replyMessage(repliedMessage)
+    internal fun replyMessage(repliedMessage: Message?) = channelControllerStateFlow.replyMessage(repliedMessage)
 }
