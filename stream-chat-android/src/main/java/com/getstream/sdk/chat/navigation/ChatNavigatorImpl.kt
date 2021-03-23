cdca724254a25053cb4d9763f6527494166b54d5
Jc Miñarro <josecarlos.minarro@gmail.com>
2021-03-23 18:26:44 +0100

    Hide internal class

--- stream-chat-android/src/main/java/com/getstream/sdk/chat/navigation/ChatNavigatorImpl.kt
+++ stream-chat-android/src/main/java/com/getstream/sdk/chat/navigation/ChatNavigatorImpl.kt
@@ -1,10 +1,8 @@
 package com.getstream.sdk.chat.navigation
 
 import com.getstream.sdk.chat.navigation.destinations.ChatDestination
-import io.getstream.chat.android.core.internal.InternalStreamChatApi
 
-@InternalStreamChatApi
-public class ChatNavigatorImpl(private val handler: ChatNavigationHandler) : ChatNavigator {
+internal class ChatNavigatorImpl(private val handler: ChatNavigationHandler) : ChatNavigator {
 
     override fun navigate(destination: ChatDestination) {
         val handled = handler.navigate(destination)
@@ -17,10 +15,10 @@ public class ChatNavigatorImpl(private val handler: ChatNavigationHandler) : Cha
         destination.navigate()
     }
 
-    public companion object {
+    companion object {
 
         @JvmField
-        public val EMPTY_HANDLER: ChatNavigationHandler = object : ChatNavigationHandler {
+        val EMPTY_HANDLER: ChatNavigationHandler = object : ChatNavigationHandler {
             override fun navigate(destination: ChatDestination) = false
         }
     }
