3300e742fdddaa0dfb0d2c1ddae5dc94974a6b4e
Oleg Kuzmin <oleg@getstream.io>
2021-03-24 18:12:30 +0100

    Convert tests to simple unit tests. Cover insert methods

--- stream-chat-android-offline/src/test/java/io/getstream/chat/android/livedata/repository/ChannelConfigRepositoryTest.kt
+++ stream-chat-android-offline/src/test/java/io/getstream/chat/android/livedata/repository/ChannelConfigRepositoryTest.kt
@@ -1,47 +1,73 @@
 package io.getstream.chat.android.livedata.repository
 
-import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.google.common.truth.Truth
-import io.getstream.chat.android.livedata.BaseDomainTest2
-import io.getstream.chat.android.livedata.model.ChannelConfig
-import kotlinx.coroutines.runBlocking
+import com.nhaarman.mockitokotlin2.argThat
+import com.nhaarman.mockitokotlin2.mock
+import com.nhaarman.mockitokotlin2.verify
+import io.getstream.chat.android.livedata.randomChannelConfig
+import io.getstream.chat.android.livedata.randomConfig
+import io.getstream.chat.android.livedata.repository.domain.channelconfig.ChannelConfigDao
+import io.getstream.chat.android.livedata.repository.domain.channelconfig.ChannelConfigEntity
+import io.getstream.chat.android.livedata.repository.domain.channelconfig.ChannelConfigRepository
+import io.getstream.chat.android.livedata.repository.domain.channelconfig.ChannelConfigRepositoryImpl
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runBlockingTest
+import org.junit.Before
 import org.junit.Test
-import org.junit.runner.RunWith
 
-@RunWith(AndroidJUnit4::class)
-internal class ChannelConfigRepositoryTest : BaseDomainTest2() {
-    private val repositoryFacade by lazy { chatDomainImpl.repos }
+@ExperimentalCoroutinesApi
+internal class ChannelConfigRepositoryTest {
 
-    @Test
-    fun testInsertAndRead() = runBlocking {
-        repositoryFacade.insertChannelConfig(ChannelConfig("messaging", data.config1))
-        val config = repositoryFacade.selectChannelConfig("messaging")
-        Truth.assertThat(config).isEqualTo(config)
+    private lateinit var dao: ChannelConfigDao
+    private lateinit var sut: ChannelConfigRepository
+
+    @Before
+    fun before() {
+        dao = mock()
+        sut = ChannelConfigRepositoryImpl(dao)
     }
 
     @Test
-    fun testLoadAndRead() = runBlocking {
-        repositoryFacade.insertChannelConfig(ChannelConfig("messaging", data.config1))
-        repositoryFacade.clearChannelConfigsCache()
-        var config = repositoryFacade.selectChannelConfig("messaging")
-        Truth.assertThat(config).isNull()
-        repositoryFacade.cacheChannelConfigs()
-        config = repositoryFacade.selectChannelConfig("messaging")
-        Truth.assertThat(config?.config).isEqualTo(data.config1)
+    fun `When insert channel config Should store this value in DB`() = runBlockingTest {
+        sut.insertChannelConfig(randomChannelConfig(type = "messaging", config = randomConfig(name = "configName")))
+
+        verify(dao).insert(
+            argThat<ChannelConfigEntity> {
+                channelConfigInnerEntity.channelType == "messaging" &&
+                    channelConfigInnerEntity.name == "configName"
+            }
+        )
     }
 
     @Test
-    fun testUpdate() = runBlocking {
-        val config1 = data.config1
-        repositoryFacade.insertChannelConfig(ChannelConfig("messaging", config1))
-        val config2 = data.config1.copy(maxMessageLength = 200)
-        repositoryFacade.insertChannelConfig(ChannelConfig("messaging", config2))
+    fun `Given inserted channel When select config Should get config from cache`() = runBlockingTest {
+        sut.insertChannelConfig(randomChannelConfig(type = "messaging", config = randomConfig(name = "configName")))
 
-        repositoryFacade.clearChannelConfigsCache()
-        repositoryFacade.cacheChannelConfigs()
+        val result = sut.selectChannelConfig("messaging")
 
-        val config = repositoryFacade.selectChannelConfig("messaging")?.config
-        Truth.assertThat(config).isEqualTo(config2)
-        Truth.assertThat(config!!.maxMessageLength).isEqualTo(200)
+        Truth.assertThat(result).isNotNull()
+        Truth.assertThat(result!!.config.name).isEqualTo("configName")
+    }
+
+    @Test
+    fun `When insert configs Should store these values in DB`() = runBlockingTest {
+        val config1 = randomChannelConfig(type = "messaging1", config = randomConfig(name = "configName1"))
+        val config2 = randomChannelConfig(type = "messaging2", config = randomConfig(name = "configName2"))
+
+        sut.insertChannelConfigs(listOf(config1, config2))
+
+        verify(dao).insert(
+            argThat<List<ChannelConfigEntity>> {
+                size == 2 &&
+                    any {
+                        it.channelConfigInnerEntity.channelType == "messaging1" &&
+                            it.channelConfigInnerEntity.name == "configName1"
+                    } &&
+                    any {
+                        it.channelConfigInnerEntity.channelType == "messaging2" &&
+                            it.channelConfigInnerEntity.name == "configName2"
+                    }
+            }
+        )
     }
 }
