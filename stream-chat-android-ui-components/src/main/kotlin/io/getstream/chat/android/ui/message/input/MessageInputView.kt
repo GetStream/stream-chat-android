f96cb4bdb3d815f4c2f110221cb68cfa6ee344fc
David Marin <david.marin@hopin.to>
2021-03-24 09:46:06 +0100

    Remove hardcoded background from the message input view and merging the layout to avoid adding the extra ConstraintLayout

--- stream-chat-android-ui-components/src/main/kotlin/io/getstream/chat/android/ui/message/input/MessageInputView.kt
+++ stream-chat-android-ui-components/src/main/kotlin/io/getstream/chat/android/ui/message/input/MessageInputView.kt
@@ -7,6 +7,7 @@ import android.content.Context
 import android.util.AttributeSet
 import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.view.isVisible
+import androidx.core.view.updatePadding
 import com.getstream.sdk.chat.model.AttachmentMetaData
 import com.getstream.sdk.chat.utils.extensions.focusAndShowKeyboard
 import com.getstream.sdk.chat.utils.extensions.inflater
@@ -193,7 +194,7 @@ public class MessageInputView : ConstraintLayout {
 
     @SuppressLint("CustomViewStyleable")
     private fun init(context: Context, attr: AttributeSet? = null) {
-        binding = StreamUiMessageInputBinding.inflate(context.inflater, this, true)
+        binding = StreamUiMessageInputBinding.inflate(context.inflater, this)
         style = MessageInputViewStyle(context, attr)
 
         configAttachmentButton()
@@ -205,6 +206,8 @@ public class MessageInputView : ConstraintLayout {
         binding.dismissInputMode.setOnClickListener { dismissInputMode(inputMode) }
         setMentionsEnabled(style.mentionsEnabled)
         setCommandsEnabled(style.commandsEnabled)
+        val horizontalPadding = resources.getDimensionPixelSize(R.dimen.stream_ui_spacing_tiny)
+        updatePadding(left = horizontalPadding, right = horizontalPadding)
     }
 
     private fun dismissInputMode(inputMode: InputMode) {
