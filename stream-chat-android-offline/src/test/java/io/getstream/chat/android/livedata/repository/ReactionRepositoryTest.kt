a755947e1b0aa0466c254a828c14b0aae95c8977
Samuel Urbanowicz <samiuelson@gmail.com>
2021-03-26 20:05:50 +0100

    Refactor ReactionRepositoryTest.kt
    * Decouple test from BaseDomainTest class
    * Refactor it to be a unit test

--- stream-chat-android-offline/src/test/java/io/getstream/chat/android/livedata/repository/ReactionRepositoryTest.kt
+++ stream-chat-android-offline/src/test/java/io/getstream/chat/android/livedata/repository/ReactionRepositoryTest.kt
@@ -1,86 +1,102 @@
 package io.getstream.chat.android.livedata.repository
 
-import androidx.test.ext.junit.runners.AndroidJUnit4
-import com.google.common.truth.Truth
-import com.nhaarman.mockitokotlin2.any
-import com.nhaarman.mockitokotlin2.doReturn
+import com.nhaarman.mockitokotlin2.argThat
+import com.nhaarman.mockitokotlin2.mock
+import com.nhaarman.mockitokotlin2.verify
+import com.nhaarman.mockitokotlin2.verifyZeroInteractions
 import com.nhaarman.mockitokotlin2.whenever
-import io.getstream.chat.android.client.models.Message
-import io.getstream.chat.android.client.utils.Result
+import io.getstream.chat.android.client.models.Reaction
+import io.getstream.chat.android.client.models.User
 import io.getstream.chat.android.client.utils.SyncStatus
-import io.getstream.chat.android.livedata.BaseDomainTest
-import io.getstream.chat.android.test.TestCall
+import io.getstream.chat.android.livedata.randomReaction
+import io.getstream.chat.android.livedata.randomUser
+import io.getstream.chat.android.livedata.repository.domain.reaction.ReactionDao
+import io.getstream.chat.android.livedata.repository.domain.reaction.ReactionRepository
+import io.getstream.chat.android.livedata.repository.domain.reaction.ReactionRepositoryImpl
+import io.getstream.chat.android.livedata.repository.domain.reaction.toEntity
+import io.getstream.chat.android.test.randomString
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.runBlocking
+import kotlinx.coroutines.test.runBlockingTest
 import org.amshove.kluent.`should be equal to`
-import org.amshove.kluent.`should not be`
 import org.junit.Before
 import org.junit.Test
-import org.junit.runner.RunWith
+import org.junit.jupiter.api.assertThrows
 
-@RunWith(AndroidJUnit4::class)
-internal class ReactionRepositoryTest : BaseDomainTest() {
-    private val helper by lazy { chatDomainImpl.repos }
+@ExperimentalCoroutinesApi
+internal class ReactionRepositoryTest {
+    private lateinit var reactionDao: ReactionDao
+    private lateinit var currentUser: User
+    private lateinit var reactionRepo: ReactionRepository
 
     @Before
-    fun beforeEach() {
+    fun setup() {
         runBlocking {
-            helper.insertMessage(data.message1)
+            currentUser = randomUser()
+            reactionDao = mock()
+            reactionRepo = ReactionRepositoryImpl(reactionDao) { currentUser }
         }
     }
 
     @Test
-    fun testInsertAndRead() = runBlocking {
-        helper.insertReaction(data.reaction1)
-        val reaction = helper.selectUserReactionsToMessage(data.reaction1.messageId, data.reaction1.user!!.id).first()
-        Truth.assertThat(reaction).isEqualTo(data.reaction1)
+    fun `Reaction should be stored in Db`() = runBlockingTest {
+        val messgId = randomString(10)
+        val reaction = randomReaction(messageId = messgId, user = currentUser, type = "love")
+
+        reactionRepo.insertReaction(reaction)
+
+        verify(reactionDao).insert(
+            argThat {
+                this.messageId == messageId && this.userId == currentUser.id
+            }
+        )
     }
 
     @Test
-    fun testSyncNeeded() = runBlocking {
-        data.reaction1.syncStatus = SyncStatus.FAILED_PERMANENTLY
-        val reaction2 =
-            data.reaction1.copy().apply { type = "love"; syncStatus = SyncStatus.SYNC_NEEDED }
-        helper.insertReaction(data.reaction1)
-        helper.insertReaction(reaction2)
-        var reactions = helper.selectReactionsSyncNeeded()
-        Truth.assertThat(reactions.size).isEqualTo(1)
-        Truth.assertThat(reactions.first().syncStatus).isEqualTo(SyncStatus.SYNC_NEEDED)
+    fun `Shouldn't allow storing reaction with empty messageId`() = runBlockingTest {
+        val reaction = Reaction(messageId = "", user = currentUser, type = "love")
 
-        reactions = chatDomainImpl.retryReactions()
-        Truth.assertThat(reactions.size).isEqualTo(1)
-        Truth.assertThat(reactions.first().syncStatus).isEqualTo(SyncStatus.COMPLETED)
+        assertThrows<IllegalArgumentException> { reactionRepo.insertReaction(reaction) }
 
-        reactions = helper.selectReactionsSyncNeeded()
-        Truth.assertThat(reactions.size).isEqualTo(0)
+        verifyZeroInteractions(reactionDao)
     }
 
     @Test
-    fun testUpdate() = runBlocking {
-        val reaction1Updated =
-            data.reaction1.copy().apply { extraData = mutableMapOf("theanswer" to 42.0) }
-        helper.insertReaction(data.reaction1)
-        helper.insertReaction(reaction1Updated)
+    fun `Shouldn't allow storing reaction with empty type`() = runBlockingTest {
+        val reaction = Reaction(messageId = randomString(10), user = currentUser, type = "")
 
-        val reaction = helper.selectUserReactionsToMessage(data.reaction1.messageId, data.reaction1.user!!.id).first()
-        Truth.assertThat(reaction).isEqualTo(reaction1Updated)
+        assertThrows<IllegalArgumentException> { reactionRepo.insertReaction(reaction) }
+
+        verifyZeroInteractions(reactionDao)
     }
 
     @Test
-    fun `When sending and deleting a reaction while offline Then retry reaction invocation should sync the deleted reaction`(): Unit =
-        runBlocking {
-            chatDomainImpl.setOffline()
-            channelControllerImpl.sendReaction(data.reaction1, enforceUnique = false)
-            channelControllerImpl.deleteReaction(data.reaction1)
-            val reaction = chatDomainImpl.repos
-                .selectUserReactionsToMessage(data.message1.id, data.user1.id)
-                .first()
+    fun `Shouldn't allow storing reaction with empty user`() = runBlockingTest {
+        val reaction = Reaction(messageId = randomString(10), user = null, type = "love")
 
-            reaction.syncStatus `should be equal to` SyncStatus.SYNC_NEEDED
-            reaction.deletedAt `should not be` null
+        assertThrows<IllegalArgumentException> { reactionRepo.insertReaction(reaction) }
 
-            whenever(client.deleteReaction(any(), any())) doReturn TestCall(Result(Message()))
-            val reactions = chatDomainImpl.retryReactions()
+        verifyZeroInteractions(reactionDao)
+    }
 
-            reactions.size `should be equal to` 1
+    @Test
+    fun `Should return reactions with syncNeeded==true containing user data`() = runBlockingTest {
+        whenever(reactionDao.selectSyncNeeded()).thenReturn(listOf(randomReaction(syncStatus = SyncStatus.SYNC_NEEDED).toEntity()))
+
+        reactionRepo.selectReactionsSyncNeeded().apply {
+            this.size `should be equal to` 1
+            this.first().user `should be equal to` currentUser
         }
+    }
+
+    @Test
+    fun `Should return ReactionEntity containing user data`() = runBlockingTest {
+        val messageId = randomString(10)
+        whenever(reactionDao.selectUserReactionsToMessage(messageId, currentUser.id)).thenReturn(listOf(randomReaction(syncStatus = SyncStatus.SYNC_NEEDED).toEntity()))
+
+        reactionRepo.selectUserReactionsToMessage(messageId, currentUser.id).apply {
+            this.size `should be equal to` 1
+            this.first().user `should be equal to` currentUser
+        }
+    }
 }
