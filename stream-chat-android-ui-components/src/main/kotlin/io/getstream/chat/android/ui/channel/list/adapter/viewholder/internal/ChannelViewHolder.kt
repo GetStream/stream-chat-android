4b94fca2059f0594537a9a7d11607a26e9fa0fad
Dmitrii Bychkov <mail@dbychkov.com>
2021-03-26 04:40:23 +0300

    [CAS-812] Add missing attributes on ChannelListViewStyle

--- stream-chat-android-ui-components/src/main/kotlin/io/getstream/chat/android/ui/channel/list/adapter/viewholder/internal/ChannelViewHolder.kt
+++ stream-chat-android-ui-components/src/main/kotlin/io/getstream/chat/android/ui/channel/list/adapter/viewholder/internal/ChannelViewHolder.kt
@@ -1,5 +1,6 @@
 package io.getstream.chat.android.ui.channel.list.adapter.viewholder.internal
 
+import android.content.res.ColorStateList
 import android.view.View
 import android.view.ViewGroup
 import androidx.core.view.doOnNextLayout
@@ -25,8 +26,8 @@ import io.getstream.chat.android.ui.common.extensions.internal.getLastMessage
 import io.getstream.chat.android.ui.common.extensions.internal.getLastMessagePreviewText
 import io.getstream.chat.android.ui.common.extensions.internal.isMessageRead
 import io.getstream.chat.android.ui.common.extensions.internal.isNotNull
-import io.getstream.chat.android.ui.common.extensions.internal.setTextSizePx
 import io.getstream.chat.android.ui.common.extensions.isCurrentUserOwnerOrAdmin
+import io.getstream.chat.android.ui.databinding.StreamUiChannelListItemBackgroundViewBinding
 import io.getstream.chat.android.ui.databinding.StreamUiChannelListItemForegroundViewBinding
 import io.getstream.chat.android.ui.databinding.StreamUiChannelListItemViewBinding
 
@@ -66,6 +67,8 @@ internal class ChannelViewHolder @JvmOverloads constructor(
                 deleteImageView.setOnClickListener {
                     channelDeleteListener.onClick(channel)
                 }
+
+                applyStyle(style)
             }
 
             itemForegroundView.apply {
@@ -123,21 +126,36 @@ internal class ChannelViewHolder @JvmOverloads constructor(
         return openedX.coerceAtMost(closedX)..openedX.coerceAtLeast(closedX)
     }
 
-    private fun configureBackground() {
-        configureDeleteButton()
+    override fun isSwipeEnabled(): Boolean {
+        return optionsCount > 0 && style.swipeEnabled
     }
 
-    private fun configureDeleteButton() {
-        val canDeleteChannel = channel.members.isCurrentUserOwnerOrAdmin()
-        binding.itemBackgroundView.deleteImageView.apply {
-            if (canDeleteChannel) {
-                optionsCount = 2
+    private fun configureBackground() {
+        configureBackgroundButtons()
+    }
+
+    private fun configureBackgroundButtons() {
+        var optionsCount = 0
+
+        binding.itemBackgroundView.moreOptionsImageView.apply {
+            if (style.optionsEnabled) {
                 isVisible = true
+                optionsCount++
             } else {
-                optionsCount = 1
                 isVisible = false
             }
         }
+        binding.itemBackgroundView.deleteImageView.apply {
+            val canDeleteChannel = channel.members.isCurrentUserOwnerOrAdmin()
+            if (canDeleteChannel && style.deleteEnabled) {
+                isVisible = true
+                optionsCount++
+            } else {
+                isVisible = false
+            }
+        }
+
+        this.optionsCount = optionsCount
     }
 
     private fun configureForeground(diff: ChannelListPayloadDiff) {
@@ -219,11 +237,11 @@ internal class ChannelViewHolder @JvmOverloads constructor(
         val lastMessageByCurrentUserWasRead = channel.isMessageRead(lastMessage)
         when {
             !currentUserSentLastMessage || lastMessageByCurrentUserWasRead -> {
-                messageStatusImageView.setImageResource(R.drawable.stream_ui_ic_check_double)
+                messageStatusImageView.setImageDrawable(style.indicatorReadIcon)
             }
 
             currentUserSentLastMessage && !lastMessageByCurrentUserWasRead -> {
-                messageStatusImageView.setImageResource(R.drawable.stream_ui_ic_check_single)
+                messageStatusImageView.setImageDrawable(style.indicatorSentIcon)
             }
 
             else -> determineLastMessageSyncStatus(lastMessage)
@@ -233,11 +251,11 @@ internal class ChannelViewHolder @JvmOverloads constructor(
     private fun StreamUiChannelListItemForegroundViewBinding.determineLastMessageSyncStatus(message: Message) {
         when (message.syncStatus) {
             SyncStatus.IN_PROGRESS, SyncStatus.SYNC_NEEDED -> {
-                messageStatusImageView.setImageResource(R.drawable.stream_ui_ic_clock)
+                messageStatusImageView.setImageDrawable(style.indicatorPendingSyncIcon)
             }
 
             SyncStatus.COMPLETED -> {
-                messageStatusImageView.setImageResource(R.drawable.stream_ui_ic_check_single)
+                messageStatusImageView.setImageDrawable(style.indicatorSentIcon)
             }
 
             SyncStatus.FAILED_PERMANENTLY -> {
@@ -246,11 +264,18 @@ internal class ChannelViewHolder @JvmOverloads constructor(
         }
     }
 
+    private fun StreamUiChannelListItemBackgroundViewBinding.applyStyle(style: ChannelListViewStyle) {
+        root.setBackgroundColor(style.backgroundLayoutColor)
+        deleteImageView.setImageDrawable(style.deleteIcon)
+        moreOptionsImageView.setImageDrawable(style.optionsIcon)
+    }
+
     private fun StreamUiChannelListItemForegroundViewBinding.applyStyle(style: ChannelListViewStyle) {
-        binding.apply {
-            channelNameLabel.setTextSizePx(style.channelTitleTextSize)
-            lastMessageLabel.setTextSizePx(style.lastMessageSize)
-            lastMessageTimeLabel.setTextSizePx(style.lastMessageDateTextSize)
-        }
+        root.backgroundTintList = ColorStateList.valueOf(style.foregroundLayoutColor)
+        style.channelTitleText.apply(channelNameLabel)
+        style.lastMessageText.apply(lastMessageLabel)
+        style.lastMessageDateText.apply(lastMessageTimeLabel)
+        style.unreadMessageCounterText.apply(unreadCountBadge)
+        unreadCountBadge.backgroundTintList = ColorStateList.valueOf(style.unreadMessageCounterBackgroundColor)
     }
 }
