8002297cbbfdd63dc36c8af2c729aeff61661197
Leandro Borges Ferreira <lehen01@gmail.com>
2021-03-29 16:35:02 -0300

    [CAS-585] Removing BaseConnectedIntegrationTest from ChatErrorTest

--- stream-chat-android-offline/src/test/java/io/getstream/chat/android/livedata/ChatErrorTest.kt
+++ stream-chat-android-offline/src/test/java/io/getstream/chat/android/livedata/ChatErrorTest.kt
@@ -3,38 +3,33 @@ package io.getstream.chat.android.livedata
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import com.google.common.truth.Truth
 import io.getstream.chat.android.client.errors.ChatNetworkError
-import io.getstream.chat.android.client.models.Message
 import io.getstream.chat.android.livedata.extensions.isPermanent
 import org.junit.Test
 import org.junit.runner.RunWith
 
 @RunWith(AndroidJUnit4::class)
-internal class ChatErrorTest : BaseConnectedIntegrationTest() {
-    // https://getstream.io/chat/docs/api_errors_response/?language=js
+internal class ChatErrorTest {
+
     @Test
     fun invalidMessageInput() {
-        val message = Message(text = "hi", id = "thesame")
-        client.sendMessage("messaging", data.channel1.id, message).execute()
-        // this will always fail since the id is the same
-        val result2 = client.sendMessage("messaging", data.channel1.id, message).execute()
-        val error = result2.error()
+        val error = ChatNetworkError.create(4, "a message with ID thesame already exists", 400, null)
         Truth.assertThat(error.isPermanent()).isTrue()
     }
 
     @Test
-    fun rateLimit() {
+    fun `rateLimit error should be temporary`() {
         val error = ChatNetworkError.create(9, "", 429, null)
         Truth.assertThat(error.isPermanent()).isFalse()
     }
 
     @Test
-    fun `request timeout should be temporary`() {
+    fun `request timeout should be a temporary error`() {
         val error = ChatNetworkError.create(23, "", 408, null)
         Truth.assertThat(error.isPermanent()).isFalse()
     }
 
     @Test
-    fun brokenAPI() {
+    fun `broken api should be a temporary error`() {
         val error = ChatNetworkError.create(0, "", 500, null)
         Truth.assertThat(error.isPermanent()).isFalse()
     }
