485dea82accd4cf28741c42cf01b632031263faf
Oleg Kuzmin <oleg@getstream.io>
2021-03-29 15:25:45 +0200

    Add test for many selection

--- stream-chat-android-offline/src/test/java/io/getstream/chat/android/livedata/repository/QueryChannelsImplRepositoryTest.kt
+++ stream-chat-android-offline/src/test/java/io/getstream/chat/android/livedata/repository/QueryChannelsImplRepositoryTest.kt
@@ -8,6 +8,7 @@ import com.nhaarman.mockitokotlin2.verify
 import com.nhaarman.mockitokotlin2.whenever
 import io.getstream.chat.android.client.api.models.ContainsFilterObject
 import io.getstream.chat.android.client.api.models.FilterObject
+import io.getstream.chat.android.client.api.models.NeutralFilterObject
 import io.getstream.chat.android.client.api.models.QuerySort
 import io.getstream.chat.android.client.models.Channel
 import io.getstream.chat.android.client.models.Filters
@@ -16,7 +17,9 @@ import io.getstream.chat.android.livedata.repository.domain.queryChannels.QueryC
 import io.getstream.chat.android.livedata.repository.domain.queryChannels.QueryChannelsEntity
 import io.getstream.chat.android.livedata.repository.domain.queryChannels.QueryChannelsRepositoryImpl
 import io.getstream.chat.android.livedata.repository.domain.queryChannels.QueryChannelsWithSorts
+import io.getstream.chat.android.test.randomString
 import kotlinx.coroutines.test.runBlockingTest
+import org.junit.Assert
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 
@@ -50,7 +53,11 @@ internal class QueryChannelsImplRepositoryTest {
 
     @Test
     fun `Given query channels spec in DB When select by id Should return not null result`() = runBlockingTest {
-        whenever(dao.select("id1")) doReturn randomQueryChannelsWithSorts(id = "id1", filterObject = Filters.contains("cid", "cid1"), cids = listOf("cid1"))
+        whenever(dao.select("id1")) doReturn randomQueryChannelsWithSorts(
+            id = "id1",
+            filterObject = Filters.contains("cid", "cid1"),
+            cids = listOf("cid1")
+        )
 
         val result = sut.selectById("id1")
 
@@ -61,7 +68,39 @@ internal class QueryChannelsImplRepositoryTest {
         Truth.assertThat(result.cids).isEqualTo(listOf("cid1"))
     }
 
-    private fun randomQueryChannelsWithSorts(id: String, filterObject: FilterObject, cids: List<String>): QueryChannelsWithSorts {
+    @Test
+    fun `Given no row in DB with such id When select by id Should return null`() = runBlockingTest {
+        whenever(dao.select("id1")) doReturn null
+
+        val result = sut.selectById("id1")
+
+        Truth.assertThat(result).isNull()
+    }
+
+    @Test
+    fun `Given query channels specs in DB When select by ids Should return according results`() = runBlockingTest {
+        whenever(dao.select(listOf("id1", "id2"))) doReturn listOf(
+            randomQueryChannelsWithSorts(
+                cids = listOf(
+                    "cid1",
+                    "cid2"
+                )
+            ),
+            randomQueryChannelsWithSorts(cids = listOf("cid2", "cid3"))
+        )
+
+        val result = sut.selectQueriesChannelsByIds(listOf("id1", "id2"))
+
+        Truth.assertThat(result.size).isEqualTo(2)
+        Assert.assertTrue(result.any { it.cids == listOf("cid1", "cid2") })
+        Assert.assertTrue(result.any { it.cids == listOf("cid2", "cid3") })
+    }
+
+    private fun randomQueryChannelsWithSorts(
+        id: String = randomString(),
+        filterObject: FilterObject = NeutralFilterObject,
+        cids: List<String>,
+    ): QueryChannelsWithSorts {
         val query = QueryChannelsEntity(id = id, filter = filterObject, cids = cids)
         return QueryChannelsWithSorts(query, emptyList())
     }
