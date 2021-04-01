579589547bfb8338f1a2d0fd7d331ff289196c2d
Oleg Kuzmin <oleg@getstream.io>
2021-04-01 18:31:48 +0200

    Test for offline

--- stream-chat-android-offline/src/test/java/io/getstream/chat/android/offline/querychannels/WhenQuery.kt
+++ stream-chat-android-offline/src/test/java/io/getstream/chat/android/offline/querychannels/WhenQuery.kt
@@ -2,11 +2,18 @@ package io.getstream.chat.android.offline.querychannels
 
 import com.nhaarman.mockitokotlin2.any
 import com.nhaarman.mockitokotlin2.doReturn
+import com.nhaarman.mockitokotlin2.eq
 import com.nhaarman.mockitokotlin2.mock
+import com.nhaarman.mockitokotlin2.verify
 import com.nhaarman.mockitokotlin2.whenever
 import io.getstream.chat.android.client.ChatClient
+import io.getstream.chat.android.client.api.models.QuerySort
+import io.getstream.chat.android.client.models.Channel
+import io.getstream.chat.android.client.models.Filters
 import io.getstream.chat.android.client.utils.Result
 import io.getstream.chat.android.livedata.ChatDomainImpl
+import io.getstream.chat.android.livedata.controller.QueryChannelsSpec
+import io.getstream.chat.android.livedata.repository.RepositoryFacade
 import io.getstream.chat.android.test.TestCall
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.TestCoroutineScope
@@ -18,27 +25,43 @@ import org.junit.jupiter.api.Test
 internal class WhenQuery {
     private lateinit var sut: QueryChannelsController
     private lateinit var chatClient: ChatClient
+    private lateinit var repositories: RepositoryFacade
+    private lateinit var scope: TestCoroutineScope
+    private lateinit var chatDomainImpl: ChatDomainImpl
+    private val channelId: String = "channelId"
 
     @BeforeEach
     fun before() {
-        val chatDomainImpl = mock<ChatDomainImpl> {
-            on { scope } doReturn TestCoroutineScope()
-            on { repos } doReturn mock()
+        scope = TestCoroutineScope()
+        repositories = mock()
+        chatDomainImpl = mock {
+            on { scope } doReturn scope
+            on { repos } doReturn repositories
         }
         chatClient = mock()
         sut = QueryChannelsController(mock(), mock(), chatClient, chatDomainImpl)
+
+        whenever(chatClient.queryChannels(any())) doReturn TestCall(Result(mock()))
     }
 
     @Test
-    fun `Given already loading state Should return error for query`() = runBlockingTest {
-        pauseDispatcher {
-            whenever(chatClient.queryChannels(any())) doReturn TestCall(Result(emptyList()))
-            sut.query()
+    fun `Should request query channels spec in DB`() = runBlockingTest {
+        sut.query()
 
-            val result = sut.query()
+        verify(repositories).selectById(any())
+    }
 
-            // TODO should return error
-            // Truth.assertThat(result.isError).isTrue()
-        }
+    @Test
+    fun `Given DB with query channels Should invoke selectAndEnrichChannels in ChatDomain`() = runBlockingTest {
+        whenever(repositories.selectById(any())) doReturn QueryChannelsSpec(
+            Filters.neutral(),
+            QuerySort.Companion.desc(Channel::lastMessageAt),
+            cids = listOf("cid1", "cid2")
+        )
+        whenever(chatDomainImpl.selectAndEnrichChannels(any(), any())) doReturn emptyList()
+
+        sut.query()
+
+        verify(chatDomainImpl).selectAndEnrichChannels(eq(listOf("cid1", "cid2")), any())
     }
 }
