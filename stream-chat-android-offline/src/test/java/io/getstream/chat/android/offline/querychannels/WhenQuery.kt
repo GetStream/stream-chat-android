20f619fea894128cf0cf2dea7f7f9ee622bda096
Oleg Kuzmin <oleg@getstream.io>
2021-04-02 10:21:41 +0200

    Add tests for DB channel whenQuery

--- stream-chat-android-offline/src/test/java/io/getstream/chat/android/offline/querychannels/WhenQuery.kt
+++ stream-chat-android-offline/src/test/java/io/getstream/chat/android/offline/querychannels/WhenQuery.kt
@@ -1,6 +1,8 @@
 package io.getstream.chat.android.offline.querychannels
 
+import com.google.common.truth.Truth
 import com.nhaarman.mockitokotlin2.any
+import com.nhaarman.mockitokotlin2.doAnswer
 import com.nhaarman.mockitokotlin2.doReturn
 import com.nhaarman.mockitokotlin2.eq
 import com.nhaarman.mockitokotlin2.mock
@@ -12,7 +14,9 @@ import io.getstream.chat.android.client.models.Channel
 import io.getstream.chat.android.client.models.Filters
 import io.getstream.chat.android.client.utils.Result
 import io.getstream.chat.android.livedata.ChatDomainImpl
+import io.getstream.chat.android.livedata.controller.ChannelControllerImpl
 import io.getstream.chat.android.livedata.controller.QueryChannelsSpec
+import io.getstream.chat.android.livedata.randomChannel
 import io.getstream.chat.android.livedata.repository.RepositoryFacade
 import io.getstream.chat.android.test.TestCall
 import kotlinx.coroutines.ExperimentalCoroutinesApi
@@ -39,7 +43,7 @@ internal class WhenQuery {
             on { repos } doReturn repositories
         }
         chatClient = mock()
-        sut = QueryChannelsController(mock(), mock(), chatClient, chatDomainImpl)
+        sut = QueryChannelsController(Filters.neutral(), mock(), chatClient, chatDomainImpl)
 
         whenever(chatClient.queryChannels(any())) doReturn TestCall(Result(mock()))
     }
@@ -63,5 +67,41 @@ internal class WhenQuery {
         sut.query()
 
         verify(chatDomainImpl).selectAndEnrichChannels(eq(listOf("cid1", "cid2")), any())
+    }
+
+    @Test
+    fun `Should request channels to chat client`() = runBlockingTest {
+        sut.query()
+
+        verify(chatClient).queryChannels(any())
+    }
+
+    @Test
+    fun `Given channels in DB and failed network request Should return channels from DB`() = runBlockingTest {
+        whenever(repositories.selectById(any())) doReturn QueryChannelsSpec(
+            Filters.neutral(),
+            QuerySort.Companion.desc(Channel::lastMessageAt),
+            cids = listOf("cid1", "cid2")
+        )
+        val dbChannel1 = randomChannel(cid = "cid1")
+        val dbChannel2 = randomChannel(cid = "cid2")
+        whenever(chatDomainImpl.channel(any<String>())) doAnswer { invocationOnMock ->
+            val cid = invocationOnMock.arguments[0] as String
+            mock<ChannelControllerImpl> {
+                on { toChannel() } doReturn if (cid == "cid1") dbChannel1 else dbChannel2
+            }
+        }
+        whenever(chatDomainImpl.channel(any<Channel>())) doAnswer { invocationOnMock ->
+            val channel = invocationOnMock.arguments[0] as Channel
+            mock<ChannelControllerImpl> {
+                on { toChannel() } doReturn channel
+            }
+        }
+        whenever(chatDomainImpl.selectAndEnrichChannels(any(), any())) doReturn listOf(dbChannel1, dbChannel2)
+
+        val result = sut.query()
+
+        Truth.assertThat(result.isSuccess).isTrue()
+        Truth.assertThat(result.data()).isEqualTo(listOf(dbChannel1, dbChannel2))
     }
 }
