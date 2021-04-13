14134e49a8036abfe9812f1bd6c40949073e5092
Leandro Borges Ferreira <lehen01@gmail.com>
2021-04-13 15:02:13 -0300

    [CAS-874] Fixing visibility of StylesHolder

--- stream-chat-android-ui-components/src/main/kotlin/io/getstream/chat/android/ui/style/StylesHolder.kt
+++ stream-chat-android-ui-components/src/main/kotlin/io/getstream/chat/android/ui/style/StylesHolder.kt
@@ -2,7 +2,6 @@ package io.getstream.chat.android.ui.style
 
 import io.getstream.chat.android.ui.message.list.MessageListViewStyle
 
-public object StylesHolder {
-    // public var messageListItemStyle: MessageListItemStyle? = null
-    public var messageListViewStyle: MessageListViewStyle? = null
+internal object StylesHolder {
+    var messageListViewStyle: MessageListViewStyle? = null
 }
