ad9661281b9c58371a3e18bb0f1574692abac0f4
Leandro Borges Ferreira <lehen01@gmail.com>
2021-04-19 15:01:08 -0300

    [CAS-837] Fixing KDoc

--- stream-chat-android-ui-components/src/main/kotlin/io/getstream/chat/android/ui/channel/list/ChannelListViewStyle.kt
+++ stream-chat-android-ui-components/src/main/kotlin/io/getstream/chat/android/ui/channel/list/ChannelListViewStyle.kt
@@ -33,6 +33,8 @@ import io.getstream.chat.android.ui.common.style.TextStyle
  * @property foregroundLayoutColor - foreground color for [ChannelViewHolder]. Default - [R.color.stream_ui_white_snow]
  * @property unreadMessageCounterText - appearance for message counter text, displayed in [ChannelViewHolder]
  * @property unreadMessageCounterBackgroundColor - background color for message counter, displayed in [ChannelViewHolder]. Default - [R.color.stream_ui_accent_red]
+ * @property mutedChannelIcon - icon for muted channel, displayed in [ChannelViewHolder]. Default - [R.drawable.stream_ui_ic_mute]
+ * @property mutedChannelIconTint - tint for mutedChannelIcon. Default - [R.color.stream_ui_black]
  */
 public data class ChannelListViewStyle(
     public val optionsIcon: Drawable,
