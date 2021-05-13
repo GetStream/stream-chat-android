d74952cc4da0a8656324f846074d1a8c7c8e3115
Leandro Borges Ferreira <lehen01@gmail.com>
2021-05-13 12:28:29 -0300

    [CAS-1007] Check for changes in the repo

--- scripts/dokkasaurus-ci-check.sh
+++ scripts/dokkasaurus-ci-check.sh
@@ -1,18 +1,24 @@
-./gradlew dokkaHtmlMultiModule
-git clone --depth 1 --branch 0.3.0 git@github.com:GetStream/DokkasaurusSidebar.git
-python DokkasaurusSidebar/ci.py \
-./build/dokka/htmlMultiModule \
-Dokka \
- 5 \
- ./ \
- True \
- True \
-./dokkasaurus_filter.json
+# ./gradlew dokkaHtmlMultiModule
+# git clone --depth 1 --branch 0.3.0 git@github.com:GetStream/DokkasaurusSidebar.git
+# python DokkasaurusSidebar/ci.py \
+# ./build/dokka/htmlMultiModule \
+# Dokka \
+#  5 \
+#  ./ \
+#  True \
+#  True \
+# ./dokkasaurus_filter.json
+#
+# mkdir documentation/docs/dokka
+# mv ./build/dokka/htmlMultiModule/_category_.json documentation/docs/dokka/_category_.json
+# mv ./build/dokka/htmlMultiModule/stream-chat-android-client documentation/docs/dokka
+# mv ./build/dokka/htmlMultiModule/stream-chat-android-offline documentation/docs/dokka
+# mv ./build/dokka/htmlMultiModule/stream-chat-android-ui-components documentation/docs/dokka
 
-mkdir documentation/docs/dokka
-mv ./build/dokka/htmlMultiModule/_category_.json documentation/docs/dokka/_category_.json
-mv ./build/dokka/htmlMultiModule/stream-chat-android-client documentation/docs/dokka
-mv ./build/dokka/htmlMultiModule/stream-chat-android-offline documentation/docs/dokka
-mv ./build/dokka/htmlMultiModule/stream-chat-android-ui-components documentation/docs/dokka
+diff_output=$(git status)
 
-git diff
+if [[ $diff_output == *"nothing to commit, working tree clean"* ]]; then
+  echo "It's there!"
+fi
+
+echo $diff_output
