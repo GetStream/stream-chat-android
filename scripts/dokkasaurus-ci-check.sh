c2232235344a3e71030af5042496e92ca83fabe0
Leandro Borges Ferreira <lehen01@gmail.com>
2021-05-13 12:32:45 -0300

    [CAS-1007] adding whole script

--- scripts/dokkasaurus-ci-check.sh
+++ scripts/dokkasaurus-ci-check.sh
@@ -1,21 +1,21 @@
 set -e
 
-# ./gradlew dokkaHtmlMultiModule
-# git clone --depth 1 --branch 0.3.0 git@github.com:GetStream/DokkasaurusSidebar.git
-# python DokkasaurusSidebar/ci.py \
-# ./build/dokka/htmlMultiModule \
-# Dokka \
-#  5 \
-#  ./ \
-#  True \
-#  True \
-# ./dokkasaurus_filter.json
-#
-# mkdir documentation/docs/dokka
-# mv ./build/dokka/htmlMultiModule/_category_.json documentation/docs/dokka/_category_.json
-# mv ./build/dokka/htmlMultiModule/stream-chat-android-client documentation/docs/dokka
-# mv ./build/dokka/htmlMultiModule/stream-chat-android-offline documentation/docs/dokka
-# mv ./build/dokka/htmlMultiModule/stream-chat-android-ui-components documentation/docs/dokka
+./gradlew dokkaHtmlMultiModule
+git clone --depth 1 --branch 0.3.0 git@github.com:GetStream/DokkasaurusSidebar.git
+python DokkasaurusSidebar/ci.py \
+./build/dokka/htmlMultiModule \
+Dokka \
+ 5 \
+ ./ \
+ True \
+ True \
+./dokkasaurus_filter.json
+
+mkdir documentation/docs/dokka
+mv ./build/dokka/htmlMultiModule/_category_.json documentation/docs/dokka/_category_.json
+mv ./build/dokka/htmlMultiModule/stream-chat-android-client documentation/docs/dokka
+mv ./build/dokka/htmlMultiModule/stream-chat-android-offline documentation/docs/dokka
+mv ./build/dokka/htmlMultiModule/stream-chat-android-ui-components documentation/docs/dokka
 
 diff_output=$(git status)
 
