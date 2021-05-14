51935d9761af55dd1c8cadcd3fb758a834800fb0
Niko Berry <39569270+nikomancy@users.noreply.github.com>
2021-05-14 10:47:28 -0400

    Update MessageListViewModelBinding.kt
    minor stylistic edit for concision.

--- stream-chat-android-ui-components/src/main/kotlin/io/getstream/chat/android/ui/message/list/viewmodel/MessageListViewModelBinding.kt
+++ stream-chat-android-ui-components/src/main/kotlin/io/getstream/chat/android/ui/message/list/viewmodel/MessageListViewModelBinding.kt
@@ -23,8 +23,8 @@ import io.getstream.chat.android.ui.message.list.MessageListView
  * Binds [MessageListView] with [MessageListViewModel], updating the view's state
  * based on data provided by the ViewModel, and forwarding View events to the ViewModel.
  *
- * This function sets listeners on the view and ViewModel. Make sure to call this method
- * first before setting any additional listeners on these objects yourself.
+ * This function sets listeners on the view and ViewModel. Call this method
+ * before setting any additional listeners on these objects yourself.
  */
 @JvmName("bind")
 public fun MessageListViewModel.bindView(view: MessageListView, lifecycleOwner: LifecycleOwner) {
