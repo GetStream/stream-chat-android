dd2cb4988cfb4717564ac04cf9f2e8ee59a9fbe2
Niko Berry <39569270+nikomancy@users.noreply.github.com>
2021-05-14 10:46:37 -0400

    Update MessageListHeaderViewBinding.kt
    minor stylistic edit for concision.

--- stream-chat-android-ui-components/src/main/kotlin/io/getstream/chat/android/ui/message/list/header/viewmodel/MessageListHeaderViewBinding.kt
+++ stream-chat-android-ui-components/src/main/kotlin/io/getstream/chat/android/ui/message/list/header/viewmodel/MessageListHeaderViewBinding.kt
@@ -17,8 +17,8 @@ import io.getstream.chat.android.ui.message.list.header.MessageListHeaderView
  * Binds [MessageListHeaderView] with [MessageListHeaderViewModel], updating the view's state
  * based on data provided by the ViewModel.
  *
- * This function sets listeners on the view and ViewModel. Make sure to call this method
- * first before setting any additional listeners on these objects yourself.
+ * This function sets listeners on the view and ViewModel. Call this method
+ * before setting any additional listeners on these objects yourself.
  */
 @JvmName("bind")
 public fun MessageListHeaderViewModel.bindView(view: MessageListHeaderView, lifecycle: LifecycleOwner) {
