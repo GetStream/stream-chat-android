daee1f61e0da84afaefa0261bf03c5acce41e56d
Niko Berry <39569270+nikomancy@users.noreply.github.com>
2021-05-14 10:40:08 -0400

    Update ChannelListHeaderViewModelBinding.kt
    minor stylistic edit for concision.

--- stream-chat-android-ui-components/src/main/kotlin/io/getstream/chat/android/ui/channel/list/header/viewmodel/ChannelListHeaderViewModelBinding.kt
+++ stream-chat-android-ui-components/src/main/kotlin/io/getstream/chat/android/ui/channel/list/header/viewmodel/ChannelListHeaderViewModelBinding.kt
@@ -9,8 +9,8 @@ import io.getstream.chat.android.ui.channel.list.header.ChannelListHeaderView
  * Binds [ChannelListHeaderView] with [ChannelListHeaderViewModel], updating the view's state
  * based on data provided by the ViewModel, and propagating view events to the ViewModel as needed.
  *
- * This function sets listeners on the view and ViewModel. Make sure to call this method
- * first before setting any additional listeners on these objects yourself.
+ * This function sets listeners on the view and ViewModel. Call this method
+ * before setting any additional listeners on these objects yourself.
  */
 @JvmName("bind")
 public fun ChannelListHeaderViewModel.bindView(view: ChannelListHeaderView, lifecycleOwner: LifecycleOwner) {
