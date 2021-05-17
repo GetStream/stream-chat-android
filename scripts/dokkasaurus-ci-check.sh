810c0fe7cdb2ecc8c3ff2034aa226022960c1212
Leandro Borges Ferreira <lehen01@gmail.com>
2021-05-17 08:02:56 -0300

    [CAS-1007] Updating error comment

--- scripts/dokkasaurus-ci-check.sh
+++ scripts/dokkasaurus-ci-check.sh
@@ -19,6 +19,7 @@ mv ./build/dokka/htmlMultiModule/stream-chat-android-ui-components documentation
 diff_output=$(git status)
 
 if [[ $diff_output != *"nothing to commit, working tree clean"* ]]; then
-  echo "Error! looks like there are changes in your documentation"
+  echo "Error! looks like there are changes in your documentation."
+  echo "Please update your documentation. You can run ./scripts/dokkasaurus.sh for that."
   exit 1
 fi
