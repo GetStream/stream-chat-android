43722c4df9bdd56046ffe88d8301273ffdb1e059
Leandro Borges Ferreira <lehen01@gmail.com>
2021-05-19 16:31:04 -0300

    [CAS-1007] Removing unnecessary scripts

--- scripts/dokkasaurus-ci-check.sh
+++ scripts/dokkasaurus-ci-check.sh
@@ -1,20 +1,6 @@
 set -e
 
-./gradlew dokkaHtmlMultiModule
-python temp/ci.py \
-./build/dokka/htmlMultiModule \
-Dokka \
- 5 \
- ./ \
- True \
- True \
-./dokkasaurus_filter.json
-
-mkdir documentation/docs/dokka
-mv ./build/dokka/htmlMultiModule/_category_.json documentation/docs/dokka/_category_.json
-mv ./build/dokka/htmlMultiModule/stream-chat-android-client documentation/docs/dokka
-mv ./build/dokka/htmlMultiModule/stream-chat-android-offline documentation/docs/dokka
-mv ./build/dokka/htmlMultiModule/stream-chat-android-ui-components documentation/docs/dokka
+./gradlew dokkaHtmlMultiModule docusaurusSidebar
 
 diff_output=$(git status)
 
--- scripts/dokkasaurus.sh
+++
@@ -1,8 +0,0 @@
-./gradlew dokkaHtmlMultiModule
-dokkasaurus build/dokka/htmlMultiModule Dokka --root-position 5 --auto-generated --multi-module --filter-file dokkasaurus_filter.json
-
-mkdir documentation/docs/dokka
-mv ./build/dokka/htmlMultiModule/_category_.json documentation/docs/dokka/_category_.json
-mv ./build/dokka/htmlMultiModule/stream-chat-android-client documentation/docs/dokka
-mv ./build/dokka/htmlMultiModule/stream-chat-android-offline documentation/docs/dokka
-mv ./build/dokka/htmlMultiModule/stream-chat-android-ui-components documentation/docs/dokka
