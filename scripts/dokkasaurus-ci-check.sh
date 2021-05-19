1f4499327aafe60dff875dabff9cdeb6fa4db438
Leandro Borges Ferreira <lehen01@gmail.com>
2021-05-19 17:00:29 -0300

    [CAS-1007] Updating check script

--- scripts/dokkasaurus-ci-check.sh
+++ scripts/dokkasaurus-ci-check.sh
@@ -1,12 +1,14 @@
 set -e
 
-  ./gradlew dokkaHtmlMultiModule
-  ./gradlew docusaurusSidebar
+./gradlew dokkaHtmlMultiModule
+./gradlew docusaurusSidebar
 
 diff_output=$(git status)
 
 if [[ $diff_output != *"nothing to commit, working tree clean"* ]]; then
   echo "Error! looks like there are changes in your documentation."
-  echo "Please update your documentation. You can run ./scripts/dokkasaurus.sh for that."
+  echo "Please update your documentation. You can run for that:"
+  echo "./gradlew dokkaHtmlMultiModule"
+  echo "./gradlew docusaurusSidebar"
   exit 1
 fi
