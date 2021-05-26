6bca4a3b4d89b51e3c4a64747316a21debd2107c
Leandro Borges Ferreira <lehen01@gmail.com>
2021-05-26 17:49:19 -0300

    [CAS-972] Removing comment

--- stream-chat-android-offline/src/test/java/io/getstream/chat/android/offline/integration/BaseConnectedIntegrationTest.kt
+++ stream-chat-android-offline/src/test/java/io/getstream/chat/android/offline/integration/BaseConnectedIntegrationTest.kt
@@ -47,7 +47,6 @@ internal open class BaseConnectedIntegrationTest : BaseDomainTest() {
         val backgroundSyncEnabled = false
         chatDomainImpl = ChatDomainImpl(
             client,
-            // data.user1,
             db,
             handler,
             offlineEnabled,
