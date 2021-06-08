243ac54a48be7a18641fe6ba15749080400cb8b4
Leandro Borges Ferreira <lehen01@gmail.com>
2021-06-08 17:11:05 -0300

    Name change

--- buildSrc/src/main/kotlin/com/getstream/sdk/chat/sidebar/IndexLinksFix.kt
+++ buildSrc/src/main/kotlin/com/getstream/sdk/chat/sidebar/IndexLinksFix.kt
@@ -14,6 +14,8 @@ fun fixFirstIndexLinksForFile(file: File, modulesToFix: List<String>) {
 private fun fixLink(line: String, modulesToFix: List<String>): String {
     var linkState = NavigationLinkState.NO_LINK
 
+    var contentStart = 0
+    var contentEnd = 0
     var linkStart = 0
     var linkEnd = 0
 
@@ -29,21 +31,27 @@ private fun fixLink(line: String, modulesToFix: List<String>): String {
 
             linkState == NavigationLinkState.COMMENT_2 && char == '[' && line[i - 1] == '/' -> {
                 linkState = NavigationLinkState.INSIDE_CONTENT
+                contentStart = i + 1
             }
 
             linkState == NavigationLinkState.INSIDE_CONTENT && char == ']' -> {
                 linkState = NavigationLinkState.OUTSIDE_CONTENT
+                contentEnd = i
             }
 
             linkState == NavigationLinkState.OUTSIDE_CONTENT && char == '(' -> {
                 linkState = NavigationLinkState.INSIDE_LINK
-                linkStart = i
+                linkStart = i + 1
             }
 
             linkState == NavigationLinkState.INSIDE_LINK && char == ')' -> {
                 linkEnd = i
 
-                return reduceParentDir(line, linkStart, linkEnd)
+                return if (shouldBeReduced(line, contentStart, contentEnd, linkStart, linkEnd, modulesToFix)) {
+                    reduceParentDir(line, linkStart, linkEnd)
+                } else {
+                    line
+                }
             }
         }
     }
@@ -51,23 +59,33 @@ private fun fixLink(line: String, modulesToFix: List<String>): String {
     return line
 }
 
-private fun hasModule(line: String, linkStart: Int, linkEnd: Int, modulesToFix: List<String>): Boolean {
-    val content = line.subSequence(linkStart, linkEnd)
+private fun shouldBeReduced(
+    line: String,
+    contentStart : Int,
+    contentEnd : Int,
+    linkStart: Int,
+    linkEnd: Int,
+    modulesToFix: List<String>
+): Boolean {
+    val linkContent = line.subSequence(contentStart, contentEnd)
+    val linkDestiny = line.subSequence(linkStart, linkEnd)
 
-    return modulesToFix.any { module -> module == content }
+    return modulesToFix.any { module ->
+        module == linkContent
+    } && linkDestiny.contains("../index.md")
 }
 
 
 private fun reduceParentDir(line: String, linkStart: Int, linkEnd: Int): String {
     return StringBuilder().apply {
-        append(line.subSequence(0, linkStart))
+        append(line.subSequence(0, linkStart - 1))
         append("(")
         append(line.subSequence(linkStart + parentPathSize(), linkEnd))
         append(line.subSequence(linkEnd, line.length))
     }.toString()
 }
 
-private fun parentPathSize() = "(../".length
+private fun parentPathSize() = "../".length
 
 private enum class NavigationLinkState {
     NO_LINK, COMMENT_1, COMMENT_2, INSIDE_CONTENT, OUTSIDE_CONTENT, INSIDE_LINK
