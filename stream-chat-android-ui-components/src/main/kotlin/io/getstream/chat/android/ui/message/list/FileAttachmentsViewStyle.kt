d62e3887958c45e5643aba3d8cdfcc28f0f776ab
Samuel Urbanowicz <samiuelson@gmail.com>
2021-06-11 16:06:37 +0200

    Parse attrs in FileAttachmentsViewStyle

--- stream-chat-android-ui-components/src/main/kotlin/io/getstream/chat/android/ui/message/list/FileAttachmentsViewStyle.kt
+++ stream-chat-android-ui-components/src/main/kotlin/io/getstream/chat/android/ui/message/list/FileAttachmentsViewStyle.kt
@@ -1,16 +1,73 @@
 package io.getstream.chat.android.ui.message.list
 
 import android.content.Context
-import android.graphics.Color
+import android.graphics.Typeface
+import android.graphics.drawable.Drawable
 import android.util.AttributeSet
 import androidx.annotation.ColorInt
+import io.getstream.chat.android.ui.R
+import io.getstream.chat.android.ui.common.extensions.internal.getColorCompat
+import io.getstream.chat.android.ui.common.extensions.internal.getDrawableCompat
+import io.getstream.chat.android.ui.common.extensions.internal.use
+import io.getstream.chat.android.ui.common.style.TextStyle
 
 public data class FileAttachmentsViewStyle(
-    @ColorInt val backgroundColor: Int,
+    @ColorInt public val backgroundColor: Int,
+    public val progressIcon: Drawable,
+    public val actionButtonIcon: Drawable,
+    val titleTextStyle: TextStyle,
+    val fileSizeTextStyle: TextStyle,
 ) {
     internal companion object {
         operator fun invoke(context: Context, attrs: AttributeSet?): FileAttachmentsViewStyle {
-            return FileAttachmentsViewStyle(Color.BLUE)
+            context.obtainStyledAttributes(attrs, R.styleable.FileAttachmentView).use { attrsArray ->
+                val progressIcon =
+                    attrsArray.getDrawable(R.styleable.FileAttachmentView_streamUiFileAttachmentProgressIcon)
+                        ?: context.getDrawableCompat(R.drawable.stream_ui_rotating_indeterminate_progress_gradient)!!
+
+                val bgColor = attrsArray.getColor(
+                    R.styleable.FileAttachmentView_streamUiFileAttachmentBackgroundColor,
+                    context.getColorCompat(R.color.stream_ui_grey_whisper)
+                )
+
+                val actionIcon =
+                    attrsArray.getDrawable(R.styleable.FileAttachmentView_streamUiFileAttachmentActionButton)
+                        ?: context.getDrawableCompat(R.drawable.stream_ui_ic_icon_download)!!
+
+                val titleTextStyle = TextStyle.Builder(attrsArray)
+                    .size(R.styleable.FileAttachmentView_streamUiFileAttachmentTitleTextSize)
+                    .color(
+                        R.styleable.FileAttachmentView_streamUiFileAttachmentTitleTextColor,
+                        context.getColorCompat(R.color.stream_ui_text_color_primary)
+                    )
+                    .font(
+                        R.styleable.FileAttachmentView_streamUiFileAttachmentTitleFontAssets,
+                        R.styleable.FileAttachmentView_streamUiFileAttachmentTitleTextFont
+                    )
+                    .style(R.styleable.FileAttachmentView_streamUiFileAttachmentTitleTextStyle, Typeface.NORMAL)
+                    .build()
+
+                val fileSizeTextStyle = TextStyle.Builder(attrsArray)
+                    .size(R.styleable.FileAttachmentView_streamUiFileAttachmentFileSizeTextSize)
+                    .color(
+                        R.styleable.FileAttachmentView_streamUiFileAttachmentFileSizeTextColor,
+                        context.getColorCompat(R.color.stream_ui_text_color_primary)
+                    )
+                    .font(
+                        R.styleable.FileAttachmentView_streamUiFileAttachmentFileSizeFontAssets,
+                        R.styleable.FileAttachmentView_streamUiFileAttachmentFileSizeTextFont
+                    )
+                    .style(R.styleable.FileAttachmentView_streamUiFileAttachmentTitleTextStyle, Typeface.NORMAL)
+                    .build()
+
+                return FileAttachmentsViewStyle(
+                    backgroundColor = bgColor,
+                    progressIcon = progressIcon,
+                    actionButtonIcon = actionIcon,
+                    titleTextStyle = titleTextStyle,
+                    fileSizeTextStyle = fileSizeTextStyle,
+                )
+            }
         }
     }
 }
