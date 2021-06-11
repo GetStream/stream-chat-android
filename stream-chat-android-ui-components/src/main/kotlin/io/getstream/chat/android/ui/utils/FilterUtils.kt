c7ffad4dc59a00ff88db33619c0bd361db4c610c
Leandro Borges Ferreira <lehen01@gmail.com>
2021-06-11 09:48:02 -0300

    [CAS-972] Removing log from FilterUtils

--- stream-chat-android-ui-components/src/main/kotlin/io/getstream/chat/android/ui/utils/FilterUtils.kt
+++ stream-chat-android-ui-components/src/main/kotlin/io/getstream/chat/android/ui/utils/FilterUtils.kt
@@ -1,7 +1,6 @@
 package io.getstream.chat.android.ui.utils
 
 import io.getstream.chat.android.client.api.models.FilterObject
-import io.getstream.chat.android.client.logger.ChatLogger
 import io.getstream.chat.android.client.models.Filters
 import io.getstream.chat.android.offline.ChatDomain
 
@@ -9,9 +8,6 @@ internal object FilterUtils {
     fun userFilter(chatDomain: ChatDomain): FilterObject {
         return chatDomain.user.value?.id?.let { id ->
             Filters.`in`("members", id)
-        } ?: Filters.neutral().also {
-            ChatLogger.get("ChannelListViewModel")
-                .logE("User is not set in ChatDomain, default filter won't work")
-        }
+        } ?: Filters.neutral()
     }
 }
