e762323fbf00f0542568aee36a83a15f94166d2a
Márton Braun <marton@getstream.io>
2021-06-23 09:16:27 +0200

    [CAS-1091] Mark AttachmentSelectionListener as experimental API
    Its callback parameters might change in the future as AttachmentMetadata
    class is not fully prepared for public use yet.

--- stream-chat-android-ui-components/src/main/kotlin/io/getstream/chat/android/ui/message/input/attachment/AttachmentSelectionListener.kt
+++ stream-chat-android-ui-components/src/main/kotlin/io/getstream/chat/android/ui/message/input/attachment/AttachmentSelectionListener.kt
@@ -1,7 +1,9 @@
 package io.getstream.chat.android.ui.message.input.attachment
 
 import com.getstream.sdk.chat.model.AttachmentMetaData
+import io.getstream.chat.android.core.ExperimentalStreamChatApi
 
+@ExperimentalStreamChatApi
 public fun interface AttachmentSelectionListener {
     /**
      * Called when attachment picking has been completed.
