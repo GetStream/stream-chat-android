52636d350d7fa90bb568b2695c1f9008c8c9a7bc
Leandro Borges Ferreira <lehen01@gmail.com>
2021-06-23 14:29:17 -0300

    [CAS-1074] Returning Worker when there's no pending attachment

--- stream-chat-android-offline/src/main/java/io/getstream/chat/android/offline/message/attachment/UploadAttachmentsWorker.kt
+++ stream-chat-android-offline/src/main/java/io/getstream/chat/android/offline/message/attachment/UploadAttachmentsWorker.kt
@@ -17,13 +17,21 @@ internal class UploadAttachmentsWorker(private val appContext: Context) {
                         setUserWithoutConnectingIfNeeded()
                     }
 
-                    chatClient.getCurrentUser()?.let(::setUser)
+                    chatClient.getCurrentUser()?.let(::initRepositoryFacade)
                 }
             }
 
             val message = domainImpl.repos.selectMessage(messageId)!!
-            val attachments = domainImpl.channel(channelType, channelId)
-                .uploadAttachments(message)
+
+            val hasPendingAttachment = message.attachments.any { attachment ->
+                attachment.uploadState == Attachment.UploadState.InProgress
+            }
+
+            if (!hasPendingAttachment) {
+                return Result.success(Unit)
+            }
+
+            val attachments = domainImpl.channel(channelType, channelId).uploadAttachments(message)
 
             if (attachments.all { it.uploadState == Attachment.UploadState.Success }) {
                 Result.success(Unit)
