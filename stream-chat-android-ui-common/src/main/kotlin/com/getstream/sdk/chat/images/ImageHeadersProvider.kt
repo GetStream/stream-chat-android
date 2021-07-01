a12782bd6feabcec4d9ed44e562c319ff8de655b
Rafał Adasiewicz <rafal@getstream.io>
2021-07-01 12:18:42 +0200

    Change DefaultImageHeadersProvider visibility

--- stream-chat-android-ui-common/src/main/kotlin/com/getstream/sdk/chat/images/ImageHeadersProvider.kt
+++ stream-chat-android-ui-common/src/main/kotlin/com/getstream/sdk/chat/images/ImageHeadersProvider.kt
@@ -1,12 +1,9 @@
 package com.getstream.sdk.chat.images
 
-import io.getstream.chat.android.core.internal.InternalStreamChatApi
-
 public interface ImageHeadersProvider {
     public fun getImageRequestHeaders(): Map<String, String>
 }
 
-@InternalStreamChatApi
-public object DefaultImageHeadersProvider : ImageHeadersProvider {
+internal object DefaultImageHeadersProvider : ImageHeadersProvider {
     override fun getImageRequestHeaders(): Map<String, String> = emptyMap()
 }
