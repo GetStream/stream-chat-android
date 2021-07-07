1bcc14d5b7f9dd444a1b880c166c72fee1062aae
Oleg Kuzmin <oleg@getstream.io>
2021-07-07 15:38:08 +0200

    Make chat module as a class field of the ChatClient

--- stream-chat-android-client/src/main/java/io/getstream/chat/android/client/api/ChatClientConfig.kt
+++ stream-chat-android-client/src/main/java/io/getstream/chat/android/client/api/ChatClientConfig.kt
@@ -1,6 +1,7 @@
 package io.getstream.chat.android.client.api
 
 import io.getstream.chat.android.client.logger.ChatLogger
+import io.getstream.chat.android.client.models.Config
 
 public class ChatClientConfig(
     public val apiKey: String,
@@ -16,4 +17,13 @@ public class ChatClientConfig(
     public var isAnonymous: Boolean = false
 
     internal var enableMoshi: Boolean = false
+
+    public var offlineMode: OfflineMode = OfflineMode.None
+        internal set
+}
+
+public sealed class OfflineMode {
+    public object None : OfflineMode()
+    public object Persistence : OfflineMode()
+    public object InMemory : OfflineMode()
 }
--- stream-chat-android-client/src/main/java/io/getstream/chat/android/client/offline/OfflineModule.kt
+++ stream-chat-android-client/src/main/java/io/getstream/chat/android/client/offline/OfflineModule.kt
@@ -1,32 +1,44 @@
 package io.getstream.chat.android.client.offline
 
 import android.content.Context
+import io.getstream.chat.android.client.ChatClient
 import io.getstream.chat.android.client.models.Config
 import io.getstream.chat.android.client.models.User
 import io.getstream.chat.android.client.offline.repository.RepositoryFacade
 import io.getstream.chat.android.client.offline.repository.builder.RepositoryFacadeBuilder
 import io.getstream.chat.android.client.offline.repository.database.ChatDatabase
+import io.getstream.chat.android.core.internal.coroutines.DispatcherProvider
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.SupervisorJob
+import kotlinx.coroutines.cancelChildren
 
-public class OfflineModule(
-    private val scope: CoroutineScope,
-    private val appContext: Context,
-    private val defaultConfig: Config,
-) {
+public class OfflineModule(private val appContext: Context, private val persist: Boolean) {
+    private val job = SupervisorJob()
+    private val scope = CoroutineScope(job + DispatcherProvider.IO)
+    private val defaultConfig = Config(isConnectEvents = true, isMutes = true)
 
     internal var database: ChatDatabase? = null
 
     private var repositoryFacade: RepositoryFacade = createNoOpsRepo()
 
-    public fun buildRepo(user: User, offlineEnabled: Boolean = true) {
+    internal fun init(chatClient: ChatClient) {
+        chatClient.preSetUserListeners.add(::buildRepo)
+        chatClient.disconnectListeners.add { dispose() }
+    }
+
+    private fun buildRepo(user: User) {
         repositoryFacade = RepositoryFacadeBuilder {
             context(appContext)
             database(database)
             currentUser(user)
             scope(scope)
             defaultConfig(defaultConfig)
-            setOfflineEnabled(offlineEnabled)
+            setOfflineEnabled(persist)
         }.build()
+    }
+
+    private fun dispose() {
+        job.cancelChildren()
     }
 
     private fun createNoOpsRepo() = RepositoryFacadeBuilder {
