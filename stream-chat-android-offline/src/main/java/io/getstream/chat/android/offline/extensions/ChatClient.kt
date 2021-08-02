8bf605a6fb294a084d4ddfddfa94d831b9487ac4
Oleg Kuzmin <oleg@getstream.io>
2021-08-02 16:46:04 +0200

    Use state extension instead of offlinePlugin

--- stream-chat-android-offline/src/main/java/io/getstream/chat/android/offline/extensions/ChatClient.kt
+++ stream-chat-android-offline/src/main/java/io/getstream/chat/android/offline/extensions/ChatClient.kt
@@ -2,8 +2,9 @@ package io.getstream.chat.android.offline.extensions
 
 import io.getstream.chat.android.client.ChatClient
 import io.getstream.chat.android.offline.plugin.OfflinePlugin
+import io.getstream.chat.android.offline.plugin.state.StateRegistry
 
-public val ChatClient.offlinePlugin: OfflinePlugin
-    get() = requireNotNull(plugins.firstOrNull { it.name == OfflinePlugin.MODULE_NAME } as? OfflinePlugin) {
+public val ChatClient.state: StateRegistry
+    get() = requireNotNull((plugins.firstOrNull { it.name == OfflinePlugin.MODULE_NAME } as? OfflinePlugin)?.state) {
         "Offline plugin must be configured in ChatClient"
     }
