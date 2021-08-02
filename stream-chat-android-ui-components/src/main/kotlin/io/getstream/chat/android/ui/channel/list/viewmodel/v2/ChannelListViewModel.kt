cb081334e94c3449bde7d1b7708765e27f910d32
Oleg Kuzmin <oleg@getstream.io>
2021-08-02 16:46:04 +0200

    Use state extension instead of offlinePlugin

--- stream-chat-android-ui-components/src/main/kotlin/io/getstream/chat/android/ui/channel/list/viewmodel/v2/ChannelListViewModel.kt
+++ stream-chat-android-ui-components/src/main/kotlin/io/getstream/chat/android/ui/channel/list/viewmodel/v2/ChannelListViewModel.kt
@@ -22,7 +22,7 @@ import io.getstream.chat.android.client.utils.Result
 import io.getstream.chat.android.core.internal.exhaustive
 import io.getstream.chat.android.livedata.utils.Event
 import io.getstream.chat.android.offline.ChatDomain
-import io.getstream.chat.android.offline.extensions.offlinePlugin
+import io.getstream.chat.android.offline.extensions.state
 import io.getstream.chat.android.offline.querychannels.state.ChannelsState
 import io.getstream.chat.android.offline.querychannels.state.QueryChannelsState
 import io.getstream.chat.android.ui.common.extensions.internal.isMuted
@@ -76,7 +76,7 @@ public class ChannelListViewModel(
     private fun initData(filterObject: FilterObject) {
         stateMerger.value = INITIAL_STATE
 
-        queryChannelsState = chatClient.offlinePlugin.state.queryChannels(filterObject, sort).also { queryChannelsState ->
+        queryChannelsState = chatClient.state.queryChannels(filterObject, sort).also { queryChannelsState ->
             val channelState = chatDomain.user.filterNotNull().flatMapConcat { currentUser ->
                 queryChannelsState.channelsState.map { channelState ->
                     channelState to currentUser
