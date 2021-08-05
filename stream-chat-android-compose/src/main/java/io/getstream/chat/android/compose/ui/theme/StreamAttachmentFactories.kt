aae581ad644946ff5ff8c4a1ff4d2c7596a3cc49
Filip Babic <fbabic96@gmail.com>
2021-08-05 16:12:19 +0200

    2128 - Changed the way we show and preview Link attachments

--- stream-chat-android-compose/src/main/java/io/getstream/chat/android/compose/ui/theme/StreamAttachmentFactories.kt
+++ stream-chat-android-compose/src/main/java/io/getstream/chat/android/compose/ui/theme/StreamAttachmentFactories.kt
@@ -62,7 +62,7 @@ public object StreamAttachmentFactories {
     public val defaultFactories: List<AttachmentFactory> = listOf(
         AttachmentFactory(
             { state -> LinkAttachmentFactory(state) },
-            { message -> message.attachments.any { it.ogUrl != null } }
+            { message -> message.attachments.any { it.titleLink != null || it.ogUrl != null } }
         ),
         AttachmentFactory(
             { state -> ImageAttachmentFactory(state) },
@@ -92,8 +92,13 @@ public object StreamAttachmentFactories {
         val (message, _) = messageItem
 
         val context = LocalContext.current
-        val attachment = message.attachments.firstOrNull { it.ogUrl != null }
-        val previewUrl = attachment?.ogUrl
+        val attachment = message.attachments.firstOrNull { it.titleLink != null || it.ogUrl != null }
+
+        requireNotNull(attachment) {
+            IllegalStateException("Missing link attachment.")
+        }
+
+        val previewUrl = attachment.titleLink ?: attachment.ogUrl
 
         requireNotNull(previewUrl) {
             IllegalStateException("Missing preview URL.")
