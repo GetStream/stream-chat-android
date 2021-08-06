88a016c9d5f25b47c99e0f17ce5863c81b1bff6e
Leandro Borges Ferreira <lehen01@gmail.com>
2021-08-06 10:33:43 -0300

    [CAS-1109] Applying stream inflater for dialogs and fragments

--- stream-chat-android-ui-components/src/main/kotlin/io/getstream/chat/android/ui/common/extensions/internal/ViewGroup.kt
+++ stream-chat-android-ui-components/src/main/kotlin/io/getstream/chat/android/ui/common/extensions/internal/ViewGroup.kt
@@ -1,11 +1,11 @@
 package io.getstream.chat.android.ui.common.extensions.internal
 
-import android.app.Activity
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.ViewGroup
 
 internal val ViewGroup.streamThemeInflater: LayoutInflater
     get() = LayoutInflater.from(context.createStreamThemeWrapper())
 
-internal val Activity.streamThemeInflater: LayoutInflater
+internal val Context.streamThemeInflater: LayoutInflater
     get() = LayoutInflater.from(this.createStreamThemeWrapper())
