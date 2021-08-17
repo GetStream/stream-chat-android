6dc0a8f5fef812bd27fd1f15b8705aceda38677b
bychkovdmitry <9600921+bychkovdmitry@users.noreply.github.com>
2021-08-18 01:13:37 +0300

    Update stream-chat-android-ui-components/src/main/kotlin/io/getstream/chat/android/ui/common/extensions/internal/Fragment.kt
    Co-authored-by: Márton Braun <marton@getstream.io>

--- stream-chat-android-ui-components/src/main/kotlin/io/getstream/chat/android/ui/common/extensions/internal/Fragment.kt
+++ stream-chat-android-ui-components/src/main/kotlin/io/getstream/chat/android/ui/common/extensions/internal/Fragment.kt
@@ -3,9 +3,5 @@ package io.getstream.chat.android.ui.common.extensions.internal
 import androidx.fragment.app.Fragment
 
 internal inline fun <reified T> Fragment.findListener(): T? {
-    return when {
-        parentFragment is T -> parentFragment as T
-        activity is T -> activity as T
-        else -> null
-    }
+    return (parentFragment as? T) ?: (activity as? T)
 }
