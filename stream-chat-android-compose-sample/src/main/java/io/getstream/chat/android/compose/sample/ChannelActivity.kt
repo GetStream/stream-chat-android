8a0a2feb7aec3e30d88f1f91829a5e8bb984193a
Filip Babic <fbabic96@gmail.com>
2021-08-17 14:42:45 +0200

    Removed redundant statement

--- stream-chat-android-compose-sample/src/main/java/io/getstream/chat/android/compose/sample/ChannelActivity.kt
+++ stream-chat-android-compose-sample/src/main/java/io/getstream/chat/android/compose/sample/ChannelActivity.kt
@@ -67,7 +67,6 @@ class ChannelActivity : AppCompatActivity() {
          * */
         setContent {
             ChatTheme {
-                ChatTheme.colors.appBackground
                 ChannelsScreen(
                     title = stringResource(id = R.string.app_name),
                     isShowingHeader = true,
