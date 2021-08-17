75fe971516e016fd935b5a556464ffe492ae200b
Leandro Borges Ferreira <lehen01@gmail.com>
2021-08-17 19:45:42 -0300

    [CAS-1198] Increasing max size in 5%

--- stream-chat-android-ui-components/src/main/kotlin/io/getstream/chat/android/ui/message/list/adapter/viewholder/decorator/internal/MaxPossibleWidthDecorator.kt
+++ stream-chat-android-ui-components/src/main/kotlin/io/getstream/chat/android/ui/message/list/adapter/viewholder/decorator/internal/MaxPossibleWidthDecorator.kt
@@ -42,7 +42,7 @@ internal class MaxPossibleWidthDecorator : BaseDecorator() {
     }
 
     companion object {
-        private const val MAX_POSSIBLE_WIDTH_FACTOR = .30f
+        private const val MAX_POSSIBLE_WIDTH_FACTOR = .25f
         private const val START_PERCENT = 0f
         private const val END_PERCENT = 1f
         private const val MINE_START_PERCENT = START_PERCENT + MAX_POSSIBLE_WIDTH_FACTOR
