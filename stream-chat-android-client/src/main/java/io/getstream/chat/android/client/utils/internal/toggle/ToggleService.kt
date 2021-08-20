448bc0c2edbf3f5d02e6d06c7b59daf48400b5cf
Oleg Kuzmin <oleg@getstream.io>
2021-08-20 18:04:30 +0200

    Toggle service init

---
+++ stream-chat-android-client/src/main/java/io/getstream/chat/android/client/utils/internal/toggle/ToggleService.kt
@@ -0,0 +1,41 @@
+package io.getstream.chat.android.client.utils.internal.toggle
+
+import android.content.Context
+import android.content.SharedPreferences
+import io.getstream.chat.android.core.internal.InternalStreamChatApi
+
+@InternalStreamChatApi
+public class ToggleService private constructor(private val sharedPreferences: SharedPreferences) {
+
+    public fun isEnabled(featureKey: String): Boolean = sharedPreferences.getBoolean(featureKey, false)
+
+    public fun setToggle(featureKey: String, value: Boolean) {
+        sharedPreferences.edit()
+            .putBoolean(featureKey, value)
+            .commit()
+    }
+
+    @Suppress("UNCHECKED_CAST")
+    public fun getToggles(): Map<String, Boolean> = sharedPreferences.all.filter { it.value is Boolean }.toMap() as Map<String, Boolean>
+
+    @InternalStreamChatApi
+    public companion object {
+        private const val PREFS_NAME = "toggle_storage"
+
+        private var instance: ToggleService? = null
+
+        @InternalStreamChatApi
+        public fun instance(): ToggleService = requireNotNull(instance) {
+            "Toggle service must be initialized via the init method!"
+        }
+
+        @InternalStreamChatApi
+        public fun init(appContext: Context, predefinedValues: Map<String, Boolean> = emptyMap()): ToggleService {
+            val sp = appContext.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE).also {
+                predefinedValues.entries.forEach { (key, value) -> it.edit().putBoolean(key, value).commit() }
+            }
+
+            return ToggleService(sp).also { instance = it }
+        }
+    }
+}
