a3a20f35e54165678e8bf88e56bd5a8564212e23
Jc Miñarro <josecarlos.minarro@gmail.com>
2021-08-26 13:58:12 +0200

    Configure maven-publish plugin to support SNAPSHOT versions

--- buildSrc/src/main/kotlin/io/getstream/chat/android/Configuration.kt
+++ buildSrc/src/main/kotlin/io/getstream/chat/android/Configuration.kt
@@ -4,6 +4,10 @@ object Configuration {
     const val compileSdkVersion = 30
     const val targetSdkVersion = 30
     const val minSdkVersion = 21
-    const val versionName = "4.17.0"
+    const val majorVersion = 4
+    const val minorVersion = 17
+    const val pathVersion = 0
+    const val versionName = "$majorVersion.$minorVersion.$pathVersion"
+    const val snapshotVersionName = "$majorVersion.$minorVersion.${pathVersion + 1}-SNAPSHOT"
     const val artifactGroup = "io.getstream"
 }
