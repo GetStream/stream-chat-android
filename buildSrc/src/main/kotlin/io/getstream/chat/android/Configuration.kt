08077fca8243b3b1a22415eca209d614b83ce1e8
Jc Miñarro <josecarlos.minarro@gmail.com>
2021-08-30 09:28:22 +0200

    Fix typo

--- buildSrc/src/main/kotlin/io/getstream/chat/android/Configuration.kt
+++ buildSrc/src/main/kotlin/io/getstream/chat/android/Configuration.kt
@@ -6,8 +6,8 @@ object Configuration {
     const val minSdkVersion = 21
     const val majorVersion = 4
     const val minorVersion = 17
-    const val pathVersion = 0
-    const val versionName = "$majorVersion.$minorVersion.$pathVersion"
-    const val snapshotVersionName = "$majorVersion.$minorVersion.${pathVersion + 1}-SNAPSHOT"
+    const val patchVersion = 0
+    const val versionName = "$majorVersion.$minorVersion.$patchVersion"
+    const val snapshotVersionName = "$majorVersion.$minorVersion.${patchVersion + 1}-SNAPSHOT"
     const val artifactGroup = "io.getstream"
 }
