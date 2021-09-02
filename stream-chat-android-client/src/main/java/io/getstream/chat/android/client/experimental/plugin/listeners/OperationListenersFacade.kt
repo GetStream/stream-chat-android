30d5d86d3b2e9328aa8607374d8ca41b7b9b9754
Oleg Kuzmin <oleg@getstream.io>
2021-09-02 16:40:56 +0200

    Deprecate createDistinctChannel

--- stream-chat-android-client/src/main/java/io/getstream/chat/android/client/experimental/plugin/listeners/OperationListenersFacade.kt
+++ stream-chat-android-client/src/main/java/io/getstream/chat/android/client/experimental/plugin/listeners/OperationListenersFacade.kt
@@ -3,4 +3,4 @@ package io.getstream.chat.android.client.experimental.plugin.listeners
 import io.getstream.chat.android.core.ExperimentalStreamChatApi
 
 @ExperimentalStreamChatApi
-public interface OperationListenersFacade : QueryChannelsListener, SendMessageListener
+public interface OperationListenersFacade : QueryChannelsListener, QueryChannelListener, SendMessageListener
--- stream-chat-android-client/src/main/java/io/getstream/chat/android/client/experimental/plugin/listeners/QueryChannelsListener.kt
+++ stream-chat-android-client/src/main/java/io/getstream/chat/android/client/experimental/plugin/listeners/QueryChannelsListener.kt
@@ -1,6 +1,5 @@
 package io.getstream.chat.android.client.experimental.plugin.listeners
 
-import io.getstream.chat.android.client.api.models.QueryChannelRequest
 import io.getstream.chat.android.client.api.models.QueryChannelsRequest
 import io.getstream.chat.android.client.models.Channel
 import io.getstream.chat.android.client.utils.Result
@@ -8,19 +7,6 @@ import io.getstream.chat.android.core.ExperimentalStreamChatApi
 
 @ExperimentalStreamChatApi
 public interface QueryChannelsListener {
-    public suspend fun onQueryChannelRequest(
-        channelType: String,
-        channelId: String,
-        request: QueryChannelRequest,
-    ) {}
-
-    public suspend fun onQueryChannelResult(
-        result: Result<Channel>,
-        channelType: String,
-        channelId: String,
-        request: QueryChannelRequest,
-    ) {}
-
     public suspend fun onQueryChannelsRequest(request: QueryChannelsRequest) {}
 
     public suspend fun onQueryChannelsResult(result: Result<List<Channel>>, request: QueryChannelsRequest) {}
