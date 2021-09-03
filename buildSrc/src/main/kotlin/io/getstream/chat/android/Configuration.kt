133162ea877fa68f71da59e96dd81a2178319861
Leandro Borges Ferreira <lehen01@gmail.com>
2021-09-03 12:28:47 -0300

    Fixing version for 4.17.3

--- buildSrc/src/main/kotlin/io/getstream/chat/android/Configuration.kt
+++ buildSrc/src/main/kotlin/io/getstream/chat/android/Configuration.kt
@@ -6,7 +6,7 @@ object Configuration {
     const val minSdkVersion = 21
     const val majorVersion = 4
     const val minorVersion = 17
-    const val patchVersion = 2
+    const val patchVersion = 3
     const val versionName = "$majorVersion.$minorVersion.$patchVersion"
     const val snapshotVersionName = "$majorVersion.$minorVersion.${patchVersion + 1}-SNAPSHOT"
     const val artifactGroup = "io.getstream"
