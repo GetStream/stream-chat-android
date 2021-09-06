4e00a45825815505daf9b6715b56a458721b0c0f
Dmitrii Bychkov <mail@dbychkov.com>
2021-09-06 23:54:15 +0300

    [CAS-1201] Add docs for search warnings

--- stream-chat-android-client/src/main/java/io/getstream/chat/android/client/models/SearchWarning.kt
+++ stream-chat-android-client/src/main/java/io/getstream/chat/android/client/models/SearchWarning.kt
@@ -1,5 +1,9 @@
 package io.getstream.chat.android.client.models
 
+/**
+ * Represents a warning related to message search results. For example, if there are more
+ * than 500 channels that match the channel filter.
+ */
 public data class SearchWarning(
     /**
      * Channel CIDs for the searched channels
