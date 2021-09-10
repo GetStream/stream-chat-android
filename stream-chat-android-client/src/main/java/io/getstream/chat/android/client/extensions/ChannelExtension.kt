1cb69c8824085c35162fb1b3fb76843894fa9d33
Rafał Adasiewicz <rafal@getstream.io>
2021-09-10 16:18:34 +0200

    Remove parameter from getUsersWithoutCurrent

--- stream-chat-android-client/src/main/java/io/getstream/chat/android/client/extensions/ChannelExtension.kt
+++ stream-chat-android-client/src/main/java/io/getstream/chat/android/client/extensions/ChannelExtension.kt
@@ -15,5 +15,5 @@ public fun Channel.isAnonymousChannel(): Boolean = id.isAnonymousChannelId()
 public fun Channel.isMutedFor(user: User): Boolean = user.channelMutes.any { mute -> mute.channel.cid == cid }
 
 @InternalStreamChatApi
-public fun Channel.getUsersExcludingCurrent(excludeUserId: String = ChatClient.instance().getCurrentUser()?.id ?: ""): List<User> =
-    members.map { it.user }.filterNot { it.id == excludeUserId }
+public fun Channel.getUsersExcludingCurrent(): List<User> =
+    members.map { it.user }.filterNot { it.id == ChatClient.instance().getCurrentUser()?.id ?: "" }
--- stream-chat-android-ui-common/src/main/kotlin/com/getstream/sdk/chat/utils/extensions/Channel.kt
+++ stream-chat-android-ui-common/src/main/kotlin/com/getstream/sdk/chat/utils/extensions/Channel.kt
@@ -3,9 +3,6 @@ package com.getstream.sdk.chat.utils.extensions
 import io.getstream.chat.android.client.extensions.getUsersExcludingCurrent
 import io.getstream.chat.android.client.models.Channel
 import io.getstream.chat.android.core.internal.InternalStreamChatApi
-import io.getstream.chat.android.offline.ChatDomain
 
 @InternalStreamChatApi
-public fun Channel.isDirectMessaging(currentUserId: String = currentUserId()): Boolean = getUsersExcludingCurrent(currentUserId).size == 1
-
-private fun currentUserId(): String = ChatDomain.instance().user.value?.id ?: ""
+public fun Channel.isDirectMessaging(): Boolean = getUsersExcludingCurrent().size == 1
