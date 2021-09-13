4d9903200725656c4f8e2bab005b4dab9fcd1596
Márton Braun <marton@getstream.io>
2021-09-13 13:56:46 +0200

    Reorganize push notification docs (#2328)
    * Reorganize push notification docs
    
    * Clarify some wording about ChatClient initialization
    
    Co-authored-by: Rafal Adasiewicz <rafal@getstream.io>

--- stream-chat-android-client/src/main/java/io/getstream/chat/android/client/notifications/handler/NotificationConfig.kt
+++ stream-chat-android-client/src/main/java/io/getstream/chat/android/client/notifications/handler/NotificationConfig.kt
@@ -1,8 +1,6 @@
 package io.getstream.chat.android.client.notifications.handler
 
-import android.content.Context
 import io.getstream.chat.android.client.R
-import io.getstream.chat.android.client.models.Device
 
 public data class NotificationConfig(
     val notificationChannelId: Int = R.string.stream_chat_notification_channel_id,
@@ -20,8 +18,3 @@ public data class NotificationConfig(
     val pushNotificationsEnabled: Boolean = true,
     val pushDeviceGenerators: List<PushDeviceGenerator> = listOf(),
 )
-
-public interface PushDeviceGenerator {
-    public fun isValidForThisDevice(context: Context): Boolean
-    public fun asyncGenerateDevice(onDeviceGenerated: (device: Device) -> Unit)
-}
