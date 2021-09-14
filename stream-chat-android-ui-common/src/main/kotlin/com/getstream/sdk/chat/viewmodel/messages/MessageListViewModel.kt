c2c025dac3d0b4fbcd47a78ecf35e9a4367d8347
Leandro Borges Ferreira <lehen01@gmail.com>
2021-09-14 16:50:47 -0300

    [CAS-1317] Default value fix

--- stream-chat-android-ui-common/src/main/kotlin/com/getstream/sdk/chat/viewmodel/messages/MessageListViewModel.kt
+++ stream-chat-android-ui-common/src/main/kotlin/com/getstream/sdk/chat/viewmodel/messages/MessageListViewModel.kt
@@ -196,7 +196,7 @@ public class MessageListViewModel @JvmOverloads constructor(
                 onBackButtonPressed()
             }
             is Event.DeleteMessage -> {
-                domain.deleteMessage(event.message, true).enqueue()
+                domain.deleteMessage(event.message, false).enqueue()
             }
             is Event.FlagMessage -> {
                 client.flagMessage(event.message.id).enqueue { result ->
