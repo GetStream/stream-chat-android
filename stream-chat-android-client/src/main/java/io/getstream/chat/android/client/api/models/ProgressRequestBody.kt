44e07de0106615888190d8ca3f6f2a3284dcd7bf
Márton Braun <marton@getstream.io>
2021-09-14 16:43:10 +0200

    Add upload progress size calculation

--- stream-chat-android-client/src/main/java/io/getstream/chat/android/client/api/models/ProgressRequestBody.kt
+++ stream-chat-android-client/src/main/java/io/getstream/chat/android/client/api/models/ProgressRequestBody.kt
@@ -2,6 +2,7 @@ package io.getstream.chat.android.client.api.models
 
 import android.os.Handler
 import android.os.Looper
+import io.getstream.chat.android.client.di.BaseChatModule
 import io.getstream.chat.android.client.extensions.getMediaType
 import io.getstream.chat.android.client.utils.ProgressCallback
 import okhttp3.MediaType
@@ -14,6 +15,8 @@ internal class ProgressRequestBody(
     private val file: File,
     private val callback: ProgressCallback
 ) : RequestBody() {
+
+    private var writeCount: Int = 0
 
     override fun contentType(): MediaType = file.getMediaType()
 
@@ -29,16 +32,35 @@ internal class ProgressRequestBody(
             var read: Int
             val handler = Handler(Looper.getMainLooper())
             while (fis.read(buffer).also { read = it } != -1) {
-                handler.post {
-                    callback.onProgress((100 * uploaded / total))
+                /**
+                 * Only do progress updates if we've already
+                 * ignored enough writes of this body.
+                 */
+                if (writeCount >= PROGRESS_UPDATES_TO_SKIP) {
+                    handler.post {
+                        callback.onProgress((100 * uploaded / total))
+                    }
                 }
                 uploaded += read.toLong()
                 sink.write(buffer, 0, read)
             }
         }
+
+        writeCount++
     }
 
     companion object {
         private const val DEFAULT_BUFFER_SIZE = 2048
+
+        /**
+         * A number of writes to ignore (not issue progress updates for).
+         *
+         * This accounts for the [HttpLoggingInterceptor] and [CurlInterceptor] configured in
+         * [BaseChatModule] that will trigger writing the body into their logs.
+         *
+         * We don't want to issue progress updates for these writes, only the real write that's
+         * going out to the network that happens after these.
+         */
+        private const val PROGRESS_UPDATES_TO_SKIP = 2
     }
 }
