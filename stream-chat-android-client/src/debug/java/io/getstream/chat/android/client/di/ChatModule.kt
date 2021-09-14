72864da8bb293f6c50d5bf65e686034f83f35dd2
Leandro Borges Ferreira <lehen01@gmail.com>
2021-09-14 16:39:59 -0300

    [CAS-1310] Adding custom api call logger interceptor

--- stream-chat-android-client/src/debug/java/io/getstream/chat/android/client/di/ChatModule.kt
+++ stream-chat-android-client/src/debug/java/io/getstream/chat/android/client/di/ChatModule.kt
@@ -2,10 +2,12 @@ package io.getstream.chat.android.client.di
 
 import android.content.Context
 import io.getstream.chat.android.client.api.ChatClientConfig
+import io.getstream.chat.android.client.api.interceptor.HttpLoggingInterceptor
 import io.getstream.chat.android.client.notifications.handler.ChatNotificationHandler
 import io.getstream.chat.android.client.parser.ChatParser
 import io.getstream.chat.android.client.token.TokenManager
 import io.getstream.chat.android.client.uploader.FileUploader
+import okhttp3.Interceptor
 import okhttp3.OkHttpClient
 import java.util.concurrent.Executor
 
@@ -22,7 +24,15 @@ internal class ChatModule(
     uploader: FileUploader?,
     tokenManager: TokenManager,
     callbackExecutor: Executor?,
-) : BaseChatModule(appContext, config, notificationsHandler, uploader, tokenManager, callbackExecutor) {
+    loggingInterceptor: Interceptor,
+) : BaseChatModule(appContext,
+    config,
+    notificationsHandler,
+    uploader,
+    tokenManager,
+    callbackExecutor,
+    loggingInterceptor
+) {
 
     override fun clientBuilder(
         timeout: Long,
