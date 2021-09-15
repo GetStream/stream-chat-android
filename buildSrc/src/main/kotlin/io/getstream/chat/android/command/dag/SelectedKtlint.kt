9a323801a3bdb0e44f38db7786fd150a71069adc
Leandro Borges Ferreira <lehen01@gmail.com>
2021-09-15 08:46:16 -0300

    buildSrc command

---
+++ buildSrc/src/main/kotlin/io/getstream/chat/android/command/dag/SelectedKtlint.kt
@@ -0,0 +1,12 @@
+package io.getstream.chat.android.command.dag
+
+import org.gradle.api.DefaultTask
+import org.gradle.api.tasks.TaskAction
+
+class SelectedKtlint: DefaultTask() {
+
+    @TaskAction
+    private fun command() {
+        println("To implement!")
+    }
+}
