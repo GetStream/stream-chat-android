eb0cfd2ed55081108ed721f9dfeb1c4a94f8c7e5
Leandro Borges Ferreira <lehen01@gmail.com>
2021-09-17 09:18:44 -0300

    [CAS-1305] Feature - Ktlint and unit tests for affected modules (#2337)
    * [CAS-1305] Adding dag-command
    
    * [CAS-1305] Adding dag-command
    
    * Adding android-cli
    
    * buildSrc command
    
    * [CAS-1305] Base for ktlintSelected command
    
    * [CAS-1305] Filtering modules with ktlint
    
    * [CAS-1305] Adding ktlint selection to buildSrc
    
    * [CAS-1305] Deleting android-cli
    
    * [CAS-1305] Adding selected unit tests with static module list
    
    * [CAS-1305] Filtering modules with unit tests in a non static way
    
    * [CAS-1305] Changing commands name and handling non existant changed modules file
    
    * [CAS-1305] Changing default branch
    
    * [CAS-1305] Fixing buildDir
    
    * [CAS-1305] Removing moshi dependency
    
    * [CAS-1305] FIxing generateGradleCommand package name
    
    * [CAS-1305] Code clean and error message
    
    * [CAS-1305] Code simplification
    
    * [CAS-1305] Fixing filter for unit tests
    
    * [CAS-1305] Using env var for github case
    
    * [CAS-1305] Fixing unit tests filter. Not filtering out application

---
+++ buildSrc/src/main/kotlin/io/getstream/chat/android/command/dag/unittest/plugin/UnitTestsCommandExtesion.kt
@@ -0,0 +1,6 @@
+package io.getstream.chat.android.command.dag.unittest.plugin
+
+open class UnitTestsCommandExtesion {
+    var changeModulesPath: String = "."
+    var outputPath: String = "unit-tests-selected.sh"
+}
