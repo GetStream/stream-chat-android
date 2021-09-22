f876d5a07c7caf77a387d91918b23e60e5b5ea9d
Leandro Borges Ferreira <lehen01@gmail.com>
2021-09-22 10:40:56 -0300

    Filtering modules with tests

--- buildSrc/src/main/kotlin/io/getstream/chat/android/command/dag/unittest/filter/UnitTestsFilter.kt
+++ buildSrc/src/main/kotlin/io/getstream/chat/android/command/dag/unittest/filter/UnitTestsFilter.kt
@@ -3,10 +3,20 @@ package io.getstream.chat.android.command.dag.unittest.filter
 import io.getstream.chat.android.command.dag.unittest.model.TestType
 import io.getstream.chat.android.command.utils.generateGradleCommand
 import org.gradle.api.Project
+import java.io.File
 
 fun List<String>.unitTestCommand(rootProject: Project): String {
+    val modulesWithTest = filterModulesWithTests()
+
     return filterUnitTestableModules(rootProject)
+        .filter { (testableModule, _) -> modulesWithTest.contains(testableModule) }
         .generateGradleCommand { (module, testType) -> "$module:${testType.testCommand}" }
+}
+
+private fun List<String>.filterModulesWithTests(): List<String> {
+    return filter { module ->
+        File("$module/src/test").exists()
+    }
 }
 
 private fun List<String>.filterUnitTestableModules(rootProject: Project): List<Pair<String, TestType>> {
@@ -16,10 +26,6 @@ private fun List<String>.filterUnitTestableModules(rootProject: Project): List<P
             val testType = when {
                 project.tasks.any { task -> task.name == "testDebugUnitTest" } -> {
                     TestType.ANDROID_LIBRARY_TEST
-                }
-
-                project.tasks.any { task -> task.name == "testDemoDebugUnitTest" } -> {
-                    TestType.APPLICATION_TEST
                 }
 
                 else -> TestType.JAVA_LIBRARY_TEST
--- buildSrc/src/main/kotlin/io/getstream/chat/android/command/dag/unittest/model/TestType.kt
+++ buildSrc/src/main/kotlin/io/getstream/chat/android/command/dag/unittest/model/TestType.kt
@@ -3,5 +3,4 @@ package io.getstream.chat.android.command.dag.unittest.model
 enum class TestType(val testCommand: String) {
     JAVA_LIBRARY_TEST("test"),
     ANDROID_LIBRARY_TEST("testDebugUnitTest"),
-    APPLICATION_TEST("testDemoDebugUnitTest"),
 }
