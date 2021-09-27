f4639c1cfeb94d97c35d5c54f42a6603ed1a6c24
Rafał Adasiewicz <rafal@getstream.io>
2021-09-27 17:34:48 +0200

    Remove handling NotificationChannelMutesUpdatedEvent in QueryChannelsController

--- stream-chat-android-offline/src/main/java/io/getstream/chat/android/offline/querychannels/QueryChannelsController.kt
+++ stream-chat-android-offline/src/main/java/io/getstream/chat/android/offline/querychannels/QueryChannelsController.kt
@@ -14,7 +14,6 @@ import io.getstream.chat.android.client.events.CidEvent
 import io.getstream.chat.android.client.events.MarkAllReadEvent
 import io.getstream.chat.android.client.events.NotificationAddedToChannelEvent
 import io.getstream.chat.android.client.events.NotificationChannelDeletedEvent
-import io.getstream.chat.android.client.events.NotificationChannelMutesUpdatedEvent
 import io.getstream.chat.android.client.events.UserPresenceChangedEvent
 import io.getstream.chat.android.client.events.UserStartWatchingEvent
 import io.getstream.chat.android.client.events.UserStopWatchingEvent
@@ -150,10 +149,6 @@ public class QueryChannelsController internal constructor(
 
         if (event is MarkAllReadEvent) {
             refreshAllChannels()
-        }
-
-        if (event is NotificationChannelMutesUpdatedEvent) {
-            _mutedChannelIds.value = event.me.channelMutes.toChannelsId()
         }
 
         if (event is CidEvent) {
