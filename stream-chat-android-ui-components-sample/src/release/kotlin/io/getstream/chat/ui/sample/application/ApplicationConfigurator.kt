3981cffb6852026405b0f553eb0948d9c23625dd
Jc Miñarro <josecarlos.minarro@gmail.com>
2021-09-27 15:37:56 +0200

    Update stream-chat-android-ui-components-sample/src/release/kotlin/io/getstream/chat/ui/sample/application/ApplicationConfigurator.kt
    Co-authored-by: Márton Braun <marton@getstream.io>

--- stream-chat-android-ui-components-sample/src/release/kotlin/io/getstream/chat/ui/sample/application/ApplicationConfigurator.kt
+++ stream-chat-android-ui-components-sample/src/release/kotlin/io/getstream/chat/ui/sample/application/ApplicationConfigurator.kt
@@ -3,6 +3,6 @@ package io.getstream.chat.ui.sample.application
 import android.app.Application
 
 object ApplicationConfigurator {
-    val HUAWEI_APP_ID = "104598369"
+    const val HUAWEI_APP_ID = "104598369"
     fun configureApp(application: Application) = Unit
 }
