af4d23059107c6cc4b26df40556c46ef5e9439b4
Jc Miñarro <josecarlos.minarro@gmail.com>
2021-09-29 15:36:24 +0200

    Update NotificationHandlerFactory

--- stream-chat-android-client/src/main/java/io/getstream/chat/android/client/notifications/handler/NotificationHandlerFactory.kt
+++ stream-chat-android-client/src/main/java/io/getstream/chat/android/client/notifications/handler/NotificationHandlerFactory.kt
@@ -1,10 +1,26 @@
 package io.getstream.chat.android.client.notifications.handler
 
 import android.content.Context
+import android.content.Intent
+import android.os.Build
 
-internal object NotificationHandlerFactory {
+public object NotificationHandlerFactory {
 
-    internal fun createNotificationHandler(context: Context, notificationConfig: NotificationConfig): NotificationHandler {
-        return ChatNotificationHandler(context, notificationConfig)
+    public var newMessageIntent: ((messageId: String, channelType: String, channelId: String) -> Intent)? = null
+
+    public fun createNotificationHandler(context: Context, notificationConfig: NotificationConfig): NotificationHandler {
+        return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            MessagingStyleNotificationHandler(context, notificationConfig, getNewMessageIntent(context))
+        } else {
+            ChatNotificationHandler(context, notificationConfig, getNewMessageIntent(context))
+        }
     }
+
+    private fun getNewMessageIntent(context: Context): (messageId: String, channelType: String, channelId: String) -> Intent {
+        return newMessageIntent
+            ?: { _, _, _ -> createDefaultNewMessageIntent(context) }
+    }
+
+    private fun createDefaultNewMessageIntent(context: Context): Intent =
+        context.packageManager!!.getLaunchIntentForPackage(context.packageName)!!
 }
