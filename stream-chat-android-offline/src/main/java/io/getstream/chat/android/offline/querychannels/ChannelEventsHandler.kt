b218678a9ca6f1bfb8a04ae04fcb5875e175e89c
Oleg Kuzmin <oleg@getstream.io>
2021-10-05 11:42:19 +0200

    Introduced new ChannelEventsHandler interface

---
+++ stream-chat-android-offline/src/main/java/io/getstream/chat/android/offline/querychannels/ChannelEventsHandler.kt
@@ -0,0 +1,56 @@
+package io.getstream.chat.android.offline.querychannels
+
+import io.getstream.chat.android.client.events.ChannelDeletedEvent
+import io.getstream.chat.android.client.events.ChannelUpdatedByUserEvent
+import io.getstream.chat.android.client.events.ChannelUpdatedEvent
+import io.getstream.chat.android.client.events.HasChannel
+import io.getstream.chat.android.client.events.NotificationAddedToChannelEvent
+import io.getstream.chat.android.client.events.NotificationChannelDeletedEvent
+
+/**
+ * Events handler that handles events related to channels and compute which kind of action should be applied.
+ */
+public fun interface ChannelEventsHandler {
+    /**
+     * Function that computes result of handling event.
+     *
+     * @param event Event that contains particular channel. See more [HasChannel]
+     * @return [EventHandlingResult] Result of handling.
+     */
+    public fun onChannelEvent(event: HasChannel): EventHandlingResult
+}
+
+/**
+ * Enum representing possible outcome of channels event handling.
+ */
+public enum class EventHandlingResult {
+    /**
+     * Add a channel to a query channels collection.
+     */
+    ADD,
+    /**
+     * Remove a channel from a query channels collection.
+     */
+    REMOVE,
+    /**
+     * Skip handling of this event.
+     */
+    SKIP
+}
+
+public abstract class BaseChannelEventsHandler : ChannelEventsHandler {
+    public abstract fun onNotificationAddedToChannelEvent(event: NotificationAddedToChannelEvent): EventHandlingResult
+    public abstract fun onChannelUpdatedByUserEvent(event: ChannelUpdatedByUserEvent): EventHandlingResult
+    public abstract fun onChannelUpdatedEvent(event: ChannelUpdatedEvent): EventHandlingResult
+
+    override fun onChannelEvent(event: HasChannel): EventHandlingResult {
+        return when (event) {
+            is NotificationAddedToChannelEvent -> onNotificationAddedToChannelEvent(event)
+            is ChannelDeletedEvent -> EventHandlingResult.REMOVE
+            is NotificationChannelDeletedEvent -> EventHandlingResult.REMOVE
+            is ChannelUpdatedByUserEvent -> onChannelUpdatedByUserEvent(event)
+            is ChannelUpdatedEvent -> onChannelUpdatedEvent(event)
+            else -> EventHandlingResult.SKIP
+        }
+    }
+}
