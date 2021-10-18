c75deaf7de03ac1c409464938d405fa17c7d684a
Oleg Kuzmin <oleg@getstream.io>
2021-10-18 15:47:57 +0200

    Use interface UserCredentialStorage in ChatClient

--- stream-chat-android-client/src/main/java/io/getstream/chat/android/client/ChatClient.kt
+++ stream-chat-android-client/src/main/java/io/getstream/chat/android/client/ChatClient.kt
@@ -84,6 +84,7 @@ import io.getstream.chat.android.client.token.TokenProvider
 import io.getstream.chat.android.client.uploader.FileUploader
 import io.getstream.chat.android.client.uploader.StreamCdnImageMimeTypes
 import io.getstream.chat.android.client.user.CredentialConfig
+import io.getstream.chat.android.client.user.storage.UserCredentialStorage
 import io.getstream.chat.android.client.utils.ProgressCallback
 import io.getstream.chat.android.client.utils.Result
 import io.getstream.chat.android.client.utils.TokenUtils
@@ -114,7 +115,7 @@ public class ChatClient internal constructor(
     private val tokenManager: TokenManager = TokenManagerImpl(),
     private val socketStateService: SocketStateService = SocketStateService(),
     private val queryChannelsPostponeHelper: QueryChannelsPostponeHelper,
-    private val encryptedUserConfigStorage: EncryptedPushNotificationsConfigStore,
+    private val userCredentialStorage: UserCredentialStorage,
     private val userStateService: UserStateService = UserStateService(),
     private val tokenUtils: TokenUtils = TokenUtils,
     private val scope: CoroutineScope,
@@ -326,7 +327,7 @@ public class ChatClient internal constructor(
             return
         }
 
-        encryptedUserConfigStorage.get()?.let { config ->
+        userCredentialStorage.get()?.let { config ->
             initializeClientWithUser(
                 user = User(id = config.userId).apply { name = config.userName },
                 tokenProvider = ConstantTokenProvider(config.userToken),
@@ -336,7 +337,7 @@ public class ChatClient internal constructor(
 
     @InternalStreamChatApi
     public fun containsStoredCredentials(): Boolean {
-        return encryptedUserConfigStorage.get() != null
+        return userCredentialStorage.get() != null
     }
 
     private fun notifySetUser(user: User) {
@@ -344,7 +345,7 @@ public class ChatClient internal constructor(
     }
 
     private fun storePushNotificationsConfig(userId: String, userName: String) {
-        encryptedUserConfigStorage.put(
+        userCredentialStorage.put(
             CredentialConfig(
                 userToken = getCurrentToken() ?: "",
                 userId = userId,
@@ -700,7 +701,7 @@ public class ChatClient internal constructor(
         socketStateService.onDisconnectRequested()
         userStateService.onLogout()
         socket.disconnect()
-        encryptedUserConfigStorage.clear()
+        userCredentialStorage.clear()
         lifecycleObserver.dispose()
     }
 
@@ -1595,6 +1596,7 @@ public class ChatClient internal constructor(
         private val tokenManager: TokenManager = TokenManagerImpl()
         private var plugins: List<Plugin> = emptyList()
         private var customOkHttpClient: OkHttpClient? = null
+        private var userCredentialStorage: UserCredentialStorage? = null
 
         /**
          * Sets the log level to be used by the client.
@@ -1757,6 +1759,10 @@ public class ChatClient internal constructor(
             plugins += plugin
         }
 
+        public fun credentialStorage(credentialStorage: UserCredentialStorage): Builder = apply {
+            userCredentialStorage = credentialStorage
+        }
+
         @InternalStreamChatApi
         @Deprecated(
             message = "It shouldn't be used outside of SDK code. Created for testing purposes",
@@ -1799,7 +1805,7 @@ public class ChatClient internal constructor(
                     customOkHttpClient,
                 )
 
-            val result = ChatClient(
+            return ChatClient(
                 config,
                 module.api(),
                 module.socket(),
@@ -1807,13 +1813,12 @@ public class ChatClient internal constructor(
                 tokenManager,
                 module.socketStateService,
                 module.queryChannelsPostponeHelper,
-                EncryptedPushNotificationsConfigStore(appContext),
+                userCredentialStorage = userCredentialStorage ?: EncryptedPushNotificationsConfigStore(appContext),
                 module.userStateService,
                 scope = module.networkScope,
                 appContext = appContext,
                 plugins = plugins,
             )
-            return result
         }
     }
 
