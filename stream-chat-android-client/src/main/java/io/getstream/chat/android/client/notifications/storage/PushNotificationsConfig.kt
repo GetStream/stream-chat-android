b0af874debcb20793db672a63f48c787ee950031
Oleg Kuzmin <oleg@getstream.io>
2021-10-18 14:59:58 +0200

    Introduced CredentialConfig

--- stream-chat-android-client/src/main/java/io/getstream/chat/android/client/notifications/storage/PushNotificationsConfig.kt
+++ stream-chat-android-client/src/main/java/io/getstream/chat/android/client/notifications/storage/PushNotificationsConfig.kt
@@ -1,9 +1,14 @@
 package io.getstream.chat.android.client.notifications.storage
 
-internal data class PushNotificationsConfig(
-    val userId: String,
-    val userToken: String,
-    val userName: String,
-) {
-    internal fun isValid(): Boolean = userId.isNotEmpty() && userToken.isNotEmpty() && userName.isNotEmpty()
+import io.getstream.chat.android.client.user.CredentialConfig
+
+internal class PushNotificationsConfig(private val credentialConfig: CredentialConfig) {
+
+    internal constructor(userId: String, userToken: String, userName: String) : this(CredentialConfig(userId, userToken, userName))
+
+    val userId: String by credentialConfig::userId
+    val userToken: String by credentialConfig::userToken
+    val userName: String by credentialConfig::userName
+
+    internal fun isValid(): Boolean = credentialConfig.isValid()
 }
