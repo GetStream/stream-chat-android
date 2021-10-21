9e9e8819345635ee9ad5068d4c716fbdd0f1586c
Leandro Borges Ferreira <lehen01@gmail.com>
2021-10-21 09:37:51 -0300

    [CAS-1373] removing unused imports

--- stream-chat-android-ui-components-sample/src/main/kotlin/io/getstream/chat/ui/sample/feature/component_browser/reactions/ComponentBrowserEditReactionsFragment.kt
+++ stream-chat-android-ui-components-sample/src/main/kotlin/io/getstream/chat/ui/sample/feature/component_browser/reactions/ComponentBrowserEditReactionsFragment.kt
@@ -1,6 +1,5 @@
 package io.getstream.chat.ui.sample.feature.component_browser.reactions
 
-import android.graphics.Bitmap
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
