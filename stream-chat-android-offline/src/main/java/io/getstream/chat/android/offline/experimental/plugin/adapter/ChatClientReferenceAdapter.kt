484ce4f443ee736570487e0c07647329cd4ca369
Oleg Kuzmin <oleg@getstream.io>
2021-11-01 18:17:08 +0100

    Add QueryChannelReference

--- stream-chat-android-offline/src/main/java/io/getstream/chat/android/offline/experimental/plugin/adapter/ChatClientReferenceAdapter.kt
+++ stream-chat-android-offline/src/main/java/io/getstream/chat/android/offline/experimental/plugin/adapter/ChatClientReferenceAdapter.kt
@@ -1,13 +1,16 @@
 package io.getstream.chat.android.offline.experimental.plugin.adapter
 
 import io.getstream.chat.android.client.ChatClient
+import io.getstream.chat.android.client.api.models.QueryChannelRequest
 import io.getstream.chat.android.client.api.models.QueryChannelsRequest
 import io.getstream.chat.android.core.ExperimentalStreamChatApi
 import io.getstream.chat.android.core.internal.InternalStreamChatApi
+import io.getstream.chat.android.offline.experimental.channel.QueryChannelReference
 import io.getstream.chat.android.offline.experimental.querychannels.QueryChannelsReference
 
 @InternalStreamChatApi
 @ExperimentalStreamChatApi
 public class ChatClientReferenceAdapter(private val chatClient: ChatClient) {
     public fun queryChannels(request: QueryChannelsRequest): QueryChannelsReference = QueryChannelsReference(request, chatClient)
+    public fun queryChannel(channelType: String, channelId: String, request: QueryChannelRequest): QueryChannelReference = QueryChannelReference(channelType, channelId, request, chatClient)
 }
