92750b9686875fad54dbf9271a62a588dea4c31d
Amit Kumar <amit.kumar@getstream.io>
2021-11-11 15:53:48 +0530

    Simply concatenate delete command

--- scripts/ci-unit-tests.sh
+++ scripts/ci-unit-tests.sh
@@ -2,12 +2,5 @@ command_file="unit-tests-command.sh"
 
 ./gradlew test-debug && \
 chmod +x $command_file && \
-./$command_file
-
-# Exit code of gradle test command output
-exitCode=$?
-
+./$command_file && \
 rm $command_file
-
-# Exit with test command output to set the GH action's check run status
-exit $exitCode
