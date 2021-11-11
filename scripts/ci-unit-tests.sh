466e54269b5042d4ca0216537ad2bffecea759ee
Amit Kumar <amit.kumar@getstream.io>
2021-11-11 15:26:01 +0530

    Fix unit test script to return test command exit code

--- scripts/ci-unit-tests.sh
+++ scripts/ci-unit-tests.sh
@@ -4,4 +4,10 @@ command_file="unit-tests-command.sh"
 chmod +x $command_file && \
 ./$command_file
 
+# Exit code of gradle test command output
+exitCode=$?
+
 rm $command_file
+
+# Exit with test command output to set the GH action's check run status
+exit $exitCode
