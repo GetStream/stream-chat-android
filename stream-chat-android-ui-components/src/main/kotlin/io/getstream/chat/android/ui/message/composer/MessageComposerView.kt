31dfa76efb6de5348091d47169572822538e833f
Samuel Urbanowicz <samiuelson@gmail.com>
2021-11-18 19:16:08 +0100

    Draft basic MessageComposerView API

---
+++ stream-chat-android-ui-components/src/main/kotlin/io/getstream/chat/android/ui/message/composer/MessageComposerView.kt
@@ -0,0 +1,132 @@
+package io.getstream.chat.android.ui.message.composer
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.view.isVisible
+import io.getstream.chat.android.client.models.Attachment
+import io.getstream.chat.android.common.state.MessageAction
+import io.getstream.chat.android.core.ExperimentalStreamChatApi
+import io.getstream.chat.android.ui.common.extensions.internal.streamThemeInflater
+import io.getstream.chat.android.ui.databinding.StreamUiMessageComposerBinding
+import io.getstream.chat.android.ui.databinding.StreamUiMessageComposerDefaultCenterContentBinding
+import io.getstream.chat.android.ui.databinding.StreamUiMessageComposerDefaultLeadingContentBinding
+import io.getstream.chat.android.ui.databinding.StreamUiMessageComposerDefaultTrailingContentBinding
+
+@ExperimentalStreamChatApi
+public class MessageComposerView : ConstraintLayout {
+
+    private lateinit var binding: StreamUiMessageComposerBinding
+
+    private val messageInputState: MessageInputState = MessageInputState()
+
+    /**
+     * @param onSendMessageAction Handler when the user taps on the send message button.
+     */
+    public var onSendMessageAction: () -> Unit = {}// TODO make required
+
+    /**
+     * @param onCancelAction Handler invoked when the user cancels the active action (in Reply or Edit modes).
+     */
+    public var onCancelAction: () -> Unit = {}
+
+    private val defaultLeadingContent: View by lazy {
+        val binding = StreamUiMessageComposerDefaultLeadingContentBinding.inflate(
+            streamThemeInflater,
+            binding.trailingContent,
+            false
+        )
+        binding.root
+    }
+    public var leadingContent: MessageComposerView.() -> View = {
+        defaultLeadingContent
+    }
+        set(value) {
+            field = value
+            binding.leadingContent.apply {
+                removeAllViews()
+                addView(value())
+            }
+        }
+
+    private val defaultCenterContent: View by lazy {
+        val binding = StreamUiMessageComposerDefaultCenterContentBinding.inflate(
+            streamThemeInflater,
+            binding.trailingContent,
+            false
+        )
+        binding.root
+    }
+    public var centerContent: MessageComposerView.() -> View = {
+        defaultCenterContent
+    }
+        set(value) {
+            field = value
+            binding.centerContent.apply {
+                removeAllViews()
+                addView(value())
+            }
+        }
+
+    private val defaultTrailingContent: View by lazy {
+        val binding = StreamUiMessageComposerDefaultTrailingContentBinding.inflate(
+            streamThemeInflater,
+            binding.trailingContent,
+            false
+        ).apply {
+            sendMessageButtonDisabled.isVisible = true
+            sendMessageButtonEnabled.isVisible = false
+        }
+        binding.root
+    }
+
+    public var trailingContent: MessageComposerView.() -> View = { defaultTrailingContent }
+        set(value) {
+            field = value
+            binding.trailingContent.apply {
+                removeAllViews()
+                addView(value())
+            }
+        }
+
+    public constructor(context: Context) : this(context, null)
+
+    public constructor(context: Context, attrs: AttributeSet?) : this(context, attrs, 0)
+
+    public constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(context,
+        attrs,
+        defStyleAttr) {
+        init()
+    }
+
+    private fun init() {
+        binding = StreamUiMessageComposerBinding.inflate(streamThemeInflater, this)
+        binding.leadingContent.apply {
+            removeAllViews()
+            addView(leadingContent())
+        }
+        binding.centerContent.apply {
+            removeAllViews()
+            addView(centerContent())
+        }
+        binding.trailingContent.apply {
+            removeAllViews()
+            addView(trailingContent())
+        }
+        invalidate()
+    }
+}
+
+/**
+ * Represents the state within the message input.
+ *
+ * @param inputValue The current text value that's within the input.
+ * @param attachments The currently selected attachments.
+ * @param action The currently active [MessageAction].
+ */
+public data class MessageInputState(
+    val inputValue: String = "",
+    val attachments: List<Attachment> = emptyList(),
+    val action: MessageAction? = null,
+)
