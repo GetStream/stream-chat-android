27b1af048d721e4de1c7f3dab5da3992e96967a1
Samuel Urbanowicz <samiuelson@gmail.com>
2021-11-18 19:16:46 +0100

    Draft MessageComposerViewModel binding API

---
+++ stream-chat-android-ui-components/src/main/kotlin/io/getstream/chat/android/ui/message/composer/MessageComposerViewModelBinding.kt
@@ -0,0 +1,7 @@
+package io.getstream.chat.android.ui.message.composer
+
+import androidx.lifecycle.LifecycleOwner
+
+public fun MessageComposerViewModel.bindView(view: MessageComposerView, lifecycleOwner: LifecycleOwner) {
+
+}
