017a5f74e7323cddcd77898958c09ac7313a9945
Oleg Kuzmin <oleg@getstream.io>
2021-11-19 18:34:48 +0100

    Add reference adapter for thread replies query. Use it in compose

--- stream-chat-android-offline/src/main/java/io/getstream/chat/android/offline/experimental/plugin/adapter/ChatClientReferenceAdapter.kt
+++ stream-chat-android-offline/src/main/java/io/getstream/chat/android/offline/experimental/plugin/adapter/ChatClientReferenceAdapter.kt
@@ -8,6 +8,7 @@ import io.getstream.chat.android.core.ExperimentalStreamChatApi
 import io.getstream.chat.android.core.internal.InternalStreamChatApi
 import io.getstream.chat.android.offline.ChatDomain
 import io.getstream.chat.android.offline.experimental.channel.QueryChannelReference
+import io.getstream.chat.android.offline.experimental.channel.thread.RepliesQueryReference
 import io.getstream.chat.android.offline.experimental.querychannels.QueryChannelsReference
 import io.getstream.chat.android.offline.request.QueryChannelPaginationRequest
 
@@ -35,6 +36,10 @@ public class ChatClientReferenceAdapter(private val chatClient: ChatClient) {
         val request = QueryChannelPaginationRequest(limit).toWatchChannelRequest(userPresence)
         return queryChannel(channelType, channelId, request)
     }
+
+    /** Reference request of the get thread replies query. */
+    public fun getReplies(messageId: String, limit: Int = DEFAULT_MESSAGE_LIMIT): RepliesQueryReference =
+        RepliesQueryReference(messageId, limit, chatClient)
 
     private companion object {
         private const val DEFAULT_MESSAGE_LIMIT = 30
