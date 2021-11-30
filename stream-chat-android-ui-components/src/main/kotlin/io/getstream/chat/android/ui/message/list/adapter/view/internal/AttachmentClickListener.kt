172d0be051db9e07c45afd96fefdcc87b74f4204
Márton Braun <marton@getstream.io>
2021-11-30 13:46:46 +0100

    Fix formatting

--- stream-chat-android-ui-components/src/main/kotlin/io/getstream/chat/android/ui/message/list/adapter/view/internal/AttachmentClickListener.kt
+++ stream-chat-android-ui-components/src/main/kotlin/io/getstream/chat/android/ui/message/list/adapter/view/internal/AttachmentClickListener.kt
@@ -3,13 +3,13 @@ package io.getstream.chat.android.ui.message.list.adapter.view.internal
 import io.getstream.chat.android.client.models.Attachment
 
 internal fun interface AttachmentClickListener {
-     fun onAttachmentClick(attachment: Attachment)
+    fun onAttachmentClick(attachment: Attachment)
 }
 
 internal fun interface AttachmentLongClickListener {
-     fun onAttachmentLongClick()
+    fun onAttachmentLongClick()
 }
 
 internal fun interface AttachmentDownloadClickListener {
-     fun onAttachmentDownloadClick(attachment: Attachment)
+    fun onAttachmentDownloadClick(attachment: Attachment)
 }
