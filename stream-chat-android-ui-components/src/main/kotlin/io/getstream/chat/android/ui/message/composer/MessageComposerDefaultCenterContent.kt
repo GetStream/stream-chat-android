9ba42782c2f0578bc95869cd61c95351702ab117
Samuel Urbanowicz <samiuelson@gmail.com>
2021-12-10 19:13:54 +0100

    Allow attachments clearing

--- stream-chat-android-ui-components/src/main/kotlin/io/getstream/chat/android/ui/message/composer/MessageComposerDefaultCenterContent.kt
+++ stream-chat-android-ui-components/src/main/kotlin/io/getstream/chat/android/ui/message/composer/MessageComposerDefaultCenterContent.kt
@@ -12,7 +12,6 @@ import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.internal.TextWatcherAdapter
 import io.getstream.chat.android.client.models.Attachment
 import io.getstream.chat.android.common.state.MessageInputState
-import io.getstream.chat.android.ui.common.extensions.internal.context
 import io.getstream.chat.android.ui.common.extensions.internal.streamThemeInflater
 import io.getstream.chat.android.ui.databinding.StreamUiMessageComposerAttachmentContainerBinding
 import io.getstream.chat.android.ui.databinding.StreamUiMessageComposerDefaultCenterContentBinding
@@ -39,7 +38,7 @@ public class MessageComposerDefaultCenterContent : FrameLayout, MessageComposerC
     private lateinit var binding: StreamUiMessageComposerDefaultCenterContentBinding
 
     private val attachmentsAdapter: MessageComposerAttachmentsAdapter =
-        MessageComposerAttachmentsAdapter(onAttachmentRemovedListener)
+        MessageComposerAttachmentsAdapter { onAttachmentRemovedListener(it) }
 
     public constructor(context: Context) : this(context, null)
 
@@ -64,6 +63,7 @@ public class MessageComposerDefaultCenterContent : FrameLayout, MessageComposerC
         binding.clearCommandButton.setOnClickListener {
             onClearButtonClickListener()
         }
+        binding.attachmentsRecyclerView.adapter = attachmentsAdapter
     }
 
     /**
@@ -80,20 +80,28 @@ public class MessageComposerDefaultCenterContent : FrameLayout, MessageComposerC
                 setText(state.inputValue)
             }
         }
+
+        attachmentsAdapter.setAttachments(state.attachments)
+
+        binding.selectedAttachmentsContainer.isVisible = state.attachments.isNotEmpty()
     }
 
-    public fun setAttachmentViewFactory(factory: AttachmentViewFactory) {
+    /**
+     * Allows to override default attachment previews. Useful when you want to add support for custom attachments previews.
+     */
+    public fun setAttachmentViewFactory(factory: DefaultMessageComposerAttachmentPreviewFactory) {
         attachmentsAdapter.viewFactory = factory
         attachmentsAdapter.notifyDataSetChanged()
     }
 }
 
 internal class MessageComposerAttachmentsAdapter(
-    private val attachmentClearedListener: (Attachment) -> Unit = {},
+    private val onAttachmentRemovedCallback: (Attachment) -> Unit,
 ) : RecyclerView.Adapter<MessageComposerViewHolder>() {
     private val attachments: MutableList<Attachment> = mutableListOf()
 
-    public var viewFactory: AttachmentViewFactory = AttachmentViewFactory()
+    public var viewFactory: DefaultMessageComposerAttachmentPreviewFactory =
+        DefaultMessageComposerAttachmentPreviewFactory()
 
     internal fun setAttachments(attachments: List<Attachment>) {
         this.attachments.apply {
@@ -115,7 +123,7 @@ internal class MessageComposerAttachmentsAdapter(
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MessageComposerViewHolder {
-        return MessageComposerViewHolder(parent, viewFactory, attachmentClearedListener)
+        return MessageComposerViewHolder(parent, viewFactory, { onAttachmentRemovedCallback(it) })
     }
 
     override fun onBindViewHolder(holder: MessageComposerViewHolder, position: Int) {
@@ -127,27 +135,41 @@ internal class MessageComposerAttachmentsAdapter(
 
 internal class MessageComposerViewHolder(
     private val parent: ViewGroup,
-    private val attachmentViewFactory: AttachmentViewFactory,
-    private val onAttachmentClearedListener: (Attachment) -> Unit,
-) : RecyclerView.ViewHolder(parent) {
-
-    internal val binding: StreamUiMessageComposerAttachmentContainerBinding =
-        StreamUiMessageComposerAttachmentContainerBinding.inflate(context.streamThemeInflater, parent, false)
+    private val attachmentViewFactory: MessageComposerAttachmentPreviewFactory,
+    private val onAttachmentRemovedCallback: (Attachment) -> Unit,
+    private val binding: StreamUiMessageComposerAttachmentContainerBinding =
+        StreamUiMessageComposerAttachmentContainerBinding.inflate(parent.streamThemeInflater, parent, false),
+) : RecyclerView.ViewHolder(binding.root) {
 
     fun bindData(attachment: Attachment) {
         val attachmentContainer = binding.root
-        val view = attachmentViewFactory.createViewForAttachment(parent, attachment, onAttachmentClearedListener)
+        val view = attachmentViewFactory.createViewForAttachment(parent, attachment) {
+            onAttachmentRemovedCallback(attachment)
+        }
         attachmentContainer.removeAllViews()
         attachmentContainer.addView(view)
     }
 }
 
-public open class AttachmentViewFactory {
-    public open fun createViewForAttachment(
+public open class DefaultMessageComposerAttachmentPreviewFactory : MessageComposerAttachmentPreviewFactory {
+    public override fun createViewForAttachment(
         parent: ViewGroup,
         attachment: Attachment,
-        attachmentRemoveListener: (Attachment) -> Unit,
+        onAttachmentRemovedCallback: (Attachment) -> Unit,
     ): View {
-        return TextView(parent.context).apply { text = "attachment" }
+        return TextView(parent.context).apply {
+            text = "attachment"
+            setOnClickListener {
+                onAttachmentRemovedCallback(attachment)
+            }
+        }
     }
+}
+
+public interface MessageComposerAttachmentPreviewFactory {
+    public fun createViewForAttachment(
+        parent: ViewGroup,
+        attachment: Attachment,
+        onAttachmentRemovedCallback: (Attachment) -> Unit,
+    ): View
 }
