d7ee565dc631d004c147527abbd5d44a9812442f
Samuel Urbanowicz <samiuelson@gmail.com>
2021-12-20 20:39:03 +0100

    Implement default commands list content

---
+++ stream-chat-android-ui-components/src/main/kotlin/io/getstream/chat/android/ui/message/composer/suggestions/command/CommandViewHolder.kt
@@ -0,0 +1,35 @@
+package io.getstream.chat.android.ui.message.composer.suggestions.command
+
+import io.getstream.chat.android.client.models.Command
+import io.getstream.chat.android.ui.R
+import io.getstream.chat.android.ui.common.internal.SimpleListAdapter
+import io.getstream.chat.android.ui.databinding.StreamUiItemCommandBinding
+
+/**
+ * [RecyclerView.ViewHolder] used for rendering command. Used by [CommandsAdapter].
+ *
+ * @param binding Handle to [StreamUiItemCommandBinding] instance.
+ * @param onCommandSelected Callback invoked when command suggestion item is clicked.
+ */
+internal class CommandViewHolder(
+    val binding: StreamUiItemCommandBinding,
+    private val onCommandSelected: (Command) -> Unit,
+) :
+    SimpleListAdapter.ViewHolder<Command>(binding.root) {
+
+    /**
+     * Updates [itemView] elements for a given [Command] object.
+     *
+     * @param command Single command suggestion represented by [Command] class.
+     */
+    override fun bind(command: Command) {
+        binding.apply {
+            commandNameTextView.text = command.name.replaceFirstChar(Char::uppercase)
+            commandQueryTextView.text = itemView.context.getString(
+                R.string.stream_ui_message_input_command_template,
+                command.name,
+                command.args
+            )
+        }
+    }
+}
---
+++ stream-chat-android-ui-components/src/main/kotlin/io/getstream/chat/android/ui/message/composer/suggestions/command/CommandsAdapter.kt
@@ -0,0 +1,27 @@
+package io.getstream.chat.android.ui.message.composer.suggestions.command
+
+import android.view.ViewGroup
+import io.getstream.chat.android.client.models.Command
+import io.getstream.chat.android.ui.common.extensions.internal.streamThemeInflater
+import io.getstream.chat.android.ui.common.internal.SimpleListAdapter
+import io.getstream.chat.android.ui.databinding.StreamUiItemCommandBinding
+
+/**
+ * [RecyclerView.Adapter] responsible for displaying command suggestions in the RecyclerView in [DefaultCommandSuggestionsContent].
+ */
+internal class CommandsAdapter(private inline val onCommandSelected: (Command) -> Unit) :
+    SimpleListAdapter<Command, CommandViewHolder>() {
+
+    /**
+     * Inflates layout and instantiates [CommandViewHolder].
+     *
+     * @param parent Container item layout provided by [RecyclerView.Adapter].
+     * @param viewType View type provided by [RecyclerView.Adapter]. In case of this [CommandsAdapter] it's not used.
+     *
+     * @return [CommandViewHolder] instance.
+     */
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CommandViewHolder {
+        val binding = StreamUiItemCommandBinding.inflate(parent.streamThemeInflater, parent, false)
+        return CommandViewHolder(binding, onCommandSelected)
+    }
+}
