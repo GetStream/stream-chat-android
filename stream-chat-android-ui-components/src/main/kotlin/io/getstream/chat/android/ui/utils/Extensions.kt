25cc0eb3a4eda3d66e1c7bf5fa6d2de3a8c9f8f6
Leandro Borges Ferreira <lehen01@gmail.com>
2022-01-06 16:58:50 -0300

    encapsulation for isRtlLayout extension

--- stream-chat-android-ui-components/src/main/kotlin/io/getstream/chat/android/ui/utils/Extensions.kt
+++ stream-chat-android-ui-components/src/main/kotlin/io/getstream/chat/android/ui/utils/Extensions.kt
@@ -3,5 +3,5 @@ package io.getstream.chat.android.ui.utils
 import android.content.Context
 import android.view.View
 
-public val Context.isRtlLayout: Boolean
+internal val Context.isRtlLayout: Boolean
     get() = resources.configuration.layoutDirection == View.LAYOUT_DIRECTION_RTL
