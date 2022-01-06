d333d65fa753a87e74e5dfdaea6c3c02eda099d5
bychkovdmitry <9600921+bychkovdmitry@users.noreply.github.com>
2022-01-06 12:29:15 +0300

    [2676] Fix channel list autoscroll (#2887)
    * [2676] Fix channel list autoscroll
    
    * [2676] Update the change log
    
    * [2676] Hoist background from channel item to channels screen

--- stream-chat-android-compose/src/main/java/io/getstream/chat/android/compose/ui/channels/list/Channels.kt
+++ stream-chat-android-compose/src/main/java/io/getstream/chat/android/compose/ui/channels/list/Channels.kt
@@ -1,12 +1,15 @@
 package io.getstream.chat.android.compose.ui.channels.list
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import io.getstream.chat.android.compose.handlers.LoadMoreHandler
 import io.getstream.chat.android.compose.state.channel.list.ChannelItemState
 import io.getstream.chat.android.compose.state.channel.list.ChannelsState
@@ -38,6 +41,10 @@ public fun Channels(
         state = listState,
         horizontalAlignment = Alignment.CenterHorizontally,
     ) {
+        item {
+            DummyFirstChannelItem()
+        }
+
         items(
             items = channelItems,
             key = { it.channel.cid }
@@ -59,4 +66,19 @@ public fun Channels(
             onLastItemReached()
         }
     }
+}
+
+/**
+ * Represents an almost invisible dummy item to be added to the top of the list.
+ *
+ * If the list is scrolled to the top and a channel new item is added or moved
+ * to the position above, then the list will automatically autoscroll to it.
+ */
+@Composable
+private fun DummyFirstChannelItem() {
+    Box(
+        modifier = Modifier
+            .height(1.dp)
+            .fillMaxWidth()
+    )
 }
