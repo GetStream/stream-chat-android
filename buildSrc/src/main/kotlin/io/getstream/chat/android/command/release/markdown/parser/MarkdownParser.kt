b6c15948ad8a700fae7f7a2d6cde7ee44f57c931
Leandro Borges Ferreira <lehen01@gmail.com>
2022-01-07 05:06:53 -0300

    [CAS-1330] Adding command to automate release write when tag of release if created (#2784)
    Co-authored-by: Rafal Adasiewicz <rafal@getstream.io>

---
+++ buildSrc/src/main/kotlin/io/getstream/chat/android/command/release/markdown/parser/MarkdownParser.kt
@@ -0,0 +1,74 @@
+package io.getstream.chat.android.command.release.markdown.parser
+
+import io.getstream.chat.android.command.release.model.Document
+import io.getstream.chat.android.command.release.model.Project
+import io.getstream.chat.android.command.release.model.Section
+import java.io.File
+
+fun parseChangelogFile(file: File): Document {
+    return file.readLines()
+        .filterListSection()
+        .parseReleaseDocument()
+}
+
+private fun List<String>.parseReleaseDocument(): Document {
+    val document = Document(mutableListOf())
+    var currentProjectSections = mutableListOf<Section>()
+    var currentSectionLines = mutableListOf<String>()
+
+    this.forEachIndexed { i, line ->
+        when {
+            isStartOfProject(line) || i == this.lastIndex -> {
+                if (i == this.lastIndex) {
+                    currentSectionLines.add(line)
+                }
+
+                currentProjectSections.add(Section(currentSectionLines))
+                document.add(Project(currentProjectSections))
+
+                currentSectionLines = mutableListOf(line)
+                currentProjectSections = mutableListOf()
+            }
+
+            isStartOfSection(line) -> {
+                currentProjectSections.add(Section(currentSectionLines))
+                currentSectionLines = mutableListOf(line)
+            }
+
+            line.startsWith("-") -> {
+                currentSectionLines.add(line)
+            }
+        }
+    }
+
+    return document
+}
+
+fun isStartOfProject(line: String) = line.startsWith("##") && !line.startsWith("###")
+
+fun isStartOfSection(line: String) = line.startsWith("###")
+
+private fun List<String>.filterListSection(
+    start: String = "<!-- UNRELEASED START -->",
+    end: String = "<!-- UNRELEASED END -->"
+): List<String> {
+    var shouldAdd = false
+
+    val filteredList = filter { line ->
+        if (line.trim() == start) {
+            shouldAdd = true
+        }
+
+        if (line.trim() == end) {
+            shouldAdd = false
+        }
+
+        shouldAdd
+    }
+
+    return if (!shouldAdd && filteredList.isNotEmpty()) {
+        filteredList
+    } else {
+        throw IllegalStateException("Could not find the start or end of unreleased section")
+    }
+}
