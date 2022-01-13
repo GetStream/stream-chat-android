9660d44e3142b5e1bcb176664e845e000f136728
Leandro Borges Ferreira <lehen01@gmail.com>
2022-01-13 14:50:29 -0300

    Adding lastMessageSeenDate to ChannelUserReadEntity

--- stream-chat-android-offline/src/main/java/io/getstream/chat/android/offline/repository/domain/channel/userread/ChannelUserReadEntity.kt
+++ stream-chat-android-offline/src/main/java/io/getstream/chat/android/offline/repository/domain/channel/userread/ChannelUserReadEntity.kt
@@ -7,4 +7,6 @@ import java.util.Date
  * Efficiently store the channel user read info.
  */
 @JsonClass(generateAdapter = true)
-internal data class ChannelUserReadEntity(val userId: String, val lastRead: Date?, val unreadMessages: Int)
+internal data class ChannelUserReadEntity(
+    val userId: String, val lastRead: Date?, val unreadMessages: Int, val lastMessageSeenDate: Date?
+)
