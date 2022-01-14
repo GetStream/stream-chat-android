e8c510ff5b0fa726f4515a0c47475e01267cdda3
Leandro Borges Ferreira <lehen01@gmail.com>
2022-01-14 16:15:15 -0300

    Fixing kdocs for ChannelUserRead

--- stream-chat-android-client/src/main/java/io/getstream/chat/android/client/models/ChannelUserRead.kt
+++ stream-chat-android-client/src/main/java/io/getstream/chat/android/client/models/ChannelUserRead.kt
@@ -5,17 +5,14 @@ import java.util.Date
 /**
  * Information about how many messages are unread in the channel by a given user
  *
+ * @property user - The user which has read some of the message and may have some unread messages
+ * @property lastRead - The time of the last read message
+ * @property unreadMessages - How many messages are unread
+ * @property lastMessageSeenDate - The time of the last message that the SDK is aware of. If new messages arrive the the createdAt newer than this one, that means that the count of unread messages should be incremented.
  */
 public data class ChannelUserRead(
-    /* The user which has read some of the message and may have some unread messages */
     override var user: User,
-    /* The time of the last read message */
     var lastRead: Date? = null,
-    /* How many messages are unread */
     var unreadMessages: Int = 0,
-    /*
-    * The time of the last message that the SDK is aware of. If new messages arrive the the createdAt newer than this
-    * one, that means that the count of unread messages should be incremented.
-    */
     var lastMessageSeenDate: Date? = null
 ) : UserEntity
