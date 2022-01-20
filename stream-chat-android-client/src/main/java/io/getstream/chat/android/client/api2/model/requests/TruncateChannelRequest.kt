c32b3fc06c69cbe652672bc06e9f9ed156bf43a5
Dmitrii Bychkov <mail@dbychkov.com>
2022-01-20 15:55:15 +0300

    [2946] Add missing KDoc

--- stream-chat-android-client/src/main/java/io/getstream/chat/android/client/api2/model/requests/TruncateChannelRequest.kt
+++ stream-chat-android-client/src/main/java/io/getstream/chat/android/client/api2/model/requests/TruncateChannelRequest.kt
@@ -3,6 +3,11 @@ package io.getstream.chat.android.client.api2.model.requests
 import com.squareup.moshi.JsonClass
 import io.getstream.chat.android.client.api2.model.dto.UpstreamMessageDto
 
+/**
+ * Represents the body part of the truncate channel request.
+ *
+ * @param message The system message that will be shown in the channel.
+ */
 @JsonClass(generateAdapter = true)
 internal data class TruncateChannelRequest(
     val message: UpstreamMessageDto?,
