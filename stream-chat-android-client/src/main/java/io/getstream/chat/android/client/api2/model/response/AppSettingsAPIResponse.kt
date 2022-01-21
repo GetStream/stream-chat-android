c5d5e29874962b894b452467a77684a68614dab8
Leandro Borges Ferreira <lehen01@gmail.com>
2022-01-21 10:35:49 -0300

    [ISSUE-2877] Removing Json(name = "")

--- stream-chat-android-client/src/main/java/io/getstream/chat/android/client/api2/model/response/AppSettingsAPIResponse.kt
+++ stream-chat-android-client/src/main/java/io/getstream/chat/android/client/api2/model/response/AppSettingsAPIResponse.kt
@@ -4,29 +4,27 @@ import com.squareup.moshi.Json
 import com.squareup.moshi.JsonClass
 
 @JsonClass(generateAdapter = true)
-internal data class AppSettingsAPIResponse(
-    @Json(name = "app") val appDto: AppDto
-)
+internal data class AppSettingsAPIResponse(val appDto: AppDto)
 
 @JsonClass(generateAdapter = true)
 internal data class AppDto(
-    @Json(name = "name") val name: String,
-    @Json(name = "file_upload_config") val fileUploadConfigDto: FileUploadConfigDto,
-    @Json(name = "image_upload_config") val imageUploadConfigDto: FileUploadConfigDto
+    val name: String,
+    val file_upload_config: FileUploadConfigDto,
+    val image_upload_config: FileUploadConfigDto,
 )
 
 @JsonClass(generateAdapter = true)
 internal data class FileUploadConfigDto(
-    @Json(name = "allowed_file_extensions") val allowedFileExtensions: List<String>,
-    @Json(name = "allowed_mime_types") val allowedMimeTypes: List<String>,
-    @Json(name = "blocked_file_extensions") val blockedFileExtensions: List<String>,
-    @Json(name = "blocked_mime_types") val blockedMimeTypes: List<String>,
+    val allowed_file_extensions: List<String>,
+    val allowed_mime_types: List<String>,
+    val blocked_file_extensions: List<String>,
+    val blocked_mime_types: List<String>,
 )
 
 @JsonClass(generateAdapter = true)
 internal data class ImageUploadConfigDto(
-    @Json(name = "allowed_file_extensions") val allowedFileExtensions: List<String>,
-    @Json(name = "allowed_mime_types") val allowedMimeTypes: List<String>,
-    @Json(name = "blocked_file_extensions") val blockedFileExtensions: List<String>,
-    @Json(name = "blocked_mime_types") val blockedMimeTypes: List<String>,
+    val allowed_file_extensions: List<String>,
+    val allowed_mime_types: List<String>,
+    val blocked_file_extensions: List<String>,
+    val blocked_mime_types: List<String>,
 )
