a7715a5a86131560ab6abb5f79ab4a8de268daad
Leandro Borges Ferreira <lehen01@gmail.com>
2022-01-21 12:56:21 -0300

    [ISSUE-2877] Adding app end point (#2960)
    * [ISSUE-2877] Adding /app endpoint
    
    * [ISSUE-2877] Changelog update
    
    * [ISSUE-2877] Adding API change
    
    * Update CHANGELOG.md
    
    Co-authored-by: Rafal Adasiewicz <rafal@getstream.io>
    
    * [ISSUE-2877] Encapsulating internal classes
    
    * [ISSUE-2877] Removing Json(name = "")
    
    * [ISSUE-2877] Fixing name of variables
    
    * [ISUUE-2877] Removing unused import
    
    * [ISSUE-2877] KDocs
    
    * [ISUUE-2877] Fixing API
    
    * Update stream-chat-android-client/src/main/java/io/getstream/chat/android/client/api2/ConfigApi.kt
    
    Co-authored-by: Rafal Adasiewicz <rafal@getstream.io>
    
    * Update stream-chat-android-client/src/main/java/io/getstream/chat/android/client/models/AppSettings.kt
    
    Co-authored-by: Rafal Adasiewicz <rafal@getstream.io>
    
    * [ISSUE-2877] Improving KDocs
    
    Co-authored-by: Rafal Adasiewicz <rafal@getstream.io>

---
+++ stream-chat-android-client/src/main/java/io/getstream/chat/android/client/api2/ConfigApi.kt
@@ -0,0 +1,16 @@
+package io.getstream.chat.android.client.api2
+
+import io.getstream.chat.android.client.api.AuthenticatedApi
+import io.getstream.chat.android.client.api2.model.response.AppSettingsAPIResponse
+import io.getstream.chat.android.client.call.RetrofitCall
+import retrofit2.http.GET
+
+/**
+ * API for configurations, settings in the dashboard with read and write possibilities (not mandatorily).
+ */
+@AuthenticatedApi
+internal interface ConfigApi {
+
+    @GET("/app")
+    fun getAppSettings(): RetrofitCall<AppSettingsAPIResponse>
+}
--- stream-chat-android-client/src/main/java/io/getstream/chat/android/client/api2/MoshiChatApi.kt
+++ stream-chat-android-client/src/main/java/io/getstream/chat/android/client/api2/MoshiChatApi.kt
@@ -45,6 +45,7 @@ import io.getstream.chat.android.client.api2.model.requests.UpdateChannelPartial
 import io.getstream.chat.android.client.api2.model.requests.UpdateChannelRequest
 import io.getstream.chat.android.client.api2.model.requests.UpdateCooldownRequest
 import io.getstream.chat.android.client.api2.model.requests.UpdateUsersRequest
+import io.getstream.chat.android.client.api2.model.response.AppSettingsAPIResponse
 import io.getstream.chat.android.client.api2.model.response.BannedUserResponse
 import io.getstream.chat.android.client.api2.model.response.ChannelResponse
 import io.getstream.chat.android.client.api2.model.response.TranslateMessageRequest
@@ -56,6 +57,7 @@ import io.getstream.chat.android.client.errors.ChatError
 import io.getstream.chat.android.client.events.ChatEvent
 import io.getstream.chat.android.client.extensions.enrichWithCid
 import io.getstream.chat.android.client.logger.ChatLogger
+import io.getstream.chat.android.client.models.AppSettings
 import io.getstream.chat.android.client.models.BannedUser
 import io.getstream.chat.android.client.models.BannedUsersSort
 import io.getstream.chat.android.client.models.Channel
@@ -86,6 +88,7 @@ internal class MoshiChatApi(
     private val deviceApi: DeviceApi,
     private val moderationApi: ModerationApi,
     private val generalApi: GeneralApi,
+    private val configApi: ConfigApi,
     private val coroutineScope: CoroutineScope,
 ) : ChatApi {
 
@@ -109,6 +112,10 @@ internal class MoshiChatApi(
     override fun setConnection(userId: String, connectionId: String) {
         this.userId = userId
         this.connectionId = connectionId
+    }
+
+    override fun appSettings(): Call<AppSettings> {
+        return configApi.getAppSettings().map(AppSettingsAPIResponse::toDomain)
     }
 
     override fun sendMessage(channelType: String, channelId: String, message: Message): Call<Message> {
