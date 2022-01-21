713462a516a8ef00d11ccb95b9c136f0ac810931
Leandro Borges Ferreira <lehen01@gmail.com>
2022-01-21 11:16:57 -0300

    [ISSUE-2877] Fixing name of variables

--- stream-chat-android-client/src/main/java/io/getstream/chat/android/client/api2/mapping/AppConfigurationMapping.kt
+++ stream-chat-android-client/src/main/java/io/getstream/chat/android/client/api2/mapping/AppConfigurationMapping.kt
@@ -18,25 +18,25 @@ internal fun AppSettingsAPIResponse.toDomain(): AppSettings {
 internal fun AppDto.toDomain(): App {
     return App(
         name = name,
-        fileUploadConfig = fileUploadConfigDto.toDomain(),
-        imageUploadConfig = imageUploadConfigDto.toDomain(),
+        fileUploadConfig = file_upload_config.toDomain(),
+        imageUploadConfig = image_upload_config.toDomain(),
     )
 }
 
 internal fun FileUploadConfigDto.toDomain(): FileUploadConfig {
     return FileUploadConfig(
-        allowedFileExtensions = allowedFileExtensions,
-        allowedMimeTypes = allowedMimeTypes,
-        blockedFileExtensions = blockedFileExtensions,
-        blockedMimeTypes = blockedMimeTypes,
+        allowedFileExtensions = allowed_file_extensions,
+        allowedMimeTypes = allowed_mime_types,
+        blockedFileExtensions = blocked_file_extensions,
+        blockedMimeTypes = blocked_mime_types,
     )
 }
 
 internal fun ImageUploadConfigDto.toDomain(): ImageUploadConfig {
     return ImageUploadConfig(
-        allowedFileExtensions = allowedFileExtensions,
-        allowedMimeTypes = allowedMimeTypes,
-        blockedFileExtensions = blockedFileExtensions,
-        blockedMimeTypes = blockedMimeTypes,
+        allowedFileExtensions = allowed_file_extensions,
+        allowedMimeTypes = allowed_mime_types,
+        blockedFileExtensions = blocked_file_extensions,
+        blockedMimeTypes = blocked_mime_types,
     )
 }
