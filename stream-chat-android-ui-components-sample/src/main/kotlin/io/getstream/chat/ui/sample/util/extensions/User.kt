9e2f1c80c967a9c7badd4c956e6d27bd69af1d6f
MarinTolic <marin.t91@gmail.com>
2022-01-26 17:09:34 +0100

    Testing how user friendly it is to replace destinations

--- stream-chat-android-ui-components-sample/src/main/kotlin/io/getstream/chat/ui/sample/util/extensions/User.kt
+++ stream-chat-android-ui-components-sample/src/main/kotlin/io/getstream/chat/ui/sample/util/extensions/User.kt
@@ -1,11 +1,11 @@
 package io.getstream.chat.ui.sample.util.extensions
 
+import io.getstream.chat.android.client.ChatClient
 import io.getstream.chat.android.client.models.User
-import io.getstream.chat.android.livedata.ChatDomain
 
 internal fun User.isCurrentUser(): Boolean {
-    return if (ChatDomain.isInitialized) {
-        id == ChatDomain.instance().user.value?.id
+    return if (ChatClient.isInitialized) {
+        id == ChatClient.instance().getCurrentUser()?.id
     } else {
         false
     }
