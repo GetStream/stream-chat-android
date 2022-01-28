e48ae4b2693586d67f78cc9f9f6b909ec80e0a1b
Leandro Borges Ferreira <lehen01@gmail.com>
2022-01-28 16:49:18 -0300

    [ISSUE-2697] Adding KDocs to new classes

--- stream-chat-android-ui-common/src/main/kotlin/com/getstream/sdk/chat/disposable/CoilDisposable.kt
+++ stream-chat-android-ui-common/src/main/kotlin/com/getstream/sdk/chat/disposable/CoilDisposable.kt
@@ -1,10 +1,18 @@
 package com.getstream.sdk.chat.disposable
 
+/**
+ * Wrapper around the Coil Disposable.
+ *
+ * @param disposable [coil.request.Disposable]
+ */
 public class CoilDisposable(private val disposable: coil.request.Disposable) : Disposable {
 
     override val isDisposed: Boolean
         get() = disposable.isDisposed
 
+    /**
+     * Dispose all the source. Use it when the resource is already used or the result is no longer needed.
+     */
     override fun dispose() {
         disposable.dispose()
     }
--- stream-chat-android-ui-common/src/main/kotlin/com/getstream/sdk/chat/disposable/DisposableExtension.kt
+++ stream-chat-android-ui-common/src/main/kotlin/com/getstream/sdk/chat/disposable/DisposableExtension.kt
@@ -1,5 +1,10 @@
 package com.getstream.sdk.chat.disposable
 
+/**
+ * Helper function to add a new disposable to disposableList
+ *
+ * @param disposableList [DisposableList]
+ */
 public fun Disposable.addToDisposableList(disposableList: DisposableList) {
     disposableList.add(this)
 }
