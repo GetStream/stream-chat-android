1579593b7375471642ec514eb4f4129135c8b1d9
Jc Miñarro <josecarlos.minarro@gmail.com>
2022-01-31 17:36:32 +0100

    Add KDocs to TokenManager interface

--- stream-chat-android-client/src/main/java/io/getstream/chat/android/client/token/TokenManager.kt
+++ stream-chat-android-client/src/main/java/io/getstream/chat/android/client/token/TokenManager.kt
@@ -1,11 +1,46 @@
 package io.getstream.chat.android.client.token
 
 internal interface TokenManager {
+    /**
+     * Ensure a token has been loaded.
+     */
     fun ensureTokenLoaded()
+
+    /**
+     * Load a new token.
+     */
     fun loadSync(): String
+
+    /**
+     * Expire the current token.
+     */
     fun expireToken()
+
+    /**
+     * Check if a [TokenProvider] has been provided.
+     *
+     * @return true if a token provider has been provided, false on another case.
+     */
     fun hasTokenProvider(): Boolean
+
+    /**
+     * Inject a new [CacheableTokenProvider]
+     *
+     * @param provider A [CacheableTokenProvider]
+     */
     fun setTokenProvider(provider: CacheableTokenProvider)
+
+    /**
+     * Obtain last token loaded.
+     *
+     * @return the last token loaded. If the token was expired an empty [String] will be returned.
+     */
     fun getToken(): String
+
+    /**
+     * Check if a token was loaded.
+     *
+     * @return true if a token was loaded and it is not expired, false on another case.
+     */
     fun hasToken(): Boolean
 }
