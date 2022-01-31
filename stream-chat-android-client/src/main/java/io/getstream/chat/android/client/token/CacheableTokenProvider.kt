996630709f12a6aab4462ed727ec938d6fbebad6
Jc Miñarro <josecarlos.minarro@gmail.com>
2022-01-31 17:10:07 +0100

    Update stream-chat-android-client/src/main/java/io/getstream/chat/android/client/token/CacheableTokenProvider.kt
    Co-authored-by: Rafal Adasiewicz <rafal@getstream.io>

--- stream-chat-android-client/src/main/java/io/getstream/chat/android/client/token/CacheableTokenProvider.kt
+++ stream-chat-android-client/src/main/java/io/getstream/chat/android/client/token/CacheableTokenProvider.kt
@@ -4,7 +4,7 @@ package io.getstream.chat.android.client.token
  * An implementation of [TokenProvider] that keeps previous values of the loaded token.
  * This implementation delegate the process to obtain a new token to another tokenProvider.
  *
- * @property tokenProvider The [TokenProvider] used to obtain new tokens
+ * @property tokenProvider The [TokenProvider] used to obtain new tokens.
  */
 internal class CacheableTokenProvider(private val tokenProvider: TokenProvider) : TokenProvider {
     private var cachedToken = ""
