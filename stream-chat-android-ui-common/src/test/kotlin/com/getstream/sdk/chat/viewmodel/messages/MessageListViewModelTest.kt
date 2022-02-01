80ef770ce8b35a8be51f2ba01c8ee9e35f4006e9
Leandro Borges Ferreira <lehen01@gmail.com>
2022-02-01 15:43:07 -0300

    [ISSUE-2897] Fixing unit tests

--- stream-chat-android-ui-common/src/test/kotlin/com/getstream/sdk/chat/viewmodel/messages/MessageListViewModelTest.kt
+++ stream-chat-android-ui-common/src/test/kotlin/com/getstream/sdk/chat/viewmodel/messages/MessageListViewModelTest.kt
@@ -104,7 +104,7 @@ internal class MessageListViewModelTest {
         whenever(domain.threadLoadMore(any(), any(), any())) doReturn threadLoadMoreCall
         whenever(threadLoadMoreResult.isSuccess) doReturn true
         whenever(threadLoadMoreResult.data()) doReturn emptyList()
-        whenever(domain.deleteMessage(any(), any())) doReturn deleteMessageCall
+        whenever(client.deleteMessageAndUpdateLocalData(any(), any())) doReturn deleteMessageCall
         whenever(domain.getThread(any(), any())) doReturn getThreadCall
         whenever(getThreadResult.isSuccess) doReturn true
         whenever(getThreadResult.data()) doReturn threadController
@@ -157,7 +157,7 @@ internal class MessageListViewModelTest {
 
         viewModel.onEvent(MessageListViewModel.Event.DeleteMessage(MESSAGE, hard = false))
 
-        verify(domain).deleteMessage(MESSAGE, false)
+        verify(client).deleteMessageAndUpdateLocalData(MESSAGE, false)
     }
 
     @Test
@@ -167,7 +167,7 @@ internal class MessageListViewModelTest {
 
         viewModel.onEvent(MessageListViewModel.Event.DeleteMessage(MESSAGE, hard = true))
 
-        verify(domain).deleteMessage(MESSAGE, true)
+        verify(client).deleteMessageAndUpdateLocalData(MESSAGE, true)
     }
 
     @Test
