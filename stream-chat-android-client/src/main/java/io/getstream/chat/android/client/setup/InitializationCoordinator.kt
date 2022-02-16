4a8c85e7588f522c5c5fb2055f32ccb10aaad2e3
Leandro Borges Ferreira <lehen01@gmail.com>
2022-02-16 16:01:29 -0300

    [ISSUE-3072] Making InitializationCoordinator internal

--- stream-chat-android-client/src/main/java/io/getstream/chat/android/client/setup/InitializationCoordinator.kt
+++ stream-chat-android-client/src/main/java/io/getstream/chat/android/client/setup/InitializationCoordinator.kt
@@ -2,32 +2,31 @@ package io.getstream.chat.android.client.setup
 
 import io.getstream.chat.android.client.models.User
 
-// Make it internal once ChatDomain is merged with ChatClient.
-public class InitializationCoordinator private constructor() {
+internal class InitializationCoordinator private constructor() {
 
     private val userDisconnectedListeners: MutableList<(User?) -> Unit> = mutableListOf()
     private val userConnectedListeners: MutableList<(User) -> Unit> = mutableListOf()
 
-    public fun addUserConnectedListener(listener: (User) -> Unit) {
+    fun addUserConnectedListener(listener: (User) -> Unit) {
         userConnectedListeners.add(listener)
     }
 
-    public fun addUserDisconnectedListener(listener: (User?) -> Unit) {
+    fun addUserDisconnectedListener(listener: (User?) -> Unit) {
         userDisconnectedListeners.add(listener)
     }
 
-    public fun userConnected(user: User) {
+    fun userConnected(user: User) {
         userConnectedListeners.forEach { function -> function.invoke(user) }
     }
 
-    public fun userDisconnected(user: User) {
+    fun userDisconnected(user: User) {
         userConnectedListeners.forEach { function -> function.invoke(user) }
     }
 
-    public companion object {
+    companion object {
         private var instance: InitializationCoordinator? = null
 
-        public fun getOrCreate(): InitializationCoordinator =
+        fun getOrCreate(): InitializationCoordinator =
             instance ?: InitializationCoordinator().also { instance = it }
     }
 }
