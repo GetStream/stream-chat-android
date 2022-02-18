e6f2e5d6576abdcbcf3b93e62f3966cefa7e0a01
Dmitrii Bychkov <mail@dbychkov.com>
2022-02-18 23:03:16 +0300

    [2880] Remove ExoPlayer from UI components

--- stream-chat-android-ui-components/src/main/kotlin/io/getstream/chat/android/ui/gallery/AttachmentMediaActivity.kt
+++ stream-chat-android-ui-components/src/main/kotlin/io/getstream/chat/android/ui/gallery/AttachmentMediaActivity.kt
@@ -1,13 +1,18 @@
 package io.getstream.chat.android.ui.gallery
 
+import android.content.Context
+import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
+import android.view.KeyEvent
+import android.widget.MediaController
 import android.widget.Toast
+import android.widget.VideoView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isVisible
-import com.devbrackets.android.exomedia.listener.OnPreparedListener
 import io.getstream.chat.android.client.logger.ChatLogger
 import io.getstream.chat.android.ui.R
+import io.getstream.chat.android.ui.common.extensions.internal.getColorCompat
 import io.getstream.chat.android.ui.common.extensions.internal.streamThemeInflater
 import io.getstream.chat.android.ui.databinding.StreamUiActivityAttachmentMediaBinding
 
@@ -17,29 +22,19 @@ import io.getstream.chat.android.ui.databinding.StreamUiActivityAttachmentMediaB
 public class AttachmentMediaActivity : AppCompatActivity() {
     private lateinit var binding: StreamUiActivityAttachmentMediaBinding
 
+    private val url: String? by lazy { intent.getStringExtra(KEY_URL) }
+    private val title: String? by lazy { intent.getStringExtra(KEY_TITLE) }
+    private val type: String? by lazy { intent.getStringExtra(KEY_TYPE) }
+    private val mimeType: String? by lazy { intent.getStringExtra(KEY_MIME_TYPE) }
+
     private val logger = ChatLogger.get("AttachmentMediaActivity")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = StreamUiActivityAttachmentMediaBinding.inflate(streamThemeInflater)
         setContentView(binding.root)
-        init()
-    }
 
-    private fun init() {
-        val type = intent.getStringExtra(TYPE_KEY)
-        val fileType = intent.getStringExtra(FILE_TYPE_KEY)
-        val url = intent.getStringExtra(URL_KEY)
-        binding.apply {
-            headerLeftActionButton.setOnClickListener {
-                onBackPressed()
-            }
-            intent.getStringExtra(HEADER_TITLE_KEY)?.let { title ->
-                headerTitleTextView.text = title
-            }
-        }
-
-        if ((type.isNullOrEmpty() && fileType.isNullOrEmpty()) || url.isNullOrEmpty()) {
+        if ((type.isNullOrEmpty() && mimeType.isNullOrEmpty()) || url.isNullOrEmpty()) {
             logger.logE("This file can't be displayed. The TYPE or the URL are null")
             Toast.makeText(
                 this,
@@ -49,44 +44,106 @@ public class AttachmentMediaActivity : AppCompatActivity() {
             return
         }
 
-        binding.ivAudio.isVisible = type?.contains("audio") == true || fileType?.contains("audio") == true
-
-        playVideo(url)
+        setupSystemUi()
+        setupViews()
+        setupVideoView()
     }
 
     /**
-     * Play media file with url
-     *
-     * @param url media url
+     * Initializes the toolbar and audio icon.
      */
-    private fun playVideo(url: String?) {
-        binding.videoView.isPlaying // Workaround to init some internals of the library
-        binding.videoView.setVideoURI(Uri.parse(url))
-        binding.videoView.setOnPreparedListener(object : OnPreparedListener {
-            override fun onPrepared() {
-                binding.videoView.start()
+    private fun setupViews() {
+        binding.headerLeftActionButton.setOnClickListener { onBackPressed() }
+        binding.headerTitleTextView.text = title
+        binding.ivAudio.isVisible = type?.contains("audio") == true || mimeType?.contains("audio") == true
+    }
+
+    /**
+     * Responsible for updating the system UI.
+     */
+    private fun setupSystemUi() {
+        window.navigationBarColor = getColorCompat(R.color.stream_ui_literal_black)
+        window.statusBarColor = getColorCompat(R.color.stream_ui_literal_black)
+    }
+
+    /**
+     * Initializes the [VideoView] with media controls and starts the playback.
+     */
+    private fun setupVideoView() {
+        val mediaController = createMediaController(this)
+        mediaController.setAnchorView(binding.contentContainer)
+
+        binding.videoView.apply {
+            setMediaController(mediaController)
+            setOnPreparedListener {
+                start()
+                mediaController.show()
             }
-        })
+            setVideoURI(Uri.parse(url))
+        }
+    }
+
+    /**
+     * Creates a custom instance of [MediaController] which no longer intercepts
+     * back press actions to hide media controls.
+     *
+     * @param context The Context used to create the [MediaController].
+     */
+    private fun createMediaController(context: Context): MediaController {
+        return object : MediaController(context) {
+            override fun dispatchKeyEvent(event: KeyEvent): Boolean {
+                if (event.keyCode == KeyEvent.KEYCODE_BACK) {
+                    finish()
+                }
+                return super.dispatchKeyEvent(event)
+            }
+        }
     }
 
     public companion object {
         /**
-         * Key representing attachment's mime type put into the intent
+         * Represents the key for the media URL.
          */
-        public const val TYPE_KEY: String = "type"
+        private const val KEY_URL: String = "url"
 
         /**
-         * Key representing attachment's type put into the intent
+         * Represents the key for the title that will be shown in the header.
          */
-        public const val FILE_TYPE_KEY: String = "file_type"
+        private const val KEY_TITLE: String = "title"
 
         /**
-         * Key representing attachment's url put into the intent
+         * Represents the key for the MIME type of the file.
          */
-        public const val URL_KEY: String = "url"
+        private const val KEY_MIME_TYPE: String = "mime_type"
+
         /**
-         * Key representing header title put into the intent
+         * Represents the key for attachment type.
          */
-        public const val HEADER_TITLE_KEY: String = "header_title"
+        private const val KEY_TYPE: String = "type"
+
+        /**
+         * Used to build an [Intent] to start the [AttachmentMediaActivity] with the required data.
+         *
+         * @param context
+         * @param url The URL of the media file.
+         * @param title The title that will be shown in the header.
+         * @param mimeType The MIME type of the file.
+         * @param type The type of the attachment.
+         * @return The [Intent] to start the [AttachmentMediaActivity].
+         */
+        public fun createIntent(
+            context: Context,
+            url: String,
+            title: String,
+            mimeType: String? = null,
+            type: String? = null,
+        ): Intent {
+            return Intent(context, AttachmentMediaActivity::class.java).apply {
+                putExtra(KEY_URL, url)
+                putExtra(KEY_TITLE, title)
+                putExtra(KEY_MIME_TYPE, mimeType)
+                putExtra(KEY_TYPE, type)
+            }
+        }
     }
 }
