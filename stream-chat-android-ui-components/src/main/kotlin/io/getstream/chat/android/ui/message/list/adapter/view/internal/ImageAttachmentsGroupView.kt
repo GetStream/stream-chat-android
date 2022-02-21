4b50bdd618f2fde926f32a3cd5013d894e1ddfce
filbabic <fbabic96@gmail.com>
2022-02-21 16:47:42 +0100

    Updated Compose, Kotlin, Moshi and Accompanist

--- stream-chat-android-ui-components/src/main/kotlin/io/getstream/chat/android/ui/message/list/adapter/view/internal/ImageAttachmentsGroupView.kt
+++ stream-chat-android-ui-components/src/main/kotlin/io/getstream/chat/android/ui/message/list/adapter/view/internal/ImageAttachmentsGroupView.kt
@@ -191,6 +191,7 @@ internal class ImageAttachmentsGroupView : ConstraintLayout {
                 stateCopy.viewThree.setImageShapeByCorners(0f, 0f, 0f, bottomLeftCorner)
                 stateCopy.viewFour.setImageShapeByCorners(0f, 0f, bottomRightCorner, 0f)
             }
+            else -> Unit
         }
     }
 
