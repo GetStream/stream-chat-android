e32f2c76ad9c46d22407ce97b8b957dc0964a2a6
Rafal Adasiewicz <rafal@getstream.io>
2022-03-04 12:36:28 +0100

    Add image and name properties to Channel and User objects (#3139)
    * Add image and name properties to Channel and User objects
    
    * Update docs
    
    Co-authored-by: Amit Kumar <amit.kumar@getstream.io>

--- stream-chat-android-client/src/test/java/io/getstream/chat/android/client/models/ExtensionsTests.kt
+++ stream-chat-android-client/src/test/java/io/getstream/chat/android/client/models/ExtensionsTests.kt
@@ -1,67 +1,9 @@
 package io.getstream.chat.android.client.models
 
-import org.amshove.kluent.shouldBeEmpty
 import org.amshove.kluent.shouldBeEqualTo
-import org.amshove.kluent.shouldBeNull
 import org.junit.Test
 
 internal class ExtensionsTests {
-
-    val nameValue = "bob"
-    val nameField = "name"
-    val imageValue = "http://image"
-    val imageField = "image"
-    val nonStringValue = 13
-
-    @Test
-    fun userPropertyFields() {
-        val user = User()
-        user.name.shouldBeEmpty()
-        user.extraData[nameField].shouldBeNull()
-
-        user.image.shouldBeEmpty()
-        user.extraData[imageField].shouldBeNull()
-
-        user.name = nameValue
-        user.image = imageValue
-
-        user.name shouldBeEqualTo nameValue
-        user.extraData[nameField] shouldBeEqualTo nameValue
-
-        user.image shouldBeEqualTo imageValue
-        user.extraData[imageField] shouldBeEqualTo imageValue
-    }
-
-    @Test
-    fun channelPropertyFields() {
-        val channel = Channel()
-        channel.name.shouldBeEmpty()
-        channel.extraData[nameField].shouldBeNull()
-
-        channel.image.shouldBeEmpty()
-        channel.extraData[imageField].shouldBeNull()
-
-        channel.name = nameValue
-        channel.image = imageValue
-
-        channel.name shouldBeEqualTo nameValue
-        channel.extraData[nameField] shouldBeEqualTo nameValue
-
-        channel.image shouldBeEqualTo imageValue
-        channel.extraData[imageField] shouldBeEqualTo imageValue
-    }
-
-    @Test
-    fun getExternalFieldTest() {
-        val user = User()
-        getExternalField(user, nameField).shouldBeEmpty()
-
-        user.name = nameValue
-        getExternalField(user, nameField) shouldBeEqualTo nameValue
-
-        user.extraData[nameField] = nonStringValue
-        getExternalField(user, nameField).shouldBeEmpty()
-    }
 
     @Test
     fun noUnread() {
