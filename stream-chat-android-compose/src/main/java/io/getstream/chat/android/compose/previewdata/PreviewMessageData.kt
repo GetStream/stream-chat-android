12258da3f207c96a01c1c53a7a66eac2d4b17b01
bychkovdmitry <9600921+bychkovdmitry@users.noreply.github.com>
2022-03-30 14:12:32 +0300

    [3229] Handle failed messages (#3263)

--- stream-chat-android-compose/src/main/java/io/getstream/chat/android/compose/previewdata/PreviewMessageData.kt
+++ stream-chat-android-compose/src/main/java/io/getstream/chat/android/compose/previewdata/PreviewMessageData.kt
@@ -11,18 +11,21 @@ import java.util.Date
 internal object PreviewMessageData {
 
     val message1: Message = Message().apply {
+        id = "message-id-1"
         text = "Lorem ipsum dolor sit amet, consectetuer adipiscing elit."
         createdAt = Date()
         type = ModelType.message_regular
     }
 
     val message2: Message = Message().apply {
+        id = "message-id-2"
         text = "Aenean commodo ligula eget dolor."
         createdAt = Date()
         type = ModelType.message_regular
     }
 
     val messageWithOwnReaction: Message = Message().apply {
+        id = "message-id-3"
         text = "Pellentesque leo dui, finibus et nibh et, congue aliquam lectus"
         createdAt = Date()
         type = ModelType.message_regular
