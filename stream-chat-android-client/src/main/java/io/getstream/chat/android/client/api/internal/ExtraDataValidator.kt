487c71303e72c70dfcf04586f4d42b99942ed29e
kanat <kijalbaev@gmail.com>
2022-04-01 19:55:36 +0400

    [2893] Improve flow for putting reserved fields in extraData

---
+++ stream-chat-android-client/src/main/java/io/getstream/chat/android/client/api/internal/ExtraDataValidator.kt
@@ -0,0 +1,167 @@
+package io.getstream.chat.android.client.api.internal
+
+import io.getstream.chat.android.client.api.ChatApi
+import io.getstream.chat.android.client.api.ErrorCall
+import io.getstream.chat.android.client.call.Call
+import io.getstream.chat.android.client.errors.ChatError
+import io.getstream.chat.android.client.models.Channel
+import io.getstream.chat.android.client.models.CustomObject
+import io.getstream.chat.android.client.models.Message
+import io.getstream.chat.android.client.models.User
+
+/**
+ * Intercepts [ChatApi] calls and validates [CustomObject.extraData] keys to prevent passing reserved names.
+ */
+internal class ExtraDataValidator(
+    private val delegate: ChatApi,
+) : ChatApi by delegate {
+
+    override fun updateChannel(
+        channelType: String,
+        channelId: String,
+        extraData: Map<String, Any>,
+        updateMessage: Message?,
+    ): Call<Channel> {
+        val reservedInChannel = extraData.findReserved<Channel>()
+        val reservedInMessage = updateMessage?.findReserved() ?: emptyList()
+        return when (reservedInChannel.isEmpty() && reservedInMessage.isEmpty()) {
+            true -> delegate.updateChannel(channelType, channelId, extraData, updateMessage)
+            else -> ErrorCall(
+                ChatError(
+                    message = when (reservedInMessage.isEmpty()) {
+                        true -> "[ChatApi.updateChannel] 'extraData' param contains reserved keys: $reservedInChannel"
+                        else -> "[ChatApi.updateChannel] 'updateMessage.extraData' property contains reserved keys: $reservedInMessage"
+                    }
+                )
+            )
+        }
+    }
+
+    override fun updateChannelPartial(
+        channelType: String,
+        channelId: String,
+        set: Map<String, Any>,
+        unset: List<String>,
+    ): Call<Channel> {
+        val reserved = set.findReserved<Channel>()
+        return when (reserved.isEmpty()) {
+            true -> delegate.updateChannelPartial(channelType, channelId, set, unset)
+            else -> ErrorCall(
+                ChatError(
+                    message = "[ChatApi.updateChannelPartial] 'set' param contains reserved keys: $reserved"
+                )
+            )
+        }
+    }
+
+    override fun updateMessage(message: Message): Call<Message> {
+        val reserved = message.findReserved()
+        return when (reserved.isEmpty()) {
+            true -> delegate.updateMessage(message)
+            else -> ErrorCall(
+                ChatError(
+                    message = "[ChatApi.updateMessage] 'message.extraData' property contains reserved keys: $reserved"
+                )
+            )
+        }
+    }
+
+    override fun partialUpdateMessage(messageId: String, set: Map<String, Any>, unset: List<String>): Call<Message> {
+        val reserved = set.findReserved<Message>()
+        return when (reserved.isEmpty()) {
+            true -> delegate.partialUpdateMessage(messageId, set, unset)
+            else -> ErrorCall(
+                ChatError(
+                    message = "[ChatApi.partialUpdateMessage] 'set' param contains reserved keys: $reserved"
+                )
+            )
+        }
+    }
+
+    override fun updateUsers(users: List<User>): Call<List<User>> {
+        val (user, reserved) = users.findReserved()
+        return when (user == null || reserved == null) {
+            true -> delegate.updateUsers(users)
+            else -> ErrorCall(
+                ChatError(
+                    message = "[ChatApi.updateUsers] 'user(id=${user.id}).extraData' property contains reserved keys: $reserved"
+                )
+            )
+        }
+    }
+
+    override fun partialUpdateUser(id: String, set: Map<String, Any>, unset: List<String>): Call<User> {
+        val reserved = set.findReserved<User>()
+        return when (reserved.isEmpty()) {
+            true -> delegate.partialUpdateUser(id, set, unset)
+            else -> ErrorCall(
+                ChatError(
+                    message = "[ChatApi.partialUpdateUser] 'set' param contains reserved keys: $reserved"
+                )
+            )
+        }
+    }
+
+    private fun <T : CustomObject> List<T>.findReserved(): Pair<T?, List<String>?> {
+        for (obj in this) {
+            val reserved = obj.findReserved()
+            if (reserved.isNotEmpty()) {
+                return Pair(obj, reserved)
+            }
+        }
+        return Pair(null, null)
+    }
+
+    private fun CustomObject.findReserved(): List<String> {
+        return when (this) {
+            is Channel -> extraData.keys.filter(reservedInChannelPredicate)
+            is Message -> extraData.keys.filter(reservedInChannelPredicate)
+            is User -> extraData.keys.filter(reservedInChannelPredicate)
+            else -> emptyList()
+        }
+    }
+
+    private inline fun <reified T : CustomObject> Map<String, Any>.findReserved(): List<String> {
+        return when (T::class) {
+            Channel::class -> keys.filter(reservedInChannelPredicate)
+            Message::class -> keys.filter(reservedInMessagePredicate)
+            User::class -> keys.filter(reservedInUserPredicate)
+            else -> emptyList()
+        }
+    }
+
+    private companion object {
+        private val reservedInChannelPredicate: (String) -> Boolean = { reservedInChannel.contains(it) }
+        private val reservedInMessagePredicate: (String) -> Boolean = { reservedInChannel.contains(it) }
+        private val reservedInUserPredicate: (String) -> Boolean = { reservedInChannel.contains(it) }
+
+        private val reservedInChannel = arrayOf(
+            "cid",
+            "id",
+            "type",
+            "created_at",
+            "deleted_at",
+            "updated_at",
+            "member_count",
+            "created_by",
+            "last_message_at",
+            "own_capabilities",
+            "config",
+        )
+
+        private val reservedInMessage = arrayOf(
+            "id",
+            "cid",
+            "created_at",
+            "updated_at",
+            "deleted_at",
+        )
+
+        private val reservedInUser = arrayOf(
+            "id",
+            "cid",
+            "created_at",
+            "updated_at",
+        )
+    }
+}
---
+++ stream-chat-android-client/src/test/java/io/getstream/chat/android/client/api/internal/ExtraDataValidatorTests.kt
@@ -0,0 +1,156 @@
+package io.getstream.chat.android.client.api.internal
+
+import io.getstream.chat.android.client.api.ChatApi
+import io.getstream.chat.android.client.models.Channel
+import io.getstream.chat.android.client.models.Message
+import io.getstream.chat.android.client.models.User
+import io.getstream.chat.android.client.utils.Result
+import io.getstream.chat.android.test.TestCall
+import org.amshove.kluent.`should be equal to`
+import org.amshove.kluent.shouldBeFalse
+import org.junit.Test
+import org.mockito.kotlin.doReturn
+import org.mockito.kotlin.mock
+import org.mockito.kotlin.whenever
+
+internal class ExtraDataValidatorTests {
+
+    private val chatApi: ChatApi = mock()
+    private val validator = ExtraDataValidator(chatApi)
+
+    @Test
+    fun testUpdateChannel() {
+        /* Given */
+        val channel: Channel = mock()
+        val channelId = "channel-id"
+        val channelType = "channel-type"
+        val extraData = mapOf("id" to "another-id")
+        val updateMessage = null
+
+        whenever(chatApi.updateChannel(channelType, channelId, extraData, updateMessage)) doReturn channel.toTestCall()
+
+        /* When */
+        val result: Result<Channel> = validator.updateChannel(
+            channelId = channelId,
+            channelType = channelType,
+            extraData = extraData,
+            updateMessage = updateMessage
+        ).execute()
+
+        /* Then */
+        println("[testUpdateChannel] error.message: \"${result.error().message}\"")
+        result.isSuccess.shouldBeFalse()
+        result.error().message?.contains("id") `should be equal to` true
+    }
+
+    @Test
+    fun testUpdateChannelPartial() {
+        /* Given */
+        val channel: Channel = mock()
+        val channelId = "channel-id"
+        val channelType = "channel-type"
+        val set = mapOf("type" to "another-type")
+        val unset = emptyList<String>()
+
+        whenever(chatApi.updateChannelPartial(channelType, channelId, set, unset)) doReturn channel.toTestCall()
+
+        /* When */
+        val result: Result<Channel> = validator.updateChannelPartial(
+            channelId = channelId,
+            channelType = channelType,
+            set = set,
+            unset = unset
+        ).execute()
+
+        /* Then */
+        println("[testUpdateChannelPartial] error.message: \"${result.error().message}\"")
+        result.isSuccess.shouldBeFalse()
+        result.error().message?.contains("type") `should be equal to` true
+    }
+
+    @Test
+    fun testUpdateMessage() {
+        /* Given */
+        val message: Message = mock()
+        val extraData: MutableMap<String, Any> = mutableMapOf("cid" to "another-cid")
+
+        whenever(message.extraData) doReturn extraData
+        whenever(chatApi.updateMessage(message)) doReturn message.toTestCall()
+
+        /* When */
+        val result: Result<Message> = validator.updateMessage(message).execute()
+
+        /* Then */
+        println("[testUpdateMessage] error.message: \"${result.error().message}\"")
+        result.isSuccess.shouldBeFalse()
+        result.error().message?.contains("cid") `should be equal to` true
+    }
+
+    @Test
+    fun testPartialUpdateMessage() {
+        /* Given */
+        val messageId = "message-id"
+        val message: Message = mock()
+        val set: MutableMap<String, Any> = mutableMapOf("created_at" to "another-date")
+        val unset = emptyList<String>()
+
+        whenever(chatApi.partialUpdateMessage(messageId, set, unset)) doReturn message.toTestCall()
+
+        /* When */
+        val result: Result<Message> = validator.partialUpdateMessage(messageId, set, unset).execute()
+
+        /* Then */
+        println("[testPartialUpdateMessage] error.message: \"${result.error().message}\"")
+        result.isSuccess.shouldBeFalse()
+        result.error().message?.contains("created_at") `should be equal to` true
+    }
+
+    @Test
+    fun testUpdateUsers() {
+        /* Given */
+        val user: User = mock()
+        val users = listOf(user)
+        val extraData: MutableMap<String, Any> = mutableMapOf(
+            "cid" to "another-cid",
+            "updated_at" to "another-date"
+        )
+        whenever(user.extraData) doReturn extraData
+        whenever(chatApi.updateUsers(users)) doReturn users.toTestCall()
+
+        /* When */
+        val result: Result<List<User>> = validator.updateUsers(users).execute()
+
+        /* Then */
+        println("[testUpdateUsers] error.message: \"${result.error().message}\"")
+        result.isSuccess.shouldBeFalse()
+        result.error().message?.contains("cid") `should be equal to` true
+        result.error().message?.contains("updated_at") `should be equal to` true
+    }
+
+    @Test
+    fun testPartialUpdateUser() {
+        /* Given */
+        val userId = "user-id"
+        val user: User = mock()
+        val set: MutableMap<String, Any> = mutableMapOf(
+            "updated_at" to "another-date",
+            "created_at" to "another-date"
+        )
+        val unset = emptyList<String>()
+        whenever(chatApi.partialUpdateUser(userId, set, unset)) doReturn user.toTestCall()
+
+        /* When */
+        val result: Result<User> = validator.partialUpdateUser(userId, set, unset).execute()
+
+        /* Then */
+        println("[testPartialUpdateUser] error.message: \"${result.error().message}\"")
+        result.isSuccess.shouldBeFalse()
+        result.error().message?.contains("updated_at") `should be equal to` true
+        result.error().message?.contains("created_at") `should be equal to` true
+        println(result.error().message)
+    }
+
+    private fun <T : Any> T.toTestCall(): TestCall<T> {
+        return TestCall(Result.success(this))
+    }
+}
