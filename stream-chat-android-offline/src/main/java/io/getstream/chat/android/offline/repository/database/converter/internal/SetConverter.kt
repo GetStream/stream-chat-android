ebedc48a9e9c6e5a82aca3e8937c37bd814a3e71
kanat <kijalbaev@gmail.com>
2022-04-05 17:00:36 +0400

    [3114] Migrate DB entities to new TypeConverter analyzer

--- stream-chat-android-offline/src/main/java/io/getstream/chat/android/offline/repository/database/converter/internal/SetConverter.kt
+++ stream-chat-android-offline/src/main/java/io/getstream/chat/android/offline/repository/database/converter/internal/SetConverter.kt
@@ -31,8 +31,21 @@ internal class SetConverter {
         return adapter.fromJson(data) ?: emptySet()
     }
 
+    /**
+     * @return Nullable [String] to let KSP know this function cannot be used for "Nullable to NonNull" converting.
+     *
+     * An example of the incorrect behaviour:
+     *     val stringifiedObject: String? = anotherConverter.objectToString(...)
+     *     val stringList: Set<String> = SetConverter.stringToSortedSet(stringifiedObject)
+     *     val nonNullStringifiedObject: String = SetConverter.sortedSetToString(stringifiedObject)
+     *     // ... binding nonNullStringifiedObject to table's column
+     *
+     * The current behavior:
+     *     val stringifiedObject: String? = anotherConverter.objectToString(...)
+     *     // ... binding stringifiedObject to table's column
+     */
     @TypeConverter
-    fun sortedSetToString(someObjects: Set<String>?): String {
+    fun sortedSetToString(someObjects: Set<String>?): String? {
         return adapter.toJson(someObjects)
     }
 }
