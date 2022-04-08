57eecc44c4d5d7f4da3c98ed6c4529754e6d3d52
Leandro Borges Ferreira <lehen01@gmail.com>
2022-04-08 12:47:30 -0300

    Simplifying RepositoryProvider

--- stream-chat-android-client/src/main/java/io/getstream/chat/android/client/persistence/repository/provider/RepositoryProvider.kt
+++ stream-chat-android-client/src/main/java/io/getstream/chat/android/client/persistence/repository/provider/RepositoryProvider.kt
@@ -1,44 +1,20 @@
 package io.getstream.chat.android.client.persistence.repository.provider
 
-import io.getstream.chat.android.client.models.Message
-import io.getstream.chat.android.client.models.User
-import io.getstream.chat.android.client.persistence.repository.AttachmentRepository
-import io.getstream.chat.android.client.persistence.repository.ChannelConfigRepository
-import io.getstream.chat.android.client.persistence.repository.ChannelRepository
-import io.getstream.chat.android.client.persistence.repository.MessageRepository
-import io.getstream.chat.android.client.persistence.repository.QueryChannelsRepository
-import io.getstream.chat.android.client.persistence.repository.ReactionRepository
-import io.getstream.chat.android.client.persistence.repository.SyncStateRepository
-import io.getstream.chat.android.client.persistence.repository.UserRepository
 import io.getstream.chat.android.client.persistence.repository.factory.InMemoryRepositoryFactory
 import io.getstream.chat.android.client.persistence.repository.factory.RepositoryFactory
 
-public object RepositoryProvider : RepositoryFactory {
+internal class RepositoryProvider private constructor(
+    private val repositoryFactory: RepositoryFactory
+) : RepositoryFactory by repositoryFactory {
 
-    private var repositoryFactory: RepositoryFactory = InMemoryRepositoryFactory()
+    internal companion object {
 
-    public fun setRepositoryFactory(repositoryFactory: RepositoryFactory) {
-        this.repositoryFactory = repositoryFactory
+        private var instance: RepositoryProvider = RepositoryProvider(InMemoryRepositoryFactory())
+
+        fun setRepositoryFactory(repositoryFactory: RepositoryFactory) {
+            instance = RepositoryProvider(repositoryFactory)
+        }
+
+        fun get(): RepositoryProvider = instance
     }
-
-    override fun userRepository(): UserRepository = repositoryFactory.userRepository()
-
-    override fun messageRepository(getUser: suspend (userId: String) -> User): MessageRepository =
-        repositoryFactory.messageRepository(getUser)
-
-    override fun channelConfigRepository(): ChannelConfigRepository = repositoryFactory.channelConfigRepository()
-
-    override fun channelRepository(
-        getUser: suspend (userId: String) -> User,
-        getMessage: suspend (messageId: String) -> Message?,
-    ): ChannelRepository = repositoryFactory.channelRepository(getUser, getMessage)
-
-    override fun queryChannelsRepository(): QueryChannelsRepository = repositoryFactory.queryChannelsRepository()
-
-    override fun reactionRepository(getUser: suspend (userId: String) -> User): ReactionRepository =
-        repositoryFactory.reactionRepository(getUser)
-
-    override fun syncStateRepository(): SyncStateRepository = repositoryFactory.syncStateRepository()
-
-    override fun attachmentRepository(): AttachmentRepository = repositoryFactory.attachmentRepository()
 }
