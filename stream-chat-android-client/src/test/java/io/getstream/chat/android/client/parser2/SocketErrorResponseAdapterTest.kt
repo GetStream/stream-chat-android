b87545769ba9345b86949c3140dc648ac9058457
Jc Miñarro <josecarlos.minarro@gmail.com>
2022-04-26 16:03:09 +0200

    Fix property name

--- stream-chat-android-client/src/test/java/io/getstream/chat/android/client/parser2/SocketErrorResponseAdapterTest.kt
+++ stream-chat-android-client/src/test/java/io/getstream/chat/android/client/parser2/SocketErrorResponseAdapterTest.kt
@@ -14,25 +14,23 @@
  * limitations under the License.
  */
 
-package io.getstream.chat.android.client.api2.mapping
+package io.getstream.chat.android.client.parser2
 
 import io.getstream.chat.android.client.api2.model.response.SocketErrorResponse
-import io.getstream.chat.android.client.socket.ErrorResponse
-import io.getstream.chat.android.client.socket.SocketErrorMessage
+import io.getstream.chat.android.client.parser2.testdata.SocketErrorResponseTestData
+import org.amshove.kluent.`should be equal to`
+import org.junit.jupiter.api.Test
 
-internal fun SocketErrorResponse.toDomain(): SocketErrorMessage {
-    return SocketErrorMessage(
-        error = error?.toDomain()
-    )
-}
+internal class SocketErrorResponseAdapterTest {
 
-internal fun SocketErrorResponse.ErrorResponse.toDomain(): ErrorResponse {
-    val dto = this
-    return ErrorResponse(
-        code = dto.code,
-        message = dto.message,
-        statusCode = dto.StatusCode,
-    ).apply {
-        duration = dto.duration
+    private val parser = MoshiChatParser()
+
+    @Test
+    fun `Deserialize JSON socket error response`() {
+        val error = parser.fromJson(
+            SocketErrorResponseTestData.json,
+            SocketErrorResponse::class.java
+        )
+        error `should be equal to` SocketErrorResponseTestData.socketErrorResponse
     }
 }
