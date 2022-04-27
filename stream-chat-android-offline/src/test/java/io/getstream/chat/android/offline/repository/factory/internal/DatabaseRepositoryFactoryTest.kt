c6a879f69f8eb0cb6d8c7db87f3ee7b62b709b35
Leandro Borges Ferreira <lehen01@gmail.com>
2022-04-27 14:43:37 -0300

    Renaming and adding license

--- stream-chat-android-offline/src/test/java/io/getstream/chat/android/offline/repository/factory/internal/DatabaseRepositoryFactoryTest.kt
+++ stream-chat-android-offline/src/test/java/io/getstream/chat/android/offline/repository/factory/internal/DatabaseRepositoryFactoryTest.kt
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2014-2022 Stream.io Inc. All rights reserved.
+ *
+ * Licensed under the Stream License;
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    https://github.com/GetStream/stream-chat-android/blob/main/LICENSE
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package io.getstream.chat.android.offline.repository.factory.internal
 
 import io.getstream.chat.android.client.persistance.repository.AttachmentRepository
@@ -17,7 +33,7 @@ import io.getstream.chat.android.offline.repository.domain.queryChannels.interna
 import io.getstream.chat.android.offline.repository.domain.reaction.internal.ReactionDao
 import io.getstream.chat.android.offline.repository.domain.syncState.internal.SyncStateDao
 import io.getstream.chat.android.offline.repository.domain.user.internal.UserDao
-import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.Assertions.assertNotNull
 import org.junit.jupiter.api.Test
 import org.mockito.kotlin.doReturn
 import org.mockito.kotlin.mock
