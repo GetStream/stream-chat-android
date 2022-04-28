20f52f1c2f53c85af4b0c1547a9bc0ae4a2eecac
MarinTolic <marin.t91@gmail.com>
2022-04-28 11:16:36 +0200

    [3418] Make `MessageInputView` responsive by setting `isFocusableInTouchMode` on its inner `EditText`

--- stream-chat-android-ui-components/src/main/kotlin/io/getstream/chat/android/ui/message/input/MessageInputView.kt
+++ stream-chat-android-ui-components/src/main/kotlin/io/getstream/chat/android/ui/message/input/MessageInputView.kt
@@ -756,7 +756,7 @@ public class MessageInputView : ConstraintLayout {
                     handleKeyStroke()
 
                     /** Debouncing when clearing the input will cause the suggestion list
-                     popup to appear briefly after clearing the input in certain cases. */
+                    popup to appear briefly after clearing the input in certain cases. */
                     if (messageText.isEmpty()) {
                         messageInputDebouncer?.cancelLastDebounce()
                         suggestionListController?.onNewMessageText(messageText)
@@ -929,6 +929,7 @@ public class MessageInputView : ConstraintLayout {
         }
         binding.messageInputFieldView.binding.messageEditText.isEnabled = canSend
         binding.messageInputFieldView.binding.messageEditText.isFocusable = canSend
+        binding.messageInputFieldView.binding.messageEditText.isFocusableInTouchMode = canSend
     }
 
     /**
