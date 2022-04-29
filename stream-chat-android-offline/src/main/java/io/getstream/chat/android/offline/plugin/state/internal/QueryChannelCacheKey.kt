6a075356cdb46a7bfb1f1e42aa26d79361a30b9f
Leandro Borges Ferreira <lehen01@gmail.com>
2022-04-29 19:21:44 -0300

    Changing visibility of QueryChannelCacheKey

--- stream-chat-android-offline/src/main/java/io/getstream/chat/android/offline/plugin/state/internal/QueryChannelCacheKey.kt
+++ stream-chat-android-offline/src/main/java/io/getstream/chat/android/offline/plugin/state/internal/QueryChannelCacheKey.kt
@@ -18,7 +18,7 @@ package io.getstream.chat.android.offline.plugin.state.internal
 
 import io.getstream.chat.android.client.api.models.QueryChannelsRequest
 
-public data class QueryChannelCacheKey(
+internal data class QueryChannelCacheKey(
     val request: QueryChannelsRequest,
     val channelType: String? = null,
     val channelId: String? = null,
