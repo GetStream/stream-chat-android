7a4227f05af6de88f62c24f32b46981d398f97b9
TGazica <tomislav.gazica@gmail.com>
2022-05-04 11:24:28 +0200

    [3276] Minor refactor

--- stream-chat-android-compose/src/main/java/io/getstream/chat/android/compose/ui/messages/list/MessageItem.kt
+++ stream-chat-android-compose/src/main/java/io/getstream/chat/android/compose/ui/messages/list/MessageItem.kt
@@ -221,12 +221,11 @@ internal fun RowScope.DefaultMessageItemLeadingContent(
         .size(24.dp)
         .align(Alignment.Bottom)
 
-    if (!messageItem.isMine &&
-        (
-            messageItem.shouldShowFooter ||
-                messageItem.groupPosition == Bottom ||
-                messageItem.groupPosition == None
-            )
+    if (!messageItem.isMine && (
+        messageItem.shouldShowFooter ||
+            messageItem.groupPosition == Bottom ||
+            messageItem.groupPosition == None
+        )
     ) {
         UserAvatar(
             modifier = modifier,
