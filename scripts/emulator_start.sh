43d7ca5bcfa0f61428d6ebe180f8c93935ee3bda
Dmitrii Bychkov <mail@dbychkov.com>
2022-05-05 12:13:52 +0300

    [3449] Snapshot scripts

--- scripts/emulator_start.sh
+++ scripts/emulator_start.sh
@@ -11,5 +11,11 @@ if $ANDROID_HOME/platform-tools/adb devices -l | grep -q emulator; then
 fi
 
 echo "Starting emulator..."
-echo "no" | $ANDROID_HOME/emulator/emulator "-avd" "Pixel_API_26" "-no-audio" "-no-boot-anim" "-gpu" "swiftshader_indirect" "-camera-back" "none" "-camera-front" "none" &
+echo "no" | $ANDROID_HOME/emulator/emulator "-avd" "Pixel_API_26" "-no-audio" "-no-boot-anim" "-gpu" "swiftshader_indirect" "-camera-back" "none" "-camera-front" "none" "-gpu" "host" &
 echo "Emulator started."
+
+echo "Waiting for emulator..."
+while [ "$(adb shell getprop sys.boot_completed | tr -d '\r' )" != "1" ];
+    do sleep 1;
+done
+echo "Emulator is ready."
