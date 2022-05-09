39782fdb7f0ea7fb3ed62ad350477e11c83e5ea3
filbabic <fbabic96@gmail.com>
2022-05-09 11:39:17 +0200

    Minor docs fixes

--- stream-chat-android-compose/src/main/java/io/getstream/chat/android/compose/viewmodel/messages/MessagesViewModelFactory.kt
+++ stream-chat-android-compose/src/main/java/io/getstream/chat/android/compose/viewmodel/messages/MessagesViewModelFactory.kt
@@ -40,11 +40,12 @@ import java.util.concurrent.TimeUnit
  * @param enforceUniqueReactions Flag to enforce unique reactions or enable multiple from the same user.
  * @param messageLimit The limit when loading messages.
  * @param maxAttachmentCount The maximum number of attachments that can be sent in a single message.
- * @param maxAttachmentSize Tne maximum file size of each attachment in bytes. By default, 20mb for Stream CDN.
+ * @param maxAttachmentSize The maximum file size of each attachment in bytes. By default, 20mb for Stream CDN.
  * @param showDateSeparators If we should show date separator items in the list.
  * @param showSystemMessages If we should show system message items in the list.
  * @param deletedMessageVisibility The behavior of deleted messages in the list and if they're visible or not.
  * @param messageFooterVisibility The behavior of message footers in the list and their visibility.
+ * @param dateSeparatorThresholdMillis The millisecond amount that represents the threshold for adding date separators.
  */
 public class MessagesViewModelFactory(
     private val context: Context,
