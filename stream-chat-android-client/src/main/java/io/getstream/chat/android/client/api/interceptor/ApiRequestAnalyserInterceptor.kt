efc6238288998e16636fd9078888e11d6a0235e3
Leandro Borges Ferreira <lehen01@gmail.com>
2022-05-11 16:24:10 -0300

    Adding licenses

--- stream-chat-android-client/src/main/java/io/getstream/chat/android/client/api/interceptor/ApiRequestAnalyserInterceptor.kt
+++ stream-chat-android-client/src/main/java/io/getstream/chat/android/client/api/interceptor/ApiRequestAnalyserInterceptor.kt
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2014-2022 Stream.io Inc. All rights reserved.
+ *
+ * Licensed under the Stream License;
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    https://github.com/GetStream/stream-chat-android/blob/main/LICENSE
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package io.getstream.chat.android.client.api.interceptor
 
 import io.getstream.chat.android.client.plugins.requests.ApiRequestsAnalyser
@@ -6,7 +22,7 @@ import okhttp3.Response
 import okio.Buffer
 import java.io.OutputStream
 
-internal class ApiRequestAnalyserInterceptor(private val requestsAnalyser: ApiRequestsAnalyser): Interceptor {
+internal class ApiRequestAnalyserInterceptor(private val requestsAnalyser: ApiRequestsAnalyser) : Interceptor {
 
     override fun intercept(chain: Interceptor.Chain): Response {
         val request = chain.request()
@@ -23,7 +39,7 @@ internal class ApiRequestAnalyserInterceptor(private val requestsAnalyser: ApiRe
     }
 }
 
-private class StringOutputStream(): OutputStream() {
+private class StringOutputStream : OutputStream() {
 
     private val stringBuilder = StringBuilder()
 
