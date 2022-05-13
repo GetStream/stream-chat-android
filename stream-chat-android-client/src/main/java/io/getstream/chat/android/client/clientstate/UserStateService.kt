baa7ef7cde5f97420c977cd73ac5c40350d9f481
Amit Kumar <amit.kumar@getstream.io>
2022-05-13 14:53:21 +0530

    [3382] Refactor Finite State Machines (#3477)
    * Refactor FSM
    
    * Simplify FSMs and make them less agressive when handling events

--- stream-chat-android-client/src/main/java/io/getstream/chat/android/client/clientstate/UserStateService.kt
+++ stream-chat-android-client/src/main/java/io/getstream/chat/android/client/clientstate/UserStateService.kt
@@ -16,10 +16,13 @@
 
 package io.getstream.chat.android.client.clientstate
 
+import io.getstream.chat.android.client.logger.ChatLogger
 import io.getstream.chat.android.client.models.User
 import io.getstream.chat.android.core.internal.fsm.FiniteStateMachine
 
 internal class UserStateService {
+    private val logger = ChatLogger.get("UserStateService")
+
     fun onUserUpdated(user: User) {
         fsm.sendEvent(UserStateEvent.UserUpdated(user))
     }
@@ -44,25 +47,26 @@ internal class UserStateService {
         get() = fsm.state
 
     private val fsm = FiniteStateMachine<UserState, UserStateEvent> {
-        defaultHandler { state, event -> error("Can't handle $event while being in state ${state::class.simpleName}") }
+        defaultHandler { state, event ->
+            logger.logE("Can't handle $event while being in state ${state::class.simpleName}")
+            state
+        }
         initialState(UserState.NotSet)
         state<UserState.NotSet> {
-            onEvent<UserStateEvent.ConnectUser> { _, event -> UserState.UserSet(event.user) }
-            onEvent<UserStateEvent.ConnectAnonymous> { _, _ -> UserState.Anonymous.Pending }
-            onEvent<UserStateEvent.UnsetUser> { _, _ -> stay() }
-            onEvent<UserStateEvent.UserUpdated> { _, _ -> stay() }
+            onEvent<UserStateEvent.ConnectUser> { event -> UserState.UserSet(event.user) }
+            onEvent<UserStateEvent.ConnectAnonymous> { UserState.Anonymous.Pending }
         }
         state<UserState.UserSet> {
-            onEvent<UserStateEvent.UserUpdated> { _, event -> UserState.UserSet(event.user) }
-            onEvent<UserStateEvent.UnsetUser> { _, _ -> UserState.NotSet }
+            onEvent<UserStateEvent.UserUpdated> { event -> UserState.UserSet(event.user) }
+            onEvent<UserStateEvent.UnsetUser> { UserState.NotSet }
         }
         state<UserState.Anonymous.Pending> {
-            onEvent<UserStateEvent.UserUpdated> { _, event -> UserState.Anonymous.AnonymousUserSet(event.user) }
-            onEvent<UserStateEvent.UnsetUser> { _, _ -> UserState.NotSet }
+            onEvent<UserStateEvent.UserUpdated> { event -> UserState.Anonymous.AnonymousUserSet(event.user) }
+            onEvent<UserStateEvent.UnsetUser> { UserState.NotSet }
         }
         state<UserState.Anonymous.AnonymousUserSet> {
-            onEvent<UserStateEvent.UserUpdated> { _, event -> UserState.Anonymous.AnonymousUserSet(event.user) }
-            onEvent<UserStateEvent.UnsetUser> { _, _ -> UserState.NotSet }
+            onEvent<UserStateEvent.UserUpdated> { event -> UserState.Anonymous.AnonymousUserSet(event.user) }
+            onEvent<UserStateEvent.UnsetUser> { UserState.NotSet }
         }
     }
 
