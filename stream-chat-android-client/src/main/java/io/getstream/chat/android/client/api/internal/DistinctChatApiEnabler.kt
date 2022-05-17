ae1c0482a0c3d3a89bf538e7390240669974916a
Leandro Borges Ferreira <lehen01@gmail.com>
2022-05-17 14:17:54 -0300

    Adding cache for all read api methods

--- stream-chat-android-client/src/main/java/io/getstream/chat/android/client/api/internal/DistinctChatApiEnabler.kt
+++ stream-chat-android-client/src/main/java/io/getstream/chat/android/client/api/internal/DistinctChatApiEnabler.kt
@@ -17,9 +17,19 @@
 package io.getstream.chat.android.client.api.internal
 
 import io.getstream.chat.android.client.api.ChatApi
+import io.getstream.chat.android.client.api.models.FilterObject
+import io.getstream.chat.android.client.api.models.PinnedMessagesPagination
 import io.getstream.chat.android.client.api.models.QueryChannelRequest
+import io.getstream.chat.android.client.api.models.QueryChannelsRequest
+import io.getstream.chat.android.client.api.models.QuerySort
 import io.getstream.chat.android.client.call.Call
+import io.getstream.chat.android.client.models.BannedUser
+import io.getstream.chat.android.client.models.BannedUsersSort
 import io.getstream.chat.android.client.models.Channel
+import io.getstream.chat.android.client.models.Member
+import io.getstream.chat.android.client.models.Message
+import io.getstream.chat.android.client.models.Reaction
+import java.util.Date
 
 /**
  * Enables/Disables [DistinctChatApi] based on [distinctCallsEnabled] return value.
@@ -27,15 +37,78 @@ import io.getstream.chat.android.client.models.Channel
 @Suppress("UNCHECKED_CAST")
 internal class DistinctChatApiEnabler(
     private val distinctApi: DistinctChatApi,
-    private val distinctCallsEnabled: () -> Boolean
+    private val distinctCallsEnabled: () -> Boolean,
 ) : ChatApi by distinctApi.delegate {
 
     private val originalApi = distinctApi.delegate
 
-    override fun queryChannel(channelType: String, channelId: String, query: QueryChannelRequest): Call<Channel> {
-        return when (distinctCallsEnabled()) {
-            true -> distinctApi.queryChannel(channelType, channelId, query)
-            else -> originalApi.queryChannel(channelType, channelId, query)
-        }
+    override fun getRepliesMore(messageId: String, firstId: String, limit: Int): Call<List<Message>> {
+        return getApi().getRepliesMore(messageId, firstId, limit)
     }
+
+    override fun getReplies(messageId: String, limit: Int): Call<List<Message>> {
+        return getApi().getReplies(messageId, limit)
+    }
+
+    override fun getReactions(messageId: String, offset: Int, limit: Int): Call<List<Reaction>> {
+        return getApi().getReactions(messageId, offset, limit)
+    }
+
+    override fun getMessage(messageId: String): Call<Message> {
+        return getApi().getMessage(messageId)
+    }
+
+    override fun getPinnedMessages(
+        channelType: String,
+        channelId: String,
+        limit: Int,
+        sort: QuerySort<Message>,
+        pagination: PinnedMessagesPagination,
+    ): Call<List<Message>> {
+        return getApi().getPinnedMessages(channelType, channelId, limit, sort, pagination)
+    }
+
+    override fun queryChannels(query: QueryChannelsRequest): Call<List<Channel>> {
+        return getApi().queryChannels(query)
+    }
+
+    override fun queryBannedUsers(
+        filter: FilterObject,
+        sort: QuerySort<BannedUsersSort>,
+        offset: Int?,
+        limit: Int?,
+        createdAtAfter: Date?,
+        createdAtAfterOrEqual: Date?,
+        createdAtBefore: Date?,
+        createdAtBeforeOrEqual: Date?,
+    ): Call<List<BannedUser>> {
+        return getApi().queryBannedUsers(
+            filter,
+            sort,
+            offset,
+            limit,
+            createdAtAfter,
+            createdAtAfterOrEqual,
+            createdAtBefore,
+            createdAtBeforeOrEqual,
+        )
+    }
+
+    override fun queryMembers(
+        channelType: String,
+        channelId: String,
+        offset: Int,
+        limit: Int,
+        filter: FilterObject,
+        sort: QuerySort<Member>,
+        members: List<Member>,
+    ): Call<List<Member>> {
+        return getApi().queryMembers(channelType, channelId, offset, limit, filter, sort, members)
+    }
+
+    override fun queryChannel(channelType: String, channelId: String, query: QueryChannelRequest): Call<Channel> {
+        return getApi().queryChannel(channelType, channelId, query)
+    }
+
+    private fun getApi() = if (distinctCallsEnabled()) distinctApi else originalApi
 }
