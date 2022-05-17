dac88421fe04e3d0e258f898dd01dde7ca242da7
Leandro Borges Ferreira <lehen01@gmail.com>
2022-05-17 16:19:52 -0300

    Fixing detelt

--- stream-chat-android-offline/src/main/java/io/getstream/chat/android/offline/plugin/listener/internal/QueryChannelsListenerImpl.kt
+++ stream-chat-android-offline/src/main/java/io/getstream/chat/android/offline/plugin/listener/internal/QueryChannelsListenerImpl.kt
@@ -55,6 +55,7 @@ internal class QueryChannelsListenerImpl(private val logic: LogicRegistry) : Que
     }
 
     override suspend fun onQueryChannelsResult(result: Result<List<Channel>>, request: QueryChannelsRequest) {
+        // Nothing to do
     }
 
     private companion object {
