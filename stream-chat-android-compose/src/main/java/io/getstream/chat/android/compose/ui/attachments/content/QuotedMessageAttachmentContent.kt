382e2be9cfbd065c272b1505779d77bf723ebebe
Tomislav Gazica <38032787+TGazica@users.noreply.github.com>
2022-05-17 12:46:38 +0200

    [2238] Improve Quoted Messages (#3472)
    * [2238] Add scroll to message logic
    
    * [2238] Refactor listeners
    
    * [2238] Add quoted message layouts
    
    * [2238] Refactor quotes with empty message text
    
    * [2238] Refactor quoted message click indicator
    
    * [2238] Refactor the way quoted message is queried and scrolled to
    
    * [2238] Refactor the way quoted message is queried and scrolled to
    
    * [2238] Add documentation
    
    * [2238] Fix QuotedAttachmentFactory name
    
    * [2238] Update changelog
    
    * [2238] Refactor the way scroll to quoted message is handled
    
    * [2238] Revert hat client changes
    
    * [2238] Add scroll to position offset
    
    * [2238] Fix current user quoted message layout
    
    * [2238] Refactor scrollToSelectedMessage kdoc
    
    * [2238] Refactor kdocs and quoted message composables
    
    * [2238] Refactor how message scroll offset is determined and propagated to Messages composable
    
    * Minor improvements & left some TODOs
    
    * Minor improvements & left some TODOs
    
    * [2238] Refactor how message scroll offset is determined and propagated to Messages composable
    
    * [2238] Refactor quoted message content layout
    
    * [2238] Updated docs with QuotedMessageFactory guide
    
    * [2238] Updated changelog
    
    * [2238] Updated deprecations and added TODO to update before release
    
    * Custom message example edit pass
    
    * Remove extra space
    
    * PR cleanup and edit
    
    * PR cleanup
    
    * PR cleanup
    
    * Api dump
    
    * Minor improvement
    
    * Formatting
    
    * Minor typo
    
    Co-authored-by: Marin Tolić <37080097+MarinTolic@users.noreply.github.com>
    
    Co-authored-by: filbabic <fbabic96@gmail.com>
    Co-authored-by: Marin Tolić <37080097+MarinTolic@users.noreply.github.com>

---
+++ stream-chat-android-compose/src/main/java/io/getstream/chat/android/compose/ui/attachments/content/QuotedMessageAttachmentContent.kt
@@ -0,0 +1,66 @@
+/*
+ * Copyright (c) 2014-2022 Stream.io Inc. All rights reserved.
+ *
+ * Licensed under the Stream License;
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    https://github.com/GetStream/stream-chat-android/blob/main/LICENSE
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.getstream.chat.android.compose.ui.attachments.content
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import io.getstream.chat.android.client.models.Message
+import io.getstream.chat.android.compose.state.imagepreview.ImagePreviewResult
+import io.getstream.chat.android.compose.state.messages.attachments.AttachmentState
+import io.getstream.chat.android.compose.ui.theme.ChatTheme
+
+/**
+ * Represents the content that's shown in a quoted message if the attachments are not empty.
+ *
+ * @param message The message that contains the attachments.
+ * @param modifier Modifier for styling.
+ * @param onLongItemClick Handler for long item taps.
+ * @param onImagePreviewResult Handler when the user selects a message option in the Image Preview screen.
+ */
+@Composable
+public fun QuotedMessageAttachmentContent(
+    message: Message,
+    onLongItemClick: (Message) -> Unit,
+    modifier: Modifier = Modifier,
+    onImagePreviewResult: (ImagePreviewResult?) -> Unit = {},
+) {
+    val attachments = message.attachments
+
+    /**
+     * Looks for quoted message attachment factory and if none can handle it looks for a standard attachment factory
+     * that can handle the attachmentContent.
+     */
+    val quoteAttachmentFactory = if (attachments.isNotEmpty()) {
+        val quotedFactory = ChatTheme.quotedAttachmentFactories.firstOrNull {
+            it.canHandle(message.attachments.take(1))
+        }
+        quotedFactory ?: ChatTheme.attachmentFactories.firstOrNull { it.canHandle(message.attachments.take(1)) }
+    } else {
+        null
+    }
+
+    val attachmentState = AttachmentState(
+        message = message,
+        onLongItemClick = onLongItemClick,
+        onImagePreviewResult = onImagePreviewResult
+    )
+
+    quoteAttachmentFactory?.content?.invoke(
+        modifier = modifier,
+        attachmentState = attachmentState
+    )
+}
