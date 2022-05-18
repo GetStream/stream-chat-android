0ba3b166f9df5a7f97184f58c4e24f505350c41f
MarinTolic <marin.t91@gmail.com>
2022-05-18 12:05:15 +0200

    [3533] Add a Stream API Error template so that tests that simulate errors have a concrete error body to provide

--- stream-chat-android-client/src/test/java/io/getstream/chat/android/client/utils/RetroError.kt
+++ stream-chat-android-client/src/test/java/io/getstream/chat/android/client/utils/RetroError.kt
@@ -18,15 +18,25 @@ package io.getstream.chat.android.client.utils
 
 import io.getstream.chat.android.client.call.RetrofitCall
 import io.getstream.chat.android.client.parser2.MoshiChatParser
+import okhttp3.MediaType
 import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.Request
+import okhttp3.ResponseBody
 import okhttp3.ResponseBody.Companion.toResponseBody
 import okio.Timeout
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
-internal class RetroError<T : Any>(val statusCode: Int) : Call<T> {
+internal class RetroError<T : Any>(
+    private val statusCode: Int,
+    private val streamCode: Int? = FAKE_ERROR_CODE,
+    private val message: String? = "Test Error",
+    private val exceptionFields: Map<String, String> = mapOf(Pair("Test", "Error")),
+    private val duration: Float? = 0.2f,
+    private val moreInfo: String = "https://getstream.io/chat/docs/api_errors_response",
+    private val mediaType: MediaType = "text/plain".toMediaType(),
+) : Call<T> {
 
     fun toRetrofitCall(): RetrofitCall<T> {
         return RetrofitCall(
@@ -59,7 +69,15 @@ internal class RetroError<T : Any>(val statusCode: Int) : Call<T> {
     override fun execute(): Response<T> {
         return Response.error(
             statusCode,
-            "{Server error}".toResponseBody("text/plain".toMediaType())
+            toStreamApiErrorResponseBody(
+                statusCode = statusCode,
+                streamCode = streamCode,
+                message = message,
+                exceptionFields = exceptionFields,
+                duration = duration,
+                moreInfo = moreInfo,
+                mediaType = mediaType
+            )
         )
     }
 
@@ -69,5 +87,59 @@ internal class RetroError<T : Any>(val statusCode: Int) : Call<T> {
 
     override fun timeout(): Timeout {
         return Timeout()
+    }
+
+    /**
+     * Creates a facsimile of the error response sent by the backend.
+     */
+    private fun toStreamApiErrorResponseBody(
+        streamCode: Int? = null,
+        message: String? = null,
+        exceptionFields: Map<String, String> = mapOf(),
+        statusCode: Int? = null,
+        duration: Float? = null,
+        moreInfo: String? = null,
+        mediaType: MediaType,
+    ): ResponseBody {
+        val errorJsonMembers = mutableListOf<String>()
+
+        if (streamCode != null) {
+            errorJsonMembers.add(""""code":"$streamCode"""")
+        }
+
+        if (message != null) {
+            errorJsonMembers.add(""""message":"Sync failed with error: $message"""")
+        }
+
+        if (exceptionFields.isNotEmpty()) {
+            val exceptionFieldMembers = mutableListOf<String>()
+
+            exceptionFields.forEach { (key, value) ->
+                exceptionFieldMembers.add(""""$key":"$value"""")
+            }
+
+            errorJsonMembers.add(
+                """"exception_fields":"""
+                    + exceptionFieldMembers.joinToString(separator = ",", prefix = "{", postfix = "}")
+            )
+        }
+
+        if (statusCode != null) {
+            errorJsonMembers.add(""""StatusCode":"$statusCode"""")
+        }
+
+        if (duration != null) {
+            errorJsonMembers.add(""""duration":"${String.format(format = "%.2f", duration)}ms"""")
+        }
+
+        if (moreInfo != null) {
+            errorJsonMembers.add(""""more_info":"$moreInfo"""")
+        }
+
+        return errorJsonMembers.joinToString(",", prefix = "{", postfix = "}").toResponseBody(mediaType)
+    }
+
+    companion object {
+        internal const val FAKE_ERROR_CODE = -1
     }
 }
