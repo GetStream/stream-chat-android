bcf3f02adad04b37146da26749b3fb6cf7f62688
Jc Miñarro <josecarlos.minarro@gmail.com>
2022-05-18 13:13:13 +0200

    Add `equals()` and `hashCode()` methods to `ChatError` class

--- stream-chat-android-core/src/main/java/io/getstream/chat/android/client/errors/ChatError.kt
+++ stream-chat-android-core/src/main/java/io/getstream/chat/android/client/errors/ChatError.kt
@@ -19,4 +19,24 @@ package io.getstream.chat.android.client.errors
 public open class ChatError(
     public val message: String? = null,
     public val cause: Throwable? = null
-)
+) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        return (other as? ChatError)?.let {
+            message == it.message && cause.equalCause(it.cause)
+        } ?: false
+    }
+
+    private fun Throwable?.equalCause(other: Throwable?): Boolean {
+        if ((this == null && other == null) || this === other) return true
+        return this?.message == other?.message && this?.cause.equalCause(other?.cause)
+    }
+
+    override fun hashCode(): Int {
+        var result = message?.hashCode() ?: 0
+        result = 31 * result + (cause?.hashCode() ?: 0)
+        return result
+    }
+}
