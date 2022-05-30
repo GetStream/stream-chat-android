2918879aa4c941a9d79c18a9e67f734747b41c07
Leandro Borges Ferreira <lehen01@gmail.com>
2022-05-30 19:13:20 -0300

    Fixing visibility of fields of SortAttribute

--- stream-chat-android-client/src/main/java/io/getstream/chat/android/client/api/models/querysort/SortAttribute.kt
+++ stream-chat-android-client/src/main/java/io/getstream/chat/android/client/api/models/querysort/SortAttribute.kt
@@ -21,12 +21,12 @@ import kotlin.reflect.KProperty1
 /** Inner representation of sorting feature specification. */
 internal sealed class SortAttribute<T> {
     /** Name of attribute */
-    public abstract val name: String
+    abstract val name: String
 
     /** KProperty referenced attribute. */
-    public data class FieldSortAttribute<T>(val field: KProperty1<T, Comparable<*>?>, override val name: String) :
+    data class FieldSortAttribute<T>(val field: KProperty1<T, Comparable<*>?>, override val name: String) :
         SortAttribute<T>()
 
     /** Referenced by name attribute. */
-    public data class FieldNameSortAttribute<T>(override val name: String) : SortAttribute<T>()
+    data class FieldNameSortAttribute<T>(override val name: String) : SortAttribute<T>()
 }
