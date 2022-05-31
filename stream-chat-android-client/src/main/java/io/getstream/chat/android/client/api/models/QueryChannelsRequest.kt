3da3b189712f87838b5b49c0f876c08d98953993
Leandro Borges Ferreira <lehen01@gmail.com>
2022-05-31 14:47:00 -0300

    Fixing build

--- stream-chat-android-client/src/main/java/io/getstream/chat/android/client/api/models/QueryChannelsRequest.kt
+++ stream-chat-android-client/src/main/java/io/getstream/chat/android/client/api/models/QueryChannelsRequest.kt
@@ -16,6 +16,8 @@
 
 package io.getstream.chat.android.client.api.models
 
+import io.getstream.chat.android.client.api.models.querysort.QuerySortByMap
+import io.getstream.chat.android.client.api.models.querysort.QuerySorter
 import io.getstream.chat.android.client.models.Channel
 
 /**
@@ -32,7 +34,7 @@ public data class QueryChannelsRequest(
     public val filter: FilterObject,
     public var offset: Int = 0,
     public var limit: Int,
-    public val querySort: QuerySort<Channel> = QuerySort(),
+    public val querySort: QuerySorter<Channel> = QuerySortByMap(),
     public var messageLimit: Int = 0,
     public var memberLimit: Int = 0,
 ) : ChannelRequest<QueryChannelsRequest> {
