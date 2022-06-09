53a4ad2e711e53e8ac7799358e298a52c3ca6481
kanat <kijalbaev@gmail.com>
2022-06-09 12:09:48 +0400

    [3468] add ability to enable sequential event handling

--- stream-chat-android-offline/src/main/java/io/getstream/chat/android/offline/plugin/factory/StreamOfflinePluginFactory.kt
+++ stream-chat-android-offline/src/main/java/io/getstream/chat/android/offline/plugin/factory/StreamOfflinePluginFactory.kt
@@ -184,6 +184,7 @@ public class StreamOfflinePluginFactory(
         }
 
         val eventHandler: EventHandler = createEventHandler(
+            useSequentialEventHandler = config.useSequentialEventHandler,
             scope = scope,
             client = chatClient,
             logicRegistry = logic,
@@ -241,6 +242,7 @@ public class StreamOfflinePluginFactory(
     }
 
     private fun createEventHandler(
+        useSequentialEventHandler: Boolean,
         scope: CoroutineScope,
         client: ChatClient,
         logicRegistry: LogicRegistry,
@@ -249,7 +251,7 @@ public class StreamOfflinePluginFactory(
         repos: RepositoryFacade,
         syncManager: SyncManager
     ): EventHandler {
-        return when (BuildConfig.DEBUG) {
+        return when (BuildConfig.DEBUG || useSequentialEventHandler) {
             true -> EventHandlerSequential(
                 scope = scope,
                 recoveryEnabled = true,
