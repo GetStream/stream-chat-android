5f3c42b77507dd4911b06fda5a738198460fd108
MarinTolic <marin.t91@gmail.com>
2022-06-09 21:06:24 +0200

    Create an MVP proof of concept to show how to create a custom overlay for message composer input field

--- stream-chat-android-compose-sample/src/main/java/io/getstream/chat/android/compose/sample/ui/MessagesActivity.kt
+++ stream-chat-android-compose-sample/src/main/java/io/getstream/chat/android/compose/sample/ui/MessagesActivity.kt
@@ -23,8 +23,6 @@ import androidx.activity.compose.setContent
 import androidx.activity.viewModels
 import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -33,22 +31,28 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.layout.wrapContentSize
-import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
-import androidx.compose.material.ripple.rememberRipple
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.Alignment.Companion.CenterVertically
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import io.getstream.chat.android.client.models.ChannelCapabilities
+import io.getstream.chat.android.common.composer.MessageComposerState
 import io.getstream.chat.android.common.state.DeletedMessageVisibility
 import io.getstream.chat.android.common.state.MessageMode
 import io.getstream.chat.android.common.state.Reply
@@ -63,7 +67,6 @@ import io.getstream.chat.android.compose.ui.components.messageoptions.defaultMes
 import io.getstream.chat.android.compose.ui.components.reactionpicker.ReactionsPicker
 import io.getstream.chat.android.compose.ui.components.selectedmessage.SelectedMessageMenu
 import io.getstream.chat.android.compose.ui.components.selectedmessage.SelectedReactionsMenu
-import io.getstream.chat.android.compose.ui.messages.MessagesScreen
 import io.getstream.chat.android.compose.ui.messages.attachments.AttachmentsPicker
 import io.getstream.chat.android.compose.ui.messages.composer.MessageComposer
 import io.getstream.chat.android.compose.ui.messages.list.MessageList
@@ -94,13 +97,7 @@ class MessagesActivity : BaseConnectedActivity() {
 
         setContent {
             ChatTheme(dateFormatter = ChatApp.dateFormatter) {
-                MessagesScreen(
-                    channelId = channelId,
-                    onBackPressed = { finish() },
-                    onHeaderActionClick = {}
-                )
-
-                // MyCustomUi()
+                MyCustomUi()
             }
         }
     }
@@ -236,63 +233,156 @@ class MessagesActivity : BaseConnectedActivity() {
 
     @Composable
     fun MyCustomComposer() {
+        val isOverlayShowing = remember {
+            mutableStateOf(false)
+        }
+
         MessageComposer(
             modifier = Modifier
                 .fillMaxWidth()
                 .wrapContentHeight(),
             viewModel = composerViewModel,
-            integrations = {},
-            input = { inputState ->
-                MessageInput(
-                    modifier = Modifier
-                        .fillMaxWidth()
-                        .weight(7f)
-                        .padding(start = 8.dp),
-                    messageComposerState = inputState,
-                    onValueChange = { composerViewModel.setMessageInput(it) },
-                    onAttachmentRemoved = { composerViewModel.removeSelectedAttachment(it) },
-                    label = {
-                        Row(
-                            Modifier.wrapContentWidth(),
-                            verticalAlignment = Alignment.CenterVertically
-                        ) {
-                            Icon(
-                                painter = painterResource(id = R.drawable.stream_compose_ic_gallery),
-                                contentDescription = null
-                            )
-
-                            Text(
-                                modifier = Modifier.padding(start = 4.dp),
-                                text = "Type something",
-                                color = ChatTheme.colors.textLowEmphasis
-                            )
-                        }
-                    },
-                    innerTrailingContent = {
-                        Icon(
-                            modifier = Modifier
-                                .size(24.dp)
-                                .clickable(
-                                    interactionSource = remember { MutableInteractionSource() },
-                                    indication = rememberRipple()
-                                ) {
-                                    val state = composerViewModel.messageComposerState.value
-
-                                    composerViewModel.sendMessage(
-                                        composerViewModel.buildNewMessage(
-                                            state.inputValue, state.attachments
-                                        )
-                                    )
-                                },
-                            painter = painterResource(id = R.drawable.stream_compose_ic_send),
-                            tint = ChatTheme.colors.primaryAccent,
-                            contentDescription = null
-                        )
-                    },
+            integrations = {
+                MyCustomIntegrations(
+                    messageInputState = it,
+                    onAttachmentsClick = { },
+                    onCommandsClick = { },
+                    onVoiceRecordingClicked = { isOverlayShowing.value = !isOverlayShowing.value },
+                    ownCapabilities = composerViewModel.ownCapabilities.collectAsState().value
                 )
             },
-            trailingContent = { Spacer(modifier = Modifier.size(8.dp)) }
+            input = { inputState ->
+                if (isOverlayShowing.value) {
+                    Text(
+                        modifier = Modifier
+                            .padding(horizontal = 0.dp, vertical = 8.dp)
+                            .background(Color.Blue, shape = ChatTheme.shapes.inputField)
+                            .fillMaxWidth()
+                            .padding(vertical = 8.dp)
+                            .weight(1f)
+                            .align(CenterVertically),
+                        text = "overlay"
+                    )
+                } else {
+                    MessageInput(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(vertical = 8.dp)
+                            .weight(1f),
+                        label = {
+                            val text =
+                                if (composerViewModel.ownCapabilities.collectAsState().value.contains(
+                                        ChannelCapabilities.SEND_MESSAGE
+                                    )
+                                ) {
+                                    stringResource(id = io.getstream.chat.android.compose.R.string.stream_compose_message_label)
+                                } else {
+                                    stringResource(id = io.getstream.chat.android.compose.R.string.stream_compose_cannot_send_messages_label)
+                                }
+
+                            Text(
+                                text = text,
+                                color = ChatTheme.colors.textLowEmphasis
+                            )
+                        },
+                        messageComposerState = inputState,
+                        onValueChange = { composerViewModel.setMessageInput(it) },
+                        onAttachmentRemoved = { composerViewModel.removeSelectedAttachment(it) },
+                    )
+                }
+            },
         )
+    }
+
+    @Composable
+    private fun MyCustomIntegrations(
+        messageInputState: MessageComposerState,
+        onAttachmentsClick: () -> Unit,
+        onCommandsClick: () -> Unit,
+        onVoiceRecordingClicked: () -> Unit,
+        ownCapabilities: Set<String>,
+    ) {
+        val hasTextInput = messageInputState.inputValue.isNotEmpty()
+        val hasAttachments = messageInputState.attachments.isNotEmpty()
+        val hasCommandInput = messageInputState.inputValue.startsWith("/")
+        val hasCommandSuggestions = messageInputState.commandSuggestions.isNotEmpty()
+        val hasMentionSuggestions = messageInputState.mentionSuggestions.isNotEmpty()
+
+        val isAttachmentsButtonEnabled = !hasCommandInput && !hasCommandSuggestions && !hasMentionSuggestions
+        val isCommandsButtonEnabled = !hasTextInput && !hasAttachments
+
+        val canSendMessage = ownCapabilities.contains(ChannelCapabilities.SEND_MESSAGE)
+        val canSendAttachments = ownCapabilities.contains(ChannelCapabilities.UPLOAD_FILE)
+
+        if (canSendMessage) {
+            Row(
+                modifier = Modifier
+                    .height(44.dp)
+                    .padding(horizontal = 4.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                if (canSendAttachments) {
+                    IconButton(
+                        enabled = isAttachmentsButtonEnabled,
+                        modifier = Modifier
+                            .size(32.dp)
+                            .padding(4.dp),
+                        content = {
+                            Icon(
+                                painter = painterResource(id = io.getstream.chat.android.compose.R.drawable.stream_compose_ic_attachments),
+                                contentDescription = stringResource(id = io.getstream.chat.android.compose.R.string.stream_compose_attachments),
+                                tint = if (isAttachmentsButtonEnabled) {
+                                    ChatTheme.colors.textLowEmphasis
+                                } else {
+                                    ChatTheme.colors.disabled
+                                },
+                            )
+                        },
+                        onClick = onAttachmentsClick
+                    )
+                }
+
+                val commandsButtonTint = if (hasCommandSuggestions && isCommandsButtonEnabled) {
+                    ChatTheme.colors.primaryAccent
+                } else if (isCommandsButtonEnabled) {
+                    ChatTheme.colors.textLowEmphasis
+                } else {
+                    ChatTheme.colors.disabled
+                }
+
+                IconButton(
+                    modifier = Modifier
+                        .size(32.dp)
+                        .padding(4.dp),
+                    enabled = isCommandsButtonEnabled,
+                    content = {
+                        Icon(
+                            painter = painterResource(id = io.getstream.chat.android.compose.R.drawable.stream_compose_ic_command),
+                            contentDescription = null,
+                            tint = commandsButtonTint,
+                        )
+                    },
+                    onClick = onCommandsClick
+                )
+
+                IconButton(
+                    modifier = Modifier
+                        .size(32.dp)
+                        .padding(4.dp),
+                    content = {
+                        Icon(
+                            painter = painterResource(id = R.drawable.ic_baseline_mic_24),
+                            contentDescription = null,
+                            tint = ChatTheme.colors.textLowEmphasis
+
+                        )
+                    },
+                    onClick = onVoiceRecordingClicked
+                )
+            }
+        } else {
+            Spacer(modifier = Modifier.width(12.dp))
+        }
     }
 
     companion object {
