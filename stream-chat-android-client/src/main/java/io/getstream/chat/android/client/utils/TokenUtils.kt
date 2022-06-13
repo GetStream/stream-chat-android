8bd3ab2cd8bc1a41e62e92a0c191b7fcdc379105
Jc Miñarro <josecarlos.minarro@gmail.com>
2022-06-13 03:14:05 -0700

    Handle invalid token values properly (#3666)
    Co-authored-by: Kanat Kiyalbaev <kijalbaev@gmail.com>

--- stream-chat-android-client/src/main/java/io/getstream/chat/android/client/utils/TokenUtils.kt
+++ stream-chat-android-client/src/main/java/io/getstream/chat/android/client/utils/TokenUtils.kt
@@ -24,17 +24,22 @@ import java.nio.charset.StandardCharsets
 
 internal object TokenUtils {
 
+    val logger = ChatLogger.get("TokenUtils")
+
     fun getUserId(token: String): String = try {
         JSONObject(
-            String(
-                Base64.decode(
-                    (token.split(".")[1]).toByteArray(StandardCharsets.UTF_8),
-                    Base64.NO_WRAP
-                )
-            )
+            token
+                .takeIf { it.contains(".") }
+                ?.split(".")
+                ?.getOrNull(1)
+                ?.let { String(Base64.decode(it.toByteArray(StandardCharsets.UTF_8), Base64.NO_WRAP)) }
+                ?: ""
         ).optString("user_id")
     } catch (e: JSONException) {
-        ChatLogger.get("TokenUtils").logE("Unable to obtain userId from JWT Token Payload", e)
+        logger.logE("Unable to obtain userId from JWT Token Payload", e)
+        ""
+    } catch (e: IllegalArgumentException) {
+        logger.logE("Unable to obtain userId from JWT Token Payload", e)
         ""
     }
 }
