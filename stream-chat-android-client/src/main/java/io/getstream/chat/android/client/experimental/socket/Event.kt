91751a0c3997d3d699be7dbeda4047fa26312c34
Amit Kumar <amit.kumar@getstream.io>
2022-06-20 13:37:47 +0530

    Remove toString from data classes

--- stream-chat-android-client/src/main/java/io/getstream/chat/android/client/experimental/socket/Event.kt
+++ stream-chat-android-client/src/main/java/io/getstream/chat/android/client/experimental/socket/Event.kt
@@ -39,20 +39,12 @@ internal sealed class Event {
             data class WithReason(
                 val cause: DisconnectCause,
                 val shutdownReason: ShutdownReason = ShutdownReason.GRACEFUL,
-            ) : Stopped(disconnectCause = cause) {
-                override fun toString(): String {
-                    return "Event.Lifecycle.Stopped.WithReason"
-                }
-            }
+            ) : Stopped(disconnectCause = cause)
 
             /**
              * Stop and discard all pending messages.
              */
-            data class AndAborted(val cause: DisconnectCause) : Stopped(disconnectCause = cause) {
-                override fun toString(): String {
-                    return "Event.Lifecycle.Stopped.Aborted"
-                }
-            }
+            data class AndAborted(val cause: DisconnectCause) : Stopped(disconnectCause = cause)
         }
 
         object Terminate : Lifecycle() {
@@ -69,34 +61,18 @@ internal sealed class Event {
             }
         }
 
-        data class OnConnectionOpened<out WEB_SOCKET : Any>(val webSocket: WEB_SOCKET) : WebSocket() {
-            override fun toString(): String {
-                return "Event.WebSocket.OnConnectionOpened"
-            }
-        }
+        data class OnConnectionOpened<out WEB_SOCKET : Any>(val webSocket: WEB_SOCKET) : WebSocket()
 
-        data class OnConnectedEventReceived(val connectedEvent: ConnectedEvent) : WebSocket() {
-            override fun toString(): String {
-                return "Event.WebSocket.OnConnectedEventReceived"
-            }
-        }
+        data class OnConnectedEventReceived(val connectedEvent: ConnectedEvent) : WebSocket()
 
-        data class OnMessageReceived(val message: String) : WebSocket() {
-            override fun toString(): String {
-                return "Event.WebSocket.OnMessageReceived"
-            }
-        }
+        data class OnMessageReceived(val message: String) : WebSocket()
 
         /**
          * Invoked when the peer has indicated that no more incoming messages will be transmitted.
          *
          * @property shutdownReason Reason to shutdown from the peer.
          */
-        data class OnConnectionClosing(val shutdownReason: ShutdownReason) : WebSocket() {
-            override fun toString(): String {
-                return "Event.WebSocket.OnConnectionClosing"
-            }
-        }
+        data class OnConnectionClosing(val shutdownReason: ShutdownReason) : WebSocket()
 
         /**
          * Invoked when both peers have indicated that no more messages will be transmitted and the connection has been
@@ -104,11 +80,7 @@ internal sealed class Event {
          *
          * @property shutdownReason Reason to shutdown from the peer.
          */
-        data class OnConnectionClosed(val shutdownReason: ShutdownReason) : WebSocket() {
-            override fun toString(): String {
-                return "Event.WebSocket.OnConnectionClosed"
-            }
-        }
+        data class OnConnectionClosed(val shutdownReason: ShutdownReason) : WebSocket()
 
         /**
          * Invoked when a web socket has been closed due to an error reading from or writing to the network.
@@ -116,11 +88,7 @@ internal sealed class Event {
          *
          * @property throwable The error causing the failure.
          */
-        data class OnConnectionFailed(val throwable: Throwable) : WebSocket() {
-            override fun toString(): String {
-                return "Event.WebSocket.OnConnectionFailed"
-            }
-        }
+        data class OnConnectionFailed(val throwable: Throwable) : WebSocket()
     }
 }
 
