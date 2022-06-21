d64af993c06024df6c7194ce74082c9041eaf221
TGazica <tomislav.gazica@gmail.com>
2022-06-21 10:48:39 +0200

    Refactor permission check

--- stream-chat-android-ui-common/src/main/kotlin/com/getstream/sdk/chat/utils/extensions/Context.kt
+++ stream-chat-android-ui-common/src/main/kotlin/com/getstream/sdk/chat/utils/extensions/Context.kt
@@ -36,3 +36,23 @@ public fun Context.openSystemSettings() {
     }
     startActivity(intent)
 }
+
+/**
+ * @param permission The permission we want to check if it was requested before.
+ *
+ * @return If the permission was requested before or not.
+ */
+public fun Context.wasPermissionRequested(permission: String): Boolean {
+    return getSharedPreferences(PERMISSIONS_PREFS, Context.MODE_PRIVATE).getBoolean(permission, false)
+}
+
+/**
+ * Saves to shared prefs that a permission has been requested.
+ *
+ * @param permission The permission in question.
+ */
+public fun Context.onPermissionRequested(permission: String) {
+    return getSharedPreferences(PERMISSIONS_PREFS, Context.MODE_PRIVATE).edit().putBoolean(permission, true).apply()
+}
+
+private const val PERMISSIONS_PREFS = "stream_permissions"
