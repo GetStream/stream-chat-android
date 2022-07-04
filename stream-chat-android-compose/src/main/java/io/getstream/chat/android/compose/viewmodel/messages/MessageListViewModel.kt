14b28ef6c81d54f6244a3bbac366719a89ae8096
TGazica <tomislav.gazica@gmail.com>
2022-07-04 11:10:36 +0200

    Fix update messages check inside MessageListViewModel

--- stream-chat-android-compose/src/main/java/io/getstream/chat/android/compose/viewmodel/messages/MessageListViewModel.kt
+++ stream-chat-android-compose/src/main/java/io/getstream/chat/android/compose/viewmodel/messages/MessageListViewModel.kt
@@ -1276,7 +1276,7 @@ public class MessageListViewModel(
      * @param focusedItemExists Whether there is a focused message inside the messages list or not.
      * */
     private fun updateMessages(messages: List<MessageListItemState>, focusedItemExists: Boolean) {
-        if (!isInThread) {
+        if (isInThread) {
             this.threadMessagesState =
                 threadMessagesState.copy(
                     messageItems = messages,
