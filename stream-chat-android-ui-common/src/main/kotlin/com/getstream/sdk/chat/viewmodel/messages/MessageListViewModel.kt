ebb69c7b3ec02e6cc2b8a6e97030d0c8c8be1b99
Tomislav Gazica <38032787+TGazica@users.noreply.github.com>
2022-07-14 15:41:32 +0200

    [3727] Compose bi directional pagination (#3818)
    * Initial impl adding ScrollToStartState and scroll to bottom from n pages after
    
    * Add scroll to bottom
    
    * Fixed scroll when focusing message
    
    * Code cleanup
    
    * Update docs and refactor code
    
    * Fix thread scroll to bottom button
    
    * Code cleanup
    
    * Refactor determinePaginationEnd inside ChannelLogic
    
    * Update changelog
    
    * Fix upsert message check
    
    * Refactor upsert message check
    
    * Refactor watch to request newest messages and isFilteringMessages logic
    
    * Revert testing change
    
    * Update stream-chat-android-compose/src/main/java/io/getstream/chat/android/compose/state/messages/ScrollToPositionState.kt
    
    Co-authored-by: Filip Babic <fbabic96@gmail.com>
    
    * Code cleanup and refactor
    
    * Deprecated old Messages composable and updated deprecations
    
    * Update deprecations
    
    * Fix update messages check inside MessageListViewModel
    
    * Fix compose ui tests
    
    * Code refactor after merge
    
    * Refactor compose scroll to bottom
    
    * Update changelog
    
    * Refactor channel state and logic
    
    * Fix unread count and tests
    
    * Fix refresh list check
    
    * Fix unread count clear
    
    * Fix refresh check
    
    * Update stream-chat-android-compose/src/main/java/io/getstream/chat/android/compose/viewmodel/messages/MessageListViewModel.kt
    
    Co-authored-by: Filip Babic <fbabic96@gmail.com>
    
    * Refactor scrolling on scroll to bottom
    
    * Fix load more new messages
    
    * Feature/3727 UI comp scroll to bottom (#3849)
    
    * Refactor scroll to bottom in ui-comp
    
    * Fix scroll while paginating down
    
    * Refactor loading indicator logic
    
    * Code cleanup
    
    * updated changelog
    
    * Fix tests
    
    * Fix test
    
    * Code cleanup
    
    * Fix after merge
    
    * Kdocs
    
    * Update DEPRECATIONS.md
    
    Co-authored-by: Rafal Adasiewicz <rafal@getstream.io>
    
    * Update DEPRECATIONS.md
    
    Co-authored-by: Rafal Adasiewicz <rafal@getstream.io>
    
    * Fix search scroll issue
    
    * Docs
    
    Co-authored-by: Filip Babic <fbabic96@gmail.com>
    Co-authored-by: Rafal Adasiewicz <rafal@getstream.io>

--- stream-chat-android-ui-common/src/main/kotlin/com/getstream/sdk/chat/viewmodel/messages/MessageListViewModel.kt
+++ stream-chat-android-ui-common/src/main/kotlin/com/getstream/sdk/chat/viewmodel/messages/MessageListViewModel.kt
@@ -48,6 +48,7 @@ import io.getstream.chat.android.offline.extensions.cancelEphemeralMessage
 import io.getstream.chat.android.offline.extensions.getRepliesAsState
 import io.getstream.chat.android.offline.extensions.loadMessageById
 import io.getstream.chat.android.offline.extensions.loadNewerMessages
+import io.getstream.chat.android.offline.extensions.loadNewestMessages
 import io.getstream.chat.android.offline.extensions.loadOlderMessages
 import io.getstream.chat.android.offline.extensions.setMessageForReply
 import io.getstream.chat.android.offline.extensions.watchChannelAsState
@@ -326,7 +327,8 @@ public class MessageListViewModel(
             isThread = false,
             dateSeparatorHandler = dateSeparatorHandler,
             deletedMessageVisibility = deletedMessageVisibility,
-            messageFooterVisibility = messageFooterVisibility
+            messageFooterVisibility = messageFooterVisibility,
+            endOfNewMessages = channelState.endOfNewerMessages.asLiveData(),
         )
         _reads.addSource(channelState.reads.asLiveData()) { _reads.value = it }
         _loadMoreLiveData.addSource(channelState.loadingOlderMessages.asLiveData()) { _loadMoreLiveData.value = it }
@@ -373,7 +375,8 @@ public class MessageListViewModel(
             true,
             threadDateSeparatorHandler,
             deletedMessageVisibility,
-            messageFooterVisibility
+            messageFooterVisibility,
+            MutableLiveData(true)
         )
         threadListData?.let { tld ->
             messageListData?.let { mld ->
@@ -565,9 +568,7 @@ public class MessageListViewModel(
                 val attachmentToBeDeleted = event.attachment
                 chatClient.loadMessageById(
                     cid,
-                    event.messageId,
-                    DEFAULT_MESSAGES_LIMIT,
-                    DEFAULT_MESSAGES_LIMIT
+                    event.messageId
                 ).enqueue { result ->
                     if (result.isSuccess) {
                         val message = result.data()
@@ -597,9 +598,7 @@ public class MessageListViewModel(
                 val cid = event.cid
                 chatClient.loadMessageById(
                     cid,
-                    messageId,
-                    DEFAULT_MESSAGES_LIMIT,
-                    DEFAULT_MESSAGES_LIMIT
+                    messageId
                 ).enqueue { result ->
                     if (result.isSuccess) {
                         val message = result.data()
@@ -625,6 +624,26 @@ public class MessageListViewModel(
         }
 
         return (messageItem as? MessageListItem.MessageItem)?.message
+    }
+
+    /**
+     * When the user clicks the scroll to bottom button we need to take the user to the bottom of the newest
+     * messages. If the messages are not loaded we need to load them first and then scroll to the bottom of the
+     * list.
+     */
+    public fun scrollToBottom(scrollToBottom: () -> Unit) {
+        if (_mode.value is Mode.Thread || messageListData?.value?.areNewestMessagesLoaded == true) {
+            scrollToBottom()
+        } else {
+            chatClient.loadNewestMessages(cid, DEFAULT_MESSAGES_LIMIT).enqueue { result ->
+                if (result.isSuccess) {
+                    scrollToBottom()
+                } else {
+                    val error = result.error()
+                    logger.e { "Could not load newest messages. Cause: ${error.cause?.message}" }
+                }
+            }
+        }
     }
 
     /**
@@ -695,9 +714,9 @@ public class MessageListViewModel(
         currentMode.run {
             when (this) {
                 is Mode.Normal -> {
-                    messageListData?.loadingMoreChanged(true)
+                    messageListData?.loadingMoreOldMessagesChanged(true)
                     chatClient.loadOlderMessages(cid, DEFAULT_MESSAGES_LIMIT).enqueue {
-                        messageListData?.loadingMoreChanged(false)
+                        messageListData?.loadingMoreOldMessagesChanged(false)
                     }
                 }
                 is Mode.Thread -> threadLoadMore(this)
@@ -710,10 +729,10 @@ public class MessageListViewModel(
      */
     private fun onBottomEndRegionReached(baseMessageId: String?) {
         if (baseMessageId != null) {
-            messageListData?.loadingMoreChanged(true)
+            messageListData?.loadingMoreNewMessagesChanged(true)
             chatClient.loadNewerMessages(cid, baseMessageId, DEFAULT_MESSAGES_LIMIT)
                 .enqueue { result ->
-                    messageListData?.loadingMoreChanged(false)
+                    messageListData?.loadingMoreNewMessagesChanged(false)
                 }
         } else {
             logger.e { "There's no base message to request more message at bottom of limit" }
@@ -726,17 +745,17 @@ public class MessageListViewModel(
      * @param threadMode Current thread mode.
      */
     private fun threadLoadMore(threadMode: Mode.Thread) {
-        threadListData?.loadingMoreChanged(true)
+        threadListData?.loadingMoreOldMessagesChanged(true)
         if (threadMode.threadState != null) {
             chatClient.getRepliesMore(
                 messageId = threadMode.parentMessage.id,
                 firstId = threadMode.threadState.oldestInThread.value?.id ?: threadMode.parentMessage.id,
                 limit = DEFAULT_MESSAGES_LIMIT,
             ).enqueue {
-                threadListData?.loadingMoreChanged(false)
+                threadListData?.loadingMoreOldMessagesChanged(false)
             }
         } else {
-            threadListData?.loadingMoreChanged(false)
+            threadListData?.loadingMoreOldMessagesChanged(false)
             logger.w { "Thread state must be not null for offline plugin thread load more!" }
         }
     }
