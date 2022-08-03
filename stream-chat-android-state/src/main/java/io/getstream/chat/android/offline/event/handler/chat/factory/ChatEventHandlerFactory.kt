a085fb6f49f8bb36f81371911aa75066db192543
Rafal Adasiewicz <rafal@getstream.io>
2022-08-03 19:41:12 +0200

    Refactor chat event handling related classes (#3991)
    * Refactor chat event handling related classes
    
    * Fix tests
    
    * Replace chatClient with clientState
    
    * Remove adding the channel after receiving NewMessage event

--- stream-chat-android-state/src/main/java/io/getstream/chat/android/offline/event/handler/chat/factory/ChatEventHandlerFactory.kt
+++ stream-chat-android-state/src/main/java/io/getstream/chat/android/offline/event/handler/chat/factory/ChatEventHandlerFactory.kt
@@ -16,15 +16,19 @@
 
 package io.getstream.chat.android.offline.event.handler.chat.factory
 
+import io.getstream.chat.android.client.ChatClient
 import io.getstream.chat.android.client.models.Channel
+import io.getstream.chat.android.client.setup.state.ClientState
 import io.getstream.chat.android.offline.event.handler.chat.ChatEventHandler
 import io.getstream.chat.android.offline.event.handler.chat.DefaultChatEventHandler
 import kotlinx.coroutines.flow.StateFlow
 
 /**
-* A [ChatEventHandler] factory. Allows passing visible channels` list.
-*/
-public open class ChatEventHandlerFactory {
+ * A [ChatEventHandler] factory. Allows passing visible channels` list.
+ *
+ * @param clientState The client used to obtain current user.
+ */
+public open class ChatEventHandlerFactory(private val clientState: ClientState = ChatClient.instance().clientState) {
 
     /**
      * Creates a [ChatEventHandler] instance.
@@ -32,6 +36,6 @@ public open class ChatEventHandlerFactory {
      * @param channels The visible channels` list.
      */
     public open fun chatEventHandler(channels: StateFlow<List<Channel>?>): ChatEventHandler {
-        return DefaultChatEventHandler(channels = channels)
+        return DefaultChatEventHandler(channels = channels, clientState = clientState)
     }
 }
