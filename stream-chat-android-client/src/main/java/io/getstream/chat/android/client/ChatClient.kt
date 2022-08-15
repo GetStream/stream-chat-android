9b8405faaa28ea64c624d53fa45ace93267b6784
Leandro Borges Ferreira <lehen01@gmail.com>
2022-08-15 19:03:48 -0300

    Moving userStateService

--- stream-chat-android-client/src/main/java/io/getstream/chat/android/client/ChatClient.kt
+++ stream-chat-android-client/src/main/java/io/getstream/chat/android/client/ChatClient.kt
@@ -402,6 +402,7 @@ internal constructor(
                 logger.v { "[setUser] user is NotSet" }
                 initializeClientWithUser(user, cacheableTokenProvider, isAnonymous)
                 clientState.toMutableState()?.setUser(user)
+                userStateService.onSetUser(user, isAnonymous)
                 if (ToggleService.isSocketExperimental()) {
                     chatSocketExperimental.connectUser(user, isAnonymous)
                 } else {
@@ -456,7 +457,6 @@ internal constructor(
         tokenProvider: CacheableTokenProvider,
         isAnonymous: Boolean,
     ) {
-        userStateService.onSetUser(user, isAnonymous)
         _repositoryFacade = createRepositoryFacade(createRepositoryFactory(user))
         plugins = pluginFactories.map { it.get(user) }
         // fire a handler here that the chatDomain and chatUI can use
