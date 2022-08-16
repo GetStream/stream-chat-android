0171d2ab7ab383c225aa4433ac8ca307d9596c8c
Jc Miñarro <josecarlos.minarro@gmail.com>
2022-08-16 17:51:31 +0200

    Move Experimental ChatSocket to a new package

--- stream-chat-android-client/src/main/java/io/getstream/chat/android/client/socket/experimental/ChatSocketStateService.kt
+++ stream-chat-android-client/src/main/java/io/getstream/chat/android/client/socket/experimental/ChatSocketStateService.kt
@@ -14,7 +14,7 @@
  * limitations under the License.
  */
 
-package io.getstream.chat.android.client.experimental.socket
+package io.getstream.chat.android.client.socket.experimental
 
 import io.getstream.chat.android.client.errors.ChatError
 import io.getstream.chat.android.client.errors.ChatNetworkError
--- stream-chat-android-client/src/test/java/io/getstream/chat/android/client/socket/experimental/ChatSocketStateServiceTest.kt
+++ stream-chat-android-client/src/test/java/io/getstream/chat/android/client/socket/experimental/ChatSocketStateServiceTest.kt
@@ -14,13 +14,13 @@
  * limitations under the License.
  */
 
-package io.getstream.chat.android.client.experimental.socket
+package io.getstream.chat.android.client.socket.experimental
 
 import io.getstream.chat.android.client.Mother
 import io.getstream.chat.android.client.errors.ChatNetworkError
 import io.getstream.chat.android.client.events.ConnectedEvent
-import io.getstream.chat.android.client.experimental.socket.ChatSocketStateService.State
 import io.getstream.chat.android.client.socket.SocketFactory
+import io.getstream.chat.android.client.socket.experimental.ChatSocketStateService.State
 import io.getstream.chat.android.test.randomBoolean
 import org.amshove.kluent.`should be equal to`
 import org.junit.jupiter.params.ParameterizedTest
