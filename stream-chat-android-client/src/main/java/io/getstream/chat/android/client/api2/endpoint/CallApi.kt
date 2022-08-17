1b3ea1986e64056ce010ec17683674369c1cb101
skydoves <skydoves@naver.com>
2022-08-17 10:24:20 +0900

    Implement CallApi retrofit call

---
+++ stream-chat-android-client/src/main/java/io/getstream/chat/android/client/api2/endpoint/CallApi.kt
@@ -0,0 +1,41 @@
+/*
+ * Copyright (c) 2014-2022 Stream.io Inc. All rights reserved.
+ *
+ * Licensed under the Stream License;
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    https://github.com/GetStream/stream-chat-android/blob/main/LICENSE
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.getstream.chat.android.client.api2.endpoint
+
+import io.getstream.chat.android.client.api.AuthenticatedApi
+import io.getstream.chat.android.client.api2.model.requests.CallTokenRequest
+import io.getstream.chat.android.client.api2.model.response.CallTokenResponse
+import io.getstream.chat.android.client.api2.model.response.CreateCallResponse
+import io.getstream.chat.android.client.call.RetrofitCall
+import retrofit2.http.Body
+import retrofit2.http.POST
+import retrofit2.http.Path
+
+@AuthenticatedApi
+internal interface CallApi {
+
+    @POST("/channels/{type}/{id}/call")
+    fun createCall(
+        @Path("type") callType: String,
+        @Path("id") channelId: String,
+    ): RetrofitCall<CreateCallResponse>
+
+    @POST("/calls")
+    fun getCallToken(
+        @Body request: CallTokenRequest,
+    ): RetrofitCall<CallTokenResponse>
+}
