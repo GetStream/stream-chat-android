800a4c6f61821b536196ef9470e9339cb64be99c
Leandro Borges Ferreira <lehen01@gmail.com>
2022-08-19 17:39:11 -0300

    init

---
+++ stream-chat-android-offline/src/main/java/io/getstream/chat/android/offline/plugin/listener/internal/SendMessageListenerDatabase.kt
@@ -0,0 +1,81 @@
+package io.getstream.chat.android.offline.plugin.listener.internal
+
+import io.getstream.chat.android.client.errors.ChatError
+import io.getstream.chat.android.client.errors.ChatErrorCode
+import io.getstream.chat.android.client.errors.ChatNetworkError
+import io.getstream.chat.android.client.extensions.enrichWithCid
+import io.getstream.chat.android.client.extensions.internal.users
+import io.getstream.chat.android.client.extensions.isPermanent
+import io.getstream.chat.android.client.models.Message
+import io.getstream.chat.android.client.persistance.repository.MessageRepository
+import io.getstream.chat.android.client.persistance.repository.UserRepository
+import io.getstream.chat.android.client.plugin.listeners.SendMessageListener
+import io.getstream.chat.android.client.utils.Result
+import io.getstream.chat.android.client.utils.SyncStatus
+import io.getstream.chat.android.client.utils.internal.toMessageSyncDescription
+import io.getstream.logging.StreamLog
+import java.util.Date
+
+private const val TAG = "Chat:SendMessageHandlerDB"
+
+internal class SendMessageListenerDatabase(
+    private val userRepository: UserRepository,
+    private val messageRepository: MessageRepository
+): SendMessageListener {
+
+    override suspend fun onMessageSendResult(
+        result: Result<Message>,
+        channelType: String,
+        channelId: String,
+        message: Message,
+    ) {
+        val cid = "$channelType:$channelId"
+        if (result.isSuccess) {
+            handleSendMessageSuccess(cid, result.data())
+        } else {
+            handleSendMessageFail(message, result.error())
+        }
+    }
+
+    private suspend fun handleSendMessageSuccess(
+        cid: String,
+        processedMessage: Message,
+    ) {
+        processedMessage.enrichWithCid(cid)
+            .copy(
+                syncStatus = SyncStatus.COMPLETED,
+                syncDescription = null
+            )
+            .also { message ->
+                userRepository.insertUsers(message.users())
+                messageRepository.insertMessage(message, cache = false)
+            }
+    }
+
+    private suspend fun handleSendMessageFail(
+        message: Message,
+        error: ChatError,
+    ) {
+        val isPermanentError = error.isPermanent()
+        val isMessageModerationFailed = error is ChatNetworkError &&
+            error.streamCode == ChatErrorCode.MESSAGE_MODERATION_FAILED.code
+
+        StreamLog.w(TAG) {
+            "[handleSendMessageFail] isPermanentError: $isPermanentError" +
+                ", isMessageModerationFailed: $isMessageModerationFailed"
+        }
+
+        message.copy(
+            syncStatus = if (isPermanentError) {
+                SyncStatus.FAILED_PERMANENTLY
+            } else {
+                SyncStatus.SYNC_NEEDED
+            },
+            syncDescription = error.toMessageSyncDescription(),
+            updatedLocallyAt = Date(),
+        ).also {
+            userRepository.insertUsers(message.users())
+            messageRepository.insertMessage(message, cache = false)
+        }
+    }
+}
