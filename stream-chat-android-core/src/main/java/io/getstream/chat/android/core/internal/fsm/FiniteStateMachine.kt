2b293cd5284419ba009f8d2816d447eb90933c9d
Jc Miñarro <josecarlos.minarro@gmail.com>
2022-08-22 15:39:57 +0200

    Remove `mutex` access when reading the property

--- stream-chat-android-core/src/main/java/io/getstream/chat/android/core/internal/fsm/FiniteStateMachine.kt
+++ stream-chat-android-core/src/main/java/io/getstream/chat/android/core/internal/fsm/FiniteStateMachine.kt
@@ -45,14 +45,6 @@ public class FiniteStateMachine<S : Any, E : Any>(
     private val mutex = Mutex()
     private val _state: MutableStateFlow<S> = MutableStateFlow(initialState)
 
-    private suspend inline fun <T> Mutex.withLockIfNotLocked(action: () -> T): T {
-        return if (isLocked.not()) {
-            withLock { action() }
-        } else {
-            action()
-        }
-    }
-
     /**
      * The current state as [StateFlow].
      */
@@ -62,9 +54,7 @@ public class FiniteStateMachine<S : Any, E : Any>(
      * The current state.
      */
     public val state: S
-        get() = runBlocking {
-            mutex.withLockIfNotLocked { _state.value }
-        }
+        get() = _state.value
 
     /**
      * Sends an event to the state machine. The entry point to change state.
