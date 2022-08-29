abdfcad7930010184478538b2a63dd60f09ba343
TGazica <tomislav.gazica@gmail.com>
2022-08-29 12:10:34 +0200

    Revert debug code

--- stream-chat-android-ui-components/src/main/kotlin/io/getstream/chat/android/ui/channel/list/viewmodel/ChannelListViewModelBinding.kt
+++ stream-chat-android-ui-components/src/main/kotlin/io/getstream/chat/android/ui/channel/list/viewmodel/ChannelListViewModelBinding.kt
@@ -45,11 +45,6 @@ public fun ChannelListViewModel.bindView(
         }
 
         var list: List<ChannelListItem> = state?.channels?.map(ChannelListItem::ChannelItem) ?: emptyList()
-
-        list.filterIsInstance<ChannelListItem.ChannelItem>().map { it.channel.cid }.forEach {
-            println(it)
-        }
-
         if (paginationState?.loadingMore == true) {
             list = list + ChannelListItem.LoadingMoreItem
         }
