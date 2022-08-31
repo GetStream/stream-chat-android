dad82e443eb4c555152179608bd4b50f2ff27582
Leandro Borges Ferreira <lehen01@gmail.com>
2022-08-31 16:09:56 -0300

    Removing logs

--- stream-chat-android-state/src/main/java/io/getstream/chat/android/offline/event/handler/internal/QueryChannelsTrack.kt
+++ stream-chat-android-state/src/main/java/io/getstream/chat/android/offline/event/handler/internal/QueryChannelsTrack.kt
@@ -6,24 +6,11 @@ import io.getstream.logging.TaggedLogger
 import java.util.Date
 
 internal class QueryChannelsTrack {
-
-    private val logger: TaggedLogger = StreamLog.getLogger("SyncEventsTrack")
-
+    
     private var lastSyncedDate: Date? = null
 
     internal fun isLastSyncBeforeEvent(chatEvent: ChatEvent): Boolean {
-        return lastSyncedDate
-            .also { lastSync ->
-                logger.d {
-                    "Last sync: $lastSync, event date: ${chatEvent.createdAt}"
-                }
-            }
-            ?.before(chatEvent.createdAt)
-            .also { isBefore ->
-                logger.d {
-                    "is last sync before event: $isBefore"
-                }
-            }?: false
+        return lastSyncedDate?.before(chatEvent.createdAt) ?: false
     }
 
     internal fun markSynced(date: Date) {
