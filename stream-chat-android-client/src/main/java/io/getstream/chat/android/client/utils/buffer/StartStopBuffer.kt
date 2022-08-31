99cae72fd641ec5045235a30fac6418144abb1fb
Leandro Borges Ferreira <lehen01@gmail.com>
2022-08-31 11:11:13 -0300

    Fixing unit tests

--- stream-chat-android-client/src/main/java/io/getstream/chat/android/client/utils/buffer/StartStopBuffer.kt
+++ stream-chat-android-client/src/main/java/io/getstream/chat/android/client/utils/buffer/StartStopBuffer.kt
@@ -16,6 +16,7 @@
 
 package io.getstream.chat.android.client.utils.buffer
 
+import io.getstream.chat.android.core.internal.InternalStreamChatApi
 import io.getstream.chat.android.core.internal.coroutines.DispatcherProvider
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
@@ -26,6 +27,7 @@ import java.util.concurrent.atomic.AtomicBoolean
 
 private const val NO_LIMIT = -1
 
+@InternalStreamChatApi
 public class StartStopBuffer<T>(private val bufferLimit: Int = NO_LIMIT) {
 
     private val events: Queue<T> = ConcurrentLinkedQueue()
--- stream-chat-android-ui-components/src/test/kotlin/io/getstream/chat/android/ui/utils/StartStopBufferTest.kt
+++ stream-chat-android-ui-components/src/test/kotlin/io/getstream/chat/android/ui/utils/StartStopBufferTest.kt
@@ -16,7 +16,7 @@
 
 package io.getstream.chat.android.ui.utils
 
-import com.getstream.sdk.chat.utils.StartStopBuffer
+import io.getstream.chat.android.client.utils.buffer.StartStopBuffer
 import io.getstream.chat.android.test.TestCoroutineExtension
 import io.getstream.chat.android.test.randomString
 import kotlinx.coroutines.ExperimentalCoroutinesApi
