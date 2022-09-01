41956e92fa0e053028e64b6d0b4f79ea1190d07a
Leandro Borges Ferreira <lehen01@gmail.com>
2022-09-01 11:07:34 -0300

    Completing Kdocs for ExactDate

--- stream-chat-android-client/src/main/java/io/getstream/chat/android/client/api2/model/dto/utils/internal/ExactDate.kt
+++ stream-chat-android-client/src/main/java/io/getstream/chat/android/client/api2/model/dto/utils/internal/ExactDate.kt
@@ -20,6 +20,10 @@ import java.util.Date
 
 /**
  * DTO to keep serialized date and also the original Date as Stirng as sent by backend.
+ *
+ * @param date The [Date] was parsed from backend of created locally.
+ * @param rawDate The Date as a String. This is probably going to be the generated in the backend and can have
+ * up to nanoseconds of precision.
  */
 internal data class ExactDate(
     internal val date: Date,
