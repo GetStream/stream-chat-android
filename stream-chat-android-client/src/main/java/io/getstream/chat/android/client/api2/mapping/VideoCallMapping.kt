e92f9916e2731cd3a6a0222e8e936c3e62fc0f72
Jaewoong Eum <skydoves@naver.com>
2022-09-06 16:19:58 +0900

    Implement third party video integration APIs (#4052)
    * Add CallDto interfance and relevant data classes
    
    * Add call responses
    
    * Implement CallApi retrofit call
    
    * Add video call relevant models
    
    * Implement createVideoCall and getVideoCallToken
    
    * Implement createVideoCall and getVideoCallToken on ChatClient
    
    * Add the call type parameter to the createVideoCall request
    
    * Add explicit field name and type parameter to the call responses
    
    * Update call type and request paths
    
    * Change sequence of parameters and generate binary compatibility

---
+++ stream-chat-android-client/src/main/java/io/getstream/chat/android/client/api2/mapping/VideoCallMapping.kt
@@ -0,0 +1,57 @@
+/*
+ * Copyright (c) 2014-2022 Stream.io Inc. All rights reserved.
+ *
+ * Licensed under the Stream License;
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    https://github.com/GetStream/stream-chat-android/blob/main/LICENSE
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.getstream.chat.android.client.api2.mapping
+
+import io.getstream.chat.android.client.api2.model.dto.AgoraDto
+import io.getstream.chat.android.client.api2.model.dto.HMSDto
+import io.getstream.chat.android.client.api2.model.response.CreateVideoCallResponse
+import io.getstream.chat.android.client.api2.model.response.VideoCallTokenResponse
+import io.getstream.chat.android.client.models.AgoraChannel
+import io.getstream.chat.android.client.models.HMSRoom
+import io.getstream.chat.android.client.models.VideoCallInfo
+import io.getstream.chat.android.client.models.VideoCallToken
+
+internal fun AgoraDto.toDomain(): AgoraChannel {
+    return AgoraChannel(channel = channel)
+}
+
+internal fun HMSDto.toDomain(): HMSRoom {
+    return HMSRoom(roomId = roomId, roomName = roomName)
+}
+
+internal fun CreateVideoCallResponse.toDomain(): VideoCallInfo {
+    return VideoCallInfo(
+        callId = call.id,
+        provider = call.provider,
+        type = call.type,
+        agoraChannel = call.agora.toDomain(),
+        hmsRoom = call.hms.toDomain(),
+        videoCallToken = VideoCallToken(
+            token = token,
+            agoraUid = agoraUid,
+            agoraAppId = agoraAppId
+        )
+    )
+}
+
+internal fun VideoCallTokenResponse.toDomain(): VideoCallToken {
+    return VideoCallToken(
+        token = token,
+        agoraUid = agoraUid,
+        agoraAppId = agoraAppId
+    )
+}
