ae08b2936b4b512e1987c3844cfa23f3196882b7
MarinTolic <marin.t91@gmail.com>
2022-09-06 12:31:34 +0200

    [3369] Update deprecations

--- stream-chat-android-compose/src/main/java/io/getstream/chat/android/compose/ui/attachments/factory/QuotedAttachmentFactory.kt
+++ stream-chat-android-compose/src/main/java/io/getstream/chat/android/compose/ui/attachments/factory/QuotedAttachmentFactory.kt
@@ -19,7 +19,6 @@ package io.getstream.chat.android.compose.ui.attachments.factory
 import androidx.compose.runtime.Composable
 import io.getstream.chat.android.compose.ui.attachments.AttachmentFactory
 import io.getstream.chat.android.compose.ui.attachments.content.FileAttachmentQuotedContent
-import io.getstream.chat.android.compose.ui.attachments.content.ImageAttachmentQuotedContent
 import io.getstream.chat.android.compose.ui.attachments.content.MediaAttachmentQuotedContent
 import io.getstream.chat.android.compose.ui.util.isMedia
 import io.getstream.chat.android.uiutils.constant.AttachmentType
