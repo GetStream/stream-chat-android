3287cbbfbe1f72e39db15385a1cf49238130fe5f
kanat <kijalbaev@gmail.com>
2022-09-07 14:13:13 +0400

    [3898] fix snackbar visibility in ui-components + fix permission set

--- stream-chat-android-ui-components/src/main/kotlin/com/getstream/sdk/chat/utils/PermissionChecker.kt
+++ stream-chat-android-ui-components/src/main/kotlin/com/getstream/sdk/chat/utils/PermissionChecker.kt
@@ -22,6 +22,7 @@ import android.content.pm.PackageManager
 import android.os.Build
 import android.view.View
 import androidx.core.content.ContextCompat
+import com.google.android.material.snackbar.Snackbar
 import com.karumi.dexter.Dexter
 import com.karumi.dexter.MultiplePermissionsReport
 import com.karumi.dexter.listener.multi.BaseMultiplePermissionsListener
@@ -29,16 +30,49 @@ import com.karumi.dexter.listener.multi.DialogOnAnyDeniedMultiplePermissionsList
 import com.karumi.dexter.listener.multi.SnackbarOnAnyDeniedMultiplePermissionsListener
 import io.getstream.chat.android.core.internal.InternalStreamChatApi
 import io.getstream.chat.android.ui.common.R
+import io.getstream.chat.android.ui.common.extensions.internal.dpToPxPrecise
+
+private const val SNACKBAR_ELEVATION_IN_DP = 20
 
 @InternalStreamChatApi
 public class PermissionChecker {
 
-    public fun isGrantedStoragePermissions(context: Context): Boolean {
+    public fun isGrantedMediaPermissions(context: Context): Boolean {
         return Build.VERSION.SDK_INT < Build.VERSION_CODES.M ||
+            isAllPermissionsGranted(
+                context,
+                when (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                    true -> listOf(
+                        Manifest.permission.READ_MEDIA_IMAGES,
+                        Manifest.permission.READ_MEDIA_VIDEO,
+                    )
+                    else -> listOf(Manifest.permission.READ_EXTERNAL_STORAGE)
+                }
+            )
+    }
+
+    public fun isGrantedFilePermissions(context: Context): Boolean {
+        return Build.VERSION.SDK_INT < Build.VERSION_CODES.M ||
+            isAllPermissionsGranted(
+                context,
+                when (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                    true -> listOf(
+                        Manifest.permission.READ_MEDIA_IMAGES,
+                        Manifest.permission.READ_MEDIA_VIDEO,
+                        Manifest.permission.READ_MEDIA_AUDIO,
+                    )
+                    else -> listOf(Manifest.permission.READ_EXTERNAL_STORAGE)
+                }
+            )
+    }
+
+    private fun isAllPermissionsGranted(context: Context, permissions: List<String>): Boolean {
+        return permissions.all { permission ->
             ContextCompat.checkSelfPermission(
-            context,
-            Manifest.permission.READ_EXTERNAL_STORAGE
-        ) == PackageManager.PERMISSION_GRANTED
+                context,
+                permission
+            ) == PackageManager.PERMISSION_GRANTED
+        }
     }
 
     public fun isGrantedCameraPermissions(context: Context): Boolean =
@@ -56,8 +90,48 @@ public class PermissionChecker {
     public fun isNeededToRequestForCameraPermissions(context: Context): Boolean =
         isPermissionContainedOnManifest(context, Manifest.permission.CAMERA) && !isGrantedCameraPermissions(context)
 
-    public fun checkStoragePermissions(
+    public fun checkMediaPermissions(
         view: View,
+        onPermissionDenied: () -> Unit = { },
+        onPermissionGranted: () -> Unit,
+    ) {
+        checkStoragePermissions(
+            view,
+            when (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                true -> listOf(
+                    Manifest.permission.READ_MEDIA_IMAGES,
+                    Manifest.permission.READ_MEDIA_VIDEO,
+                )
+                else -> listOf(Manifest.permission.READ_EXTERNAL_STORAGE)
+            },
+            onPermissionDenied,
+            onPermissionGranted
+        )
+    }
+
+    public fun checkFilePermissions(
+        view: View,
+        onPermissionDenied: () -> Unit = { },
+        onPermissionGranted: () -> Unit,
+    ) {
+        checkStoragePermissions(
+            view,
+            when (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+                true -> listOf(
+                    Manifest.permission.READ_MEDIA_IMAGES,
+                    Manifest.permission.READ_MEDIA_VIDEO,
+                    Manifest.permission.READ_MEDIA_AUDIO,
+                )
+                else -> listOf(Manifest.permission.READ_EXTERNAL_STORAGE)
+            },
+            onPermissionDenied,
+            onPermissionGranted
+        )
+    }
+
+    private fun checkStoragePermissions(
+        view: View,
+        permissions: List<String>,
         onPermissionDenied: () -> Unit = { },
         onPermissionGranted: () -> Unit,
     ) {
@@ -66,14 +140,7 @@ public class PermissionChecker {
             view.context.getString(R.string.stream_ui_message_input_permission_storage_title),
             view.context.getString(R.string.stream_ui_message_input_permission_storage_message),
             view.context.getString(R.string.stream_ui_message_input_permission_setting_message),
-            when (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-                true -> listOf(
-                    Manifest.permission.READ_MEDIA_IMAGES,
-                    Manifest.permission.READ_MEDIA_AUDIO,
-                    Manifest.permission.READ_MEDIA_VIDEO,
-                )
-                else -> listOf(Manifest.permission.READ_EXTERNAL_STORAGE)
-            },
+            permissions,
             onPermissionDenied,
             onPermissionGranted
         )
@@ -89,14 +156,7 @@ public class PermissionChecker {
             view.context.getString(R.string.stream_ui_message_input_permission_storage_title),
             view.context.getString(R.string.stream_ui_message_input_permission_storage_message),
             view.context.getString(R.string.stream_ui_message_input_permission_setting_message),
-            when (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-                true -> listOf(
-                    Manifest.permission.READ_MEDIA_IMAGES,
-                    Manifest.permission.READ_MEDIA_AUDIO,
-                    Manifest.permission.READ_MEDIA_VIDEO,
-                )
-                else -> listOf(Manifest.permission.READ_EXTERNAL_STORAGE)
-            },
+            listOf(Manifest.permission.WRITE_EXTERNAL_STORAGE),
             onPermissionDenied,
             onPermissionGranted
         )
@@ -178,6 +238,11 @@ public class PermissionChecker {
         SnackbarOnAnyDeniedMultiplePermissionsListener.Builder
             .with(view, snackbarMessage)
             .withOpenSettingsButton(R.string.stream_ui_message_input_permissions_setting_button)
+            .withCallback(object : Snackbar.Callback() {
+                override fun onShown(sb: Snackbar?) {
+                    sb?.view?.elevation = SNACKBAR_ELEVATION_IN_DP.dpToPxPrecise()
+                }
+            })
             .build()
 
     private fun dialogPermissionsListener(
