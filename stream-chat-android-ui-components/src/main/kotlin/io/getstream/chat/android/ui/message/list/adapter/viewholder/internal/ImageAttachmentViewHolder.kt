4064b649d363821cc06dc9863b4adc52b053053d
Marin Tolić <37080097+MarinTolic@users.noreply.github.com>
2022-09-12 16:50:42 +0200

    [3742]  Fix message click area (#4151)
    * [3742] Re arrange views the click listeners are set on in order to not allow trigger them when clicking on the sides of message containers/ bubbles (white space)
    
    * [3742] Re-set `LongClickFriendlyLinkMovementMethod` to the message container instead of layout root
    
    * [3742] Move listeners in plain text viewholder from being set on the root to being set on the message container itself
    
    * [3742] Update the changelog

--- stream-chat-android-ui-components/src/main/kotlin/io/getstream/chat/android/ui/message/list/adapter/viewholder/internal/ImageAttachmentViewHolder.kt
+++ stream-chat-android-ui-components/src/main/kotlin/io/getstream/chat/android/ui/message/list/adapter/viewholder/internal/ImageAttachmentViewHolder.kt
@@ -129,7 +129,7 @@ internal class ImageAttachmentViewHolder(
     private fun initializeListeners() {
         binding.run {
             listeners?.let { container ->
-                root.setOnClickListener {
+                messageContainer.setOnClickListener {
                     container.messageClickListener.onMessageClick(data.message)
                 }
                 reactionsView.setReactionClickListener {
@@ -138,7 +138,7 @@ internal class ImageAttachmentViewHolder(
                 footnote.setOnThreadClickListener {
                     container.threadClickListener.onThreadClick(data.message)
                 }
-                root.setOnLongClickListener {
+                messageContainer.setOnLongClickListener {
                     container.messageLongClickListener.onMessageLongClick(data.message)
                     true
                 }
@@ -162,7 +162,7 @@ internal class ImageAttachmentViewHolder(
         listeners?.let { container ->
             LongClickFriendlyLinkMovementMethod.set(
                 textView = binding.messageText,
-                longClickTarget = binding.root,
+                longClickTarget = binding.messageContainer,
                 onLinkClicked = container.linkClickListener::onLinkClick
             )
         }
