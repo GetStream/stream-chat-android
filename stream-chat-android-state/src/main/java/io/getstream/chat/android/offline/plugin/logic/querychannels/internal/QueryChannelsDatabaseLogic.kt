703085d6b7399b84ef6ae1f77da2aa7ab74e1f77
Leandro Borges Ferreira <lehen01@gmail.com>
2022-09-13 18:03:21 -0300

    Adding licenses and fixing build

--- stream-chat-android-state/src/main/java/io/getstream/chat/android/offline/plugin/logic/querychannels/internal/QueryChannelsDatabaseLogic.kt
+++ stream-chat-android-state/src/main/java/io/getstream/chat/android/offline/plugin/logic/querychannels/internal/QueryChannelsDatabaseLogic.kt
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2014-2022 Stream.io Inc. All rights reserved.
+ *
+ * Licensed under the Stream License;
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    https://github.com/GetStream/stream-chat-android/blob/main/LICENSE
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package io.getstream.chat.android.offline.plugin.logic.querychannels.internal
 
 import androidx.annotation.VisibleForTesting
@@ -15,11 +31,11 @@ import io.getstream.chat.android.client.persistance.repository.UserRepository
 import io.getstream.chat.android.client.query.QueryChannelsSpec
 import io.getstream.chat.android.client.query.pagination.AnyChannelPaginationRequest
 import io.getstream.chat.android.client.query.pagination.isRequestingMoreThanLastMessage
-import io.getstream.logging.StreamLog
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.async
 import kotlinx.coroutines.awaitAll
 
+@Suppress("LongParameterList")
 internal class QueryChannelsDatabaseLogic(
     private val queryChannelsRepository: QueryChannelsRepository,
     private val channelConfigRepository: ChannelConfigRepository,
@@ -29,8 +45,6 @@ internal class QueryChannelsDatabaseLogic(
     private val scope: CoroutineScope,
     private val defaultConfig: Config,
 ) {
-
-    private val logger = StreamLog.getLogger("QueryChannelsDatabaseLogic")
 
     private suspend fun selectChannels(
         channelIds: List<String>,
