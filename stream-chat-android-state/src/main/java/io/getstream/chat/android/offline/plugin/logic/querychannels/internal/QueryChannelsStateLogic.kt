1d38053ad875a97db2d6fd964ac1d11494e8cc32
Leandro Borges Ferreira <lehen01@gmail.com>
2022-09-14 17:24:15 -0300

    Adding kdocs

--- stream-chat-android-state/src/main/java/io/getstream/chat/android/offline/plugin/logic/querychannels/internal/QueryChannelsStateLogic.kt
+++ stream-chat-android-state/src/main/java/io/getstream/chat/android/offline/plugin/logic/querychannels/internal/QueryChannelsStateLogic.kt
@@ -30,37 +30,103 @@ internal interface QueryChannelsStateLogic {
 
     fun handleChatEvent(event: ChatEvent, cachedChannel: Channel?): EventHandlingResult
 
+    /**
+     * Returns the loading status.
+     */
     fun isLoading(): Boolean
 
+    /**
+     * Returns the current channel offset.
+     */
     fun getChannelsOffset(): Int
 
+    /**
+     * Get all the channels that were queried so far.
+     */
     fun getChannels(): Map<String, Channel>?
 
+    /**
+     * The the specs of the query.
+     */
     fun getQuerySpecs(): QueryChannelsSpec
 
+    /**
+     * Get the state of the query.
+     */
     fun getState(): QueryChannelsState
 
+    /**
+     * Set the loading state.
+     *
+     * @param isLoading Boolean
+     */
     fun setLoading(isLoading: Boolean)
 
+    /**
+     * Set the current request being made.
+     *
+     * @param request [QueryChannelsRequest]
+     */
     fun setCurrentRequest(request: QueryChannelsRequest)
 
+    /**
+     * Set the end of channels.
+     *
+     * @parami isEnd Boolean
+     */
     fun setEndOfChannels(isEnd: Boolean)
 
+    /**
+     * Sets if recovery is needed.
+     *
+     * @param recoveryNeeded Boolean
+     */
     fun setRecoveryNeeded(recoveryNeeded: Boolean)
 
+    /**
+     * Set the offset of the channels.
+     *
+     * @param offset Int
+     */
     fun setChannelsOffset(offset: Int)
 
+    /**
+     * Increments the channels offset.
+     *
+     * @param size Int
+     */
     fun incrementChannelsOffset(size: Int)
 
+    /**
+     * MutableStateFlow<Boolean> for the current state. It returns the accordingly with loading first page
+     * or loading more channels.
+     */
     fun loadingForCurrentRequest(): MutableStateFlow<Boolean>
 
+    /**
+     * Add channels to state
+     *
+     * @param channels List<Channel>.
+     */
     fun addChannelsState(channels: List<Channel>)
 
+    /**
+     * Remove channels to state.
+     */
     fun removeChannels(cidSet: Set<String>)
 
+    /**
+     * Initialized the chanels as a empty list, if needed.
+     */
     fun initializeChannelsIfNeeded()
 
+    /**
+     * Refresh all the channels.
+     */
     fun refreshChannels(cidList: Collection<String>)
 
+    /**
+     * Refresh members of the state.
+     */
     fun refreshMembersStateForUser(newUser: User)
 }
