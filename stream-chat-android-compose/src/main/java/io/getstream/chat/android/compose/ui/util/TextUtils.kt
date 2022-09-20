ca7af588e5db2f1039daf04c3489ff8c3232c9e3
TGazica <tomislav.gazica@gmail.com>
2022-09-20 11:46:20 +0200

    Expanded changelog explanation and added missing kdoc param

--- stream-chat-android-compose/src/main/java/io/getstream/chat/android/compose/ui/util/TextUtils.kt
+++ stream-chat-android-compose/src/main/java/io/getstream/chat/android/compose/ui/util/TextUtils.kt
@@ -46,6 +46,7 @@ internal fun buildAnnotatedMessageText(message: Message): AnnotatedString {
  * if there are any links available.
  *
  * @param text The message text to style.
+ * @param isOwnMessage Whether the message is the current users message or not.
  *
  * @return The annotated String, with clickable links, if applicable.
  */
