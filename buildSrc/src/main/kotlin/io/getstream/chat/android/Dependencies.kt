703287c170abd758aa0bad1250d5d5aa10ad6049
Jc Miñarro <josecarlos.minarro@gmail.com>
2022-09-20 12:10:05 +0200

    Upgrade Threetenbp Dependency

--- buildSrc/src/main/kotlin/io/getstream/chat/android/Dependencies.kt
+++ buildSrc/src/main/kotlin/io/getstream/chat/android/Dependencies.kt
@@ -72,7 +72,7 @@ object Versions {
     internal const val SPOTLESS = "6.7.2"
     internal const val STFALCON_IMAGE_VIEWER = "1.0.1"
     internal const val TEST_PARAMETER_INJECTOR = "1.8"
-    internal const val THREETENBP = "1.6.0"
+    internal const val THREETENBP = "1.6.1"
     internal const val THREETENABP = "1.4.0"
     internal const val TIMBER = "5.0.1"
     internal const val TURBINE = "0.8.0"
