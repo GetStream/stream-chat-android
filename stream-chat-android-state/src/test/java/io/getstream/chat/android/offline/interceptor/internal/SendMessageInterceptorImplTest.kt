ee1b441fac7614d459f24b5f91f72dd744f40d98
Leandro Borges Ferreira <lehen01@gmail.com>
2022-09-20 09:25:11 -0300

    removing unused imports and blank lines

--- stream-chat-android-state/src/test/java/io/getstream/chat/android/offline/interceptor/internal/SendMessageInterceptorImplTest.kt
+++ stream-chat-android-state/src/test/java/io/getstream/chat/android/offline/interceptor/internal/SendMessageInterceptorImplTest.kt
@@ -32,14 +32,12 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.test.TestScope
 import kotlinx.coroutines.test.runTest
 import org.amshove.kluent.`should be`
-import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Test
 import org.mockito.kotlin.any
 import org.mockito.kotlin.argThat
 import org.mockito.kotlin.doAnswer
 import org.mockito.kotlin.doReturn
 import org.mockito.kotlin.mock
-import org.mockito.kotlin.reset
 import org.mockito.kotlin.verify
 import org.mockito.kotlin.whenever
 
@@ -127,8 +125,6 @@ internal class SendMessageInterceptorImplTest {
 
     @Test
     fun `when send message in thread without show in channel, message should be added to threadState`() = runTest {
-
-
         val messageToSend = randomMessage(text = randomString(), parentId = randomString(), showInChannel = false)
 
         logic.channelFromMessage(messageToSend) `should be` null
