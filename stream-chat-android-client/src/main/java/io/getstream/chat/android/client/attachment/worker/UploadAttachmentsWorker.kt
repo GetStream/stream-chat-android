d552cefca08f439cbc16016033d9cf2c0e2a9bc9
Leandro Borges Ferreira <lehen01@gmail.com>
2022-09-30 15:36:37 -0300

    Changing visibility

--- stream-chat-android-client/src/main/java/io/getstream/chat/android/client/attachment/worker/UploadAttachmentsWorker.kt
+++ stream-chat-android-client/src/main/java/io/getstream/chat/android/client/attachment/worker/UploadAttachmentsWorker.kt
@@ -28,8 +28,10 @@ import io.getstream.chat.android.client.utils.ProgressCallback
 import io.getstream.chat.android.client.utils.Result
 import io.getstream.chat.android.client.utils.SyncStatus
 import io.getstream.chat.android.client.utils.recover
+import io.getstream.chat.android.core.internal.InternalStreamChatApi
 
-internal class UploadAttachmentsWorker(
+@InternalStreamChatApi
+public class UploadAttachmentsWorker(
     private val channelType: String,
     private val channelId: String,
     private val channelStateLogic: ChannelMessagesUpdateLogic?,
@@ -39,7 +41,8 @@ internal class UploadAttachmentsWorker(
 ) {
 
     @Suppress("TooGenericExceptionCaught")
-    suspend fun uploadAttachmentsForMessage(messageId: String): Result<Unit> {
+    @InternalStreamChatApi
+    public suspend fun uploadAttachmentsForMessage(messageId: String): Result<Unit> {
         val message = messageRepository.selectMessage(messageId)
 
         return try {
