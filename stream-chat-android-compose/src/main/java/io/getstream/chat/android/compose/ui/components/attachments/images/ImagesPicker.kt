202fb8ac032674135dcff6f0f53084d9cc1d8a1e
MarinTolic <marin.t91@gmail.com>
2022-10-21 13:19:16 +0200

    [4112] Host the video duration information and logo in a layout with a rounded background

--- stream-chat-android-compose/src/main/java/io/getstream/chat/android/compose/ui/components/attachments/images/ImagesPicker.kt
+++ stream-chat-android-compose/src/main/java/io/getstream/chat/android/compose/ui/components/attachments/images/ImagesPicker.kt
@@ -19,16 +19,22 @@ package io.getstream.chat.android.compose.ui.components.attachments.images
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.BoxScope
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Icon
 import androidx.compose.material.Text
 import androidx.compose.material.ripple.rememberRipple
@@ -36,8 +42,6 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Brush
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalLayoutDirection
@@ -160,42 +164,42 @@ internal fun DefaultImagesPickerItem(
  * @param modifier Modifier for styling.
  */
 @Composable
-private fun VideoThumbnailOverlay(
+private fun BoxScope.VideoThumbnailOverlay(
     videoLength: Long,
     modifier: Modifier = Modifier,
 ) {
-    Box(
+    Row(
         modifier = modifier
-            .fillMaxSize()
+            .wrapContentSize()
+            .padding(horizontal = 4.dp, vertical = 6.dp)
             .background(
-                brush = Brush.verticalGradient(
-                    colors = listOf(
-                        Color.Transparent,
-                        Color.Transparent,
-                        Color.Black
-                    )
-                )
+                shape = RoundedCornerShape(10.dp),
+                color = ChatTheme.colors.barsBackground
             )
+            .align(Alignment.BottomCenter)
+            .padding(4.dp),
+        horizontalArrangement = Arrangement.Center
     ) {
+
         Icon(
             modifier = Modifier
-                .padding(4.dp)
-                .size(24.dp)
+                .size(16.dp)
+                .aspectRatio(1f)
                 .mirrorRtl(LocalLayoutDirection.current)
-                .align(Alignment.BottomStart),
+                .align(Alignment.CenterVertically),
             painter = painterResource(id = R.drawable.stream_compose_ic_video),
             contentDescription = null,
-            tint = Color.White
+            tint = ChatTheme.colors.textHighEmphasis
         )
 
         if (videoLength != 0L) {
             Text(
                 modifier = Modifier
-                    .padding(horizontal = 4.dp, vertical = 8.dp)
-                    .align(Alignment.BottomEnd),
+                    .padding(start = 4.dp, end = 2.dp)
+                    .align(Alignment.CenterVertically),
                 text = MediaStringUtil.convertVideoLength(videoLength),
-                style = ChatTheme.typography.bodyBold,
-                color = Color.White
+                style = ChatTheme.typography.footnote,
+                color = ChatTheme.colors.textHighEmphasis
             )
         }
     }
