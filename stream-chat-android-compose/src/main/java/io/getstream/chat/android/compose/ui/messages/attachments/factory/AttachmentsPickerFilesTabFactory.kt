d4203c652b5df4a8d44763a8877309cc8d9c955b
Marin Tolić <37080097+MarinTolic@users.noreply.github.com>
2022-10-26 09:44:44 +0200

    [4112] Improve video information design in the picker (#4299)
    * [4112] Host the video duration information and logo in a layout with a rounded background
    
    * [4112] Return a fallback string if the value duration is less or equal to 0. This will include a valid edge case where videos are shorter than a second but is mostly aimed at videos with unknown duration.
    
    * [4112] Implement a more readable video length information readout in the UI Comp (XML) sdk
    
    * [4112] Minor UI/UX tweaks
    
    * [4112] Bring the composable function in line with the standard naming convention
    
    * [4112] Update the changelog
    
    * [4112] Switch to using a pre existing extension function for drawable tinting
    
    * [4112] Remove tint from image view
    
    * [4112] Account for view visibility being changed to gone

--- stream-chat-android-compose/src/main/java/io/getstream/chat/android/compose/ui/messages/attachments/factory/AttachmentsPickerFilesTabFactory.kt
+++ stream-chat-android-compose/src/main/java/io/getstream/chat/android/compose/ui/messages/attachments/factory/AttachmentsPickerFilesTabFactory.kt
@@ -61,7 +61,7 @@ public class AttachmentsPickerFilesTabFactory : AttachmentsPickerTabFactory {
      * @param isSelected If the tab is selected.
      */
     @Composable
-    override fun pickerTabIcon(isEnabled: Boolean, isSelected: Boolean) {
+    override fun PickerTabIcon(isEnabled: Boolean, isSelected: Boolean) {
         Icon(
             painter = painterResource(id = R.drawable.stream_compose_ic_file_picker),
             contentDescription = stringResource(id = R.string.stream_compose_files_option),
@@ -83,7 +83,7 @@ public class AttachmentsPickerFilesTabFactory : AttachmentsPickerTabFactory {
      */
     @OptIn(ExperimentalPermissionsApi::class)
     @Composable
-    override fun pickerTabContent(
+    override fun PickerTabContent(
         attachments: List<AttachmentPickerItemState>,
         onAttachmentsChanged: (List<AttachmentPickerItemState>) -> Unit,
         onAttachmentItemSelected: (AttachmentPickerItemState) -> Unit,
