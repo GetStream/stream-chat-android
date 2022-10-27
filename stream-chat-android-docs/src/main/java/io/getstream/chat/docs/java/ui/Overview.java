438fe8658502b56266315ede81f53137a7fa067e
Rafał Adasiewicz <rafal@getstream.io>
2022-10-27 17:02:00 +0200

    Merge branch 'main' into develop

---
+++ stream-chat-android-docs/src/main/java/io/getstream/chat/docs/java/ui/Overview.java
@@ -0,0 +1,29 @@
+package io.getstream.chat.docs.java.ui;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.LifecycleOwner;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.getstream.sdk.chat.viewmodel.messages.MessageListViewModel;
+
+import io.getstream.chat.android.ui.message.list.MessageListView;
+import io.getstream.chat.android.ui.message.list.viewmodel.MessageListViewModelBinding;
+import io.getstream.chat.android.ui.message.list.viewmodel.factory.MessageListViewModelFactory;
+
+/**
+ * [Overview](https://getstream.io/chat/docs/sdk/android/ui/overview/)
+ */
+public class Overview extends AppCompatActivity {
+
+    public void viewModelsInitialization(MessageListView messageListView, LifecycleOwner viewLifecycleOwner) {
+        ViewModelProvider.Factory factory = new MessageListViewModelFactory // 1
+                .Builder()
+                .cid("channelType:channelId")
+                .build();
+        ViewModelProvider provider = new ViewModelProvider(this, factory);
+        MessageListViewModel messageListViewModel = provider.get(MessageListViewModel.class); // 2
+
+        boolean enforceUniqueReactions = true;
+        MessageListViewModelBinding.bind(messageListViewModel, messageListView, viewLifecycleOwner, enforceUniqueReactions); // 3
+    }
+}
---
+++ stream-chat-android-docs/src/main/kotlin/io/getstream/chat/docs/kotlin/ui/channels/ChannelListScreen.kt
@@ -0,0 +1,123 @@
+package io.getstream.chat.docs.kotlin.ui.channels
+
+import android.content.Context
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import io.getstream.chat.android.client.api.models.FilterObject
+import io.getstream.chat.android.client.api.models.querysort.QuerySorter
+import io.getstream.chat.android.client.models.Channel
+import io.getstream.chat.android.client.models.Message
+import io.getstream.chat.android.ui.channel.ChannelListActivity
+import io.getstream.chat.android.ui.channel.ChannelListFragment
+import io.getstream.chat.android.ui.channel.list.ChannelListView
+import io.getstream.chat.android.ui.channel.list.header.ChannelListHeaderView
+import io.getstream.chat.android.ui.search.SearchInputView
+import io.getstream.chat.android.ui.search.list.SearchResultListView
+import io.getstream.chat.docs.R
+
+/**
+ * [Channel List Screen](https://getstream.io/chat/docs/sdk/android/ui/channel-components/channel-list-screen/)
+ */
+class ChannelListScreen {
+
+    fun usage() {
+        class MyChannelListActivity : AppCompatActivity() {
+
+            override fun onCreate(savedInstanceState: Bundle?) {
+                super.onCreate(savedInstanceState)
+                setContentView(R.layout.stream_ui_fragment_container)
+
+                if (savedInstanceState == null) {
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.container, ChannelListFragment.newInstance())
+                        .commit()
+                }
+            }
+        }
+    }
+
+    fun startingActivity(context: Context) {
+        context.startActivity(ChannelListActivity.createIntent(context))
+    }
+
+    fun handlingActions() {
+        class MyChannelListActivity : AppCompatActivity(),
+            ChannelListFragment.HeaderActionButtonClickListener,
+            ChannelListFragment.HeaderUserAvatarClickListener,
+            ChannelListFragment.ChannelListItemClickListener,
+            ChannelListFragment.SearchResultClickListener {
+
+            override fun onCreate(savedInstanceState: Bundle?) {
+                super.onCreate(savedInstanceState)
+                // Add ChannelListFragment to the layout
+            }
+
+            override fun onUserAvatarClick() {
+                // Handle header avatar click
+            }
+
+            override fun onActionButtonClick() {
+                // Handle header action button click
+            }
+
+            override fun onChannelClick(channel: Channel) {
+                // Handle channel click
+            }
+
+            override fun onSearchResultClick(message: Message) {
+                // Handle search result click
+            }
+        }
+    }
+
+    fun customizations() {
+        class CustomChannelListFragment : ChannelListFragment() {
+
+            override fun setupChannelListHeader(channelListHeaderView: ChannelListHeaderView) {
+                super.setupChannelListHeader(channelListHeaderView)
+                // Customize channel list header view. For example, set a custom avatar click listener:
+                channelListHeaderView.setOnUserAvatarClickListener {
+                    // Handle avatar click
+                }
+            }
+
+            override fun setupChannelList(channelListView: ChannelListView) {
+                super.setupChannelList(channelListView)
+                // Customize channel list view
+            }
+
+            override fun setupSearchInput(searchInputView: SearchInputView) {
+                super.setupSearchInput(searchInputView)
+                // Customize search input field
+            }
+
+            override fun setupSearchResultList(searchResultListView: SearchResultListView) {
+                super.setupSearchResultList(searchResultListView)
+                // Customize search result list
+            }
+
+            override fun getFilter(): FilterObject? {
+                // Provide custom filter
+                return null
+            }
+
+            override fun getSort(): QuerySorter<Channel> {
+                // Provide custom sort
+                return super.getSort()
+            }
+        }
+
+        class CustomChannelListActivity : ChannelListActivity() {
+
+            override fun createChannelListFragment(): ChannelListFragment {
+                return ChannelListFragment.newInstance {
+                    setFragment(CustomChannelListFragment())
+                    customTheme(R.style.StreamUiTheme)
+                    showSearch(true)
+                    showHeader(true)
+                    headerTitle("Title")
+                }
+            }
+        }
+    }
+}
