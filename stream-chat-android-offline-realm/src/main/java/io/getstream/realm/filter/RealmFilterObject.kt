a0fa48540070c633dc7164d739d66236ced1f69f
Leandro Borges Ferreira <lehen01@gmail.com>
2022-10-27 18:48:56 -0300

    Fixing cast

--- stream-chat-android-offline-realm/src/main/java/io/getstream/realm/filter/RealmFilterObject.kt
+++ stream-chat-android-offline-realm/src/main/java/io/getstream/realm/filter/RealmFilterObject.kt
@@ -67,9 +67,9 @@ internal fun FilterObject.toFilterNode(): FilterNode = when (this) {
 
 @Suppress("ComplexMethod")
 internal fun FilterNode.toFilterObject(): FilterObject = when (this.filterType) {
-    KEY_AND -> Filters.and((this.value as Set<FilterNode>).map(FilterNode::toFilterObject))
-    KEY_OR -> Filters.or((this.value as Set<FilterNode>).map(FilterNode::toFilterObject))
-    KEY_NOR -> Filters.nor((this.value as Set<FilterNode>).map(FilterNode::toFilterObject))
+    KEY_AND -> Filters.and((this.value as List<FilterNode>).map(FilterNode::toFilterObject))
+    KEY_OR -> Filters.or((this.value as List<FilterNode>).map(FilterNode::toFilterObject))
+    KEY_NOR -> Filters.nor((this.value as List<FilterNode>).map(FilterNode::toFilterObject))
     KEY_EXIST -> this.field?.let(Filters::exists) ?: Filters.neutral()
     KEY_NOT_EXIST -> this.field?.let(Filters::notExists) ?: Filters.neutral()
     KEY_EQUALS -> Filters.eq(this.field ?: "", this.value ?: false)
