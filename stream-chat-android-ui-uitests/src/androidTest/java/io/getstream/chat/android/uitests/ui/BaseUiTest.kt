590c59622db29ff6c89a1534b54fce88162e8474
bychkovdmitry <9600921+bychkovdmitry@users.noreply.github.com>
2022-11-08 00:19:23 +0300

    [4342] Fix UI tests (#4347)

--- stream-chat-android-ui-uitests/src/androidTest/java/io/getstream/chat/android/uitests/ui/BaseUiTest.kt
+++ stream-chat-android-ui-uitests/src/androidTest/java/io/getstream/chat/android/uitests/ui/BaseUiTest.kt
@@ -21,7 +21,6 @@ import androidx.compose.ui.test.junit4.createAndroidComposeRule
 import androidx.test.platform.app.InstrumentationRegistry
 import io.getstream.chat.android.client.ChatClient
 import io.getstream.chat.android.client.logger.ChatLogLevel
-import io.getstream.chat.android.client.models.UploadAttachmentsNetworkType
 import io.getstream.chat.android.state.plugin.configuration.StatePluginConfig
 import io.getstream.chat.android.state.plugin.factory.StreamStatePluginFactory
 import io.getstream.chat.android.uitests.app.login.LoginActivity
@@ -70,7 +69,6 @@ internal abstract class BaseUiTest {
             config = StatePluginConfig(
                 backgroundSyncEnabled = false,
                 userPresence = false,
-                uploadAttachmentsNetworkType = UploadAttachmentsNetworkType.NOT_ROAMING,
             ),
             appContext = context
         )
