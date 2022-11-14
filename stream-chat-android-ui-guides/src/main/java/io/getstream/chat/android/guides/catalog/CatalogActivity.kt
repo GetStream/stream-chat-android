096990186d195c3b24b288d0743fd5fdf58b7e4a
Dmitrii Bychkov <mail@dbychkov.com>
2022-11-15 02:22:48 +0300

    [4411] Update building channels screen guide

--- stream-chat-android-ui-guides/src/main/java/io/getstream/chat/android/guides/catalog/CatalogActivity.kt
+++ stream-chat-android-ui-guides/src/main/java/io/getstream/chat/android/guides/catalog/CatalogActivity.kt
@@ -46,6 +46,7 @@ import io.getstream.chat.android.compose.ui.theme.ChatTheme
 import io.getstream.chat.android.guides.R
 import io.getstream.chat.android.guides.login.LoginActivity
 import io.getstream.chat.android.guides.catalog.compose.customattachments.ChannelsActivity as ComposeCustomAttachmentsActivity
+import io.getstream.chat.android.guides.catalog.uicomponents.channelsscreen.ChannelsActivity as UiComponentsChannelsScreenActivity
 import io.getstream.chat.android.guides.catalog.uicomponents.customattachments.ChannelsActivity as UiComponentsCustomAttachmentsActivity
 
 /**
@@ -101,6 +102,13 @@ class CatalogActivity : AppCompatActivity() {
                 descriptionText = stringResource(R.string.uicomponents_custom_attachments_guide_description)
             ) {
                 startActivity(UiComponentsCustomAttachmentsActivity.createIntent(this@CatalogActivity))
+            }
+
+            GuideItem(
+                titleText = stringResource(R.string.uicomponents_channels_screen_guide_title),
+                descriptionText = stringResource(R.string.uicomponents_channels_screen_guide_description)
+            ) {
+                startActivity(UiComponentsChannelsScreenActivity.createIntent(this@CatalogActivity))
             }
         }
     }
