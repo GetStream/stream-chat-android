d5552a6a1efdc730142e7ceca9501cd7fdd62900
Dmitrii Bychkov <mail@dbychkov.com>
2022-11-14 18:43:30 +0300

    [4409] Update channel list header docs

--- stream-chat-android-docs/src/main/java/io/getstream/chat/docs/java/ui/channels/ChannelListHeader.java
+++ stream-chat-android-docs/src/main/java/io/getstream/chat/docs/java/ui/channels/ChannelListHeader.java
@@ -10,18 +10,24 @@ import io.getstream.chat.android.ui.viewmodel.channels.ChannelListHeaderViewMode
 /**
  * [Channel List Header](https://getstream.io/chat/docs/sdk/android/ui/channel-components/channel-list-header/)
  */
-public class ChannelListHeaderViewSnippets extends Fragment {
+public class ChannelListHeader extends Fragment {
 
     private ChannelListHeaderView channelListHeaderView;
 
+    /**
+     * [Usage](https://getstream.io/chat/docs/sdk/android/ui/channel-components/channel-list-header/#usage)
+     */
     public void usage() {
-        // Get ViewModel
+        // Instantiate the ViewModel
         ChannelListHeaderViewModel viewModel = new ViewModelProvider(this).get(ChannelListHeaderViewModel.class);
 
         // Bind it with ChannelListHeaderView
         ChannelListHeaderViewModelBinding.bind(viewModel, channelListHeaderView, getViewLifecycleOwner());
     }
 
+    /**
+     * [Handling Actions](https://getstream.io/chat/docs/sdk/android/ui/channel-components/channel-list-header/#handling-actions)
+     */
     public void handlingActions() {
         channelListHeaderView.setOnActionButtonClickListener(() -> {
             // Handle action button click
