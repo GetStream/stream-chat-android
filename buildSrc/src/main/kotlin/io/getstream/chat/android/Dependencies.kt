54a1dd46953aa63c3b6695f2c046c589a3e24244
Jc Miñarro <josecarlos.minarro@gmail.com>
2022-11-22 17:38:11 +0100

    Upgrade TestParameterInjector Version

--- buildSrc/src/main/kotlin/io/getstream/chat/android/Dependencies.kt
+++ buildSrc/src/main/kotlin/io/getstream/chat/android/Dependencies.kt
@@ -69,7 +69,7 @@ object Versions {
     internal const val SHOT = "5.14.1"
     internal const val SPOTLESS = "6.7.2"
     internal const val STFALCON_IMAGE_VIEWER = "1.0.1"
-    internal const val TEST_PARAMETER_INJECTOR = "1.8"
+    internal const val TEST_PARAMETER_INJECTOR = "1.10"
     internal const val THREETENBP = "1.6.1"
     internal const val TIMBER = "5.0.1"
     internal const val TURBINE = "0.12.1"
