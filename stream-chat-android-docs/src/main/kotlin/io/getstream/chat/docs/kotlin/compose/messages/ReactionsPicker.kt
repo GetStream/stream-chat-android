900130709e205e43350bc35313a2bba3a0647648
MarinTolic <marin.t91@gmail.com>
2022-12-02 19:03:03 +0100

    [4533] Update screenshots and a code snippet

--- stream-chat-android-docs/src/main/kotlin/io/getstream/chat/docs/kotlin/compose/messages/ReactionsPicker.kt
+++ stream-chat-android-docs/src/main/kotlin/io/getstream/chat/docs/kotlin/compose/messages/ReactionsPicker.kt
@@ -185,7 +185,8 @@ private object ReactionsPickerCustomizationSnippet {
             Text(
                 modifier = Modifier.padding(start = 20.dp, top = 12.dp),
                 text = "Available reactions",
-                style = ChatTheme.typography.title3Bold
+                style = ChatTheme.typography.title3Bold,
+                color = ChatTheme.colors.textHighEmphasis
             )
         }
     }
