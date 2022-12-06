595a71980245d2e1582c055bbd8103fc7172c95d
Leandro Borges Ferreira <lehen01@gmail.com>
2022-12-06 10:08:20 +0100

    Simplyfing code and more documentation

--- stream-chat-android-offline-realm/src/main/java/io/getstream/realm/entity/QueryChannelsEntityRealm.kt
+++ stream-chat-android-offline-realm/src/main/java/io/getstream/realm/entity/QueryChannelsEntityRealm.kt
@@ -37,40 +37,43 @@ import io.realm.kotlin.types.annotations.PrimaryKey
 internal class QueryChannelsEntityRealm : RealmObject {
     @PrimaryKey
     var id: String = ""
-    var filterAsString: String? = null
+    var filter_as_string: String? = null
     var query_sort: QuerySorterInfoEntityRealm? = null
     var cids: RealmList<String> = realmListOf()
 }
 
-@OptIn(ExperimentalStdlibApi::class)
 internal fun QueryChannelsSpec.toRealm(): QueryChannelsEntityRealm {
     val thisQuery = this
-    val adapter = Moshi.Builder()
-        .addAdapter(FilterNodeAdapter())
-        .build()
-        .adapter(FilterNode::class.java)
+    val adapter = filterNodeAdapter()
 
     return QueryChannelsEntityRealm().apply {
         id = generateQuerySpecId(thisQuery.filter, thisQuery.querySort)
-        filterAsString = adapter.toJson(thisQuery.filter.toFilterNode())
+        filter_as_string = adapter.toJson(thisQuery.filter.toFilterNode())
         query_sort = thisQuery.querySort.toRealm()
         cids = thisQuery.cids.toRealmList()
     }
 }
 
 @OptIn(ExperimentalStdlibApi::class)
-internal fun QueryChannelsEntityRealm.toDomain(): QueryChannelsSpec {
-    val thisEntity = this
-
-    val adapter = Moshi.Builder()
+private fun filterNodeAdapter() =
+    Moshi.Builder()
         .addAdapter(FilterNodeAdapter())
         .build()
         .adapter(FilterNode::class.java)
 
+internal fun generateQuerySpecId(filter: FilterObject, querySort: QuerySorter<out Channel>): String {
+    return "${filter.hashCode()}-${querySort.toDto().hashCode()}"
+}
+
+internal fun QueryChannelsEntityRealm.toDomain(): QueryChannelsSpec {
+    val thisEntity = this
+
+    val adapter = filterNodeAdapter()
     val querySort: QuerySorter<Channel> = query_sort?.toDomain() ?: QuerySortByField.ascByName("name")
 
-    val filterAsString = filterAsString
-        ?.let(adapter::fromJson)?.toFilterObject()
+    val filterAsString = filter_as_string
+        ?.let(adapter::fromJson)
+        ?.toFilterObject()
         ?: Filters.neutral()
 
     return QueryChannelsSpec(
@@ -79,8 +82,4 @@ internal fun QueryChannelsEntityRealm.toDomain(): QueryChannelsSpec {
     ).apply {
         cids = thisEntity.cids.toSet()
     }
-}
-
-internal fun generateQuerySpecId(filter: FilterObject, querySort: QuerySorter<out Channel>): String {
-    return "${filter.hashCode()}-${querySort.toDto().hashCode()}"
 }
