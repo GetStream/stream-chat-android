a528874206807dcc7f0d2e2b4b4ba87661084702
MarinTolic <marin.t91@gmail.com>
2022-12-15 13:41:44 +0100

    [4598] Exchange the regex pattern for simple substring operations

--- stream-chat-android-state/src/main/java/io/getstream/chat/android/offline/extensions/internal/Attachment.kt
+++ stream-chat-android-state/src/main/java/io/getstream/chat/android/offline/extensions/internal/Attachment.kt
@@ -19,16 +19,16 @@ package io.getstream.chat.android.offline.extensions.internal
 import io.getstream.chat.android.client.models.Attachment
 
 /**
- * Uses a regex pattern to extract the file name from the attachment URL.
+ * Uses substrings to extract the file name from the attachment URL.
  *
  * Useful in situations where no attachment name or title has been provided and
  * we need a name in order to download the file to storage.
  */
 internal fun Attachment.getAttachmentFallbackName(): String? {
     val url = when (this.type) {
-        "image" -> this.imageUrl ?: this.thumbUrl ?: this.assetUrl
+        "image" -> this.imageUrl ?: this.assetUrl ?: this.thumbUrl
         else -> this.assetUrl ?: this.imageUrl ?: this.thumbUrl
     }
 
-    return url?.let { Regex("""[^/]+(?=\?)""").find(it)?.value }
+    return url?.substringAfterLast("/")?.substringBefore("?")
 }
