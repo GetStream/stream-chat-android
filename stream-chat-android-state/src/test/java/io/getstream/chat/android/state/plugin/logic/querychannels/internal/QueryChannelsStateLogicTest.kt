fdc7103334eeb4167467c3e6fa5f772b9c67abbb
Leandro Borges Ferreira <lehen01@gmail.com>
2023-01-13 21:10:17 +0100

    unit tests passing locally

--- stream-chat-android-state/src/test/java/io/getstream/chat/android/state/plugin/logic/querychannels/internal/QueryChannelsStateLogicTest.kt
+++ stream-chat-android-state/src/test/java/io/getstream/chat/android/state/plugin/logic/querychannels/internal/QueryChannelsStateLogicTest.kt
@@ -121,7 +121,7 @@ internal class QueryChannelsStateLogicTest {
 
         whenever(mutableState.queryChannelsSpec) doReturn queryChannelsSpec
 
-        queryChannelsStateLogic.addChannelsState(channels)
+        queryChannelsStateLogic.addChannelsState(channels, false)
 
         queryChannelsSpec.cids `should contain same` setOf(testCid, channel1.cid, channel2.cid)
         verify(mutableState).setChannels(channels.associateBy { it.cid })
