f1fcbcb2eabf00349ac36897d63f1c20cd5dda50
Leandro Borges Ferreira <lehen01@gmail.com>
2023-01-20 18:52:00 +0100

    Fixing identation

--- stream-chat-android-offline/src/test/java/io/getstream/chat/android/offline/repository/domain/channel/internal/DatabaseChannelRepositoryTest.kt
+++ stream-chat-android-offline/src/test/java/io/getstream/chat/android/offline/repository/domain/channel/internal/DatabaseChannelRepositoryTest.kt
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2014-2023 Stream.io Inc. All rights reserved.
+ *
+ * Licensed under the Stream License;
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    https://github.com/GetStream/stream-chat-android/blob/main/LICENSE
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package io.getstream.chat.android.offline.repository.domain.channel.internal
 
 import io.getstream.chat.android.client.test.randomChannel
@@ -43,9 +59,11 @@ internal class DatabaseChannelRepositoryTest {
 
         channelRepository.upsertChannel(backendChannel)
 
-        verify(channelDao).insert(argThat { channelEntity ->
-            channelEntity.lastMessageId == backendMessage.id
-        })
+        verify(channelDao).insert(
+            argThat { channelEntity ->
+                channelEntity.lastMessageId == backendMessage.id
+            }
+        )
     }
 
     @Test
@@ -68,9 +86,11 @@ internal class DatabaseChannelRepositoryTest {
 
         channelRepository.upsertChannel(backendChannel)
 
-        verify(channelDao).insert(argThat { channelEntity ->
-            channelEntity.lastMessageId == dbLastMessageId
-        })
+        verify(channelDao).insert(
+            argThat { channelEntity ->
+                channelEntity.lastMessageId == dbLastMessageId
+            }
+        )
     }
 
     @Test
@@ -93,8 +113,10 @@ internal class DatabaseChannelRepositoryTest {
 
         channelRepository.upsertChannel(backendChannel)
 
-        verify(channelDao).insert(argThat { channelEntity ->
-            channelEntity.lastMessageId == dbLastMessageId
-        })
+        verify(channelDao).insert(
+            argThat { channelEntity ->
+                channelEntity.lastMessageId == dbLastMessageId
+            }
+        )
     }
 }
