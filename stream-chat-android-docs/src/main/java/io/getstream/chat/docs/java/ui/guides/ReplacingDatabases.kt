cdbaf3c7a752a5ad3904ea592ff43bce21c4dfdc
Leandro Borges Ferreira <lehen01@gmail.com>
2023-01-31 12:32:54 +0100

    Fixing imports

--- stream-chat-android-docs/src/main/java/io/getstream/chat/docs/java/ui/guides/ReplacingDatabases.kt
+++ stream-chat-android-docs/src/main/java/io/getstream/chat/docs/java/ui/guides/ReplacingDatabases.kt
@@ -11,12 +11,19 @@ import io.getstream.chat.android.client.persistance.repository.ReactionRepositor
 import io.getstream.chat.android.client.persistance.repository.SyncStateRepository
 import io.getstream.chat.android.client.persistance.repository.UserRepository
 import io.getstream.chat.android.client.persistance.repository.factory.RepositoryFactory
-import io.getstream.chat.android.models.Channel
 import io.getstream.chat.android.models.Message
 import io.getstream.chat.android.models.User
 import io.getstream.chat.docs.java.ui.guides.realm.entities.ChannelEntityRealm
 import io.getstream.chat.docs.java.ui.guides.realm.entities.toDomain
 import io.getstream.chat.docs.java.ui.guides.realm.entities.toRealm
+import io.getstream.chat.docs.java.ui.guides.realm.repository.RealmAttachmentRepository
+import io.getstream.chat.docs.java.ui.guides.realm.repository.RealmChannelConfigRepository
+import io.getstream.chat.docs.java.ui.guides.realm.repository.RealmChannelRepository
+import io.getstream.chat.docs.java.ui.guides.realm.repository.RealmMessageRepository
+import io.getstream.chat.docs.java.ui.guides.realm.repository.RealmQueryChannelsRepository
+import io.getstream.chat.docs.java.ui.guides.realm.repository.RealmReactionRepository
+import io.getstream.chat.docs.java.ui.guides.realm.repository.RealmSyncStateRepository
+import io.getstream.chat.docs.java.ui.guides.realm.repository.RealmUserRepository
 import io.realm.kotlin.Realm
 import io.realm.kotlin.RealmConfiguration
 import io.realm.kotlin.UpdatePolicy
@@ -67,39 +74,5 @@ class ReplacingDatabases {
                 getMessage: suspend (messageId: String) -> Message?,
             ): ChannelRepository = RealmChannelRepository(realm)
         }
-    }
-
-    public class RealmChannelRepository(private val realm: Realm) : ChannelRepository {
-        override suspend fun clear() {
-            realm.writeBlocking {
-                query<ChannelEntityRealm>().find().let(this::delete)
-            }
-        }
-
-        override suspend fun deleteChannel(cid: String) {
-            val channel = realm.query<ChannelEntityRealm>("cid == '$cid'")
-                .first()
-                .find()
-
-            realm.writeBlocking {
-                channel?.let(::findLatest)?.let(::delete)
-            }
-        }
-
-        override suspend fun insertChannel(channel: Channel) {
-            realm.writeBlocking {
-                this.copyToRealm(channel.toRealm(), updatePolicy = UpdatePolicy.ALL)
-            }
-        }
-
-        override suspend fun selectChannelByCid(cid: String): Channel? =
-            selectChannelByCidRealm(cid)?.toDomain()
-
-        private fun selectChannelByCidRealm(cid: String): ChannelEntityRealm? =
-            realm.query<ChannelEntityRealm>("cid == '$cid'")
-                .first()
-                .find()
-
-        // All the other methods must be implemented, following this same approach.
     }
 }
