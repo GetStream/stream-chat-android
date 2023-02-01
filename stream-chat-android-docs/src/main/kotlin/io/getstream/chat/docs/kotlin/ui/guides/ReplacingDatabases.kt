f4f3e9b72872da29a36f7d33802209194188bc36
Leandro Borges Ferreira <lehen01@gmail.com>
2023-02-01 10:57:41 +0100

    Moving kotlin snipets to kotlin directory in docs module

---
+++ stream-chat-android-docs/src/main/kotlin/io/getstream/chat/docs/kotlin/ui/guides/ReplacingDatabases.kt
@@ -0,0 +1,113 @@
+package io.getstream.chat.docs.kotlin.ui.guides
+
+import androidx.fragment.app.Fragment
+import io.getstream.chat.android.client.ChatClient
+import io.getstream.chat.android.client.persistance.repository.AttachmentRepository
+import io.getstream.chat.android.client.persistance.repository.ChannelConfigRepository
+import io.getstream.chat.android.client.persistance.repository.ChannelRepository
+import io.getstream.chat.android.client.persistance.repository.MessageRepository
+import io.getstream.chat.android.client.persistance.repository.QueryChannelsRepository
+import io.getstream.chat.android.client.persistance.repository.ReactionRepository
+import io.getstream.chat.android.client.persistance.repository.SyncStateRepository
+import io.getstream.chat.android.client.persistance.repository.UserRepository
+import io.getstream.chat.android.client.persistance.repository.factory.RepositoryFactory
+import io.getstream.chat.android.models.Message
+import io.getstream.chat.android.models.User
+import io.getstream.chat.docs.kotlin.ui.guides.realm.entities.AttachmentEntityRealm
+import io.getstream.chat.docs.kotlin.ui.guides.realm.entities.ChannelEntityRealm
+import io.getstream.chat.docs.kotlin.ui.guides.realm.entities.ChannelUserReadEntityRealm
+import io.getstream.chat.docs.kotlin.ui.guides.realm.entities.CommandEntityRealm
+import io.getstream.chat.docs.kotlin.ui.guides.realm.entities.ConfigEntityRealm
+import io.getstream.chat.docs.kotlin.ui.guides.realm.entities.MemberEntityRealm
+import io.getstream.chat.docs.kotlin.ui.guides.realm.entities.MessageEntityRealm
+import io.getstream.chat.docs.kotlin.ui.guides.realm.entities.QueryChannelsEntityRealm
+import io.getstream.chat.docs.kotlin.ui.guides.realm.entities.QuerySorterInfoEntityRealm
+import io.getstream.chat.docs.kotlin.ui.guides.realm.entities.ReactionCountEntityRealm
+import io.getstream.chat.docs.kotlin.ui.guides.realm.entities.ReactionEntityRealm
+import io.getstream.chat.docs.kotlin.ui.guides.realm.entities.ReactionScoreEntityRealm
+import io.getstream.chat.docs.kotlin.ui.guides.realm.entities.SortSpecificationEntityRealm
+import io.getstream.chat.docs.kotlin.ui.guides.realm.entities.SyncStateEntityRealm
+import io.getstream.chat.docs.kotlin.ui.guides.realm.entities.UploadStateEntityRealm
+import io.getstream.chat.docs.kotlin.ui.guides.realm.entities.UserEntityRealm
+import io.getstream.chat.docs.kotlin.ui.guides.realm.repository.RealmAttachmentRepository
+import io.getstream.chat.docs.kotlin.ui.guides.realm.repository.RealmChannelConfigRepository
+import io.getstream.chat.docs.kotlin.ui.guides.realm.repository.RealmChannelRepository
+import io.getstream.chat.docs.kotlin.ui.guides.realm.repository.RealmMessageRepository
+import io.getstream.chat.docs.kotlin.ui.guides.realm.repository.RealmQueryChannelsRepository
+import io.getstream.chat.docs.kotlin.ui.guides.realm.repository.RealmReactionRepository
+import io.getstream.chat.docs.kotlin.ui.guides.realm.repository.RealmSyncStateRepository
+import io.getstream.chat.docs.kotlin.ui.guides.realm.repository.RealmUserRepository
+import io.realm.kotlin.Realm
+import io.realm.kotlin.RealmConfiguration
+import io.realm.kotlin.types.RealmObject
+import kotlin.reflect.KClass
+
+/**
+ * [Adding Custom Attachments](https://getstream.io/chat/docs/sdk/android/client/guides/replace-database/)
+ */
+class ReplacingDatabases {
+
+    /**
+     * [Using a custom RepositoryFactoryProvider](https://getstream.io/chat/docs/sdk/android/client/guides/replace-database/#using-a-custom-repositoryfactoryprovider)
+     */
+    class UsingACustomRepositoryFactoryProvider : Fragment() {
+
+        private fun configureRealm(): Realm =
+            RealmConfiguration.Builder(schema = realmSchema())
+                .schemaVersion(1)
+                .deleteRealmIfMigrationNeeded()
+                .build()
+                .let(Realm::open)
+
+        private fun realmSchema(): Set<KClass<out RealmObject>> =
+            setOf(
+                MessageEntityRealm::class,
+                ChannelEntityRealm::class,
+                UserEntityRealm::class,
+                QueryChannelsEntityRealm::class,
+                MemberEntityRealm::class,
+                ChannelUserReadEntityRealm::class,
+                ReactionEntityRealm::class,
+                ReactionCountEntityRealm::class,
+                ReactionScoreEntityRealm::class,
+                ConfigEntityRealm::class,
+                CommandEntityRealm::class,
+                QuerySorterInfoEntityRealm::class,
+                SortSpecificationEntityRealm::class,
+                SyncStateEntityRealm::class,
+                AttachmentEntityRealm::class,
+                UploadStateEntityRealm::class,
+            )
+
+        val client = ChatClient.Builder("api_key_here", requireContext())
+            .withRepositoryFactoryProvider { RealmRepositoryFactory(configureRealm()) }
+            .build()
+
+        class RealmRepositoryFactory(private val realm: Realm) : RepositoryFactory {
+            override fun createUserRepository(): UserRepository = RealmUserRepository(realm)
+
+            override fun createChannelConfigRepository(): ChannelConfigRepository = RealmChannelConfigRepository(realm)
+
+            override fun createQueryChannelsRepository(): QueryChannelsRepository = RealmQueryChannelsRepository(realm)
+
+            override fun createSyncStateRepository(): SyncStateRepository = RealmSyncStateRepository(realm)
+
+            override fun createAttachmentRepository(): AttachmentRepository = RealmAttachmentRepository(realm)
+
+            override fun createReactionRepository(
+                getUser: suspend (userId: String) -> User,
+            ): ReactionRepository = RealmReactionRepository(realm)
+
+            override fun createMessageRepository(
+                getUser: suspend (userId: String) -> User,
+            ): MessageRepository = RealmMessageRepository(realm)
+
+            override fun createChannelRepository(
+                getUser: suspend (userId: String) -> User,
+                getMessage: suspend (messageId: String) -> Message?,
+            ): ChannelRepository = RealmChannelRepository(realm)
+        }
+    }
+
+
+}
---
+++ stream-chat-android-docs/src/main/kotlin/io/getstream/chat/docs/kotlin/ui/guides/realm/repository/RealmReactionRepository.kt
@@ -0,0 +1,103 @@
+/*
+ * Copyright (c) 2014-2022 Stream.io Inc. All rights reserved.
+ *
+ * Licensed under the Stream License;
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    https://github.com/GetStream/stream-chat-android/blob/main/LICENSE
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.getstream.chat.docs.kotlin.ui.guides.realm.repository
+
+import io.getstream.chat.android.client.persistance.repository.ReactionRepository
+import io.getstream.chat.android.models.Reaction
+import io.getstream.chat.android.models.SyncStatus
+import io.getstream.chat.docs.kotlin.ui.guides.realm.entities.ReactionEntityRealm
+import io.getstream.chat.docs.kotlin.ui.guides.realm.entities.toDomain
+import io.getstream.chat.docs.kotlin.ui.guides.realm.entities.toRealm
+import io.getstream.chat.docs.kotlin.ui.guides.realm.utils.toRealmInstant
+import io.realm.kotlin.Realm
+import io.realm.kotlin.UpdatePolicy
+import io.realm.kotlin.ext.query
+import java.util.Date
+
+internal class RealmReactionRepository(private val realm: Realm) : ReactionRepository {
+
+    override suspend fun insertReaction(reaction: Reaction) {
+        realm.writeBlocking {
+            copyToRealm(reaction.toRealm(), UpdatePolicy.ALL)
+        }
+    }
+
+    override suspend fun updateReactionsForMessageByDeletedDate(userId: String, messageId: String, deletedAt: Date) {
+        val query = "message_id == '$messageId' AND user_id == '$userId' AND deleted_at == $0"
+        val entities = realm.query<ReactionEntityRealm>(query, deletedAt.toRealmInstant()).find()
+
+        realm.writeBlocking {
+            entities.forEach { reactionEntity ->
+                reactionEntity.deleted_at = deletedAt.toRealmInstant()
+                copyToRealm(reactionEntity, UpdatePolicy.ALL)
+            }
+        }
+    }
+
+    override suspend fun selectReactionById(id: Int): Reaction? =
+        realm.query<ReactionEntityRealm>("id == '$id'")
+            .first()
+            .find()
+            ?.toDomain()
+
+    override suspend fun selectReactionsByIds(ids: List<Int>): List<Reaction> {
+        val idsList = ids.joinToString(prefix = "{ ", postfix = " }")
+
+        return realm.query<ReactionEntityRealm>("id IN $idsList")
+            .find()
+            .map { reactionEntity -> reactionEntity.toDomain() }
+    }
+
+    override suspend fun selectReactionIdsBySyncStatus(syncStatus: SyncStatus): List<Int> =
+        realm.query<ReactionEntityRealm>("sync_status == ${syncStatus.status}")
+            .find()
+            .map { reactionEntity -> reactionEntity.id }
+
+    override suspend fun selectReactionsBySyncStatus(syncStatus: SyncStatus): List<Reaction> =
+        realm.query<ReactionEntityRealm>("sync_status == ${syncStatus.status}")
+            .find()
+            .map { reactionEntity -> reactionEntity.toDomain() }
+
+    override suspend fun selectUserReactionToMessage(
+        reactionType: String,
+        messageId: String,
+        userId: String,
+    ): Reaction? {
+        val query = "type == '$reactionType' AND message_id == '$messageId' AND user_id == '$userId'"
+
+        return realm.query<ReactionEntityRealm>(query)
+            .first()
+            .find()
+            ?.toDomain()
+    }
+
+    override suspend fun selectUserReactionsToMessage(messageId: String, userId: String): List<Reaction> {
+        val query = "message_id == '$messageId' AND user_id == '$userId'"
+
+        return realm.query<ReactionEntityRealm>(query)
+            .find()
+            .map { reactionEntity -> reactionEntity.toDomain() }
+    }
+
+    override suspend fun clear() {
+        val allReactions = realm.query<ReactionEntityRealm>().find()
+
+        realm.write {
+            delete(allReactions)
+        }
+    }
+}
