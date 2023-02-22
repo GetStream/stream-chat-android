a7ebf3016a2c329eef9e8d7908f0fa907c63a728
Tomislav Gazica <38032787+TGazica@users.noreply.github.com>
2023-02-22 15:59:48 +0100

    Version bump (#4693)

--- buildSrc/src/main/kotlin/io/getstream/chat/android/Configuration.kt
+++ buildSrc/src/main/kotlin/io/getstream/chat/android/Configuration.kt
@@ -6,7 +6,7 @@ object Configuration {
     const val sampleTargetSdk = 33
     const val minSdk = 21
     const val majorVersion = 5
-    const val minorVersion = 12
+    const val minorVersion = 13
     const val patchVersion = 0
     const val versionName = "$majorVersion.$minorVersion.$patchVersion"
     const val snapshotVersionName = "$majorVersion.$minorVersion.${patchVersion + 1}-SNAPSHOT"
