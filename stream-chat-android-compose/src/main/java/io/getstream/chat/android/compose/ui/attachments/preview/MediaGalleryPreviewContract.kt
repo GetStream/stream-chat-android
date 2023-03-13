6fa4310fd7b0c5f1e23019b6e466e1420f96c2c7
MarinTolic <marin.t91@gmail.com>
2023-03-13 16:48:09 +0100

    - [4722] Pass the Boolean value through

--- stream-chat-android-compose/src/main/java/io/getstream/chat/android/compose/ui/attachments/preview/MediaGalleryPreviewContract.kt
+++ stream-chat-android-compose/src/main/java/io/getstream/chat/android/compose/ui/attachments/preview/MediaGalleryPreviewContract.kt
@@ -43,6 +43,7 @@ public class MediaGalleryPreviewContract :
             attachmentPosition = input.initialPosition,
             videoThumbnailsEnabled = input.videoThumbnailsEnabled,
             streamCdnImageResizing = input.streamCdnImageResizing,
+            skipEnrichUrl = input.skipEnrichUrl
         )
     }
 
