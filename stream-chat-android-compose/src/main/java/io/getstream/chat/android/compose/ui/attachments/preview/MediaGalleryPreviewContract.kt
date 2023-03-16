fb74930ff9d2f1b50f29913399b01e7f2b0b53b8
Marin Tolić <37080097+MarinTolic@users.noreply.github.com>
2023-03-16 11:41:10 +0100

    Update stream-chat-android-compose/src/main/java/io/getstream/chat/android/compose/ui/attachments/preview/MediaGalleryPreviewContract.kt
    Co-authored-by: Jc Miñarro <josecarlos.minarro@gmail.com>

--- stream-chat-android-compose/src/main/java/io/getstream/chat/android/compose/ui/attachments/preview/MediaGalleryPreviewContract.kt
+++ stream-chat-android-compose/src/main/java/io/getstream/chat/android/compose/ui/attachments/preview/MediaGalleryPreviewContract.kt
@@ -43,7 +43,7 @@ public class MediaGalleryPreviewContract :
             attachmentPosition = input.initialPosition,
             videoThumbnailsEnabled = input.videoThumbnailsEnabled,
             streamCdnImageResizing = input.streamCdnImageResizing,
-            skipEnrichUrl = input.skipEnrichUrl
+            skipEnrichUrl = input.skipEnrichUrl,
         )
     }
 
