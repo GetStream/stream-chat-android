bfeb1c10b9117de257b4dc25636ca186a712567e
Leandro Borges Ferreira <lehen01@gmail.com>
2023-03-20 19:24:39 +0100

    Adding sketch of player

--- stream-chat-android-ui-components/src/main/kotlin/io/getstream/chat/android/ui/feature/messages/list/adapter/view/internal/AudioRecordPlayer.kt
+++ stream-chat-android-ui-components/src/main/kotlin/io/getstream/chat/android/ui/feature/messages/list/adapter/view/internal/AudioRecordPlayer.kt
@@ -5,6 +5,8 @@ import android.util.AttributeSet
 import android.view.Gravity
 import android.view.View
 import androidx.appcompat.widget.LinearLayoutCompat
+import androidx.core.view.isVisible
+import io.getstream.chat.android.ui.R
 import io.getstream.chat.android.ui.databinding.StreamUiAudioRecordPlayerBinding
 import io.getstream.chat.android.ui.utils.extensions.streamThemeInflater
 
@@ -29,5 +31,29 @@ public class AudioRecordPlayer : LinearLayoutCompat {
         }
     }
 
+    public fun setPlayCallBack(func: () -> Unit) {
+        playerView.playButton.setOnClickListener { func() }
+    }
+
+    public fun setLoading() {
+        playerView.loadingView.isVisible = true
+        playerView.playButton.isVisible = false
+    }
+
+    public fun setPlaying() {
+        playerView.loadingView.isVisible = false
+        playerView.playButton.run {
+            isVisible = true
+            setImageResource(R.drawable.stream_ui_ic_user_block)
+        }
+    }
+
+    public fun setIdle() {
+        playerView.loadingView.isVisible = false
+        playerView.playButton.run {
+            isVisible = true
+            setImageResource(R.drawable.stream_ui_ic_play)
+        }
+    }
 }
-    
+
