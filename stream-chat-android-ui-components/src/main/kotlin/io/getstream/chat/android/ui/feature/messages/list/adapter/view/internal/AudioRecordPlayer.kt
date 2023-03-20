2b666aeac8c4a203100709fddb9c3787712daf7c
Leandro Borges Ferreira <lehen01@gmail.com>
2023-03-20 17:37:58 +0100

    Showing basic audio recording view

--- stream-chat-android-ui-components/src/main/kotlin/io/getstream/chat/android/ui/feature/messages/list/adapter/view/internal/AudioRecordPlayer.kt
+++ stream-chat-android-ui-components/src/main/kotlin/io/getstream/chat/android/ui/feature/messages/list/adapter/view/internal/AudioRecordPlayer.kt
@@ -2,6 +2,7 @@ package io.getstream.chat.android.ui.feature.messages.list.adapter.view.internal
 
 import android.content.Context
 import android.util.AttributeSet
+import android.view.Gravity
 import androidx.appcompat.widget.LinearLayoutCompat
 import io.getstream.chat.android.ui.databinding.StreamUiAudioRecordPlayerBinding
 import io.getstream.chat.android.ui.utils.extensions.streamThemeInflater
@@ -13,10 +14,15 @@ public class AudioRecordPlayer : LinearLayoutCompat {
     public constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr)
 
     private val playerView =
-        StreamUiAudioRecordPlayerBinding.inflate(streamThemeInflater, this).also { addView(it.root) }
+        StreamUiAudioRecordPlayerBinding.inflate(streamThemeInflater, this)
+
+    init {
+        orientation = HORIZONTAL
+        gravity = Gravity.CENTER_VERTICAL
+    }
 
     public fun setDuration(duration: String) {
-        playerView.duration.text = duration;
+        playerView.duration.text = duration
     }
 
 }
--- stream-chat-android-ui-components/src/main/kotlin/io/getstream/chat/android/ui/feature/messages/list/adapter/viewholder/internal/MediaAttachmentsViewHolder.kt
+++ stream-chat-android-ui-components/src/main/kotlin/io/getstream/chat/android/ui/feature/messages/list/adapter/viewholder/internal/MediaAttachmentsViewHolder.kt
@@ -77,6 +77,7 @@ internal class MediaAttachmentsViewHolder(
             bindMediaAttachments()
             bindAudioRecordAttachments()
         }
+
         bindUploadingIndicator()
     }
 
