d9159b4e25a0c3f4328fbf099e94b646e1eeb99d
MarinTolic <marin.t91@gmail.com>
2023-03-28 16:54:56 +0200

    [4749] Implement a way to override the click lambdas when creating a list of default attachment factories

--- stream-chat-android-compose/src/main/java/io/getstream/chat/android/compose/ui/attachments/StreamAttachmentFactories.kt
+++ stream-chat-android-compose/src/main/java/io/getstream/chat/android/compose/ui/attachments/StreamAttachmentFactories.kt
@@ -16,8 +16,14 @@
 
 package io.getstream.chat.android.compose.ui.attachments
 
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
+import androidx.activity.compose.ManagedActivityResultLauncher
 import androidx.compose.foundation.Image
 import androidx.compose.ui.layout.ContentScale
+import io.getstream.chat.android.compose.state.mediagallerypreview.MediaGalleryPreviewResult
+import io.getstream.chat.android.compose.ui.attachments.content.onMediaAttachmentContentItemClicked
 import io.getstream.chat.android.compose.ui.attachments.factory.FileAttachmentFactory
 import io.getstream.chat.android.compose.ui.attachments.factory.GiphyAttachmentFactory
 import io.getstream.chat.android.compose.ui.attachments.factory.LinkAttachmentFactory
@@ -25,9 +31,15 @@ import io.getstream.chat.android.compose.ui.attachments.factory.MediaAttachmentF
 import io.getstream.chat.android.compose.ui.attachments.factory.QuotedAttachmentFactory
 import io.getstream.chat.android.compose.ui.attachments.factory.UnsupportedAttachmentFactory
 import io.getstream.chat.android.compose.ui.attachments.factory.UploadAttachmentFactory
+import io.getstream.chat.android.compose.ui.attachments.preview.MediaGalleryPreviewContract
+import io.getstream.chat.android.compose.ui.attachments.preview.handler.AttachmentPreviewHandler
 import io.getstream.chat.android.compose.ui.theme.StreamDimens
+import io.getstream.chat.android.models.Attachment
+import io.getstream.chat.android.models.Message
+import io.getstream.chat.android.ui.common.images.resizing.StreamCdnImageResizing
 import io.getstream.chat.android.ui.common.utils.GiphyInfoType
 import io.getstream.chat.android.ui.common.utils.GiphySizingMode
+import io.getstream.chat.android.uiutils.extension.isUploading
 
 /**
  * Provides different attachment factories that build custom message content based on a given attachment.
@@ -51,13 +63,16 @@ public object StreamAttachmentFactories {
      * [StreamDimens.attachmentsContentGiphyWidth] and [StreamDimens.attachmentsContentGiphyHeight]
      * dimensions, however you can still clip maximum dimensions using
      * [StreamDimens.attachmentsContentGiphyMaxWidth] and [StreamDimens.attachmentsContentGiphyMaxHeight].
-     *
      * Setting it to fixed size mode will make it respect all given dimensions.
      * @param contentScale Used to determine the way Giphys are scaled inside the [Image] composable.
      * @param skipEnrichUrl Used by the media gallery. If set to true will skip enriching URLs when you update the
      * message by deleting an attachment contained within it. Set to false by default.
-     * @param skipEnrichUrl If set to true will skip enriching URLs when you update the message
-     * by deleting an attachment contained within it. Set to false by default.
+     * @param onUploadContentItemClicked Lambda called when a uploading attachment content item gets clicked.
+     * @param onLinkContentItemClicked Lambda called when a link attachment content item gets clicked.
+     * @param onGiphyContentItemClick Lambda called when a giphy attachment content item gets clicked.
+     * @param onMediaContentItemClick Lambda called when a image or video attachment content item gets clicked.
+     * @param onFileContentItemClick Lambda called when a file attachment content item gets clicked.
+
      *
      * @return A [List] of various [AttachmentFactory] instances that provide different attachments support.
      */
@@ -67,18 +82,66 @@ public object StreamAttachmentFactories {
         giphySizingMode: GiphySizingMode = GiphySizingMode.ADAPTIVE,
         contentScale: ContentScale = ContentScale.Crop,
         skipEnrichUrl: Boolean = false,
+        onUploadContentItemClicked: (Attachment, List<AttachmentPreviewHandler>) -> Unit = { attachment, previewHandlers ->
+            if (!attachment.isUploading()) {
+                previewHandlers
+                    .firstOrNull { it.canHandle(attachment) }
+                    ?.handleAttachmentPreview(attachment)
+            }
+        },
+        onLinkContentItemClicked: (context: Context, previewUrl: String) -> Unit = { context, previewUrl ->
+            context.startActivity(
+                Intent(
+                    Intent.ACTION_VIEW,
+                    Uri.parse(previewUrl)
+                )
+            )
+        },
+        onGiphyContentItemClick: (context: Context, Url: String) -> Unit = { context, url ->
+            context.startActivity(
+                Intent(
+                    Intent.ACTION_VIEW,
+                    Uri.parse(url)
+                )
+            )
+        },
+        onMediaContentItemClick: (
+            mediaGalleryPreviewLauncher: ManagedActivityResultLauncher<MediaGalleryPreviewContract.Input, MediaGalleryPreviewResult?>,
+            message: Message,
+            attachmentPosition: Int,
+            videoThumbnailsEnabled: Boolean,
+            streamCdnImageResizing: StreamCdnImageResizing,
+            skipEnrichUrl: Boolean,
+        ) -> Unit = ::onMediaAttachmentContentItemClicked,
+        onFileContentItemClick: (
+            previewHandler: List<AttachmentPreviewHandler>,
+            attachment: Attachment,
+        ) -> Unit = { previewHandlers, attachment ->
+            previewHandlers
+                .firstOrNull { it.canHandle(attachment) }
+                ?.handleAttachmentPreview(attachment)
+        },
     ): List<AttachmentFactory> = listOf(
-        UploadAttachmentFactory(),
-        LinkAttachmentFactory(linkDescriptionMaxLines),
+        UploadAttachmentFactory(
+            onContentItemClicked = onUploadContentItemClicked
+        ),
+        LinkAttachmentFactory(
+            linkDescriptionMaxLines = linkDescriptionMaxLines,
+            onContentItemClicked = onLinkContentItemClicked,
+        ),
         GiphyAttachmentFactory(
             giphyInfoType = giphyInfoType,
             giphySizingMode = giphySizingMode,
             contentScale = contentScale,
+            onContentItemClick = onGiphyContentItemClick,
         ),
         MediaAttachmentFactory(
             skipEnrichUrl = skipEnrichUrl,
+            onContentItemClick = onMediaContentItemClick,
         ),
-        FileAttachmentFactory(),
+        FileAttachmentFactory(
+            onContentItemClick = onFileContentItemClick,
+        ),
         UnsupportedAttachmentFactory(),
     )
 
