e9c900236aed645ec39d5797540bf64883d8ee2a
Leandro Borges Ferreira <lehen01@gmail.com>
2023-03-28 15:41:11 +0200

    Documentation for AudioPlayer

--- stream-chat-android-client/src/main/java/io/getstream/chat/android/client/audio/AudioPlayer.kt
+++ stream-chat-android-client/src/main/java/io/getstream/chat/android/client/audio/AudioPlayer.kt
@@ -17,37 +17,99 @@
 package io.getstream.chat.android.client.audio
 
 @Suppress("TooManyFunctions")
+/**
+ * Audio player used to play audio messages.
+ */
 public interface AudioPlayer {
 
+    /**
+     * Subscribing for audio state changes for the audio of the hash
+     *
+     * @param hash the identifier of the audio track
+     * @param func The listener of the [AudioState] change.
+     */
     public fun onAudioStateChange(hash: Int, func: (AudioState) -> Unit)
 
+    /**
+     * Subscribing for progress changes for the audio of the hash. The progress is updated every 50ms
+     *
+     * @param hash the identifier of the audio track
+     * @param func The listener of the [ProgressData] change.
+     */
     public fun onProgressStateChange(hash: Int, func: (ProgressData) -> Unit)
 
+    /**
+     * Subscribing for speed changes for the audio of the hash.
+     *
+     * @param hash the identifier of the audio track
+     * @param func The listener of the speed change.
+     */
     public fun onSpeedChange(hash: Int, func: (Float) -> Unit)
 
+    /**
+     * Plays an audio track with sourceUrl.
+     *
+     * @param sourceUrl the URL of the audio track
+     * @param hash the identifier of the audio track
+     */
     public fun play(sourceUrl: String, audioHash: Int)
 
+    /**
+     * Pauses the current song.
+     */
     public fun pause()
 
-    public fun seekTo(msec: Int, audioHash: Int)
+    /**
+     * Seeks the audio track of the audio hash to the milli second position. If the hash is the same of the current
+     * playing audio track, the current audio track pauses.
+     *
+     * @param msec the position in milli seconds.
+     * @param hash the identifier of the audio track
+     */
+    public fun seekTo(msec: Int, hash: Int)
 
+    /**
+     * Informs the player that seek has started. This can be used to pause the current audio track when seek starts.
+     */
     public fun startSeek(audioHash: Int)
 
+    /**
+     * Changes the speed of reproduction. Options are 1x, 1.5x and 2x
+     */
     public fun changeSpeed()
 
+    /**
+     * Current speed of reproduction. Options are 1x, 1.5x and 2x
+     */
     public fun currentSpeed(): Float
 
+    /**
+     * Removes the current audios form the reproduction queue and removes the listeners
+     */
     public fun removeAudios(audioHashList: List<Int>)
 
+    /**
+     * Disposes the MediaPlayer and remove all audios.
+     */
     public fun dispose()
 }
 
+/**
+ * Progress data of the audio track.
+ */
 public data class ProgressData(public val duration: Int, public val progress: Double)
 
+/**
+ * State of the an audio track. When a song complete and another starts the current song goes to UNSET and the next one
+ * go to LOADING and PLAYING.
+ */
 public enum class AudioState {
     UNSET, LOADING, IDLE, PAUSE, PLAYING;
 }
 
+/**
+ * State of the AudioPlayer. This is the state of the whole player, not individual songs.
+ */
 public enum class PlayerState {
     UNSET, LOADING, IDLE, PAUSE, PLAYING;
 }
