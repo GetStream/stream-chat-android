87f272b32d054c6db63847a21f93a274622664e1
Leandro Borges Ferreira <lehen01@gmail.com>
2023-04-02 14:56:26 +0200

    Playing audio without UI feedback

--- stream-chat-android-compose/src/main/java/io/getstream/chat/android/compose/ui/attachments/content/AudioRecordAttachmentContent.kt
+++ stream-chat-android-compose/src/main/java/io/getstream/chat/android/compose/ui/attachments/content/AudioRecordAttachmentContent.kt
@@ -27,17 +27,16 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Card
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
-import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Slider
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import io.getstream.chat.android.client.ChatClient
 import io.getstream.chat.android.client.utils.attachment.isAudioRecording
 import io.getstream.chat.android.compose.R
 import io.getstream.chat.android.compose.state.messages.attachments.AttachmentState
@@ -53,6 +52,9 @@ public fun AudioRecordAttachmentContent(
     modifier: Modifier = Modifier,
     attachmentState: AttachmentState,
 ) {
+    val audioAttachment = attachmentState.message.attachments.firstOrNull { it.isAudioRecording() }
+    val audioPlayer = ChatClient.instance().audioPlayer
+
     Surface(
         modifier = modifier
             .padding(2.dp)
@@ -60,8 +62,6 @@ public fun AudioRecordAttachmentContent(
         color = ChatTheme.colors.appBackground,
         shape = ChatTheme.shapes.attachment
     ) {
-        val audioAttachment = attachmentState.message.attachments.firstOrNull { it.isAudioRecording() }
-
         //Todo remove this later!
         if (audioAttachment != null) {
             Row(
@@ -72,7 +72,14 @@ public fun AudioRecordAttachmentContent(
                 verticalAlignment = Alignment.CenterVertically
             ) {
                 Card(elevation = 2.dp, shape = CircleShape) {
-                    IconButton(onClick = {}, Modifier.width(36.dp).height(36.dp)) {
+                    IconButton(onClick = {
+                        audioAttachment.assetUrl?.let { trackUrl ->
+                            audioPlayer.play(trackUrl, audioAttachment.hashCode())
+                        }
+                    },
+                        modifier = Modifier
+                            .width(36.dp)
+                            .height(36.dp)) {
                         Icon(
                             painter = painterResource(id = R.drawable.stream_compose_ic_play),
                             contentDescription = null,
