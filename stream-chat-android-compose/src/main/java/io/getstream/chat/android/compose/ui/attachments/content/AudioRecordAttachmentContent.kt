35fd19f3be2ba2e0e49580ea561e0f270bd09bfe
Leandro Borges Ferreira <lehen01@gmail.com>
2023-04-02 14:51:48 +0200

    Showing audio player without behaviour

--- stream-chat-android-compose/src/main/java/io/getstream/chat/android/compose/ui/attachments/content/AudioRecordAttachmentContent.kt
+++ stream-chat-android-compose/src/main/java/io/getstream/chat/android/compose/ui/attachments/content/AudioRecordAttachmentContent.kt
@@ -16,23 +16,31 @@
 
 package io.getstream.chat.android.compose.ui.attachments.content
 
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.Card
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Slider
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
+import io.getstream.chat.android.client.utils.attachment.isAudioRecording
 import io.getstream.chat.android.compose.R
+import io.getstream.chat.android.compose.state.messages.attachments.AttachmentState
 import io.getstream.chat.android.compose.ui.theme.ChatTheme
 
 /**
@@ -41,7 +49,10 @@ import io.getstream.chat.android.compose.ui.theme.ChatTheme
  * @param modifier Modifier for styling.
  */
 @Composable
-public fun AudioRecordAttachmentContent(modifier: Modifier = Modifier) {
+public fun AudioRecordAttachmentContent(
+    modifier: Modifier = Modifier,
+    attachmentState: AttachmentState,
+) {
     Surface(
         modifier = modifier
             .padding(2.dp)
@@ -49,22 +60,62 @@ public fun AudioRecordAttachmentContent(modifier: Modifier = Modifier) {
         color = ChatTheme.colors.appBackground,
         shape = ChatTheme.shapes.attachment
     ) {
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .height(50.dp)
-                .padding(8.dp),
-            verticalAlignment = Alignment.CenterVertically
-        ) {
+        val audioAttachment = attachmentState.message.attachments.firstOrNull { it.isAudioRecording() }
+
+        //Todo remove this later!
+        if (audioAttachment != null) {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(50.dp)
+                    .padding(8.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Card(elevation = 2.dp, shape = CircleShape) {
+                    IconButton(onClick = {}, Modifier.width(36.dp).height(36.dp)) {
+                        Icon(
+                            painter = painterResource(id = R.drawable.stream_compose_ic_play),
+                            contentDescription = null,
+                            tint = Color.Black,
+                        )
+                    }
+                }
+
+                Text(text = "00:00",
+                    Modifier
+                        .fillMaxHeight()
+                        .padding(8.dp))
+
+                Slider(value = 0F, onValueChange = {}, modifier = Modifier.weight(1F))
+
+                Icon(
+                    painter = painterResource(id = R.drawable.stream_compose_ic_file_mp3),
+                    contentDescription = "MP3 file",
+                    modifier = Modifier.size(24.dp)
+                )
+            }
+        } else {
             Text(
                 modifier = Modifier
                     .fillMaxWidth()
                     .padding(start = 16.dp, end = 8.dp),
-                text = "Place holder for audio",
-                style = ChatTheme.typography.bodyBold,
-                overflow = TextOverflow.Ellipsis,
-                color = ChatTheme.colors.textHighEmphasis
+                text = "Not an audio attachment!!",
             )
         }
     }
 }
+
+// @Preview(showSystemUi = true, showBackground = true)
+// @Composable
+// internal fun DefaultPreview() {
+//     val attachment = Attachment(type = "audio_recording")
+//
+//     val attachmentState = AttachmentState(Message(attachments = mutableListOf(attachment)))
+//
+//     AudioRecordAttachmentContent(
+//         Modifier
+//             .width(200.dp)
+//             .height(60.dp),
+//         attachmentState,
+//     )
+// }
