4105d0590a67a8fc8f86b2bac62c5c8975713e90
kanat <unknown>
2023-05-19 14:04:54 -0700

    [4823] add kdocs, changelog, docs
    (cherry picked from commit 75909cf7469c8fb9e265d2a5a5078490a8f4f195)
    
    # Conflicts:
    #	CHANGELOG.md

--- stream-chat-android-client/src/main/java/io/getstream/chat/android/client/debugger/SendMessageDebugger.kt
+++ stream-chat-android-client/src/main/java/io/getstream/chat/android/client/debugger/SendMessageDebugger.kt
@@ -3,13 +3,57 @@ package io.getstream.chat.android.client.debugger
 import io.getstream.chat.android.client.models.Message
 import io.getstream.chat.android.client.utils.Result
 
+/**
+ * Debugs the sending message flow.
+ */
 public interface SendMessageDebugger {
+    /**
+     * Called when the sending of a message starts.
+     *
+     * @param message The message being sent.
+     */
     public fun onStart(message: Message) {}
+
+    /**
+     * Called when the interception of a message starts.
+     *
+     * @param message The message being intercepted.
+     */
     public fun onInterceptionStart(message: Message) {}
+
+    /**
+     * Called when an intercepted message is updated during interception.
+     *
+     * @param message The updated message.
+     */
     public fun onInterceptionUpdate(message: Message) {}
+
+    /**
+     * Called when the interception of a message is stopped and the result is available.
+     *
+     * @param result The result of the intercepted message.
+     */
     public fun onInterceptionStop(result: Result<Message>) {}
+
+    /**
+     * Called when the sending of a message starts after interception.
+     *
+     * @param message The message being sent.
+     */
     public fun onSendStart(message: Message) {}
+
+    /**
+     * Called when the sending of a message is stopped and the result is available.
+     *
+     * @param result The result of the sent message.
+     */
     public fun onSendStop(result: Result<Message>) {}
+
+    /**
+     * Called when the sending of a message is completely stopped and the final result is available.
+     *
+     * @param result The final result of the message.
+     */
     public fun onStop(result: Result<Message>) {}
 }
 
