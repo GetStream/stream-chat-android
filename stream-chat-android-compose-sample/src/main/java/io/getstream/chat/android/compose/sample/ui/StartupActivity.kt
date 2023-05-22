9381bc0b1ad3400f2dbabd6911e9d34b4968cf15
Jc Miñarro <josecarlos.minarro@gmail.com>
2023-05-22 10:56:27 +0200

    Fix login process in our sample app

--- stream-chat-android-compose-sample/src/main/java/io/getstream/chat/android/compose/sample/ui/StartupActivity.kt
+++ stream-chat-android-compose-sample/src/main/java/io/getstream/chat/android/compose/sample/ui/StartupActivity.kt
@@ -21,9 +21,11 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.TaskStackBuilder
+import androidx.lifecycle.lifecycleScope
 import io.getstream.chat.android.compose.sample.ChatApp
 import io.getstream.chat.android.compose.sample.ChatHelper
 import io.getstream.chat.android.compose.sample.ui.login.UserLoginActivity
+import kotlinx.coroutines.launch
 
 /**
  * An Activity without UI responsible for startup routing. It navigates the user to
@@ -38,37 +40,39 @@ class StartupActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        val userCredentials = ChatApp.credentialsRepository.loadUserCredentials()
-        if (userCredentials != null) {
-            // Ensure that the user is connected
-            ChatHelper.connectUser(userCredentials)
+        lifecycleScope.launch {
+            val userCredentials = ChatApp.credentialsRepository.loadUserCredentials()
+            if (userCredentials != null) {
+                // Ensure that the user is connected
+                ChatHelper.connectUser(userCredentials)
 
-            if (intent.hasExtra(KEY_CHANNEL_ID)) {
-                // Navigating from push, route to the messages screen
-                val channelId = requireNotNull(intent.getStringExtra(KEY_CHANNEL_ID))
-                val messageId = intent.getStringExtra(KEY_MESSAGE_ID)
-                val parentMessageId = intent.getStringExtra(KEY_PARENT_MESSAGE_ID)
+                if (intent.hasExtra(KEY_CHANNEL_ID)) {
+                    // Navigating from push, route to the messages screen
+                    val channelId = requireNotNull(intent.getStringExtra(KEY_CHANNEL_ID))
+                    val messageId = intent.getStringExtra(KEY_MESSAGE_ID)
+                    val parentMessageId = intent.getStringExtra(KEY_PARENT_MESSAGE_ID)
 
-                TaskStackBuilder.create(this)
-                    .addNextIntent(ChannelsActivity.createIntent(this))
-                    .addNextIntent(
-                        MessagesActivity.createIntent(
-                            context = this,
-                            channelId = channelId,
-                            messageId = messageId,
-                            parentMessageId = parentMessageId
+                    TaskStackBuilder.create(this@StartupActivity)
+                        .addNextIntent(ChannelsActivity.createIntent(this@StartupActivity))
+                        .addNextIntent(
+                            MessagesActivity.createIntent(
+                                context = this@StartupActivity,
+                                channelId = channelId,
+                                messageId = messageId,
+                                parentMessageId = parentMessageId
+                            )
                         )
-                    )
-                    .startActivities()
+                        .startActivities()
+                } else {
+                    // Logged in, navigate to the channels screen
+                    startActivity(ChannelsActivity.createIntent(this@StartupActivity))
+                }
             } else {
-                // Logged in, navigate to the channels screen
-                startActivity(ChannelsActivity.createIntent(this))
+                // Not logged in, start with the login screen
+                startActivity(UserLoginActivity.createIntent(this@StartupActivity))
             }
-        } else {
-            // Not logged in, start with the login screen
-            startActivity(UserLoginActivity.createIntent(this))
+            finish()
         }
-        finish()
     }
 
     companion object {
