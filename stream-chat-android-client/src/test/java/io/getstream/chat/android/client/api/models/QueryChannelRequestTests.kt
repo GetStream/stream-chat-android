d1ff26743f906f2f047d498f6b23e05671fc0315
kanat <unknown>
2023-06-23 15:03:05 -0700

    [i82] fix tests

--- stream-chat-android-client/src/test/java/io/getstream/chat/android/client/api/models/QueryChannelRequestTests.kt
+++ stream-chat-android-client/src/test/java/io/getstream/chat/android/client/api/models/QueryChannelRequestTests.kt
@@ -16,25 +16,25 @@
 
 package io.getstream.chat.android.client.api.models
 
+import io.getstream.chat.android.test.randomInt
 import org.amshove.kluent.`should be equal to`
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.params.ParameterizedTest
 import org.junit.jupiter.params.provider.MethodSource
-import org.mockito.kotlin.any
 import java.util.UUID
 
 internal class QueryChannelRequestTests {
 
     @Test
     fun `ensure 'withMembers' sets 'state' to True`() {
-        QueryChannelRequest().withMembers(any(), any()).apply {
+        QueryChannelRequest().withMembers(randomInt(), randomInt()).apply {
             state `should be equal to` true
         }
     }
 
     @Test
     fun `ensure 'withMessages' sets 'state' to True`() {
-        QueryChannelRequest().withMessages(any()).apply {
+        QueryChannelRequest().withMessages(randomInt()).apply {
             state `should be equal to` true
         }
     }
@@ -43,14 +43,14 @@ internal class QueryChannelRequestTests {
     @MethodSource("generatePaginationList")
     fun `ensure paginated 'withMessages' sets 'state' to True`(pagination: Pagination) {
         val messageId = UUID.randomUUID().toString()
-        QueryChannelRequest().withMessages(pagination, messageId, any()).apply {
+        QueryChannelRequest().withMessages(pagination, messageId, randomInt()).apply {
             state `should be equal to` true
         }
     }
 
     @Test
     fun `ensure 'withWatchers' sets 'state' to True`() {
-        QueryChannelRequest().withWatchers(any(), any()).apply {
+        QueryChannelRequest().withWatchers(randomInt(), randomInt()).apply {
             state `should be equal to` true
         }
     }
