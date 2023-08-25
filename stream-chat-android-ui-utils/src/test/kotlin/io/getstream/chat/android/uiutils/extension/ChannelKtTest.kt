fd4ea41e2f119d87e8ac00157b04eb62ea77b426
kanat <>
2023-08-25 16:35:07 -0700

    (ui-tests) fix random failures of ChannelKtTest

--- stream-chat-android-ui-utils/src/test/kotlin/io/getstream/chat/android/uiutils/extension/ChannelKtTest.kt
+++ stream-chat-android-ui-utils/src/test/kotlin/io/getstream/chat/android/uiutils/extension/ChannelKtTest.kt
@@ -63,14 +63,27 @@ internal class ChannelKtTest {
     @ParameterizedTest
     @MethodSource("arguments")
     fun `Should return proper channel name`(
+        argSetNum: Int,
         channel: Channel,
         maxMembers: Int,
         expectedName: String,
     ) {
-        channel.getDisplayName(context, currentUser, fallbackResource, maxMembers) `should be equal to` expectedName
+        try {
+            channel.getDisplayName(context, currentUser, fallbackResource, maxMembers) `should be equal to` expectedName
+        } catch (e: Throwable) {
+            System.err.println("Failed on test #$argSetNum")
+            throw e
+        }
     }
 
     companion object {
+
+        private const val ARG_SET_1 = 1
+        private const val ARG_SET_2 = 2
+        private const val ARG_SET_3 = 3
+        private const val ARG_SET_4 = 4
+        private const val ARG_SET_5 = 5
+        private const val ARG_SET_6 = 6
 
         private val context: Context = mock()
         private val fallbackResource: Int = positiveRandomInt()
@@ -79,9 +92,17 @@ internal class ChannelKtTest {
 
         @JvmStatic
         fun arguments() = listOf(
-            randomChannel().let { Arguments.of(it, positiveRandomInt(), it.name) },
+            randomChannel().let {
+                Arguments.of(
+                    ARG_SET_1,
+                    it,
+                    positiveRandomInt(),
+                    it.name,
+                )
+            },
             randomUser().let {
                 Arguments.of(
+                    ARG_SET_2,
                     randomChannel(name = "", members = listOf(randomMember(user = it))),
                     positiveRandomInt(),
                     it.name,
@@ -89,14 +110,17 @@ internal class ChannelKtTest {
             },
             List(positiveRandomInt(4)) { randomUser() }.let {
                 Arguments.of(
+                    ARG_SET_3,
                     randomChannel(name = "", members = it.map { user -> randomMember(user = user) }),
                     4,
-                    it.joinToString(", ") { user -> user.name },
+                    it.sortedBy(User::name)
+                        .joinToString(", ") { user -> user.name },
                 )
             },
             List(5) { randomUser() }.let {
                 val maxMembers = positiveRandomInt(4)
                 Arguments.of(
+                    ARG_SET_4,
                     randomChannel(name = "", members = it.map { user -> randomMember(user = user) }),
                     maxMembers,
                     "${
@@ -107,6 +131,7 @@ internal class ChannelKtTest {
                 )
             },
             Arguments.of(
+                ARG_SET_5,
                 randomChannel(
                     name = "",
                     members = listOf(
@@ -117,6 +142,7 @@ internal class ChannelKtTest {
                 currentUser.name,
             ),
             Arguments.of(
+                ARG_SET_6,
                 randomChannel(
                     name = "",
                     members = emptyList(),
