9cb15069f717f8c470e1dbf704ecbc1ad6421613
kanat <kijalbaev@gmail.com>
2023-09-29 19:51:19 -0700

    add caching to date parser

--- stream-chat-android-client/src/main/java/io/getstream/chat/android/client/parser2/adapters/internal/StreamDateFormatter.kt
+++ stream-chat-android-client/src/main/java/io/getstream/chat/android/client/parser2/adapters/internal/StreamDateFormatter.kt
@@ -16,20 +16,28 @@
 
 package io.getstream.chat.android.client.parser2.adapters.internal
 
+import androidx.collection.LruCache
 import io.getstream.chat.android.client.utils.threadLocal
 import io.getstream.chat.android.core.internal.InternalStreamChatApi
+import io.getstream.logging.StreamLog
 import java.text.SimpleDateFormat
 import java.util.Date
 import java.util.Locale
 import java.util.TimeZone
+import java.util.concurrent.atomic.AtomicInteger
 
 /**
  * This class handles parse and format date in the standard way of Stream.
  */
 @InternalStreamChatApi
-public class StreamDateFormatter {
+public class StreamDateFormatter(
+    private val src: String? = null,
+    private val cacheEnabled: Boolean = false
+) {
 
     private companion object {
+        const val TAG = "StreamDateFormatter"
+        const val STATS = true
         const val DATE_FORMAT = "yyyy-MM-dd'T'HH:mm:ss.SSS'Z'"
         const val DATE_FORMAT_WITHOUT_NANOSECONDS = "yyyy-MM-dd'T'HH:mm:ss'Z'"
     }
@@ -48,10 +56,35 @@ public class StreamDateFormatter {
 
     internal val datePattern = DATE_FORMAT
 
+    private val cache by lazy { LruCache<String, Date>(300) }
+    private val cacheHit = AtomicInteger()
+    private val allRequests = AtomicInteger()
+
     /**
      * Parses the [String] to [Date] in the standard way to Stream's API
      */
     internal fun parse(rawValue: String): Date? {
+        logCacheHitStats()
+        return parseInternal(rawValue)
+    }
+
+    private fun logCacheHitStats() {
+        val allRequests = allRequests.get()
+        val cacheHit = cacheHit.get()
+        if (STATS && cacheEnabled && allRequests % 100 == 0) {
+            val hitRate = cacheHit.toDouble() / allRequests
+            val hitRatePercent = (hitRate * 10000).toInt() / 100f
+            StreamLog.v(TAG) { "[parse] cache hit rate($src): $hitRatePercent% ($cacheHit / $allRequests)" }
+        }
+    }
+
+    private fun parseInternal(rawValue: String): Date? {
+        allRequests.incrementAndGet()
+        val cachedValue = fetchFromCache(rawValue)
+        if (cachedValue != null) {
+            cacheHit.incrementAndGet()
+            return cachedValue
+        }
         return if (rawValue.isEmpty()) {
             null
         } else {
@@ -64,7 +97,20 @@ public class StreamDateFormatter {
                     null
                 }
             }
+        }.also {
+            saveToCache(rawValue, it)
         }
+    }
+
+    private fun fetchFromCache(rawValue: String): Date? {
+        if (!cacheEnabled) return null
+        return cache[rawValue]
+    }
+
+    private fun saveToCache(rawValue: String, date: Date?) {
+        if (!cacheEnabled) return
+        if (date == null) return
+        cache.put(rawValue, date)
     }
 
     /**
