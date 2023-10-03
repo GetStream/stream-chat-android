f96760ff0e9d58a04a188c974f58caaf21da9d9a
Jc Miñarro <josecarlos.minarro@gmail.com>
2023-10-03 18:06:41 +0200

    Fix Thread Loading (#4971)
    * Fix Thread loading when `endOfOlderMessages` has been updated
    
    * Avoid multiple calls to load more messages in a thread while a previous query is running
    
    ---------
    
    Co-authored-by: Kanat Kiialbaev <kijalbaev@gmail.com>

--- stream-chat-android-ui-common/src/main/kotlin/io/getstream/chat/android/ui/common/feature/messages/list/MessageListController.kt
+++ stream-chat-android-ui-common/src/main/kotlin/io/getstream/chat/android/ui/common/feature/messages/list/MessageListController.kt
@@ -520,6 +520,20 @@ public class MessageListController(
         reads: StateFlow<List<ChannelUserRead>>,
     ) {
         threadJob = scope.launch {
+            user.onEach {
+                _threadListState.value = _threadListState.value.copy(currentUser = it)
+            }.launchIn(this)
+
+            endOfOlderMessages.onEach {
+                _threadListState.value = _threadListState.value.copy(
+                    endOfOldMessagesReached = it,
+                    isLoadingOlderMessages = when {
+                        it -> false
+                        else -> _threadListState.value.isLoadingOlderMessages
+                    },
+                )
+            }.launchIn(this)
+
             combine(
                 messages,
                 reads,
@@ -575,20 +589,6 @@ public class MessageListController(
                 _threadListState.value = newState.copy(newMessageState = newMessageState)
                 if (newMessageState != null) lastLoadedThreadMessage = newLastMessage
             }
-
-            user.onEach {
-                _messageListState.value = _threadListState.value.copy(currentUser = it)
-            }.launchIn(this)
-
-            endOfOlderMessages.onEach {
-                _threadListState.value = _threadListState.value.copy(endOfOldMessagesReached = it)
-            }.launchIn(this)
-
-            // TODO separate unreads to message list unreads and thread unreads after
-            //  https://github.com/GetStream/stream-chat-android/pull/4122 has been merged in
-            unreadCount.onEach {
-                _threadListState.value = _messageListState.value.copy(unreadCount = it)
-            }.launchIn(scope)
         }
     }
 
@@ -850,19 +850,20 @@ public class MessageListController(
      * @param messageLimit The size of the message list page to load.
      */
     private fun threadLoadMore(threadMode: MessageMode.MessageThread, messageLimit: Int = this.messageLimit) {
-        if (_threadListState.value.endOfOldMessagesReached) return
+        if (_threadListState.value.endOfOldMessagesReached ||
+            _threadListState.value.isLoadingOlderMessages ||
+            threadMode.threadState?.oldestInThread?.value == null
+        ) {
+            return
+        }
 
-        if (threadMode.threadState != null) {
-            chatClient.getRepliesMore(
-                messageId = threadMode.parentMessage.id,
-                firstId = threadMode.threadState.oldestInThread.value?.id ?: threadMode.parentMessage.id,
-                limit = messageLimit,
-            ).enqueue {
-                _threadListState.value = _threadListState.value.copy(isLoadingOlderMessages = false)
-            }
-            _threadListState.value = _threadListState.value.copy(isLoadingOlderMessages = true)
-        } else {
-            logger.w { "Thread state must be not null for offline plugin thread load more!" }
+        _threadListState.value = _threadListState.value.copy(isLoadingOlderMessages = true)
+        chatClient.getRepliesMore(
+            messageId = threadMode.parentMessage.id,
+            firstId = threadMode.threadState.oldestInThread.value?.id ?: threadMode.parentMessage.id,
+            limit = messageLimit,
+        ).enqueue {
+            _threadListState.value = _threadListState.value.copy(isLoadingOlderMessages = false)
         }
     }
 
