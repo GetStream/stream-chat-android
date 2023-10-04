508baeed5df678ec53b923ba5b6c2f2b707e9a6c
skydoves <skydoves@naver.com>
2023-10-04 12:34:17 +0900

    Update target & compile SDK level to 34

--- buildSrc/src/main/kotlin/io/getstream/chat/android/Configuration.kt
+++ buildSrc/src/main/kotlin/io/getstream/chat/android/Configuration.kt
@@ -1,9 +1,9 @@
 package io.getstream.chat.android
 
 object Configuration {
-    const val compileSdk = 33
-    const val targetSdk = 32
-    const val sampleTargetSdk = 33
+    const val compileSdk = 34
+    const val targetSdk = 34
+    const val sampleTargetSdk = 34
     const val minSdk = 21
     const val majorVersion = 6
     const val minorVersion = 0
