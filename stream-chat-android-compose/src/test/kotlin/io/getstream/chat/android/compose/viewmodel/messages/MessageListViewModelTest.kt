6994e22f225b9e888867e61c7c6d6ad874f98ebd
Jc Miñarro <josecarlos.minarro@gmail.com>
2023-10-31 12:29:36 +0100

    Fix Tests

--- stream-chat-android-compose/src/test/kotlin/io/getstream/chat/android/compose/viewmodel/messages/MessageListViewModelTest.kt
+++ stream-chat-android-compose/src/test/kotlin/io/getstream/chat/android/compose/viewmodel/messages/MessageListViewModelTest.kt
@@ -28,6 +28,7 @@ import io.getstream.chat.android.models.MessagesState
 import io.getstream.chat.android.models.Reaction
 import io.getstream.chat.android.models.TypingEvent
 import io.getstream.chat.android.models.User
+import io.getstream.chat.android.randomInt
 import io.getstream.chat.android.state.plugin.config.StatePluginConfig
 import io.getstream.chat.android.state.plugin.factory.StreamStatePluginFactory
 import io.getstream.chat.android.state.plugin.internal.StatePlugin
@@ -158,6 +159,8 @@ internal class MessageListViewModelTest {
                 whenever(it.channelData) doReturn MutableStateFlow(channelData)
                 whenever(it.channelConfig) doReturn MutableStateFlow(Config())
                 whenever(it.members) doReturn MutableStateFlow(listOf())
+                whenever(it.membersCount) doReturn MutableStateFlow(randomInt())
+                whenever(it.watcherCount) doReturn MutableStateFlow(randomInt())
                 whenever(it.messagesState) doReturn MutableStateFlow(messageState)
                 whenever(it.typing) doReturn MutableStateFlow(TypingEvent(channelId, emptyList()))
                 whenever(it.reads) doReturn MutableStateFlow(listOf())
