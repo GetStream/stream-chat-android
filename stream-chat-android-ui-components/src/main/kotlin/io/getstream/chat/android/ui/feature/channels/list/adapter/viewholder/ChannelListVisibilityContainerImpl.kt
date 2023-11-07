200b6f644003e2207a1cc9ac8fc504042e8182a2
kanat <kijalbaev@gmail.com>
2023-11-07 15:32:04 -0800

    [i209] allow override channel options visibility

--- stream-chat-android-ui-components/src/main/kotlin/io/getstream/chat/android/ui/feature/channels/list/adapter/viewholder/ChannelListVisibilityContainerImpl.kt
+++ stream-chat-android-ui-components/src/main/kotlin/io/getstream/chat/android/ui/feature/channels/list/adapter/viewholder/ChannelListVisibilityContainerImpl.kt
@@ -16,12 +16,13 @@
 
 package io.getstream.chat.android.ui.feature.channels.list.adapter.viewholder
 
+import io.getstream.chat.android.models.ChannelCapabilities
 import io.getstream.chat.android.ui.feature.channels.list.ChannelListView.ChannelOptionVisibilityPredicate
 import io.getstream.chat.android.ui.utils.ListenerDelegate
 
 internal class ChannelListVisibilityContainerImpl(
-    isMoreOptionsVisible: ChannelOptionVisibilityPredicate = ChannelOptionVisibilityPredicate.DEFAULT,
-    isDeleteOptionVisible: ChannelOptionVisibilityPredicate = ChannelOptionVisibilityPredicate.DEFAULT,
+    isMoreOptionsVisible: ChannelOptionVisibilityPredicate = moreOptionsDefault,
+    isDeleteOptionVisible: ChannelOptionVisibilityPredicate = deleteOptionDefault,
 ) : ChannelListVisibilityContainer {
 
     override var isMoreOptionsVisible: ChannelOptionVisibilityPredicate by ListenerDelegate(isMoreOptionsVisible) { realPredicate ->
@@ -33,6 +34,18 @@ internal class ChannelListVisibilityContainerImpl(
     override var isDeleteOptionVisible: ChannelOptionVisibilityPredicate by ListenerDelegate(isDeleteOptionVisible) { realPredicate ->
         ChannelOptionVisibilityPredicate { channel ->
             realPredicate().invoke(channel)
+        }
+    }
+
+    private companion object {
+        val moreOptionsDefault: ChannelOptionVisibilityPredicate = ChannelOptionVisibilityPredicate {
+            // "more options" is visible by default
+            true
+        }
+
+        val deleteOptionDefault: ChannelOptionVisibilityPredicate = ChannelOptionVisibilityPredicate {
+            // "delete option" is visible if the channel's ownCapabilities contains the delete capability
+            it.ownCapabilities.contains(ChannelCapabilities.DELETE_CHANNEL)
         }
     }
 }
