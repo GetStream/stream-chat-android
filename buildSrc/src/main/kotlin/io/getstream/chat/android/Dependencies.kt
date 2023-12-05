31ced4e53f365d6c4127d8fca6d343b090a5b602
Jc Miñarro <josecarlos.minarro@gmail.com>
2023-12-05 23:27:34 +0100

    Upgrade Stream Push dependency to version 1.1.7 (#5102)

--- buildSrc/src/main/kotlin/io/getstream/chat/android/Dependencies.kt
+++ buildSrc/src/main/kotlin/io/getstream/chat/android/Dependencies.kt
@@ -74,7 +74,7 @@ object Versions {
     internal const val SPOTLESS = "6.20.0"
     internal const val STFALCON_IMAGE_VIEWER = "1.0.1"
     internal const val STREAM_LOG = "1.1.4"
-    internal const val STREAM_PUSH = "1.1.6"
+    internal const val STREAM_PUSH = "1.1.7"
     internal const val STREAM_RESULT = "1.1.0"
     internal const val TEST_PARAMETER_INJECTOR = "1.12"
     internal const val THREETENBP = "1.6.8"
