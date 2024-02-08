2da23051b83b0de579104a5c1c64528cb1c0c295
Liviu Timar <65943217+liviu-timar@users.noreply.github.com>
2024-02-08 16:21:11 +0200

    Add Custom Message List recipe text

--- stream-chat-android-docs/src/main/kotlin/io/getstream/chat/docs/kotlin/cookbook/ui/CustomMessageListViewModel.kt
+++ stream-chat-android-docs/src/main/kotlin/io/getstream/chat/docs/kotlin/cookbook/ui/CustomMessageListViewModel.kt
@@ -1,13 +1,16 @@
 package io.getstream.chat.docs.kotlin.cookbook.ui
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import io.getstream.chat.android.client.ChatClient
 import io.getstream.chat.android.client.api.models.QueryChannelRequest
+import io.getstream.chat.android.client.channel.state.ChannelState
 import io.getstream.chat.android.models.Message
 import io.getstream.chat.android.state.extensions.loadNewestMessages
 import io.getstream.result.Result
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
@@ -16,15 +19,24 @@ class CustomMessageListViewModel(val chatClient: ChatClient = ChatClient.instanc
     private val _uiState = MutableStateFlow(MessageListUiState())
     val uiState = _uiState.asStateFlow()
 
-    fun loadNewestMessages(cid: String) {
+    fun getMessages(cid: String) {
+        val channelStateFlow: StateFlow<ChannelState?> = chatClient.watchChannelAsState(
+            cid = cid,
+            messageLimit = 30,
+            coroutineScope = viewModelScope
+        )
+
         viewModelScope.launch {
-            when (val result = chatClient.loadNewestMessages(cid = cid, messageLimit = 30).await()) {
-                is Result.Success -> {
-                    val messages: List<Message> = result.value.messages
-                    _uiState.update { it.copy(messages = messages) }
-                }
-                else -> {
-                    _uiState.update { it.copy(error = "Error loading channels") }
+            channelStateFlow.collect { channelState ->
+                if (channelState != null) {
+                    channelState.messages.collect { messages ->
+                        _uiState.update {
+                            it.copy(messages = messages.reversed(), error = null)
+                        }
+                        Log.d("[Messages]", "Count: ${uiState.value.messages.size}")
+                    }
+                } else {
+                    _uiState.update { it.copy(error = "Cannot load messages") }
                 }
             }
         }
