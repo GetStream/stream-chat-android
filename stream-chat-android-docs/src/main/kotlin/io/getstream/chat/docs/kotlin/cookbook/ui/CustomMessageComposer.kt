97ad006a67d574e0d73f61e579ad3ae8cde33e65
Liviu Timar <65943217+liviu-timar@users.noreply.github.com>
2024-02-16 18:12:05 +0200

    Add Custom Attachments Picker code

--- stream-chat-android-docs/src/main/kotlin/io/getstream/chat/docs/kotlin/cookbook/ui/CustomMessageComposer.kt
+++ stream-chat-android-docs/src/main/kotlin/io/getstream/chat/docs/kotlin/cookbook/ui/CustomMessageComposer.kt
@@ -2,17 +2,22 @@ package io.getstream.chat.docs.kotlin.cookbook.ui
 
 import androidx.activity.compose.BackHandler
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.Card
 import androidx.compose.material.Icon
 import androidx.compose.material.IconButton
 import androidx.compose.material.Scaffold
@@ -21,27 +26,33 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.outlined.AddCircle
 import androidx.compose.material.icons.outlined.Send
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
-import io.getstream.chat.android.compose.state.mediagallerypreview.MediaGalleryPreviewResultType
+import io.getstream.chat.android.compose.state.messages.attachments.Files
+import io.getstream.chat.android.compose.state.messages.attachments.Images
+import io.getstream.chat.android.compose.state.messages.attachments.MediaCapture
 import io.getstream.chat.android.compose.ui.components.composer.MessageInput
-import io.getstream.chat.android.compose.ui.messages.attachments.AttachmentsPicker
+import io.getstream.chat.android.compose.ui.messages.attachments.factory.AttachmentsPickerTabFactory
 import io.getstream.chat.android.compose.ui.messages.composer.MessageComposer
 import io.getstream.chat.android.compose.ui.messages.list.MessageList
 import io.getstream.chat.android.compose.ui.theme.ChatTheme
-import io.getstream.chat.android.compose.ui.util.rememberMessageListState
 import io.getstream.chat.android.compose.viewmodel.messages.AttachmentsPickerViewModel
 import io.getstream.chat.android.compose.viewmodel.messages.MessageComposerViewModel
 import io.getstream.chat.android.compose.viewmodel.messages.MessageListViewModel
 import io.getstream.chat.android.compose.viewmodel.messages.MessagesViewModelFactory
-import io.getstream.chat.android.ui.common.state.messages.MessageMode
-import io.getstream.chat.android.ui.common.state.messages.Reply
+import io.getstream.chat.android.core.ExperimentalStreamChatApi
+import io.getstream.chat.android.models.Attachment
+import io.getstream.chat.docs.R
 
 @Composable
 fun CustomMessageComposer(cid: String?, onBackClick: () -> Unit = {}) {
@@ -61,7 +72,6 @@ fun CustomMessageComposer(cid: String?, onBackClick: () -> Unit = {}) {
         )
 
         val isShowingAttachments = attachmentsPickerViewModel.isShowingAttachments
-
         val backAction = remember(composerViewModel, attachmentsPickerViewModel) {
             {
                 when {
@@ -94,7 +104,7 @@ fun CustomMessageComposer(cid: String?, onBackClick: () -> Unit = {}) {
                                 modifier = Modifier
                                     .padding(horizontal = 10.dp)
                                     .align(Alignment.CenterVertically),
-                                label = { // create a custom label with an icon
+                                label = {
                                     Text(
                                         modifier = Modifier.padding(start = 4.dp),
                                         text = "Type a message",
@@ -154,11 +164,8 @@ fun CustomMessageComposer(cid: String?, onBackClick: () -> Unit = {}) {
             }
 
             if (isShowingAttachments) {
-                AttachmentsPicker( // Add the picker to your UI
+                CustomAttachmentsPicker(
                     attachmentsPickerViewModel = attachmentsPickerViewModel,
-                    modifier = Modifier
-                        .align(Alignment.BottomCenter)
-                        .height(350.dp),
                     onAttachmentsSelected = { attachments ->
                         attachmentsPickerViewModel.changeAttachmentState(false)
                         composerViewModel.addSelectedAttachments(attachments)
@@ -170,5 +177,194 @@ fun CustomMessageComposer(cid: String?, onBackClick: () -> Unit = {}) {
                 )
             }
         }
+    }
+}
+
+@OptIn(ExperimentalStreamChatApi::class)
+@Composable
+private fun CustomAttachmentsPicker(
+    attachmentsPickerViewModel: AttachmentsPickerViewModel,
+    onAttachmentsSelected: (List<Attachment>) -> Unit,
+    onDismiss: () -> Unit,
+    tabFactories: List<AttachmentsPickerTabFactory> = ChatTheme.attachmentsPickerTabFactories,
+) {
+    var shouldShowMenu by remember { mutableStateOf(true) }
+    var selectedOptionIndex by remember { mutableStateOf(-1) }
+
+    Box( // Gray overlay
+        modifier = Modifier
+            .fillMaxSize()
+            .background(ChatTheme.colors.overlay)
+            .clickable(
+                onClick = onDismiss,
+                indication = null,
+                interactionSource = remember { MutableInteractionSource() },
+            ),
+    ) {
+        Card(
+            modifier = Modifier
+                .heightIn(max = 350.dp)
+                .align(Alignment.BottomCenter)
+                .clickable(
+                    indication = null,
+                    onClick = {},
+                    interactionSource = remember { MutableInteractionSource() },
+                ),
+            elevation = 4.dp,
+            shape = ChatTheme.shapes.bottomSheet,
+            backgroundColor = ChatTheme.colors.inputBackground,
+        ) {
+            Box(modifier = Modifier.padding(vertical = 24.dp)) {
+                if (shouldShowMenu) {
+                    // Show the menu with Images, Files, Camera options
+                    AttachmentsMenu(
+                        tabFactories = tabFactories,
+                        onClick = {
+                            selectedOptionIndex = it
+                            shouldShowMenu = false
+                        },
+                    )
+                } else {
+                    // Show the selected tabFactory, with back and submit buttons
+                    Column(
+                        modifier = Modifier.padding(horizontal = 8.dp),
+                    ) {
+                        AttachmentsPickerToolbar(
+                            onBackClick = {
+                                shouldShowMenu = true
+                                selectedOptionIndex = -1
+                            },
+                            isSubmitEnabled = attachmentsPickerViewModel.hasPickedAttachments,
+                            onSubmitClick = {
+                                onAttachmentsSelected(attachmentsPickerViewModel.getSelectedAttachments())
+                            },
+                        )
+
+                        tabFactories.getOrNull(selectedOptionIndex)
+                            ?.PickerTabContent(
+                                attachments = attachmentsPickerViewModel.attachments,
+                                onAttachmentItemSelected = attachmentsPickerViewModel::changeSelectedAttachments,
+                                onAttachmentsChanged = { attachmentsPickerViewModel.attachments = it },
+                                onAttachmentsSubmitted = {
+                                    onAttachmentsSelected(attachmentsPickerViewModel.getAttachmentsFromMetaData(it))
+                                },
+                            )
+                    }
+                }
+            }
+        }
+    }
+}
+
+@Composable
+private fun AttachmentsMenu(
+    tabFactories: List<AttachmentsPickerTabFactory>,
+    onClick: (Int) -> Unit,
+) {
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceEvenly,
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+        tabFactories.forEachIndexed { index, tabFactory ->
+            AttachmentsMenuItem(
+                tabFactory = tabFactory,
+                isEnabled = tabFactory.isPickerTabEnabled(),
+                index = index,
+                onClick = onClick,
+            )
+        }
+    }
+}
+
+@Composable
+private fun AttachmentsMenuItem(
+    tabFactory: AttachmentsPickerTabFactory,
+    isEnabled: Boolean,
+    index: Int,
+    onClick: (Int) -> Unit,
+) {
+    Column(
+        modifier = Modifier.clickable(enabled = isEnabled) { onClick(index) },
+        horizontalAlignment = Alignment.CenterHorizontally,
+    ) {
+        val backgroundColor: Color
+        val label: String
+
+        when (tabFactory.attachmentsPickerMode) {
+            is Images -> {
+                backgroundColor = Color(0xFFCCCCFF)
+                label = "Images"
+            }
+            is Files -> {
+                backgroundColor = Color(0xFFFFCCCC)
+                label = "Files"
+            }
+            is MediaCapture -> {
+                backgroundColor = Color(0xFFFFCC99)
+                label = "Camera"
+            }
+            else -> {
+                backgroundColor = Color.LightGray
+                label = "Other"
+            }
+        }
+
+        Box(
+            modifier = Modifier
+                .padding(8.dp)
+                .size(48.dp)
+                .background(backgroundColor, shape = CircleShape),
+            contentAlignment = Alignment.Center,
+        ) {
+            tabFactory.PickerTabIcon(isEnabled, isSelected = false)
+        }
+        Text(text = label)
+    }
+}
+
+@Composable
+fun AttachmentsPickerToolbar(
+    onBackClick: () -> Unit,
+    isSubmitEnabled: Boolean,
+    onSubmitClick: () -> Unit,
+) {
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        IconButton(onClick = onBackClick) {
+            Icon(
+                painter = painterResource(id = R.drawable.ic_back),
+                contentDescription = "Back",
+                modifier = Modifier.size(24.dp),
+            )
+        }
+        IconButton(
+            enabled = isSubmitEnabled,
+            onClick = onSubmitClick
+        ) {
+            Icon(
+                painter = painterResource(id = R.drawable.ic_check),
+                contentDescription = "Submit Attachments",
+                modifier = Modifier.size(24.dp),
+                tint = if (isSubmitEnabled) {
+                    ChatTheme.colors.primaryAccent
+                } else {
+                    ChatTheme.colors.textLowEmphasis
+                },
+            )
+        }
+    }
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewCustomAttachmentPickerOptions() {
+    ChatTheme {
+        AttachmentsMenu(
+            tabFactories = ChatTheme.attachmentsPickerTabFactories,
+        ) {}
     }
 }
