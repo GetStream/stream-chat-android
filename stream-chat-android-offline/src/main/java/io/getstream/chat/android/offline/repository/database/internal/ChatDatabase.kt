0176a6e81bc33646ca76cc84c028df2c0f8223b3
Jc Miñarro <josecarlos.minarro@gmail.com>
2024-04-30 00:26:56 +0200

    Create DB Entities

--- stream-chat-android-offline/src/main/java/io/getstream/chat/android/offline/repository/database/internal/ChatDatabase.kt
+++ stream-chat-android-offline/src/main/java/io/getstream/chat/android/offline/repository/database/internal/ChatDatabase.kt
@@ -30,6 +30,7 @@ import io.getstream.chat.android.offline.repository.database.converter.internal.
 import io.getstream.chat.android.offline.repository.database.converter.internal.MemberConverter
 import io.getstream.chat.android.offline.repository.database.converter.internal.ModerationDetailsConverter
 import io.getstream.chat.android.offline.repository.database.converter.internal.QuerySortConverter
+import io.getstream.chat.android.offline.repository.database.converter.internal.ReactionGroupConverter
 import io.getstream.chat.android.offline.repository.database.converter.internal.SetConverter
 import io.getstream.chat.android.offline.repository.database.converter.internal.SyncStatusConverter
 import io.getstream.chat.android.offline.repository.domain.channel.internal.ChannelDao
@@ -67,7 +68,7 @@ import io.getstream.chat.android.offline.repository.domain.user.internal.UserEnt
         CommandInnerEntity::class,
         SyncStateEntity::class,
     ],
-    version = 72,
+    version = 73,
     exportSchema = false,
 )
 @TypeConverters(
@@ -81,6 +82,7 @@ import io.getstream.chat.android.offline.repository.domain.user.internal.UserEnt
     DateConverter::class,
     MemberConverter::class,
     ModerationDetailsConverter::class,
+    ReactionGroupConverter::class,
 )
 internal abstract class ChatDatabase : RoomDatabase() {
     abstract fun queryChannelsDao(): QueryChannelsDao
