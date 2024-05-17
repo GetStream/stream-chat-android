6e5dab881542c6292457336912c555e90cc40cc6
Jc Miñarro <josecarlos.minarro@gmail.com>
2024-05-17 14:40:49 +0200

    Create a new interface to provide headrs to be used when loading a video url

---
+++ stream-chat-android-ui-common/src/main/kotlin/io/getstream/chat/android/ui/common/helper/VideoHeadersProvider.kt
@@ -0,0 +1,35 @@
+/*
+ * Copyright (c) 2014-2022 Stream.io Inc. All rights reserved.
+ *
+ * Licensed under the Stream License;
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    https://github.com/GetStream/stream-chat-android/blob/main/LICENSE
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.getstream.chat.android.ui.common.helper
+
+/**
+ * Provides HTTP headers for video loading requests.
+ */
+public interface VideoHeadersProvider {
+
+    /**
+     * Returns a map of headers to be used for the video loading request.
+     *
+     * @param url The URL of the video to load.
+     * @return A map of headers to be used for the video loading request.
+     */
+    public fun getVideoRequestHeaders(url: String): Map<String, String>
+}
+
+internal object DefaultVideoHeadersProvider : VideoHeadersProvider {
+    override fun getVideoRequestHeaders(url: String): Map<String, String> = emptyMap<String, String>()
+}
