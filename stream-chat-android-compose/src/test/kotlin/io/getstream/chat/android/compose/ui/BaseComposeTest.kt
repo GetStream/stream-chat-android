df4494d890c92470ca5554da0c63b2a0638f6e76
skydoves <skydoves@naver.com>
2024-06-28 10:52:52 +0900

    Import paparazzi plugin and implement BaseComposeTest

---
+++ stream-chat-android-compose/src/test/kotlin/io/getstream/chat/android/compose/ui/BaseComposeTest.kt
@@ -0,0 +1,69 @@
+/*
+ * Copyright (c) 2014-2024 Stream.io Inc. All rights reserved.
+ *
+ * Licensed under the Stream License;
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    https://github.com/GetStream/stream-chat-android/blob/main/LICENSE
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.getstream.chat.android.compose.ui
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.ui.platform.LocalInspectionMode
+import app.cash.paparazzi.Paparazzi
+import io.getstream.chat.android.compose.ui.theme.ChatTheme
+
+internal abstract class BaseComposeTest {
+
+    abstract fun basePaparazzi(): Paparazzi
+
+    fun snapshot(
+        isInDarkMode: Boolean = false,
+        composable: @Composable () -> Unit,
+    ) {
+        basePaparazzi().snapshot {
+            CompositionLocalProvider(
+                LocalInspectionMode provides true,
+            ) {
+                ChatTheme(isInDarkMode = isInDarkMode) { composable.invoke() }
+            }
+        }
+    }
+
+    fun snapshotWithDarkMode(composable: @Composable () -> Unit) {
+        basePaparazzi().snapshot {
+            CompositionLocalProvider(
+                LocalInspectionMode provides true,
+            ) {
+                Column {
+                    ChatTheme(isInDarkMode = true) { composable.invoke() }
+                    ChatTheme(isInDarkMode = false) { composable.invoke() }
+                }
+            }
+        }
+    }
+
+    fun snapshotWithDarkModeRow(composable: @Composable () -> Unit) {
+        basePaparazzi().snapshot {
+            CompositionLocalProvider(
+                LocalInspectionMode provides true,
+            ) {
+                Row {
+                    ChatTheme(isInDarkMode = true) { composable.invoke() }
+                    ChatTheme(isInDarkMode = false) { composable.invoke() }
+                }
+            }
+        }
+    }
+}
