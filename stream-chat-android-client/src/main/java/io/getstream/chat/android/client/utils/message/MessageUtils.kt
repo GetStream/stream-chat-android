a021b12e449d15d44b2b958474bafec9840348b7
Jc Miñarro <josecarlos.minarro@gmail.com>
2024-07-16 10:05:45 +0200

    Remove unneeded annotation

--- stream-chat-android-client/src/main/java/io/getstream/chat/android/client/utils/message/MessageUtils.kt
+++ stream-chat-android-client/src/main/java/io/getstream/chat/android/client/utils/message/MessageUtils.kt
@@ -97,7 +97,6 @@ public inline fun Message.isPinned(
 /**
  * @return If the message is a valid pinned message.
  */
-@InternalStreamChatApi
 @PublishedApi
 internal inline fun Message.isPinExpired(now: () -> Long): Boolean = pinExpires?.let { it.time > now() } ?: false
 
