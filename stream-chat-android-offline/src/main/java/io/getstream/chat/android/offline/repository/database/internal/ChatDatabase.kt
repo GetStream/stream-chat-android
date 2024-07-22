3c52ee86bfbac683ecfadb990855126fd6d9b103
Jc Miñarro <josecarlos.minarro@gmail.com>
2024-07-22 15:48:42 +0200

    Increase database version

--- stream-chat-android-offline/src/main/java/io/getstream/chat/android/offline/repository/database/internal/ChatDatabase.kt
+++ stream-chat-android-offline/src/main/java/io/getstream/chat/android/offline/repository/database/internal/ChatDatabase.kt
@@ -69,7 +69,7 @@ import io.getstream.chat.android.offline.repository.domain.user.internal.UserEnt
         CommandInnerEntity::class,
         SyncStateEntity::class,
     ],
-    version = 75,
+    version = 76,
     exportSchema = false,
 )
 @TypeConverters(
