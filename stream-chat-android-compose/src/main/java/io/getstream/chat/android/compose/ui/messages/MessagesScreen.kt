91236e3315b5878d347825702334ca97ec1aa7fc
Liviu Timar <65943217+liviu-timar@users.noreply.github.com>
2024-09-05 18:07:09 +0300

    Use safeDrawingPadding for MessageScreen, MessageComposer and AttachmentsPicker

--- stream-chat-android-compose/src/main/java/io/getstream/chat/android/compose/ui/messages/MessagesScreen.kt
+++ stream-chat-android-compose/src/main/java/io/getstream/chat/android/compose/ui/messages/MessagesScreen.kt
@@ -36,7 +36,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.ime
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.systemBarsPadding
+import androidx.compose.foundation.layout.safeDrawingPadding
 import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
@@ -203,7 +203,7 @@ public fun MessagesScreen(
     Box(
         modifier = Modifier
             .fillMaxSize()
-            .systemBarsPadding()
+            .safeDrawingPadding()
             .testTag("Stream_MessagesScreen"),
     ) {
         Scaffold(
