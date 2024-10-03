bf5922d4429d3bdf9637951b49a75a754322877e
Jc Miñarro <josecarlos.minarro@gmail.com>
2024-10-03 11:15:02 +0200

    Create a Dialog to ask for a new Option

--- stream-chat-android-compose/src/main/java/io/getstream/chat/android/compose/ui/components/messages/PollMessageContent.kt
+++ stream-chat-android-compose/src/main/java/io/getstream/chat/android/compose/ui/components/messages/PollMessageContent.kt
@@ -31,13 +31,20 @@ import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.AlertDialog
 import androidx.compose.material.Icon
 import androidx.compose.material.LinearProgressIndicator
 import androidx.compose.material.Text
+import androidx.compose.material.TextButton
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.focus.FocusRequester
+import androidx.compose.ui.focus.focusRequester
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.res.painterResource
@@ -51,6 +58,7 @@ import androidx.compose.ui.unit.sp
 import io.getstream.chat.android.client.utils.message.isDeleted
 import io.getstream.chat.android.compose.R
 import io.getstream.chat.android.compose.ui.components.avatar.UserAvatarRow
+import io.getstream.chat.android.compose.ui.components.composer.InputField
 import io.getstream.chat.android.compose.ui.theme.ChatTheme
 import io.getstream.chat.android.compose.ui.util.isErrorOrFailed
 import io.getstream.chat.android.models.Message
@@ -249,6 +257,54 @@ private fun PollMessageContent(
             }
         }
     }
+}
+
+@Composable
+private fun NewOptionDialog(
+    onDismiss: () -> Unit,
+    onNewOption: (newOption: String) -> Unit,
+) {
+    val newOption = remember { mutableStateOf("") }
+    val focusRequester = remember { FocusRequester() }
+    AlertDialog(
+        title = { Text(text = stringResource(R.string.stream_compose_suggest_an_option)) },
+        text = {
+            InputField(
+                value = newOption.value,
+                onValueChange = { newOption.value = it },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .focusRequester(focusRequester),
+                decorationBox = { innerTextField ->
+                    Column {
+                        innerTextField()
+                    }
+                },
+            )
+            LaunchedEffect(Unit) {
+                focusRequester.requestFocus()
+            }
+        },
+        onDismissRequest = { onDismiss.invoke() },
+        confirmButton = {
+            TextButton(
+                enabled = newOption.value.isNotBlank(),
+                onClick = {
+                    onNewOption.invoke(newOption.value)
+                    onDismiss.invoke()
+                },
+            ) {
+                Text(stringResource(R.string.stream_compose_confirm))
+            }
+        },
+        dismissButton = {
+            TextButton(
+                onClick = { onDismiss.invoke() },
+            ) {
+                Text(stringResource(R.string.stream_compose_dismiss))
+            }
+        },
+    )
 }
 
 @Composable
