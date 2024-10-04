81dcbfb7ee69abd45e116a7fc89bbe48a258940e
Jc Miñarro <josecarlos.minarro@gmail.com>
2024-10-04 18:12:44 +0200

    Create new event for Answers

--- stream-chat-android-client/src/main/java/io/getstream/chat/android/client/parser2/adapters/EventAdapter.kt
+++ stream-chat-android-client/src/main/java/io/getstream/chat/android/client/parser2/adapters/EventAdapter.kt
@@ -22,6 +22,7 @@ import com.squareup.moshi.JsonWriter
 import com.squareup.moshi.Moshi
 import com.squareup.moshi.Types
 import com.squareup.moshi.rawType
+import io.getstream.chat.android.client.api2.model.dto.AnswerCastedEventDto
 import io.getstream.chat.android.client.api2.model.dto.ChannelDeletedEventDto
 import io.getstream.chat.android.client.api2.model.dto.ChannelHiddenEventDto
 import io.getstream.chat.android.client.api2.model.dto.ChannelTruncatedEventDto
@@ -145,6 +146,7 @@ internal class EventDtoAdapter(
     private val pollClosedEventAdapter = moshi.adapter(PollClosedEventDto::class.java)
     private val voteCastedEventAdapter = moshi.adapter(VoteCastedEventDto::class.java)
     private val voteChangedEventAdapter = moshi.adapter(VoteChangedEventDto::class.java)
+    private val answerCastedEventAdapter = moshi.adapter(AnswerCastedEventDto::class.java)
     private val voteRemovedEventAdapter = moshi.adapter(VoteRemovedEventDto::class.java)
 
     @Suppress("LongMethod", "ComplexMethod", "ReturnCount")
@@ -216,8 +218,14 @@ internal class EventDtoAdapter(
             EventType.POLL_UPDATED -> pollUpdatedEventAdapter
             EventType.POLL_DELETED -> pollDeletedEventAdapter
             EventType.POLL_CLOSED -> pollClosedEventAdapter
-            EventType.POLL_VOTE_CASTED -> voteCastedEventAdapter
-            EventType.POLL_VOTE_CHANGED -> voteChangedEventAdapter
+            EventType.POLL_VOTE_CASTED -> when (map.containsAnswer()) {
+                true -> answerCastedEventAdapter
+                else -> voteCastedEventAdapter
+            }
+            EventType.POLL_VOTE_CHANGED -> when (map.containsAnswer()) {
+                true -> answerCastedEventAdapter
+                else -> voteChangedEventAdapter
+            }
             EventType.POLL_VOTE_REMOVED -> voteRemovedEventAdapter
             else -> // Custom case, early return
                 return UnknownEventDto(
@@ -229,6 +237,11 @@ internal class EventDtoAdapter(
         }
 
         return adapter.fromJsonValue(map)
+    }
+
+    private fun Map<String, Any?>.containsAnswer(): Boolean {
+        return (((this["poll_vote"] as? Map<String, Any?>)?.get("is_answer") as? Boolean) ?: false)
+            .also { println("JcLog: containsAnswer -> $it") }
     }
 
     override fun toJson(writer: JsonWriter, value: ChatEventDto?) {
