a04aa55cdc5cdd593ad12f3e9b19ba9d34ec1cc3
Jc Miñarro <josecarlos.minarro@gmail.com>
2024-10-29 17:55:33 +0100

    Extract TextViewLinkHnalder from ui common module

---
+++ stream-chat-android-ui-components/src/main/kotlin/io/getstream/chat/android/ui/utils/TextViewLinkHandler.kt
@@ -0,0 +1,51 @@
+/*
+ * Copyright (c) 2014-2024 Stream.io Inc. All rights reserved.
+ *
+ * Licensed under the Stream License;
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    https://github.com/GetStream/stream-chat-android/blob/main/LICENSE
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.getstream.chat.android.ui.utils
+
+import android.text.Spannable
+import android.text.method.LinkMovementMethod
+import android.text.style.URLSpan
+import android.view.MotionEvent
+import android.widget.TextView
+
+internal abstract class TextViewLinkHandler : LinkMovementMethod() {
+    override fun onTouchEvent(
+        widget: TextView,
+        buffer: Spannable,
+        event: MotionEvent,
+    ): Boolean {
+        if (event.action != MotionEvent.ACTION_UP) return super.onTouchEvent(widget, buffer, event)
+
+        var x = event.x.toInt()
+        var y = event.y.toInt()
+
+        x -= widget.totalPaddingLeft
+        y -= widget.totalPaddingTop
+
+        x += widget.scrollX
+        y += widget.scrollY
+
+        val layout = widget.layout
+        val line = layout.getLineForVertical(y)
+        val off = layout.getOffsetForHorizontal(line, x.toFloat())
+
+        buffer.getSpans(off, off, URLSpan::class.java).firstOrNull()?.let { onLinkClick(it.url) }
+        return true
+    }
+
+    abstract fun onLinkClick(url: String)
+}
