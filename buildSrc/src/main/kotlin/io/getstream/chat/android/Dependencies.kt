4b5774a31b3c031d65dd3a31e89919a18d5845be
PetarVelikov <petar.velikov@getstream.io>
2024-11-15 13:57:19 +0100

    Update ThreeTen to v1.7.0.

--- buildSrc/src/main/kotlin/io/getstream/chat/android/Dependencies.kt
+++ buildSrc/src/main/kotlin/io/getstream/chat/android/Dependencies.kt
@@ -82,7 +82,7 @@ object Versions {
     internal const val STREAM_PUSH = "1.1.8"
     internal const val STREAM_RESULT = "1.3.2"
     internal const val TEST_PARAMETER_INJECTOR = "1.12"
-    internal const val THREETENBP = "1.6.8"
+    internal const val THREETENBP = "1.7.0"
     internal const val TIMBER = "5.0.1"
     internal const val TURBINE = "1.0.0"
     internal const val WORK = "2.9.1"
