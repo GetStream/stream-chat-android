c324f6fed451b9291db189ee0745127a7fc2cf02
PetarVelikov <petar.velikov@getstream.io>
2024-11-25 15:27:00 +0100

    [AND-3] Implement ThreadListView for XML SDK.

---
+++ stream-chat-android-ui-components/src/main/kotlin/io/getstream/chat/android/ui/viewmodel/threads/ThreadListViewModel.kt
@@ -0,0 +1,54 @@
+/*
+ * Copyright (c) 2014-2024 Stream.io Inc. All rights reserved.
+ *
+ * Licensed under the Stream License;
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    https://github.com/GetStream/stream-chat-android/blob/main/LICENSE
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.getstream.chat.android.ui.viewmodel.threads
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.asLiveData
+import io.getstream.chat.android.ui.common.feature.threads.ThreadListController
+import io.getstream.chat.android.ui.common.state.threads.ThreadListState
+
+/**
+ * ViewModel responsible for managing the state of a threads list.
+ *
+ * @param controller The [ThreadListController] handling the business logic and the state management for the threads
+ * list.
+ */
+public class ThreadListViewModel(private val controller: ThreadListController) : ViewModel() {
+
+    /**
+     * The current thread list state.
+     */
+    public val state: LiveData<ThreadListState> = controller.state.asLiveData()
+
+    /**
+     * Loads the initial data when requested.
+     * Overrides all previously retrieved data.
+     */
+    public fun load() {
+        controller.load()
+    }
+
+    /**
+     * Loads more data when requested.
+     *
+     * Does nothing if the end of the list has already been reached or loading is already in progress.
+     */
+    public fun loadNextPage() {
+        controller.loadNextPage()
+    }
+}
