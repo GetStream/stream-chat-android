d2c78cdf351d52d7370f1b5b06e084b6cb3e4445
Alexey Alter-Pesotskiy <alex@testableapple.com>
2024-12-09 17:18:45 +0000

    [CI] Implement SonarCloud

--- buildSrc/src/main/kotlin/io/getstream/chat/android/command/unittest/model/TestType.kt
+++ buildSrc/src/main/kotlin/io/getstream/chat/android/command/unittest/model/TestType.kt
@@ -3,4 +3,5 @@ package io.getstream.chat.android.command.unittest.model
 enum class TestType(val testCommand: String) {
     JAVA_LIBRARY_TEST("test"),
     ANDROID_LIBRARY_TEST("testDebugUnitTest"),
+    JACOCO_TEST_COVERAGE("testCoverage"),
 }
