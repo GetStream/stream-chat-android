d53dbe7bfe59831aeb3d29610dc93615fbc4079d
André Mion <andremion@gmail.com>
2025-01-24 15:04:13 +0000

    run paparazzi tests

--- buildSrc/src/main/kotlin/io/getstream/chat/android/command/unittest/model/TestType.kt
+++ buildSrc/src/main/kotlin/io/getstream/chat/android/command/unittest/model/TestType.kt
@@ -3,5 +3,6 @@ package io.getstream.chat.android.command.unittest.model
 enum class TestType(val testCommand: String) {
     JAVA_LIBRARY_TEST("test"),
     ANDROID_LIBRARY_TEST("testDebugUnitTest"),
+    COMPOSE_LIBRARY_TEST("verifyPaparazziDebug"),
     JACOCO_TEST_COVERAGE("testCoverage"),
 }
