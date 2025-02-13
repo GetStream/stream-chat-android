73e3b32dfd643fc16c26e4fdeffc6127f78751fa
André Mion <andremion@gmail.com>
2025-02-13 14:19:15 +0000

    Missed testTag

--- stream-chat-android-compose/src/main/java/io/getstream/chat/android/compose/ui/components/avatar/Avatar.kt
+++ stream-chat-android-compose/src/main/java/io/getstream/chat/android/compose/ui/components/avatar/Avatar.kt
@@ -24,6 +24,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.DpOffset
@@ -63,7 +64,7 @@ public fun Avatar(
     val data = remember { imageUrl.applyStreamCdnImageResizingIfEnabled(streamCdnImageResizing) }
     StreamAsyncImage(
         data = data,
-        modifier = modifier,
+        modifier = modifier.testTag("Stream_QuotedMessageAuthorAvatar"),
         content = { state ->
             val targetPainter = when (state) {
                 is AsyncImagePainter.State.Empty -> placeholderPainter
