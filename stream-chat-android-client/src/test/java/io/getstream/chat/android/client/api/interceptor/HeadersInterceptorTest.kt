f3a7d5046d6fa309a66069996d7b6ee4d8d4fb7c
PetarVelikov <petar.velikov@getstream.io>
2025-03-03 15:35:00 +0100

    [AND-274] Apply detekt.

--- stream-chat-android-client/src/test/java/io/getstream/chat/android/client/api/interceptor/HeadersInterceptorTest.kt
+++ stream-chat-android-client/src/test/java/io/getstream/chat/android/client/api/interceptor/HeadersInterceptorTest.kt
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2014-2025 Stream.io Inc. All rights reserved.
+ *
+ * Licensed under the Stream License;
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    https://github.com/GetStream/stream-chat-android/blob/main/LICENSE
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package io.getstream.chat.android.client.api.interceptor
 
 import io.getstream.chat.android.client.api.FakeChain
@@ -11,39 +27,39 @@ import org.mockito.kotlin.whenever
 
 internal class HeadersInterceptorTest {
 
-	@Test
-	fun testAnonymousUserHeaders() {
-		// given
-		val isAnonymous = { true }
-		val headersUtil = mock<HeadersUtil>()
-		whenever(headersUtil.buildSdkTrackingHeaders()).doReturn("sdkTrackingHeaders")
-		whenever(headersUtil.buildUserAgent()).doReturn("userAgent")
-		val interceptor = HeadersInterceptor(isAnonymous, headersUtil)
-		// when
-		val response = interceptor.intercept(FakeChain(FakeResponse(200)))
-		//then
-		response.request.header("User-Agent") `should be equal to` "userAgent"
-		response.request.header("Content-Type") `should be equal to` "application/json"
-		response.request.header("stream-auth-type") `should be equal to` "anonymous"
-		response.request.header("X-Stream-Client") `should be equal to` "sdkTrackingHeaders"
-		response.request.header("Cache-Control") `should be equal to` "no-cache"
-	}
+    @Test
+    fun testAnonymousUserHeaders() {
+        // given
+        val isAnonymous = { true }
+        val headersUtil = mock<HeadersUtil>()
+        whenever(headersUtil.buildSdkTrackingHeaders()).doReturn("sdkTrackingHeaders")
+        whenever(headersUtil.buildUserAgent()).doReturn("userAgent")
+        val interceptor = HeadersInterceptor(isAnonymous, headersUtil)
+        // when
+        val response = interceptor.intercept(FakeChain(FakeResponse(200)))
+        // then
+        response.request.header("User-Agent") `should be equal to` "userAgent"
+        response.request.header("Content-Type") `should be equal to` "application/json"
+        response.request.header("stream-auth-type") `should be equal to` "anonymous"
+        response.request.header("X-Stream-Client") `should be equal to` "sdkTrackingHeaders"
+        response.request.header("Cache-Control") `should be equal to` "no-cache"
+    }
 
-	@Test
-	fun testAuthenticatedUserHeaders() {
-		// given
-		val isAnonymous = { false }
-		val headersUtil = mock<HeadersUtil>()
-		whenever(headersUtil.buildSdkTrackingHeaders()).doReturn("sdkTrackingHeaders")
-		whenever(headersUtil.buildUserAgent()).doReturn("userAgent")
-		val interceptor = HeadersInterceptor(isAnonymous, headersUtil)
-		// when
-		val response = interceptor.intercept(FakeChain(FakeResponse(200)))
-		//then
-		response.request.header("User-Agent") `should be equal to` "userAgent"
-		response.request.header("Content-Type") `should be equal to` "application/json"
-		response.request.header("stream-auth-type") `should be equal to` "jwt"
-		response.request.header("X-Stream-Client") `should be equal to` "sdkTrackingHeaders"
-		response.request.header("Cache-Control") `should be equal to` "no-cache"
-	}
+    @Test
+    fun testAuthenticatedUserHeaders() {
+        // given
+        val isAnonymous = { false }
+        val headersUtil = mock<HeadersUtil>()
+        whenever(headersUtil.buildSdkTrackingHeaders()).doReturn("sdkTrackingHeaders")
+        whenever(headersUtil.buildUserAgent()).doReturn("userAgent")
+        val interceptor = HeadersInterceptor(isAnonymous, headersUtil)
+        // when
+        val response = interceptor.intercept(FakeChain(FakeResponse(200)))
+        // then
+        response.request.header("User-Agent") `should be equal to` "userAgent"
+        response.request.header("Content-Type") `should be equal to` "application/json"
+        response.request.header("stream-auth-type") `should be equal to` "jwt"
+        response.request.header("X-Stream-Client") `should be equal to` "sdkTrackingHeaders"
+        response.request.header("Cache-Control") `should be equal to` "no-cache"
+    }
 }
