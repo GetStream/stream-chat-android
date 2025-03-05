880116227137415fbaa460a1f71c77363a77e9be
André Mion <andremion@gmail.com>
2025-03-05 16:57:03 +0000

    Spotless

--- stream-chat-android-compose-sample/src/main/java/io/getstream/chat/android/compose/sample/ui/channel/ChannelInfoHeader.kt
+++ stream-chat-android-compose-sample/src/main/java/io/getstream/chat/android/compose/sample/ui/channel/ChannelInfoHeader.kt
@@ -25,14 +25,11 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
--- stream-chat-android-compose/src/main/java/io/getstream/chat/android/compose/ui/chats/MessagesViewModelFactoryProvider.kt
+++ stream-chat-android-compose/src/main/java/io/getstream/chat/android/compose/ui/chats/MessagesViewModelFactoryProvider.kt
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2014-2025 Stream.io Inc. All rights reserved.
+ *
+ * Licensed under the Stream License;
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    https://github.com/GetStream/stream-chat-android/blob/main/LICENSE
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package io.getstream.chat.android.compose.ui.chats
 
 import android.content.Context
@@ -7,4 +23,4 @@ import io.getstream.chat.android.compose.viewmodel.messages.MessagesViewModelFac
  * A lambda function that provides a [MessagesViewModelFactory] for managing messages within a selected channel.
  */
 public typealias MessagesViewModelFactoryProvider =
-        (context: Context, selection: MessageSelection) -> MessagesViewModelFactory?
+    (context: Context, selection: MessageSelection) -> MessagesViewModelFactory?
