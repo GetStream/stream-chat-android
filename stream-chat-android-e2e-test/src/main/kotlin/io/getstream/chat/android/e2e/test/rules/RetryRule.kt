eb6b0bbcd6e028cdd4a825618b131d8eb21c485b
Alexey Alter-Pesotskiy <alex@testableapple.com>
2025-03-13 08:41:18 +0000

    [CI] Fix e2e tests retry rule issue (#5676)

--- stream-chat-android-e2e-test/src/main/kotlin/io/getstream/chat/android/e2e/test/rules/RetryRule.kt
+++ stream-chat-android-e2e-test/src/main/kotlin/io/getstream/chat/android/e2e/test/rules/RetryRule.kt
@@ -16,67 +16,92 @@
 
 package io.getstream.chat.android.e2e.test.rules
 
-import io.getstream.chat.android.e2e.test.helpers.DatabaseOperations
+import android.database.sqlite.SQLiteDatabase
+import androidx.test.platform.app.InstrumentationRegistry
 import org.junit.rules.TestRule
 import org.junit.runner.Description
 import org.junit.runners.model.Statement
+import java.io.File
 
-/**
- * Annotation to specify amount of retries of a specific test
- *
- * The value in this annotation takes precedence over Retry Rule defaultRetries value
- */
+/** Annotation to retry a specific failed test. **/
 @Retention(AnnotationRetention.RUNTIME)
 @Target(AnnotationTarget.FUNCTION)
-public annotation class Retry(val times: Int = 0)
+public annotation class Retry(val count: Int)
 
-/**
- * Rule to retry failed tests
- *
- * If @Retry annotation is not specified in the test method, it defaults to DEFAULT_RETRIES
- *
- * @param defaultRetries amount of retries.
- */
-public class RetryRule(
-    private val defaultRetries: Int = DEFAULT_RETRIES,
-) : TestRule {
+/** Rule to retry all failed tests. **/
+public class RetryRule(private val count: Int) : TestRule {
 
     override fun apply(base: Statement, description: Description): Statement = statement(base, description)
 
-    private fun statement(
-        base: Statement,
-        description: Description,
-    ): Statement {
+    private fun statement(base: Statement, description: Description): Statement {
         return object : Statement() {
             @Throws(Throwable::class)
             override fun evaluate() {
                 val retryAnnotation: Retry? = description.getAnnotation(Retry::class.java)
-                val retryCount = retryAnnotation?.times ?: DEFAULT_RETRIES
+                val retryCount = retryAnnotation?.count ?: count
                 val databaseOperations = DatabaseOperations()
-
-                System.err.println("retry_count: $retryCount")
-
                 var caughtThrowable: Throwable? = null
 
                 for (i in 0 until retryCount) {
-                    System.err.println("${description.displayName}: run ${(i + 1)}")
-
                     try {
+                        System.err.println("${description.displayName}: run #${(i + 1)} started.")
                         base.evaluate()
                         return
                     } catch (t: Throwable) {
-                        caughtThrowable = t
-                        System.err.println("${description.displayName}: run ${(i + 1)} failed.")
+                        System.err.println("${description.displayName}: run #${(i + 1)} failed.")
                         databaseOperations.clearDatabases()
+                        caughtThrowable = t
                     }
                 }
-                System.err.println("${description.displayName}: Giving up after $retryCount  failures.")
+
                 throw caughtThrowable ?: IllegalStateException()
             }
         }
     }
+}
 
-    public companion object {
-        public const val DEFAULT_RETRIES: Int = 3
+public open class DatabaseOperations {
+
+    public open fun clearDatabases() {
+        val databaseOperations = DatabaseOperations()
+        val dbFiles = databaseOperations.getAllDatabaseFiles().filterNot { shouldIgnoreFile(it.path) }
+        dbFiles.forEach { clearDatabase(it, databaseOperations) }
+    }
+
+    private fun shouldIgnoreFile(path: String): Boolean {
+        val ignoredSuffixes = arrayOf("-journal", "-shm", "-uid", "-wal")
+        return ignoredSuffixes.any { path.endsWith(it) }
+    }
+
+    private fun clearDatabase(dbFile: File, dbOperations: DatabaseOperations) {
+        dbOperations.openDatabase(dbFile).use { database ->
+            val tablesToClear = dbOperations.getTableNames(database).filterNot { it == "room_master_table" }
+            tablesToClear.forEach { dbOperations.deleteTableContent(database, it) }
+        }
+    }
+
+    private fun getAllDatabaseFiles(): List<File> {
+        return InstrumentationRegistry.getInstrumentation().targetContext.let { context ->
+            context.databaseList().map { context.getDatabasePath(it) }
+        }
+    }
+
+    private fun openDatabase(databaseFile: File): SQLiteDatabase {
+        return SQLiteDatabase.openDatabase(databaseFile.absolutePath, null, 0)
+    }
+
+    private fun getTableNames(sqLiteDatabase: SQLiteDatabase): List<String> {
+        sqLiteDatabase.rawQuery("SELECT name FROM sqlite_master WHERE type IN (?, ?)", arrayOf("table", "view"))
+            .use { cursor ->
+                val tableNames = ArrayList<String>()
+                while (cursor.moveToNext()) {
+                    tableNames.add(cursor.getString(0))
+                }
+                return tableNames
+            }
+    }
+
+    private fun deleteTableContent(sqLiteDatabase: SQLiteDatabase, tableName: String) {
+        sqLiteDatabase.delete(tableName, null, null)
     }
 }
