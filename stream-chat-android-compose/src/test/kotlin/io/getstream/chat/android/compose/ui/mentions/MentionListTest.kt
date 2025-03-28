d427b1e0b03b59c0445796bed94b14de5c37fb7a
André Mion <andremion@gmail.com>
2025-03-28 10:54:06 +0000

    MentionListTest

---
+++ stream-chat-android-compose/src/test/kotlin/io/getstream/chat/android/compose/ui/mentions/MentionListTest.kt
@@ -0,0 +1,128 @@
+/*
+ * Copyright (c) 2014-2025 Stream.io Inc. All rights reserved.
+ *
+ * Licensed under the Stream License;
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    https://github.com/GetStream/stream-chat-android/blob/main/LICENSE
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.getstream.chat.android.compose.ui.mentions
+
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.ui.Modifier
+import app.cash.paparazzi.DeviceConfig
+import app.cash.paparazzi.Paparazzi
+import io.getstream.chat.android.compose.ui.SnapshotTest
+import io.getstream.chat.android.previewdata.PreviewChannelData
+import io.getstream.chat.android.previewdata.PreviewMessageData
+import io.getstream.chat.android.previewdata.PreviewUserData
+import io.getstream.chat.android.ui.common.model.MessageResult
+import io.getstream.chat.android.ui.common.state.mentions.MentionListState
+import org.junit.Rule
+import org.junit.Test
+
+internal class MentionListTest : SnapshotTest {
+
+    @get:Rule
+    override val paparazzi = Paparazzi(deviceConfig = DeviceConfig.PIXEL_4A)
+
+    @Test
+    fun `loading mention list`() {
+        snapshotWithDarkMode {
+            MentionList(
+                state = MentionListState(
+                    isLoading = true,
+                    results = emptyList(),
+                    nextPage = null,
+                    canLoadMore = true,
+                    isLoadingMore = false,
+                ),
+                currentUser = PreviewUserData.user1,
+            )
+        }
+    }
+
+    @Test
+    fun `empty mention list`() {
+        snapshotWithDarkMode {
+            MentionList(
+                state = MentionListState(
+                    isLoading = false,
+                    results = emptyList(),
+                    nextPage = null,
+                    canLoadMore = true,
+                    isLoadingMore = false,
+                ),
+                currentUser = PreviewUserData.user1,
+            )
+        }
+    }
+
+    @Test
+    fun `loaded mention list`() {
+        snapshotWithDarkMode {
+            MentionList(
+                state = MentionListState(
+                    isLoading = false,
+                    results = listOf(
+                        MessageResult(
+                            message = PreviewMessageData.message1,
+                            channel = PreviewChannelData.channelWithImage,
+                        ),
+                        MessageResult(
+                            message = PreviewMessageData.message2,
+                            channel = PreviewChannelData.channelWithFewMembers,
+                        ),
+                        MessageResult(
+                            message = PreviewMessageData.message3,
+                            channel = PreviewChannelData.channelWithManyMembers,
+                        ),
+                    ),
+                    nextPage = null,
+                    canLoadMore = true,
+                    isLoadingMore = false,
+                ),
+                modifier = Modifier.fillMaxSize(),
+                currentUser = PreviewUserData.user1,
+            )
+        }
+    }
+
+    @Test
+    fun `loading more mention list`() {
+        snapshotWithDarkMode {
+            MentionList(
+                state = MentionListState(
+                    isLoading = false,
+                    results = listOf(
+                        MessageResult(
+                            message = PreviewMessageData.message1,
+                            channel = PreviewChannelData.channelWithImage,
+                        ),
+                        MessageResult(
+                            message = PreviewMessageData.message2,
+                            channel = PreviewChannelData.channelWithFewMembers,
+                        ),
+                        MessageResult(
+                            message = PreviewMessageData.message3,
+                            channel = PreviewChannelData.channelWithManyMembers,
+                        ),
+                    ),
+                    nextPage = null,
+                    canLoadMore = true,
+                    isLoadingMore = true,
+                ),
+                modifier = Modifier.fillMaxSize(),
+                currentUser = PreviewUserData.user1,
+            )
+        }
+    }
+}
