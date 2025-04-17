d54bedfdf83d49c6d079ea3d7847ffc1215f3160
André Mion <andremion@gmail.com>
2025-04-17 08:43:26 +0100

    Refactor: Remove unnecessary dark mode parameter from channel and mention list previews
    The dark mode parameter in the composable previews for channel and mention lists is not needed anymore, as the `ChatTheme` composable is used directly inside them to handle dark mode. This change removes the extra parameter and makes the previews simpler.
    Also removed the `darkMode: Boolean` parameters from snapshot tests.

--- stream-chat-android-compose/src/test/kotlin/io/getstream/chat/android/compose/ui/SnapshotTest.kt
+++ stream-chat-android-compose/src/test/kotlin/io/getstream/chat/android/compose/ui/SnapshotTest.kt
@@ -50,7 +50,7 @@ internal interface SnapshotTest : ComposeTest {
         }
     }
 
-    fun snapshotWithDarkMode(composable: @Composable (darkMode: Boolean) -> Unit) {
+    fun snapshotWithDarkMode(composable: @Composable () -> Unit) {
         paparazzi.snapshot {
             CompositionLocalProvider(
                 LocalInspectionMode provides true,
@@ -62,7 +62,7 @@ internal interface SnapshotTest : ComposeTest {
                                 .weight(.5f)
                                 .background(ChatTheme.colors.appBackground),
                         ) {
-                            composable(true)
+                            composable()
                         }
                     }
                     ChatTheme(isInDarkMode = false) {
@@ -71,7 +71,7 @@ internal interface SnapshotTest : ComposeTest {
                                 .weight(.5f)
                                 .background(ChatTheme.colors.appBackground),
                         ) {
-                            composable(false)
+                            composable()
                         }
                     }
                 }
@@ -79,7 +79,7 @@ internal interface SnapshotTest : ComposeTest {
         }
     }
 
-    fun snapshotWithDarkModeRow(composable: @Composable (darkMode: Boolean) -> Unit) {
+    fun snapshotWithDarkModeRow(composable: @Composable () -> Unit) {
         paparazzi.snapshot {
             CompositionLocalProvider(
                 LocalInspectionMode provides true,
@@ -91,7 +91,7 @@ internal interface SnapshotTest : ComposeTest {
                                 .weight(.5f)
                                 .background(ChatTheme.colors.appBackground),
                         ) {
-                            composable(true)
+                            composable()
                         }
                     }
                     ChatTheme(isInDarkMode = false) {
@@ -100,7 +100,7 @@ internal interface SnapshotTest : ComposeTest {
                                 .weight(.5f)
                                 .background(ChatTheme.colors.appBackground),
                         ) {
-                            composable(false)
+                            composable()
                         }
                     }
                 }
