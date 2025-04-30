58ae7c9b1d3c68006e095ce821464ebe44c9e283
André Mion <andremion@gmail.com>
2025-04-30 14:25:28 +0100

    Update channel name

--- stream-chat-android-ui-common/src/main/kotlin/io/getstream/chat/android/ui/common/feature/channel/info/ChannelInfoController.kt
+++ stream-chat-android-ui-common/src/main/kotlin/io/getstream/chat/android/ui/common/feature/channel/info/ChannelInfoController.kt
@@ -1,13 +1,33 @@
+/*
+ * Copyright (c) 2014-2025 Stream.io Inc. All rights reserved.
+ *
+ * Licensed under the Stream License;
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    https://github.com/GetStream/stream-chat-android/blob/main/LICENSE
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package io.getstream.chat.android.ui.common.feature.channel.info
 
 import io.getstream.chat.android.client.ChatClient
 import io.getstream.chat.android.core.internal.InternalStreamChatApi
 import io.getstream.chat.android.state.extensions.watchChannelAsState
-import io.getstream.chat.android.ui.common.state.channel.info.ChannelInfoState
+import io.getstream.chat.android.ui.common.state.channel.info.ChannelInfoEvent
+import io.getstream.chat.android.ui.common.state.channel.info.ChannelInfoViewState
 import io.getstream.log.taggedLogger
 import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.SharedFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asSharedFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.combine
 import kotlinx.coroutines.flow.filterNotNull
@@ -15,44 +35,60 @@ import kotlinx.coroutines.flow.flatMapLatest
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
 import kotlinx.coroutines.flow.update
+import kotlinx.coroutines.launch
 
 @InternalStreamChatApi
 public class ChannelInfoController(
-    scope: CoroutineScope,
     cid: String,
+    private val scope: CoroutineScope,
     chatClient: ChatClient = ChatClient.instance(),
 ) {
     private val logger by taggedLogger("Chat:ChannelInfoController")
 
-    private val _state = MutableStateFlow(ChannelInfoState())
+    private val channelClient = chatClient.channel(cid)
 
-    public val state: StateFlow<ChannelInfoState> = _state.asStateFlow()
+    private val _state = MutableStateFlow(ChannelInfoViewState())
+
+    public val state: StateFlow<ChannelInfoViewState> = _state.asStateFlow()
+
+    private val _events = MutableSharedFlow<ChannelInfoEvent>(extraBufferCapacity = 1)
+
+    public val events: SharedFlow<ChannelInfoEvent> = _events.asSharedFlow()
+
+    private val channelStateFlow = chatClient.watchChannelAsState(
+        cid = cid,
+        messageLimit = 0,
+        coroutineScope = scope,
+    ).filterNotNull()
 
     init {
-        chatClient.watchChannelAsState(cid = cid, messageLimit = 0, coroutineScope = scope)
-            .filterNotNull()
+        channelStateFlow
             .flatMapLatest { channelState ->
-                combine(channelState.channelData, channelState.members) { channel, members ->
-                    channel to members
+                combine(
+                    channelState.channelData,
+                    channelState.members,
+                    channelState.muted,
+                ) { channel, members, muted ->
+                    Triple(channel, members, muted)
                 }
             }
-            .onEach { (channel, members) ->
+            .onEach { (channel, members, muted) ->
                 val channelMembers = members
                     .filter { member ->
                         member.user.id != chatClient.getCurrentOrStoredUserId()
                     }
                     .map { member ->
-                        ChannelInfoState.Member(
+                        ChannelInfoViewState.Member(
                             user = member.user,
                             role = if (channel.createdBy.id == member.user.id) {
-                                ChannelInfoState.Role.Owner
+                                ChannelInfoViewState.Role.Owner
                             } else {
                                 when (member.channelRole) {
-                                    "channel_moderator" -> ChannelInfoState.Role.Moderator
-                                    "channel_member" -> ChannelInfoState.Role.Member
-                                    else -> ChannelInfoState.Role.Other(member.channelRole.orEmpty())
+                                    "channel_moderator" -> ChannelInfoViewState.Role.Moderator
+                                    "channel_member" -> ChannelInfoViewState.Role.Member
+                                    else -> ChannelInfoViewState.Role.Other(member.channelRole.orEmpty())
                                 }
-                            }
+                            },
                         )
                     }
                 val expandedMembers = channelMembers.take(EXPANDED_MEMBER_COUNT)
@@ -60,31 +96,36 @@ public class ChannelInfoController(
                 val collapsedMembers = channelMembers.takeLast(collapsedMemberCount)
 
                 logger.d {
-                    "[onSuccessContent] members: ${channelMembers.size}, " +
+                    "[onSuccessContent] name: ${channel.name}, " +
+                        "members: ${channelMembers.size}, " +
                         "expanded: ${expandedMembers.size}, " +
                         "collapsed: ${collapsedMembers.size}"
                 }
 
                 _state.update { currentState ->
                     when (currentState.content) {
-                        is ChannelInfoState.Content.Loading -> {
+                        is ChannelInfoViewState.Content.Loading -> {
                             currentState.copy(
-                                content = ChannelInfoState.Content.Success(
+                                content = ChannelInfoViewState.Content.Success(
                                     expandedMembers = expandedMembers,
                                     collapsedMembers = collapsedMembers,
                                     areMembersExpandable = collapsedMemberCount > 0,
                                     areMembersExpanded = false,
-                                )
+                                    name = channel.name,
+                                    isMuted = muted,
+                                ),
                             )
                         }
 
-                        is ChannelInfoState.Content.Success -> {
+                        is ChannelInfoViewState.Content.Success -> {
                             currentState.copy(
                                 content = currentState.content.copy(
                                     expandedMembers = expandedMembers,
                                     collapsedMembers = collapsedMembers,
                                     areMembersExpandable = collapsedMemberCount > 0,
-                                )
+                                    name = channel.name,
+                                    isMuted = muted,
+                                ),
                             )
                         }
 
@@ -97,9 +138,9 @@ public class ChannelInfoController(
 
     public fun expandMembers() {
         _state.update { currentState ->
-            (currentState.content as? ChannelInfoState.Content.Success)?.let { content ->
+            (currentState.content as? ChannelInfoViewState.Content.Success)?.let { content ->
                 currentState.copy(
-                    content = content.copy(areMembersExpanded = true)
+                    content = content.copy(areMembersExpanded = true),
                 )
             } ?: currentState
         }
@@ -107,11 +148,24 @@ public class ChannelInfoController(
 
     public fun collapseMembers() {
         _state.update { currentState ->
-            (currentState.content as? ChannelInfoState.Content.Success)?.let { content ->
+            (currentState.content as? ChannelInfoViewState.Content.Success)?.let { content ->
                 currentState.copy(
-                    content = content.copy(areMembersExpanded = false)
+                    content = content.copy(areMembersExpanded = false),
                 )
             } ?: currentState
+        }
+    }
+
+    public fun updateName(name: String) {
+        scope.launch {
+            channelClient
+                .updatePartial(set = mapOf("name" to name))
+                .await()
+                .onError { error ->
+                    _events.tryEmit(
+                        ChannelInfoEvent.UpdateNameError(message = error.message),
+                    )
+                }
         }
     }
 }
--- stream-chat-android-ui-common/src/test/kotlin/io/getstream/chat/android/ui/common/feature/channel/info/ChannelInfoControllerTest.kt
+++ stream-chat-android-ui-common/src/test/kotlin/io/getstream/chat/android/ui/common/feature/channel/info/ChannelInfoControllerTest.kt
@@ -1,7 +1,24 @@
+/*
+ * Copyright (c) 2014-2025 Stream.io Inc. All rights reserved.
+ *
+ * Licensed under the Stream License;
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    https://github.com/GetStream/stream-chat-android/blob/main/LICENSE
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package io.getstream.chat.android.ui.common.feature.channel.info
 
 import app.cash.turbine.test
 import io.getstream.chat.android.client.ChatClient
+import io.getstream.chat.android.client.channel.ChannelClient
 import io.getstream.chat.android.client.channel.state.ChannelState
 import io.getstream.chat.android.client.setup.state.ClientState
 import io.getstream.chat.android.models.Channel
@@ -14,13 +31,16 @@ import io.getstream.chat.android.state.plugin.factory.StreamStatePluginFactory
 import io.getstream.chat.android.state.plugin.internal.StatePlugin
 import io.getstream.chat.android.state.plugin.state.StateRegistry
 import io.getstream.chat.android.test.asCall
-import io.getstream.chat.android.ui.common.state.channel.info.ChannelInfoState
+import io.getstream.chat.android.ui.common.state.channel.info.ChannelInfoEvent
+import io.getstream.chat.android.ui.common.state.channel.info.ChannelInfoViewState
+import io.getstream.result.Error
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.test.runTest
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import org.mockito.kotlin.any
+import org.mockito.kotlin.doAnswer
 import org.mockito.kotlin.doReturn
 import org.mockito.kotlin.eq
 import org.mockito.kotlin.mock
@@ -34,7 +54,7 @@ internal class ChannelInfoControllerTest {
             .get(backgroundScope)
 
         sut.state.value.let { state ->
-            assertEquals(ChannelInfoState.Content.Loading, state.content)
+            assertEquals(ChannelInfoViewState.Content.Loading, state.content)
         }
     }
 
@@ -43,31 +63,35 @@ internal class ChannelInfoControllerTest {
         val currentUser = User(id = "1")
         val otherUser = User(id = "2")
         val channel = Channel(
+            name = "name",
             createdBy = otherUser,
             members = listOf(
                 Member(user = currentUser),
                 Member(user = otherUser),
             ),
         )
+        val isMuted = false
         val sut = Fixture()
             .givenCurrentUser(currentUser)
-            .givenChannel(channel)
+            .givenChannel(channel, isMuted)
             .get(backgroundScope)
 
         sut.state.test {
             skipItems(1) // Skip initial state
             val actual = awaitItem()
             assertEquals(
-                ChannelInfoState.Content.Success(
+                ChannelInfoViewState.Content.Success(
                     expandedMembers = listOf(
-                        ChannelInfoState.Member(
+                        ChannelInfoViewState.Member(
                             user = otherUser,
-                            role = ChannelInfoState.Role.Owner,
+                            role = ChannelInfoViewState.Role.Owner,
                         ),
                     ),
                     collapsedMembers = emptyList(),
                     areMembersExpandable = false,
                     areMembersExpanded = false,
+                    name = channel.name,
+                    isMuted = false,
                 ),
                 actual.content,
             )
@@ -83,29 +107,32 @@ internal class ChannelInfoControllerTest {
                 Member(user = User(id = "2")),
                 Member(user = User(id = "3")),
                 Member(user = User(id = "4")),
-            )
+            ),
         )
+        val isMuted = false
         val sut = Fixture()
             .givenCurrentUser(currentUser)
-            .givenChannel(channel)
+            .givenChannel(channel, isMuted)
             .get(backgroundScope)
 
         sut.state.test {
             skipItems(1) // Skip initial state
             val actual = awaitItem()
             assertEquals(
-                ChannelInfoState.Content.Success(
+                ChannelInfoViewState.Content.Success(
                     expandedMembers = channel.members
                         .filter { member -> member.user.id != currentUser.id }
                         .map { member ->
-                            ChannelInfoState.Member(
+                            ChannelInfoViewState.Member(
                                 user = member.user,
-                                role = ChannelInfoState.Role.Other(""),
+                                role = ChannelInfoViewState.Role.Other(""),
                             )
                         },
                     collapsedMembers = emptyList(),
                     areMembersExpandable = false,
                     areMembersExpanded = false,
+                    name = channel.name,
+                    isMuted = false,
                 ),
                 actual.content,
             )
@@ -117,53 +144,58 @@ internal class ChannelInfoControllerTest {
         val currentUser = User(id = "1")
         val channel = Channel(
             members = (2..10).map { i -> Member(user = User(id = "$i")) } +
-                Member(user = currentUser)
+                Member(user = currentUser),
         )
+        val isMuted = false
         val sut = Fixture()
             .givenCurrentUser(currentUser)
-            .givenChannel(channel)
+            .givenChannel(channel, isMuted)
             .get(backgroundScope)
 
         sut.state.test {
             skipItems(1) // Skip initial state
             val actual = awaitItem()
             assertEquals(
-                ChannelInfoState.Content.Success(
+                ChannelInfoViewState.Content.Success(
                     expandedMembers = channel.members
                         .filter { it.user.id != currentUser.id }
                         .take(5)
                         .map { member ->
-                            ChannelInfoState.Member(
+                            ChannelInfoViewState.Member(
                                 user = member.user,
-                                role = ChannelInfoState.Role.Other(""),
+                                role = ChannelInfoViewState.Role.Other(""),
                             )
                         },
                     collapsedMembers = channel.members
                         .filter { it.user.id != currentUser.id }
                         .takeLast(4)
                         .map { member ->
-                            ChannelInfoState.Member(
+                            ChannelInfoViewState.Member(
                                 user = member.user,
-                                role = ChannelInfoState.Role.Other(""),
+                                role = ChannelInfoViewState.Role.Other(""),
                             )
                         },
                     areMembersExpandable = true,
                     areMembersExpanded = false,
+                    name = channel.name,
+                    isMuted = isMuted,
                 ),
                 actual.content,
             )
         }
     }
+
     @Test
     fun `expand group channel content`() = runTest {
         val currentUser = User(id = "1")
         val channel = Channel(
             members = (2..10).map { i -> Member(user = User(id = "$i")) } +
-                Member(user = currentUser)
+                Member(user = currentUser),
         )
+        val isMuted = false
         val sut = Fixture()
             .givenCurrentUser(currentUser)
-            .givenChannel(channel)
+            .givenChannel(channel, isMuted)
             .get(backgroundScope)
 
         sut.state.test {
@@ -171,53 +203,154 @@ internal class ChannelInfoControllerTest {
             val expectedChannelMembers = channel.members.filter { it.user.id != currentUser.id }
             val actual = awaitItem()
             assertEquals(
-                ChannelInfoState.Content.Success(
+                ChannelInfoViewState.Content.Success(
                     expandedMembers = expectedChannelMembers
                         .take(5)
                         .map { member ->
-                            ChannelInfoState.Member(
+                            ChannelInfoViewState.Member(
                                 user = member.user,
-                                role = ChannelInfoState.Role.Other(""),
+                                role = ChannelInfoViewState.Role.Other(""),
                             )
                         },
                     collapsedMembers = expectedChannelMembers
                         .takeLast(4)
                         .map { member ->
-                            ChannelInfoState.Member(
+                            ChannelInfoViewState.Member(
                                 user = member.user,
-                                role = ChannelInfoState.Role.Other(""),
+                                role = ChannelInfoViewState.Role.Other(""),
                             )
                         },
                     areMembersExpandable = true,
                     areMembersExpanded = false,
+                    name = channel.name,
+                    isMuted = isMuted,
                 ),
                 actual.content,
             )
 
             sut.expandMembers()
+
             val expandedState = awaitItem()
             assertEquals(
-                ChannelInfoState.Content.Success(
+                ChannelInfoViewState.Content.Success(
                     expandedMembers = expectedChannelMembers
                         .take(5)
                         .map { member ->
-                            ChannelInfoState.Member(
+                            ChannelInfoViewState.Member(
                                 user = member.user,
-                                role = ChannelInfoState.Role.Other(""),
+                                role = ChannelInfoViewState.Role.Other(""),
                             )
                         },
                     collapsedMembers = expectedChannelMembers
                         .takeLast(4)
                         .map { member ->
-                            ChannelInfoState.Member(
+                            ChannelInfoViewState.Member(
                                 user = member.user,
-                                role = ChannelInfoState.Role.Other(""),
+                                role = ChannelInfoViewState.Role.Other(""),
                             )
                         },
                     areMembersExpandable = true,
                     areMembersExpanded = true,
+                    name = channel.name,
+                    isMuted = isMuted,
                 ),
                 expandedState.content,
+            )
+        }
+    }
+
+    @Test
+    fun `update name`() = runTest {
+        val currentUser = User(id = "1")
+        val channel = Channel(
+            name = "name",
+            members = listOf(
+                Member(user = currentUser),
+            ),
+        )
+        val isMuted = false
+        val fixture = Fixture()
+        val sut = fixture
+            .givenCurrentUser(currentUser)
+            .givenChannel(channel, isMuted)
+            .get(backgroundScope)
+
+        sut.state.test {
+            skipItems(1) // Skip initial state
+            val actual = awaitItem()
+            assertEquals(
+                ChannelInfoViewState.Content.Success(
+                    expandedMembers = emptyList(),
+                    collapsedMembers = emptyList(),
+                    areMembersExpandable = false,
+                    areMembersExpanded = false,
+                    name = channel.name,
+                    isMuted = isMuted,
+                ),
+                actual.content,
+            )
+
+            fixture.givenNewName("newName", channel)
+
+            sut.updateName("newName")
+
+            val updatedState = awaitItem()
+            assertEquals(
+                ChannelInfoViewState.Content.Success(
+                    expandedMembers = emptyList(),
+                    collapsedMembers = emptyList(),
+                    areMembersExpandable = false,
+                    areMembersExpanded = false,
+                    name = "newName",
+                    isMuted = isMuted,
+                ),
+                updatedState.content,
+            )
+        }
+    }
+
+    @Test
+    fun `update name error`() = runTest {
+        val currentUser = User(id = "1")
+        val channel = Channel(
+            name = "name",
+            members = listOf(
+                Member(user = currentUser),
+            ),
+        )
+        val error = Error.GenericError("Error updating channel name")
+        val isMuted = false
+        val fixture = Fixture()
+        val sut = fixture
+            .givenCurrentUser(currentUser)
+            .givenChannel(channel, isMuted)
+            .get(backgroundScope)
+
+        sut.state.test {
+            skipItems(1) // Skip initial state
+            val actual = awaitItem()
+            assertEquals(
+                ChannelInfoViewState.Content.Success(
+                    expandedMembers = emptyList(),
+                    collapsedMembers = emptyList(),
+                    areMembersExpandable = false,
+                    areMembersExpanded = false,
+                    name = channel.name,
+                    isMuted = isMuted,
+                ),
+                actual.content,
+            )
+
+            fixture.givenNewNameError("newName", error)
+
+            sut.updateName("newName")
+        }
+
+        sut.events.test {
+            val actual = awaitItem()
+            assertEquals(
+                ChannelInfoEvent.UpdateNameError(message = error.message),
+                actual,
             )
         }
     }
@@ -231,11 +364,15 @@ private class Fixture {
     private val channelType: String = CHANNEL_TYPE
     private val channelId: String = CHANNEL_ID
 
-    private val channelState: ChannelState = mock()
+    private val channelFlow = MutableStateFlow(Channel().toChannelData())
+    private val channelState: ChannelState = mock {
+        whenever(mock.channelData) doReturn channelFlow
+    }
     private val clientState: ClientState = mock()
     private val stateRegistry: StateRegistry = mock {
         whenever(mock.channel(channelType, channelId)) doReturn channelState
     }
+    private val channelClient: ChannelClient = mock()
     private val chatClient: ChatClient = mock {
         val statePlugin: StatePlugin = mock {
             whenever(mock.resolveDependency(StateRegistry::class)) doReturn stateRegistry
@@ -246,6 +383,7 @@ private class Fixture {
             whenever(mock.resolveDependency(StatePluginConfig::class)) doReturn StatePluginConfig()
         }
         whenever(mock.pluginFactories) doReturn listOf(statePluginFactory)
+        whenever(mock.channel(CID)) doReturn channelClient
     }
 
     fun givenCurrentUser(user: User) = apply {
@@ -255,15 +393,28 @@ private class Fixture {
         whenever(chatClient.awaitInitializationState(any())) doReturn InitializationState.COMPLETE
     }
 
-    fun givenChannel(channel: Channel) = apply {
-        whenever(channelState.channelData) doReturn MutableStateFlow(channel.toChannelData())
+    fun givenChannel(channel: Channel, isMuted: Boolean) = apply {
+        channelFlow.value = channel.toChannelData()
         whenever(channelState.members) doReturn MutableStateFlow(channel.members)
+        whenever(channelState.muted) doReturn MutableStateFlow(isMuted)
         whenever(chatClient.queryChannel(eq(channelType), eq(channelId), any(), any())) doReturn channel.asCall()
     }
 
+    fun givenNewName(name: String, channel: Channel) = apply {
+        val updatedChannel = channel.copy(name = name)
+        whenever(channelClient.updatePartial(mapOf("name" to name))) doAnswer {
+            channelFlow.value = updatedChannel.toChannelData()
+            updatedChannel.asCall()
+        }
+    }
+
+    fun givenNewNameError(name: String, error: Error) = apply {
+        whenever(channelClient.updatePartial(mapOf("name" to name))) doReturn error.asCall()
+    }
+
     fun get(scope: CoroutineScope) = ChannelInfoController(
-        scope = scope,
         cid = CID,
-        chatClient = chatClient
+        scope = scope,
+        chatClient = chatClient,
     )
 }
