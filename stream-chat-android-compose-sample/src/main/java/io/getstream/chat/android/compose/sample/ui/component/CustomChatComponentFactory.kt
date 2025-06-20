fe2e6fe1b5784744436b174b356f4f645bc8714c
PetarVelikov <petar.velikov@getstream.io>
2025-06-20 12:36:48 +0200

    Example for 'Show original translation' in `MessageMenu`.

--- stream-chat-android-compose-sample/src/main/java/io/getstream/chat/android/compose/sample/ui/component/CustomChatComponentFactory.kt
+++ stream-chat-android-compose-sample/src/main/java/io/getstream/chat/android/compose/sample/ui/component/CustomChatComponentFactory.kt
@@ -19,14 +19,24 @@ package io.getstream.chat.android.compose.sample.ui.component
 import androidx.compose.foundation.background
 import androidx.compose.foundation.lazy.LazyItemScope
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import io.getstream.chat.android.client.ChatClient
 import io.getstream.chat.android.client.extensions.isPinned
+import io.getstream.chat.android.compose.sample.R
 import io.getstream.chat.android.compose.state.channels.list.ItemState
+import io.getstream.chat.android.compose.state.messageoptions.MessageOptionItemState
 import io.getstream.chat.android.compose.ui.channels.list.ChannelItem
 import io.getstream.chat.android.compose.ui.theme.ChatComponentFactory
 import io.getstream.chat.android.compose.ui.theme.ChatTheme
+import io.getstream.chat.android.compose.ui.util.showOriginalTextAsState
 import io.getstream.chat.android.models.Channel
+import io.getstream.chat.android.models.Message
 import io.getstream.chat.android.models.User
+import io.getstream.chat.android.ui.common.feature.messages.translations.MessageOriginalTranslationsStore
+import io.getstream.chat.android.ui.common.state.messages.CustomAction
+import io.getstream.chat.android.ui.common.state.messages.MessageAction
 
 class CustomChatComponentFactory : ChatComponentFactory {
 
@@ -53,5 +63,72 @@ class CustomChatComponentFactory : ChatComponentFactory {
             onChannelClick = onChannelClick,
             onChannelLongClick = onChannelLongClick,
         )
+    }
+
+    @Composable
+    override fun MessageMenu(
+        modifier: Modifier,
+        message: Message,
+        messageOptions: List<MessageOptionItemState>,
+        ownCapabilities: Set<String>,
+        onMessageAction: (MessageAction) -> Unit,
+        onShowMore: () -> Unit,
+        onDismiss: () -> Unit,
+    ) {
+        val isMine = message.user.id == ChatClient.instance().getCurrentUser()?.id
+        if (ChatTheme.autoTranslationEnabled && message.isTranslated() && !isMine) {
+            val showOriginalText by showOriginalTextAsState(cid = message.cid, messageId = message.id)
+            val title = if (showOriginalText) {
+                R.string.show_translation
+            } else {
+                R.string.show_original
+            }
+            val toggleOriginalTextItem = MessageOptionItemState(
+                title = title,
+                titleColor = ChatTheme.colors.textHighEmphasis,
+                iconPainter = painterResource(R.drawable.ic_translate),
+                iconColor = ChatTheme.colors.textLowEmphasis,
+                action = CustomAction(
+                    message = message,
+                    extraProperties = mapOf("type" to "toggle_original")
+                )
+            )
+            val action: (MessageAction) -> Unit = { action ->
+                if (action is CustomAction && action.extraProperties["type"] == "toggle_original") {
+                    // Handle the toggle original text action
+                    MessageOriginalTranslationsStore.forChannel(action.message.cid)
+                        .toggleOriginalText(action.message.id)
+                    onMessageAction(action)
+                } else {
+                    // Handle other actions
+                    onMessageAction(action)
+                }
+            }
+            super.MessageMenu(
+                modifier = modifier,
+                message = message,
+                messageOptions = messageOptions + toggleOriginalTextItem,
+                ownCapabilities = ownCapabilities,
+                onMessageAction = action,
+                onShowMore = onShowMore,
+                onDismiss = onDismiss,
+            )
+        } else {
+            super.MessageMenu(
+                modifier = modifier,
+                message = message,
+                messageOptions = messageOptions,
+                ownCapabilities = ownCapabilities,
+                onMessageAction = onMessageAction,
+                onShowMore = onShowMore,
+                onDismiss = onDismiss
+            )
+        }
+    }
+
+    private fun Message.isTranslated(): Boolean {
+        val userLang = ChatClient.instance().getCurrentUser()?.language.orEmpty()
+        val translatedText = getTranslation(userLang).ifEmpty { text }
+        return userLang != originalLanguage && translatedText != text
     }
 }
