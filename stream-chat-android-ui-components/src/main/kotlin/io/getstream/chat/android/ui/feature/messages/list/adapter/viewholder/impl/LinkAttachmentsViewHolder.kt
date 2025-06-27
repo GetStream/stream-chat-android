73312916e6fafaf6a0060ee05b913caf35d98d7f
Petar Velikov <petarvelikov95@gmail.com>
2025-06-27 11:42:02 +0200

    Show original translation (XML) (#5843)
    * [AND-450] Add "Show original translation" in XML SDK.
    
    * [AND-450] Update CHANGELOG.md.
    
    * Rename messagesInOriginalLanguage to messagesInOriginalLanguageIds.
    
    ---------
    
    Co-authored-by: PetarVelikov <petar.velikov@getstream.io>

--- stream-chat-android-ui-components/src/main/kotlin/io/getstream/chat/android/ui/feature/messages/list/adapter/viewholder/impl/LinkAttachmentsViewHolder.kt
+++ stream-chat-android-ui-components/src/main/kotlin/io/getstream/chat/android/ui/feature/messages/list/adapter/viewholder/impl/LinkAttachmentsViewHolder.kt
@@ -105,6 +105,9 @@ public class LinkAttachmentsViewHolder internal constructor(
                 footnote.setOnThreadClickListener {
                     container.threadClickListener.onThreadClick(data.message)
                 }
+                footnote.setOnTranslatedLabelClickListener {
+                    container.translatedLabelClickListener.onTranslatedLabelClick(data.message)
+                }
                 messageContainer.setOnLongClickListener {
                     container.messageLongClickListener.onMessageLongClick(data.message)
                     true
