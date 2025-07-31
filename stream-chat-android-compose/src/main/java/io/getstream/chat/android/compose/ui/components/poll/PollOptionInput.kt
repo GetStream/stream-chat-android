4fc8183d906b44c247e3c5d22443234fb0601555
André Mion <andremion@gmail.com>
2025-07-31 09:40:44 +0100

    Fix Poll creation button enabling on changed change (#5885)
    * Validate poll switch input even on checked state change
    
    * Using TextFieldValue to manage the text input state, which allows us to set the cursor position to the end of the text when the input is first drawn.
    
    * CHANGELOG

--- stream-chat-android-compose/src/main/java/io/getstream/chat/android/compose/ui/components/poll/PollOptionInput.kt
+++ stream-chat-android-compose/src/main/java/io/getstream/chat/android/compose/ui/components/poll/PollOptionInput.kt
@@ -31,7 +31,10 @@ import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -42,9 +45,11 @@ import androidx.compose.ui.graphics.SolidColor
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.semantics.contentDescription
 import androidx.compose.ui.semantics.semantics
+import androidx.compose.ui.text.TextRange
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.input.KeyboardCapitalization
 import androidx.compose.ui.text.input.OffsetMapping
+import androidx.compose.ui.text.input.TextFieldValue
 import androidx.compose.ui.text.input.TransformedText
 import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.text.style.TextDirection
@@ -94,6 +99,12 @@ public fun PollOptionInput(
     val colors = ChatTheme.colors
     val textColor = ChatTheme.colors.textHighEmphasis
     val focusRequester = remember { FocusRequester() }
+    // Using TextFieldValue to manage the text input state,
+    // which allows us to set the cursor position to the end of the text
+    // when the input is first drawn.
+    var textFieldValue by remember(value) {
+        mutableStateOf(TextFieldValue(text = value, selection = TextRange(value.length)))
+    }
 
     Box(modifier = modifier.height(ChatTheme.dimens.pollOptionInputHeight)) {
         BasicTextField(
@@ -104,10 +115,10 @@ public fun PollOptionInput(
                 .padding(innerPadding)
                 .focusRequester(focusRequester)
                 .semantics { contentDescription = description },
-            value = value,
+            value = textFieldValue,
             onValueChange = {
-                if (it.length <= maxLength) {
-                    onValueChange.invoke(it)
+                if (it.text.length <= maxLength) {
+                    onValueChange.invoke(it.text)
                 }
             },
             visualTransformation = {
