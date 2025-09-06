a49e94d46704cbc509fad4d0a03883ac79c6fc78
VelikovPetar <petarvelikov95@gmail.com>
2025-09-06 15:20:37 +0200

    Fix test.

--- stream-chat-android-client/src/test/java/io/getstream/chat/android/client/parser2/testdata/ChannelDtoTestData.kt
+++ stream-chat-android-client/src/test/java/io/getstream/chat/android/client/parser2/testdata/ChannelDtoTestData.kt
@@ -63,7 +63,7 @@ internal object ChannelDtoTestData {
            }
           ],
           "user_message_reminders": false,
-          "shared_locations": true
+          "shared_locations": true,
           "mark_messages_pending": false
         }
         """.withoutWhitespace()
