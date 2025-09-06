ea2d40a1a42307fd24b3544e0c3e6ef4922f2b13
VelikovPetar <petarvelikov95@gmail.com>
2025-09-06 15:00:15 +0200

    Bump ChatDatabase version.

--- stream-chat-android-offline/src/main/java/io/getstream/chat/android/offline/repository/database/internal/ChatDatabase.kt
+++ stream-chat-android-offline/src/main/java/io/getstream/chat/android/offline/repository/database/internal/ChatDatabase.kt
@@ -86,7 +86,7 @@ import io.getstream.chat.android.offline.repository.domain.user.internal.UserEnt
         ThreadOrderEntity::class,
         DraftMessageEntity::class,
     ],
-    version = 89,
+    version = 90,
     exportSchema = false,
 )
 @TypeConverters(
