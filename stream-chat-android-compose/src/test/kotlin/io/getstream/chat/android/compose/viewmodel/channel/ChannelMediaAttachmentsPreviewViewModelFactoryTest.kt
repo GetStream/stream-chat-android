d38b758d88fdb29900da2eca12b051546bb1afaa
André Mion <andremion@gmail.com>
2025-09-23 16:48:36 +0100

    Add unit tests for ChannelMediaAttachmentsPreviewViewModelFactory

---
+++ stream-chat-android-compose/src/test/kotlin/io/getstream/chat/android/compose/viewmodel/channel/ChannelMediaAttachmentsPreviewViewModelFactoryTest.kt
@@ -0,0 +1,62 @@
+/*
+ * Copyright (c) 2014-2025 Stream.io Inc. All rights reserved.
+ *
+ * Licensed under the Stream License;
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    https://github.com/GetStream/stream-chat-android/blob/main/LICENSE
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.getstream.chat.android.compose.viewmodel.channel
+
+import android.content.Context
+import androidx.lifecycle.ViewModel
+import org.junit.Assert.assertEquals
+import org.junit.Assert.assertThrows
+import org.junit.Test
+import org.junit.jupiter.api.assertInstanceOf
+import org.mockito.kotlin.doReturn
+import org.mockito.kotlin.mock
+
+internal class ChannelMediaAttachmentsPreviewViewModelFactoryTest {
+
+    @Test
+    fun `create should return correct instance`() {
+        val sut = Fixture().get()
+
+        val viewModel = sut.create(ChannelMediaAttachmentsPreviewViewModel::class.java)
+
+        assertInstanceOf<ChannelMediaAttachmentsPreviewViewModel>(viewModel)
+    }
+
+    @Test
+    fun `create should throw IllegalArgumentException for unsupported ViewModel class`() {
+        val sut = Fixture().get()
+
+        val exception = assertThrows(IllegalArgumentException::class.java) {
+            sut.create(ViewModel::class.java)
+        }
+
+        assertEquals(
+            "ChannelMediaAttachmentsPreviewViewModelFactory can only create instances of ChannelMediaAttachmentsPreviewViewModel",
+            exception.message,
+        )
+    }
+
+    private class Fixture {
+        private val mockContext: Context = mock {
+            on { applicationContext } doReturn it
+        }
+
+        fun get() = ChannelMediaAttachmentsPreviewViewModelFactory(
+            context = mockContext,
+        )
+    }
+}
