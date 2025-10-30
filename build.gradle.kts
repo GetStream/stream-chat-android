8e58e133d09d778a33d9e439f6498fa1e17d7fdd
Gian <47775302+gpunto@users.noreply.github.com>
2025-10-30 14:31:22 +0100

    Configure Spotless through common convention plugins

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,7 +22,7 @@ plugins {
     alias(libs.plugins.paparazzi) apply false
     alias(libs.plugins.google.services) apply false
     alias(libs.plugins.firebase.crashlytics) apply false
-    alias(libs.plugins.spotless) apply false
+    alias(libs.plugins.stream.project)
     alias(libs.plugins.stream.android.library) apply false
     alias(libs.plugins.stream.android.application) apply false
     alias(libs.plugins.stream.java.library) apply false
@@ -43,6 +43,12 @@ plugins {
     alias(libs.plugins.maven.publish)
 }
 
+streamProject {
+    spotless {
+        ignoredModules = setOf("stream-chat-android-docs")
+    }
+}
+
 buildscript {
     dependencies {
         // TODO: Remove this workaround after AGP 8.9.0 is released
@@ -58,10 +64,6 @@ apply(from = "${rootDir}/scripts/sonar.gradle")
 apply(from = "${rootDir}/scripts/coverage.gradle")
 
 subprojects {
-    if (name != "stream-chat-android-docs" && buildFile.exists()) {
-        apply(from = "${rootDir}/spotless/spotless.gradle")
-    }
-
     // Configure Android projects with common SDK versions as soon as either plugin is applied
     pluginManager.withPlugin("com.android.library") {
         extensions.configure<LibraryExtension> {
