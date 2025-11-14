b589c7f2b87856452598af2d910493dad29d93f9
Alexey Alter-Pesotskiy <alex@testableapple.com>
2025-11-14 14:16:20 +0000

    Update PR

--- stream-chat-android-compose-sample/src/androidTestE2eDebug/kotlin/io/getstream/chat/android/compose/tests/BackendTests.kt
+++ stream-chat-android-compose-sample/src/androidTestE2eDebug/kotlin/io/getstream/chat/android/compose/tests/BackendTests.kt
@@ -1,8 +1,23 @@
+/*
+ * Copyright (c) 2014-2025 Stream.io Inc. All rights reserved.
+ *
+ * Licensed under the Stream License;
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    https://github.com/GetStream/stream-chat-android/blob/main/LICENSE
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package io.getstream.chat.android.compose.tests
 
 import io.getstream.chat.android.compose.robots.assertDeletedMessage
 import io.getstream.chat.android.compose.robots.assertMessage
-import io.getstream.chat.android.compose.robots.assertMessageAuthor
 import io.getstream.chat.android.compose.robots.assertReaction
 import io.getstream.chat.android.compose.sample.ui.InitTestActivity
 import io.getstream.chat.android.e2e.test.mockserver.ReactionType
