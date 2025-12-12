f317f665ef93e92cf8b484bb324f1aee19fb377a
André Mion <andremion@gmail.com>
2025-12-12 15:44:05 +0000

    Refactor PollViewResultDialog to use ViewModel

---
+++ stream-chat-android-compose/src/main/java/io/getstream/chat/android/compose/viewmodel/messages/PollResultsViewModel.kt
@@ -0,0 +1,63 @@
+/*
+ * Copyright (c) 2014-2025 Stream.io Inc. All rights reserved.
+ *
+ * Licensed under the Stream License;
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    https://github.com/GetStream/stream-chat-android/blob/main/LICENSE
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package io.getstream.chat.android.compose.viewmodel.messages
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import io.getstream.chat.android.models.Poll
+import io.getstream.chat.android.ui.common.feature.messages.poll.PollResultsViewAction
+import io.getstream.chat.android.ui.common.feature.messages.poll.PollResultsViewController
+import io.getstream.chat.android.ui.common.feature.messages.poll.PollResultsViewEvent
+import io.getstream.chat.android.ui.common.state.messages.poll.PollResultsViewState
+import kotlinx.coroutines.flow.SharedFlow
+import kotlinx.coroutines.flow.StateFlow
+
+/**
+ * ViewModel for managing poll results state in Compose UI components.
+ *
+ * @param poll The poll to display results for.
+ * @param controllerProvider The provider for [PollResultsViewController].
+ */
+public class PollResultsViewModel(
+    poll: Poll,
+    controllerProvider: ViewModel.() -> PollResultsViewController = {
+        PollResultsViewController(
+            poll = poll,
+            scope = viewModelScope,
+        )
+    },
+) : ViewModel() {
+
+    private val controller: PollResultsViewController by lazy { controllerProvider() }
+
+    /**
+     * @see [PollResultsViewController.state]
+     */
+    public val state: StateFlow<PollResultsViewState> = controller.state
+
+    /**
+     * @see [PollResultsViewController.events]
+     */
+    public val events: SharedFlow<PollResultsViewEvent> = controller.events
+
+    /**
+     * @see [PollResultsViewController.onViewAction]
+     */
+    public fun onViewAction(action: PollResultsViewAction) {
+        controller.onViewAction(action)
+    }
+}
