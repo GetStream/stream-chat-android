df39c961a84f1940196dc2cb4a5cbcb42a4f221a
Petar Velikov <petarvelikov95@gmail.com>
2026-01-02 15:05:02 +0100

    Update the outdated copyright headers. (#6064)

--- stream-chat-android-client/src/test/java/io/getstream/chat/android/client/utils/observable/ChatEventsObservableTest.kt
+++ stream-chat-android-client/src/test/java/io/getstream/chat/android/client/utils/observable/ChatEventsObservableTest.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014-2022 Stream.io Inc. All rights reserved.
+ * Copyright (c) 2014-2026 Stream.io Inc. All rights reserved.
  *
  * Licensed under the Stream License;
  * you may not use this file except in compliance with the License.
