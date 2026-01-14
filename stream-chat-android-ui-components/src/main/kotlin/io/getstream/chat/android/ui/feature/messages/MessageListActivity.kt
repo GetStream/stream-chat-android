5ed465060a2bd57caf910e51e8016179a26854f3
André Mion <andremion@gmail.com>
2026-01-14 11:38:27 +0000

    Extract applyEdgeToEdgePadding extension

--- stream-chat-android-ui-components/src/main/kotlin/io/getstream/chat/android/ui/feature/messages/MessageListActivity.kt
+++ stream-chat-android-ui-components/src/main/kotlin/io/getstream/chat/android/ui/feature/messages/MessageListActivity.kt
@@ -20,12 +20,11 @@ import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import androidx.core.view.updatePadding
 import io.getstream.chat.android.client.ChatClient
 import io.getstream.chat.android.ui.R
 import io.getstream.chat.android.ui.databinding.StreamUiFragmentContainerBinding
+import io.getstream.chat.android.ui.utils.extensions.applyEdgeToEdgePadding
 
 /**
  * An Activity representing a self-contained chat screen. This Activity is simply
@@ -70,11 +69,9 @@ public open class MessageListActivity : AppCompatActivity() {
     }
 
     private fun setupEdgeToEdge() {
-        ViewCompat.setOnApplyWindowInsetsListener(binding.root) { root, windowInsets ->
-            val insets = windowInsets.getInsets(WindowInsetsCompat.Type.systemBars() or WindowInsetsCompat.Type.ime())
-            root.updatePadding(left = insets.left, top = insets.top, right = insets.right, bottom = insets.bottom)
-            WindowInsetsCompat.CONSUMED
-        }
+        binding.root.applyEdgeToEdgePadding(
+            typeMask = WindowInsetsCompat.Type.systemBars() or WindowInsetsCompat.Type.ime(),
+        )
     }
 
     public companion object {
