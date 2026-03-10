6e4c571a2ff7c8152bc9522c1d8e4c0e30c4b187
André Mion <andremion@gmail.com>
2026-03-10 13:42:27 +0000

    Fix e2e tests

--- stream-chat-android-compose/src/main/java/io/getstream/chat/android/compose/ui/components/attachments/files/FilesPicker.kt
+++ stream-chat-android-compose/src/main/java/io/getstream/chat/android/compose/ui/components/attachments/files/FilesPicker.kt
@@ -31,6 +31,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -84,6 +85,7 @@ public fun FilesPicker(
     Column(modifier = modifier) {
         Row(
             modifier = Modifier
+                .testTag("Stream_FindFilesButton")
                 .fillMaxWidth()
                 .clickable { fileSelectContract.launch(allowMultipleSelection) }
                 .padding(StreamTokens.spacingSm),
