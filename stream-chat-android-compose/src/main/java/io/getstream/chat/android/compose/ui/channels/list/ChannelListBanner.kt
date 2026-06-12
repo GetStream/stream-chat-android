977ee19caa1dcceaffad1ffd7e7a21e324fe2062
André Mion <andremion@gmail.com>
2026-06-12 14:39:14 +0100

    compose: Give the channel list retry banner a button role
    The banner is a clickable retry control, so expose Role.Button for
    accessibility services.
    
    Review: https://github.com/GetStream/stream-chat-android/pull/6492#discussion_r3403044492

--- stream-chat-android-compose/src/main/java/io/getstream/chat/android/compose/ui/channels/list/ChannelListBanner.kt
+++ stream-chat-android-compose/src/main/java/io/getstream/chat/android/compose/ui/channels/list/ChannelListBanner.kt
@@ -31,6 +31,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import io.getstream.chat.android.compose.R
@@ -57,7 +58,7 @@ internal fun ChannelListBanner(
         modifier = modifier
             .fillMaxWidth()
             .background(ChatTheme.colors.backgroundCoreSurfaceDefault)
-            .clickable { onClick() }
+            .clickable(role = Role.Button) { onClick() }
             .padding(StreamTokens.spacingSm),
         horizontalArrangement = Arrangement.spacedBy(
             StreamTokens.spacingXs,
