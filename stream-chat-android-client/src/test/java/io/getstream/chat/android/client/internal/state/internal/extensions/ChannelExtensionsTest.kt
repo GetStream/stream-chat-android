50fe62aa356fa54724a30685a270f1a856be4696
Gian <47775302+gpunto@users.noreply.github.com>
2026-07-07 14:18:22 +0200

    Migrate remaining wire date parsing to the generated IsoDateAdapter and remove DateAdapter

--- stream-chat-android-client/src/test/java/io/getstream/chat/android/client/internal/state/internal/extensions/ChannelExtensionsTest.kt
+++ stream-chat-android-client/src/test/java/io/getstream/chat/android/client/internal/state/internal/extensions/ChannelExtensionsTest.kt
@@ -25,10 +25,10 @@ import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import io.getstream.chat.android.client.extensions.internal.applyPagination
 import io.getstream.chat.android.client.internal.state.model.querychannels.pagination.internal.QueryChannelsPaginationRequest
 import io.getstream.chat.android.client.internal.state.model.querychannels.pagination.internal.toAnyChannelPaginationRequest
-import io.getstream.chat.android.client.parser2.adapters.DateAdapter
 import io.getstream.chat.android.models.Attachment
 import io.getstream.chat.android.models.Channel
 import io.getstream.chat.android.models.querysort.QuerySortByField
+import io.getstream.chat.android.network.infrastructure.IsoDateAdapter
 import io.getstream.chat.android.randomChannel
 import okio.buffer
 import okio.source
@@ -44,7 +44,7 @@ internal class ChannelExtensionsTest {
     fun `When apply pagination Should not throw any exception`() {
         val channelsFile = File(this.javaClass.classLoader!!.getResource("channels.json").toURI())
         val moshi = Moshi.Builder()
-            .add(DateAdapter())
+            .add(IsoDateAdapter())
             .add(
                 File::class.java,
                 object : JsonAdapter<File>() {
