b8bff01984d920b7495c4491aa2729a3cb432699
Gianmarco <47775302+gpunto@users.noreply.github.com>
2026-08-10 09:38:00 +0200

    Migrate sendEvent to generated SendEventRequest with custom-flattening EventRequest adapter (#6617)

--- stream-chat-android-client/src/main/java/io/getstream/chat/android/network/models/SendEventRequest.kt
+++ stream-chat-android-client/src/main/java/io/getstream/chat/android/network/models/SendEventRequest.kt
@@ -14,11 +14,23 @@
  * limitations under the License.
  */
 
-package io.getstream.chat.android.client.api2.model.requests
+@file:Suppress(
+    "ArrayInDataClass",
+    "EnumEntryName",
+    "RemoveRedundantQualifierName",
+    "UnusedImport",
+)
 
-import com.squareup.moshi.JsonClass
+package io.getstream.chat.android.network.models
 
-@JsonClass(generateAdapter = true)
+import com.squareup.moshi.Json
+
+/**
+ *
+ */
+
+@com.squareup.moshi.JsonClass(generateAdapter = true)
 internal data class SendEventRequest(
-    val event: Map<Any, Any>,
+    @Json(name = "event")
+    internal val event: io.getstream.chat.android.network.models.EventRequest,
 )
