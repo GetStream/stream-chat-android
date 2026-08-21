573cd24d17c8d0138c477dfc1927ac42f09c32f8
Gian <47775302+gpunto@users.noreply.github.com>
2026-08-21 10:33:57 +0200

    Drop the duplicate deletedAt argument in the nowMessage test helper

--- stream-chat-android-ui-common/src/test/kotlin/io/getstream/chat/android/ui/common/feature/messages/list/MessageListControllerTests.kt
+++ stream-chat-android-ui-common/src/test/kotlin/io/getstream/chat/android/ui/common/feature/messages/list/MessageListControllerTests.kt
@@ -1674,7 +1674,6 @@ internal class MessageListControllerTests {
                 syncStatus = syncStatus,
                 // randomMessage randomises silent and deletedAt, which the mark-read gate keys on.
                 silent = false,
-                deletedAt = null,
                 createdAt = nowDate,
                 updatedAt = nowDate,
                 deletedAt = null,
